c80d7b85b39c4a9c1b65362828620876eb4da913
Ido Shamun <idoesh1@gmail.com>
2020-08-03 00:25:57 +0300

    refactor: change color palette to js

---
+++ styles/colors.ts
@@ -0,0 +1,119 @@
+export const colorPepper90 = '#000000';
+export const colorPepper80 = '#151618';
+export const colorPepper70 = '#1C1E21';
+export const colorPepper60 = '#25282C';
+export const colorPepper50 = '#303237';
+export const colorPepper40 = '#393C42';
+export const colorPepper30 = '#4C5057';
+export const colorPepper20 = '#5F646D';
+export const colorPepper10 = '#686E78';
+
+export const colorSalt90 = '#A9ABB3';
+export const colorSalt80 = '#B3B6BD';
+export const colorSalt70 = '#BDC0C5';
+export const colorSalt60 = '#C8CBD0';
+export const colorSalt50 = '#D2D5D9';
+export const colorSalt40 = '#DDE0E3';
+export const colorSalt30 = '#E9EAEC';
+export const colorSalt20 = '#F4F5F6';
+export const colorSalt10 = '#FFFFFF';
+
+export const colorWater90 = '#013B7A';
+export const colorWater80 = '#024FA2';
+export const colorWater70 = '#0063CC';
+export const colorWater60 = '#0076F5';
+export const colorWater50 = '#208BFF';
+export const colorWater40 = '#47A0FF';
+export const colorWater30 = '#70B5FF';
+export const colorWater20 = '#99CAFF';
+export const colorWater10 = '#C2DFFF';
+
+export const colorBlueCheese90 = '#017279';
+export const colorBlueCheese80 = '#0098A3';
+export const colorBlueCheese70 = '#04BECB';
+export const colorBlueCheese60 = '#00E4F5';
+export const colorBlueCheese50 = '#20F0FF';
+export const colorBlueCheese40 = '#46F3FF';
+export const colorBlueCheese30 = '#70F5FF';
+export const colorBlueCheese20 = '#98F8FF';
+export const colorBlueCheese10 = '#C2FBFF';
+
+export const colorAvocado90 = '#007A3B';
+export const colorAvocado80 = '#00A34F';
+export const colorAvocado70 = '#00CC63';
+export const colorAvocado60 = '#00F576';
+export const colorAvocado50 = '#1DFF8C';
+export const colorAvocado40 = '#47FFA0';
+export const colorAvocado30 = '#70FFB5';
+export const colorAvocado20 = '#99FFCA';
+export const colorAvocado10 = '#C2FFDF';
+
+export const colorLettuce90 = '#407A01';
+export const colorLettuce80 = '#57A302';
+export const colorLettuce70 = '#6CCC00';
+export const colorLettuce60 = '#84F500';
+export const colorLettuce50 = '#96FF1E';
+export const colorLettuce40 = '#A8FF47';
+export const colorLettuce30 = '#BCFF70';
+export const colorLettuce20 = '#CFFF99';
+export const colorLettuce10 = '#E2FFC2';
+
+export const colorCheese90 = '#796601';
+export const colorCheese80 = '#A38802';
+export const colorCheese70 = '#CCAA02';
+export const colorCheese60 = '#F5CC03';
+export const colorCheese50 = '#FFDA20';
+export const colorCheese40 = '#FFE047';
+export const colorCheese30 = '#FFE770';
+export const colorCheese20 = '#FFEE9A';
+export const colorCheese10 = '#FFF5C1';
+
+export const colorBurger90 = '#794100';
+export const colorBurger80 = '#A35700';
+export const colorBurger70 = '#CC6D00';
+export const colorBurger60 = '#F58301';
+export const colorBurger50 = '#FF961F';
+export const colorBurger40 = '#FFA947';
+export const colorBurger30 = '#FFBC70';
+export const colorBurger20 = '#FFCF99';
+export const colorBurger10 = '#FFE2C2';
+
+export const colorKetchup90 = '#7A0100';
+export const colorKetchup80 = '#A20000';
+export const colorKetchup70 = '#CC0000';
+export const colorKetchup60 = '#F50002';
+export const colorKetchup50 = '#FF1E1F';
+export const colorKetchup40 = '#FF4746';
+export const colorKetchup30 = '#FF7071';
+export const colorKetchup20 = '#FF999A';
+export const colorKetchup10 = '#FEC2C2';
+
+export const colorBacon90 = '#7A002F';
+export const colorBacon80 = '#A3003F';
+export const colorBacon70 = '#CC004E';
+export const colorBacon60 = '#F5005E';
+export const colorBacon50 = '#FF1F75';
+export const colorBacon40 = '#FF468E';
+export const colorBacon30 = '#FF70A7';
+export const colorBacon20 = '#FF99C0';
+export const colorBacon10 = '#FEC2D9';
+
+export const colorCabbage90 = '#4E007A';
+export const colorCabbage80 = '#6601A3';
+export const colorCabbage70 = '#8100CC';
+export const colorCabbage60 = '#9B01F5';
+export const colorCabbage50 = '#AD20FF';
+export const colorCabbage40 = '#BC47FF';
+export const colorCabbage30 = '#CB70FF';
+export const colorCabbage20 = '#DA98FF';
+export const colorCabbage10 = '#E9C2FF';
+
+export const colorOnion90 = '#25007A';
+export const colorOnion80 = '#3101A3';
+export const colorOnion70 = '#3D00CC';
+export const colorOnion60 = '#4801F5';
+export const colorOnion50 = '#621FFF';
+export const colorOnion40 = '#7E48FF';
+export const colorOnion30 = '#9B70FF';
+export const colorOnion20 = '#B899FF';
+export const colorOnion10 = '#D3C3FF';
