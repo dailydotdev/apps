666f7ae05fbb59df70b258e5c032f45035a4cab4
Ido Shamun <ido@daily.dev>
2020-08-18 12:21:58 +0300

    feat: turn links in comments to anchor elements (#1)

--- components/utilities.tsx
+++ components/utilities.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import Linkify from 'linkifyjs/react';
 import LazyImage from './LazyImage';
 import { size10, size2, size3, size4, size6 } from '../styles/sizes';
 import { typoLil1, typoLil2Base, typoSmall } from '../styles/typography';
 import Loader from './Loader';
+import { colorWater60 } from '../styles/colors';
 
 export const RoundedImage = styled(LazyImage)`
   width: ${size10};
@@ -28,11 +30,18 @@ export const CommentPublishDate = styled.time`
   ${typoSmall}
 `;
 
-export const CommentBox = styled.div`
+export const CommentBox = styled(Linkify).attrs({
+  tagName: 'div',
+  options: { attributes: { rel: 'noopener noreferrer' } },
+})`
   padding: ${size3} ${size4};
   background: var(--theme-background-highlight);
   border-radius: ${size2};
   ${typoLil1}
+
+  a {
+    color: ${colorWater60};
+  }
 `;
 
 export const ButtonLoader = styled(Loader)`
