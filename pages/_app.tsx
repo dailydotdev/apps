6de22509ae9e3900f5607ac4285511ed7d070af7
Ido Shamun <idoesh1@gmail.com>
2020-08-18 18:26:18 +0300

    feat: set meta theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -98,6 +98,12 @@ export default function App({
         `,
             }}
           />
+          <meta name="theme-color" content="#151618" />
+          <meta name="msapplication-navbutton-color" content="#151618" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="#151618"
+          />
         </Head>
         <DefaultSeo {...Seo} />
         <GlobalStyle />
