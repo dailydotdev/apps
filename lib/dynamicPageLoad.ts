b882800fd56e7c008afd11eec519233ff5abd6f3
Ido Shamun <ido@daily.dev>
2020-12-22 21:54:26 +0200

    refactor: lazy load flipper component in profile page (#235)

--- lib/dynamicPageLoad.ts
+++ lib/dynamicPageLoad.ts
@@ -1,34 +1,11 @@
 import React from 'react';
 import { LoaderComponent } from 'next/dist/next-server/lib/dynamic';
 import dynamic from 'next/dynamic';
+import onPageLoad from './onPageLoad';
 
 export default function dynamicPageLoad<P>(
   loader: () => LoaderComponent<P>,
   readyState: DocumentReadyState = 'interactive',
 ): React.ComponentType<P> {
-  return dynamic<P>(
-    () =>
-      new Promise((resolve, reject) => {
-        const callLoader = () => loader().then(resolve).catch(reject);
-
-        if (
-          document.readyState === readyState ||
-          document.readyState === 'complete'
-        ) {
-          return callLoader();
-        }
-
-        const callback = (event: ProgressEvent<Document>) => {
-          if (
-            event.target.readyState === readyState ||
-            document.readyState === 'complete'
-          ) {
-            document.removeEventListener('readystatechange', callback);
-            return callLoader();
-          }
-        };
-        document.addEventListener('readystatechange', callback);
-      }),
-    { ssr: false },
-  );
+  return dynamic<P>(() => onPageLoad(readyState).then(loader), { ssr: false });
 }
