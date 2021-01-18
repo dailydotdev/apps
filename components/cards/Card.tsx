00f9802f8570df703af2f91e6cf88f149cf328eb
Ido Shamun <ido@daily.dev>
2021-01-18 16:08:25 +0200

    feat: add placeholder card and ad card to design system (#288)

---
+++ components/cards/Card.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components/macro';
+import { size1px, size2, size3, size4, sizeN } from '../../styles/sizes';
+import { typoBody } from '../../styles/typography';
+import { multilineTextOverflow } from '../../styles/helpers';
+import LazyImage from '../LazyImage';
+
+export const cardMaxWidth = sizeN(85);
+export const cardImageHeight = sizeN(40);
+
+export const CardTitle = styled.h2`
+  margin: ${size4} 0;
+  color: var(--theme-primary);
+  font-weight: bold;
+  -webkit-line-clamp: 3;
+  ${typoBody}
+  ${multilineTextOverflow}
+`;
+
+export const CardTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 ${size4};
+`;
+
+export const CardImage = styled(LazyImage)`
+  height: ${cardImageHeight};
+  border-radius: ${size3};
+`;
+
+export const CardSpace = styled.div`
+  flex: 1;
+`;
+
+export const CardLink = styled.a`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Card = styled.article`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  max-width: ${cardMaxWidth};
+  padding: ${size2};
+  border-radius: ${size4};
+  border: ${size1px} solid var(--theme-divider-tertiary);
+  background: var(--theme-background-secondary);
+  box-shadow: var(--theme-shadow2);
+
+  &:hover {
+    border-color: var(--theme-divider-secondary);
+  }
+
+  & > * {
+    pointer-events: none;
+  }
+
+  a,
+  button {
+    pointer-events: all;
+    z-index: 1;
+  }
+
+  ${CardLink} {
+    z-index: unset;
+  }
+`;
---
+++ components/cards/PlaceholderCard.tsx
@@ -0,0 +1,90 @@
+import React, { HTMLAttributes, ReactElement } from 'react';
+import styled, { keyframes } from 'styled-components/macro';
+import {
+  cardImageHeight,
+  cardMaxWidth,
+  CardSpace,
+  CardTextContainer,
+} from './Card';
+import { size2, size3, size4, size6 } from '../../styles/sizes';
+
+const PlaceholderShimmer = keyframes`
+  100% {
+    transform: translateX(100%);
+  }
+`;
+
+const Placeholder = styled.div`
+  position: relative;
+  overflow: hidden;
+  background: var(--theme-background-secondary);
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+      90deg,
+      var(--theme-background-secondary),
+      var(--theme-background-tertiary) 15%,
+      var(--theme-background-secondary)
+    );
+    transform: translateX(-100%);
+    animation: ${PlaceholderShimmer} 1.25s infinite linear;
+    will-change: transform;
+  }
+`;
+
+const Source = styled(Placeholder)`
+  width: 1em;
+  height: 1em;
+  font-size: ${size6};
+  border-radius: 100%;
+`;
+
+const Text = styled(Placeholder)`
+  width: 1em;
+  height: ${size3};
+  border-radius: ${size3};
+`;
+
+const Image = styled(Placeholder)`
+  border-radius: ${size3};
+  height: ${cardImageHeight};
+`;
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  max-width: ${cardMaxWidth};
+  border-radius: ${size4};
+  padding: ${size2};
+  background: var(--theme-post-disabled);
+
+  ${Source}, ${Text}, ${CardSpace}, ${Image} {
+    margin: ${size2} 0;
+  }
+`;
+
+export type PlaceholderCardProps = HTMLAttributes<HTMLDivElement>;
+
+export function PlaceholderCard(props: PlaceholderCardProps): ReactElement {
+  return (
+    <Container aria-busy {...props}>
+      <CardTextContainer>
+        <Source />
+        <Text style={{ width: '100%' }} />
+        <Text style={{ width: '100%' }} />
+        <Text style={{ width: '80%' }} />
+      </CardTextContainer>
+      <CardSpace />
+      <Image />
+      <CardTextContainer>
+        <Text style={{ width: '32%' }} />
+      </CardTextContainer>
+    </Container>
+  );
+}
