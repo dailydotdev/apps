b8975611f7c79def647ec351cfd040ceb6f46604
Ido Shamun <idoesh1@gmail.com>
2021-01-21 17:58:37 +0200

    refactor(header): remove about button

--- components/layouts/MainLayout/SecondPhaseButtons.tsx
+++ components/layouts/MainLayout/SecondPhaseButtons.tsx
@@ -1,21 +1,13 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import GiftIcon from '../../../icons/gift.svg';
-import BellNotifyIcon from '../../../icons/bell_notify.svg';
-import BellIcon from '../../../icons/bell.svg';
 import styled from 'styled-components/macro';
 import { sizeN } from '../../../styles/sizes';
 import { laptopL } from '../../../styles/media';
-import AboutModal from '../../modals/AboutModal';
 import usePersistentState from '../../../lib/usePersistentState';
-import TertiaryButton from '../../buttons/TertiaryButton';
 import { ButtonProps } from '../../buttons/BaseButton';
 import QuandaryButton from '../../buttons/QuandaryButton';
 
 const buttonMargin = sizeN(1.5);
-
-const AboutButton = styled(TertiaryButton)<ButtonProps<'button'>>`
-  margin: 0 ${buttonMargin};
-`;
 
 const GiftButton = styled(QuandaryButton).attrs({
   labelMediaQuery: laptopL,
@@ -24,19 +16,11 @@ const GiftButton = styled(QuandaryButton).attrs({
 `;
 
 export default function SecondPhaseButtons(): ReactElement {
-  const [showAbout, setShowAbout] = useState(false);
-  const [showBadge, setShowBadge] = usePersistentState('about', false, true);
   const [didClickedTshirt, setDidClickTshirt] = usePersistentState(
     'tshirt',
     true,
     false,
   );
-
-  const onAboutClick = async () => {
-    setShowAbout(true);
-    await setShowBadge(false);
-  };
-
   return (
     <>
       <GiftButton
@@ -54,15 +38,6 @@ export default function SecondPhaseButtons(): ReactElement {
       >
         {!didClickedTshirt && 'Get free T-shirt'}
       </GiftButton>
-      <AboutButton
-        onClick={onAboutClick}
-        title="About"
-        icon={showBadge ? <BellNotifyIcon /> : <BellIcon />}
-      />
-      <AboutModal
-        isOpen={showAbout}
-        onRequestClose={() => setShowAbout(false)}
-      />
     </>
   );
 }
