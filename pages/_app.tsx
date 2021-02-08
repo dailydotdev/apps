200242e3609ad3d9202f14f8fd77d90680964fc8
Ido Shamun <idoesh1@gmail.com>
2021-02-08 10:11:03 +0200

    refactor: disable debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 // Must be the first import
-if (process.env.NODE_ENV === 'development' || true) {
+if (process.env.NODE_ENV === 'development') {
   // Must use require here as import statements are only allowed
   // to exist at top-level.
   require('preact/debug');
