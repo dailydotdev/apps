8e7ea109ffbfad32f32c029119efceb8cd933f65
Ido Shamun <idoesh1@gmail.com>
2021-02-23 10:22:08 +0200

    refactor: update links to privacy and cookie policy

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,8 +4,8 @@ export const requestFeature =
 export const reportIssue =
   'https://github.com/dailydotdev/daily/issues/new?assignees=&labels=Type%3A+Bug&template=---bug-report.md&title=%F0%9F%90%9B+BUG%3A+';
 export const termsOfService = 'https://daily.dev/tos';
-export const privacyPolicy = 'https://daily.dev/privacy';
-export const cookiePolicy = 'https://daily.dev/cookies';
+export const privacyPolicy = 'https://daily.dev/privacy/applications-policy';
+export const cookiePolicy = 'https://daily.dev/privacy/applications-cookies';
 export const reputationGuide =
   'https://daily.dev/posts/what-is-reputation-how-do-i-earn-it';
 export const ownershipGuide =
