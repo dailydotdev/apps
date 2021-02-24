0471ea59b79dcf5379f272d80fbbf1d1f275b2a7
Ido Shamun <ido@daily.dev>
2021-02-24 14:01:47 +0200

    feat: add pwa support (#371)

--- next.config.js
+++ next.config.js
@@ -1,45 +1,52 @@
 const withPreact = require('next-plugin-preact');
+const withPWA = require('next-pwa');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withPreact(
-  withBundleAnalyzer({
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /icons\/.*\.svg$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              icon: true,
-              svgo: true,
-              replaceAttrValues: {
-                '#fff': 'currentcolor',
-                '#FFF': 'currentcolor',
-                '#FFFFFF': 'currentcolor',
-              },
-              svgProps: {
-                className: 'icon',
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
+  ...withPreact(
+    withBundleAnalyzer({
+      webpack: (config) => {
+        config.module.rules.push({
+          test: /icons\/.*\.svg$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                icon: true,
+                svgo: true,
+                replaceAttrValues: {
+                  '#fff': 'currentcolor',
+                  '#FFF': 'currentcolor',
+                  '#FFFFFF': 'currentcolor',
+                },
+                svgProps: {
+                  className: 'icon',
+                },
               },
             },
-          },
-        ],
-      });
+          ],
+        });
 
-      return config;
-    },
-
-    rewrites: () => [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        return config;
       },
-    ],
 
-    poweredByHeader: false,
-    reactStrictMode: false,
-  }),
-);
+      rewrites: () => [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        },
+      ],
+
+      poweredByHeader: false,
+      reactStrictMode: false,
+    }),
+  ),
+});
