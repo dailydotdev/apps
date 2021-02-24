87a04b0cbc6fc34c09d48b84cf5f00c6e72dfd4b
Ido Shamun <idoesh1@gmail.com>
2021-02-24 13:57:20 +0200

    feat: add pwa support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -152,6 +152,38 @@ function InternalApp({ Component, pageProps, router }: AppProps): ReactElement {
                 name="apple-mobile-web-app-status-bar-style"
                 content="#151618"
               />
+
+              <meta name="application-name" content="daily.dev" />
+              <meta name="apple-mobile-web-app-capable" content="yes" />
+              <meta
+                name="apple-mobile-web-app-status-bar-style"
+                content="default"
+              />
+              <meta name="apple-mobile-web-app-title" content="daily.dev" />
+              <meta name="format-detection" content="telephone=no" />
+              <meta name="mobile-web-app-capable" content="yes" />
+              <meta name="msapplication-TileColor" content="#151618" />
+              <meta name="msapplication-tap-highlight" content="no" />
+
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
+              <link rel="manifest" href="/manifest.json" />
+
               <script
                 dangerouslySetInnerHTML={{
                   __html: `window.addEventListener('load', () => { window.windowLoaded = true; }, {
