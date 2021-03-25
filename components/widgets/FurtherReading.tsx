26e0291885f23c37f41e17da2c632b4427bd94ea
Ido Shamun <idoesh1@gmail.com>
2021-03-25 16:24:23 +0200

    feat: add best discussions widget to article page

---
+++ components/widgets/FurtherReading.tsx
@@ -0,0 +1,136 @@
+import React, { ReactElement, useContext } from 'react';
+import SimilarPosts from './SimilarPosts';
+import classNames from 'classnames';
+import AuthContext from '../../contexts/AuthContext';
+import { QueryClient, useQuery, useQueryClient } from 'react-query';
+import {
+  FURTHER_READING_QUERY,
+  FurtherReadingData,
+} from '../../graphql/furtherReading';
+import request from 'graphql-request';
+import { apiUrl } from '../../lib/config';
+import useBookmarkPost from '../../hooks/useBookmarkPost';
+import { Post } from '../../graphql/posts';
+import { trackEvent } from '../../lib/analytics';
+import BestDiscussions from './BestDiscussions';
+
+export type FurtherReadingProps = {
+  postId: string;
+  tags: string[];
+  className?: string;
+};
+
+const transformPosts = (
+  posts: Post[],
+  id: string,
+  update: (oldPost: Post) => Partial<Post>,
+): Post[] =>
+  posts.map((post) =>
+    post.id === id
+      ? {
+          ...post,
+          ...update(post),
+        }
+      : post,
+  );
+
+const updatePost = (
+  queryClient: QueryClient,
+  queryKey: string[],
+  update: (oldPost: Post) => Partial<Post>,
+): (({}: { id: string }) => Promise<() => void>) => async ({ id }) => {
+  await queryClient.cancelQueries(queryKey);
+  const previousData = queryClient.getQueryData<FurtherReadingData>(queryKey);
+  queryClient.setQueryData(queryKey, {
+    trendingPosts: transformPosts(previousData.trendingPosts, id, update),
+    similarPosts: transformPosts(previousData.similarPosts, id, update),
+  });
+  return () => {
+    queryClient.setQueryData(queryKey, previousData);
+  };
+};
+
+export default function FurtherReading({
+  postId,
+  tags,
+  className,
+}: FurtherReadingProps): ReactElement {
+  const queryKey = ['furtherReading', postId];
+  const { user, showLogin } = useContext(AuthContext);
+  const queryClient = useQueryClient();
+  const { data: posts, isLoading } = useQuery<FurtherReadingData>(
+    queryKey,
+    () =>
+      request(`${apiUrl}/graphql`, FURTHER_READING_QUERY, {
+        loggedIn: !!user,
+        post: postId,
+        trendingFirst: 1,
+        similarFirst: 3,
+        discussedFirst: 4,
+        tags,
+      }),
+    {
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+      refetchOnMount: false,
+    },
+  );
+
+  const { bookmark, removeBookmark } = useBookmarkPost({
+    onBookmarkMutate: updatePost(queryClient, queryKey, () => ({
+      bookmarked: true,
+    })),
+    onRemoveBookmarkMutate: updatePost(queryClient, queryKey, () => ({
+      bookmarked: false,
+    })),
+  });
+
+  if (!posts?.similarPosts && !isLoading) {
+    return <></>;
+  }
+
+  const similarPosts = posts?.similarPosts
+    ? [
+        ...posts.trendingPosts,
+        ...posts.similarPosts.slice(
+          0,
+          Math.min(posts.similarPosts.length, 3 - posts.trendingPosts.length),
+        ),
+      ]
+    : [];
+
+  const onBookmark = async (post: Post): Promise<void> => {
+    if (!user) {
+      showLogin();
+      return;
+    }
+    const bookmarked = !post.bookmarked;
+    trackEvent({
+      category: 'Post',
+      action: 'Bookmark',
+      label: bookmarked ? 'Add' : 'Remove',
+    });
+    if (bookmarked) {
+      await bookmark({ id: post.id });
+    } else {
+      await removeBookmark({ id: post.id });
+    }
+  };
+
+  return (
+    <div className={classNames(className, 'flex flex-col')}>
+      <SimilarPosts
+        posts={similarPosts}
+        isLoading={isLoading}
+        onBookmark={onBookmark}
+      />
+      <BestDiscussions
+        posts={posts?.discussedPosts}
+        isLoading={isLoading}
+        className="mt-6"
+      />
+    </div>
+  );
+}
