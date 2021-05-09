208848bb0ec89e520914bb2575f9e9c8d67a9074
Ido Shamun <idoesh1@gmail.com>
2021-05-09 19:19:11 +0300

    build: alias peer deps of the shared package to prevent dups

--- packages/webapp/babel.config.js
+++ packages/webapp/babel.config.js
@@ -1,6 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const sharedPackage = require('../shared/package.json');
+
 module.exports = {
   presets: ['next/babel'],
-  plugins: ['macros', '@emotion'],
+  plugins: [
+    'macros',
+    '@emotion',
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.'],
+        alias: {
+          // Required to remove duplicate dependencies from the build
+          ...Object.keys(sharedPackage.peerDependencies).reduce((acc, dep) => {
+            if (dep.match(/(react)|(react-dom)/)) {
+              return {
+                ...acc,
+                [dep]: path.resolve('./node_modules/preact/compat'),
+              };
+            }
+            return { ...acc, [dep]: path.resolve(`./node_modules/${dep}`) };
+          }, {}),
+        },
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: [
