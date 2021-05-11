39e7b1cf7f2f9c8061cbc4666470a57d93749369
Ido Shamun <idoesh1@gmail.com>
2021-05-11 14:26:23 +0300

    build: fix type issues

--- packages/shared/src/components/modals/StyledModal.tsx
+++ packages/shared/src/components/modals/StyledModal.tsx
@@ -1,11 +1,24 @@
-import { ReactNode } from 'react';
-import Modal from 'react-modal';
-import { ReactModalAdapter } from './ReactModalAdapter';
 import classed from '../../lib/classed';
 import styles from './StyledModal.module.css';
+import Modal from 'react-modal';
+import React, { ReactElement, ReactNode } from 'react';
 
 export interface ModalProps extends Modal.Props {
   children?: ReactNode;
+}
+
+export function ReactModalAdapter({
+  className,
+  ...props
+}: ModalProps): ReactElement {
+  return (
+    <Modal
+      portalClassName={className.toString()}
+      overlayClassName="overlay"
+      className="modal focus-outline"
+      {...props}
+    />
+  );
 }
 
 export const StyledModal = classed(ReactModalAdapter, styles.styledModal);
