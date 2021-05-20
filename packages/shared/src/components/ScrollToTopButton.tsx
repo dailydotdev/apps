48d7e490b2b572f1c910394a013a7b931f38f053
Ido Shamun <ido@daily.dev>
2021-05-20 17:41:51 +0300

    feat: add scroll to top button in feed (#558)

---
+++ packages/shared/src/components/ScrollToTopButton.tsx
@@ -0,0 +1,49 @@
+import React, { CSSProperties, ReactElement, useEffect, useState } from 'react';
+import { Button, ButtonProps } from './buttons/Button';
+import ArrowIcon from '../../icons/arrow.svg';
+
+const baseStyle: CSSProperties = {
+  position: 'fixed',
+  transition: 'transform 0.1s ease-out, opacity 0.1s ease-out',
+  willChange: 'transform, opacity',
+};
+
+export default function ScrollToTopButton(): ReactElement {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const callback = () => {
+      setShow(document.documentElement.scrollTop >= window.innerHeight / 2);
+    };
+    window.addEventListener('scroll', callback, { passive: true });
+    return () => window.removeEventListener('scroll', callback);
+  }, []);
+
+  const props: ButtonProps<'button'> = {
+    icon: <ArrowIcon />,
+    onClick: () => window.scrollTo({ top: 0, behavior: 'smooth' }),
+  };
+
+  const style: CSSProperties = {
+    ...baseStyle,
+    transform: show ? undefined : 'translateY(1rem)',
+    opacity: show ? undefined : 0,
+  };
+
+  return (
+    <>
+      <Button
+        {...props}
+        className="btn-primary right-4 laptop:hidden z-2"
+        buttonSize="large"
+        style={{ ...style, bottom: '4.5rem' }}
+      />
+      <Button
+        {...props}
+        className="btn-primary right-8 bottom-8 hidden laptop:flex z-2"
+        buttonSize="xlarge"
+        style={style}
+      />
+    </>
+  );
+}
