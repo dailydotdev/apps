bc93c27b0cb1422ec3d4ba39ff45e7ab2aa25b1b
Ido Shamun <idoesh1@gmail.com>
2021-06-09 16:53:54 +0300

    feat: add settinntry monitoring

--- packages/webapp/next.config.js
+++ packages/webapp/next.config.js
@@ -1,56 +1,59 @@
 const withPreact = require('next-plugin-preact');
 const withPWA = require('next-pwa');
 const withTM = require('next-transpile-modules')(['@dailydotdev/shared']);
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withTM(
-  withPWA({
-    pwa: {
-      dest: 'public',
-      disable: process.env.NODE_ENV === 'development',
-    },
-    ...withPreact(
-      withBundleAnalyzer({
-        webpack: (config) => {
-          config.module.rules.push({
-            test: /icons\/.*\.svg$/,
-            exclude: /node_modules\/(?!@dailydotdev)/,
-            use: [
-              {
-                loader: '@svgr/webpack',
-                options: {
-                  icon: true,
-                  svgo: true,
-                  replaceAttrValues: {
-                    '#fff': 'currentcolor',
-                    '#FFF': 'currentcolor',
-                    '#FFFFFF': 'currentcolor',
-                  },
-                  svgProps: {
-                    className: 'icon',
+module.exports = withSentryConfig(
+  withTM(
+    withPWA({
+      pwa: {
+        dest: 'public',
+        disable: process.env.NODE_ENV === 'development',
+      },
+      ...withPreact(
+        withBundleAnalyzer({
+          webpack: (config) => {
+            config.module.rules.push({
+              test: /icons\/.*\.svg$/,
+              exclude: /node_modules\/(?!@dailydotdev)/,
+              use: [
+                {
+                  loader: '@svgr/webpack',
+                  options: {
+                    icon: true,
+                    svgo: true,
+                    replaceAttrValues: {
+                      '#fff': 'currentcolor',
+                      '#FFF': 'currentcolor',
+                      '#FFFFFF': 'currentcolor',
+                    },
+                    svgProps: {
+                      className: 'icon',
+                    },
                   },
                 },
-              },
-            ],
-          });
+              ],
+            });
 
-          return config;
-        },
-
-        rewrites: () => [
-          {
-            source: '/api/:path*',
-            destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+            return config;
           },
-        ],
 
-        poweredByHeader: false,
-        reactStrictMode: false,
-        productionBrowserSourceMaps: process.env.SOURCE_MAPS === 'true',
-      }),
-    ),
-  }),
+          rewrites: () => [
+            {
+              source: '/api/:path*',
+              destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+            },
+          ],
+
+          poweredByHeader: false,
+          reactStrictMode: false,
+          productionBrowserSourceMaps: process.env.SOURCE_MAPS === 'true',
+        }),
+      ),
+    }),
+  ),
 );
