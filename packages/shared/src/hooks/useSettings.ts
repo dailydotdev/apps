ec62e34447d64e88efc42af3cafc7ec35d4ba1b4
Mahdhi Rezvi <mahdhi_rezvi@yahoo.com>
2021-06-27 12:20:59 +0530

    Monitor for OS theme change while on auto theme mode

--- packages/shared/src/hooks/useSettings.ts
+++ packages/shared/src/hooks/useSettings.ts
@@ -1,5 +1,5 @@
 import usePersistentState from './usePersistentState';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { SettingsContextData } from '../contexts/SettingsContext';
 import { useMutation, useQuery } from 'react-query';
 import request from 'graphql-request';
@@ -27,6 +27,7 @@ const defaultSettings: Settings = {
   openNewTab: true,
   insaneMode: false,
 };
+let darkModeMediaQuery: MediaQueryList;
 
 function toggleTheme(lightMode: boolean): void {
   if (lightMode) {
@@ -38,7 +39,7 @@ function toggleTheme(lightMode: boolean): void {
   localStorage.setItem(themeCookieName, lightMode ? 'true' : 'false');
 }
 
-function isSystemThemeDark(): boolean {
+function isSystemThemeLight(): boolean {
   if (window.matchMedia) {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       // dark mode
@@ -60,7 +61,7 @@ export default function useSettings(
   );
   const [lightMode, setLightMode] = useState(false);
   const [themeMode, setThemeMode] = useState('dark');
-
+  const [isMonitoring, setIsMonitoring] = useState(false);
   const { data: remoteSettings } = useQuery<UserSettingsData>(
     ['userSettings', userId],
     () => request(`${apiUrl}/graphql`, USER_SETTINGS_QUERY),
@@ -91,14 +92,35 @@ export default function useSettings(
   }, [remoteSettings]);
 
   useEffect(() => {
-    const lightModeCookieValue = localStorage.getItem(themeCookieName);
-    if (lightModeCookieValue === 'true') {
-      setLightMode(true);
-      document.documentElement.classList.add('light');
+    if (!darkModeMediaQuery) {
+      darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     }
+
     const themeModeCookieValue = localStorage.getItem(themeModeCookieName);
     setThemeMode(themeModeCookieValue);
+    // Start monitoring OS theme change, when auto is set at page load
+    if (themeModeCookieValue == 'auto') {
+      // Make sure it is not already being monitored to avoid multiple event handlers
+      if (!isMonitoring) {
+        setIsMonitoring(true);
+        startWatchingForSystemThemeChange();
+      }
+      //Check if OS theme has changed since last saved state
+      if (isSystemThemeLight()) {
+        setLightMode(true);
+        document.documentElement.classList.add('light');
+      }
+    } else {
+      const lightModeCookieValue = localStorage.getItem(themeCookieName);
+      if (lightModeCookieValue === 'true') {
+        setLightMode(true);
+        document.documentElement.classList.add('light');
+      }
+    }
+
   }, []);
+
+
 
   const triggerThemeChange = async (isLightMode: boolean): Promise<void> => {
     setLightMode(isLightMode);
@@ -122,18 +144,40 @@ export default function useSettings(
     }
   };
 
+  const handleSystemThemeChangeCallback = useCallback((e: MediaQueryListEvent) => {
+    const newColorScheme = e.matches ? "dark" : "light";
+    triggerThemeChange(newColorScheme == 'light');
+  }, [])
+
+  const startWatchingForSystemThemeChange = () => {
+    if (darkModeMediaQuery)
+      darkModeMediaQuery.addEventListener('change', handleSystemThemeChangeCallback);
+  }
+
+  const stopWatchingForSystemThemeChange = () => {
+    if (darkModeMediaQuery)
+      darkModeMediaQuery.removeEventListener('change', handleSystemThemeChangeCallback);
+  }
+
   return {
     ...settings,
     themeMode,
     setUIThemeMode: async (mode: string) => {
       if (mode == 'auto') {
-        const isLightMode = isSystemThemeDark();
+        const isLightMode = isSystemThemeLight();
         triggerThemeChange(isLightMode);
+        if (!isMonitoring) {
+          setIsMonitoring(true);
+          startWatchingForSystemThemeChange();
+        }
       } else {
         triggerThemeChange(mode == 'light');
+        stopWatchingForSystemThemeChange();
+        setIsMonitoring(false);
       }
       setThemeMode(mode);
       localStorage.setItem(themeModeCookieName, mode);
+
     },
     toggleShowOnlyUnreadPosts: () =>
       setSettings({
