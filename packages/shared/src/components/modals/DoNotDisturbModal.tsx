5e5f7a162d767a0ef9c977564a31932e18f2358a
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-07-16 22:43:52 +0400

    feat: dnd implementation on modal

--- packages/shared/src/components/modals/DoNotDisturbModal.tsx
+++ packages/shared/src/components/modals/DoNotDisturbModal.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import classnames from 'classnames';
+import dnd, {
+  CustomTime,
+  TimeFormat,
+  getTotalMinutes,
+  getTimeFormatValue,
+} from '../../lib/dnd';
 import { Radio } from '../fields/Radio';
 import { Dropdown } from '../fields/Dropdown';
 import { TextField } from '../fields/TextField';
 import { StyledModal, ModalProps } from './StyledModal';
 import styles from './DoNotDisturbModal.module.css';
-
-enum TimeFormat {
-  HALF_HOUR = '30 minutes',
-  ONE_HOUR = '1 hour',
-  TWO_HOURS = '2 hours',
-  TOMORROW = 'Tomorrow',
-  CUSTOM = 'Custom...',
-}
-
-enum CustomTime {
-  MINUTES = 'Minutes',
-  HOURS = 'Hours',
-  DAYS = 'Days',
-}
-
-const HOUR_PER_DAY = 24;
-const MINUTES_PER_HOUR = 60;
-
-const getTotalMinutes = (customTime: CustomTime, value: number) => {
-  if (customTime === CustomTime.MINUTES) return value;
-
-  if (customTime === CustomTime.HOURS) return value * MINUTES_PER_HOUR;
-
-  return value * HOUR_PER_DAY * MINUTES_PER_HOUR;
-};
 
 export interface DoNotDisturbModalProps extends ModalProps {
   defaultTimeFormat?: TimeFormat;
@@ -44,6 +25,7 @@ const customTimeOptions = Object.values(CustomTime);
 
 const DoNotDisturbModal: React.FC<DoNotDisturbModalProps> = ({
   defaultTimeFormat = TimeFormat.HALF_HOUR,
+  onRequestClose,
   ...modalProps
 }) => {
   const [link, setLink] = React.useState('');
@@ -51,11 +33,22 @@ const DoNotDisturbModal: React.FC<DoNotDisturbModalProps> = ({
   const [customTimeIndex, setCustomTimeIndex] = React.useState(0);
   const [dndTime, setDndTime] = React.useState(defaultTimeFormat);
 
-  const handleSubmit = () => {
-    const minutes = getTotalMinutes(
-      customTimeOptions[customTimeIndex],
-      customNumber,
-    );
+  const handleSubmit = (e: React.MouseEvent<Element, MouseEvent>) => {
+    const minutes =
+      dndTime === TimeFormat.CUSTOM
+        ? getTotalMinutes(customTimeOptions[customTimeIndex], customNumber)
+        : getTimeFormatValue(dndTime);
+    const error = dnd.setDndMode({
+      link,
+      minutes,
+      timestamp: new Date(),
+    });
+
+    if (!error) return;
+
+    // display error here
+
+    onRequestClose(e);
   };
 
   return (
