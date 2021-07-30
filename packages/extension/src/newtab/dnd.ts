e9f551ce91a37b00d1331e1673abb9c5ffda2c7e
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-07-30 23:37:03 +0400

    refactor: unnecessary function

--- packages/extension/src/newtab/dnd.ts
+++ packages/extension/src/newtab/dnd.ts
@@ -16,16 +16,6 @@ export enum CustomTime {
 const DEFAULT_URL = 'https://www.google.com';
 const CHROME_DEFAULT_URL = 'chrome-search://local-ntp/local-ntp.html';
 
-export const getExpiration = (time: CustomTime, value: number): Date => {
-  const exp = new Date();
-
-  if (time === CustomTime.DAYS) return addDays(exp, value);
-
-  if (time === CustomTime.HOURS) return addHours(exp, value);
-
-  return addMinutes(exp, value);
-};
-
 export const getDefaultLink = (): string =>
   process.env.TARGET_BROWSER === 'chrome' ? CHROME_DEFAULT_URL : DEFAULT_URL;
 interface DndOption<T extends TimeFormat> {
@@ -41,35 +31,41 @@ export const dndOption: { [K in TimeFormat]: DndOption<K> } = {
     value: 30,
     label: '30 minutes',
     getExpiration(): Date {
-      return getExpiration(CustomTime.MINUTES, this.value);
+      return addMinutes(new Date(), this.value);
     },
   },
   ONE_HOUR: {
     value: 1,
     label: '1 hour',
     getExpiration(): Date {
-      return getExpiration(CustomTime.HOURS, this.value);
+      return addHours(new Date(), this.value);
     },
   },
   TWO_HOURS: {
     value: 2,
     label: '2 hours',
     getExpiration(): Date {
-      return getExpiration(CustomTime.HOURS, this.value);
+      return addHours(new Date(), this.value);
     },
   },
   TOMORROW: {
     value: 1,
     label: 'Tomorrow',
     getExpiration(): Date {
-      return getExpiration(CustomTime.DAYS, this.value);
+      return addDays(new Date(), this.value);
     },
   },
   CUSTOM: {
     value: 0,
     label: 'Custom...',
     getExpiration(time: CustomTime, value: number): Date {
-      return getExpiration(time, value);
+      const exp = new Date();
+
+      if (time === CustomTime.DAYS) return addDays(exp, value);
+
+      if (time === CustomTime.HOURS) return addHours(exp, value);
+
+      return addMinutes(exp, value);
     },
   },
 };
