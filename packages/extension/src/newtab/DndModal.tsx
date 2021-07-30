40ca46b5a45d15c5bc15a4930ffe65b04e9d2881
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-07-31 00:31:05 +0400

    feat: Modal UI when dnd is active

--- packages/extension/src/newtab/DndModal.tsx
+++ packages/extension/src/newtab/DndModal.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState, useContext } from 'react';
+import { format } from 'date-fns';
 import { getDefaultLink, dndOption, CustomTime, TimeFormat } from './dnd';
 import { Radio } from '@dailydotdev/shared/src/components/fields/Radio';
 import { Dropdown } from '@dailydotdev/shared/src/components/fields/Dropdown';
@@ -20,7 +21,7 @@ const DoNotDisturbModal: FC<ModalProps> = ({
   onRequestClose,
   ...modalProps
 }) => {
-  const { setDndSettings, isActive } = useContext(DndContext);
+  const { dndSettings, setDndSettings, isActive } = useContext(DndContext);
   const [link, setLink] = useState('');
   const [customNumber, setCustomNumber] = useState(0);
   const [customTimeIndex, setCustomTimeIndex] = useState(0);
@@ -37,42 +38,65 @@ const DoNotDisturbModal: FC<ModalProps> = ({
     onRequestClose(e);
   };
 
-  const renderForm = () => {
-    if (isActive) return <br />;
+  const turnedOnContent = (
+    <div className="grid grid-cols-2 gap-6 justify-center my-6">
+      <button
+        onClick={onRequestClose}
+        className="flex justify-center items-center w-28 h-12 rounded-xl border border-white text-base text-white font-bold"
+      >
+        Cancel
+      </button>
+      <button
+        onClick={() => setDndSettings(null)}
+        className="flex justify-center items-center w-28 h-12 rounded-xl text-base text-white font-bold bg-onion-40"
+      >
+        Turn Off
+      </button>
+    </div>
+  );
 
-    return (
-      <div className="w-full mt-2 py-4 px-10">
-        <TextField
-          inputId="defaultURL"
-          label="Default URL (optional)"
-          valueChanged={(text) => setLink(text)}
-        />
-        <Radio
-          className="mt-8 "
-          name="timeOff"
-          value={dndTime}
-          options={timeFormatOptions}
-          onChange={(value: TimeFormat) => setDndTime(value)}
-        />
-        {dndTime !== 'CUSTOM' ? null : (
-          <div className="grid grid-cols-2 gap-4 mt-4">
-            <TextField
-              className="w-40 appearance-none"
-              inputId="defaultURL"
-              label="Number"
-              type="number"
-              valueChanged={(number) => setCustomNumber(parseInt(number))}
-            />
-            <Dropdown
-              className="w-40"
-              options={customTimeOptions}
-              selectedIndex={customTimeIndex}
-              onChange={(_, index) => setCustomTimeIndex(index)}
-            />
-          </div>
-        )}
-      </div>
-    );
+  const turnedOffContent = (
+    <div className="w-full mt-2 py-4 px-10">
+      <TextField
+        inputId="defaultURL"
+        label="Default URL (optional)"
+        valueChanged={(text) => setLink(text)}
+      />
+      <Radio
+        className="mt-8 "
+        name="timeOff"
+        value={dndTime}
+        options={timeFormatOptions}
+        onChange={(value: TimeFormat) => setDndTime(value)}
+      />
+      {dndTime !== 'CUSTOM' ? null : (
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <TextField
+            className="w-40 appearance-none"
+            inputId="defaultURL"
+            label="Number"
+            type="number"
+            valueChanged={(number) => setCustomNumber(parseInt(number))}
+          />
+          <Dropdown
+            className="w-40"
+            options={customTimeOptions}
+            selectedIndex={customTimeIndex}
+            onChange={(_, index) => setCustomTimeIndex(index)}
+          />
+        </div>
+      )}
+    </div>
+  );
+
+  const getDescription = () => {
+    if (!isActive)
+      return "Choose your preferences while you're on Do Not Disturb mode";
+
+    const date = format(dndSettings.expiration, 'MM/dd/yy');
+    const time = format(dndSettings.expiration, 'hh:mm a');
+
+    return `Do Not Disturb is active and will be turned off on ${date} at ${time}`;
   };
 
   return (
@@ -90,21 +114,19 @@ const DoNotDisturbModal: FC<ModalProps> = ({
       <ModalCloseButton onClick={onRequestClose} />
       <div className="px-16 text-center">
         <h3 className="font-bold text-2xl text-white">Do Not Disturb</h3>
-        {isActive ? null : (
-          <p className="mt-1 text-sm text-salt-50">
-            Choose your preferences while you&apos;re on Do Not Disturb mode
-          </p>
-        )}
+        <p className="mt-1 text-sm text-salt-50">{getDescription()}</p>
       </div>
-      {renderForm()}
-      <div className="flex flex-row justify-center items-center w-full py-5 border-t border-salt-90 border-opacity-40">
-        <button
-          className="flex flex-row justify-center items-center w-40 h-10 rounded-xl py-5 font-bold text-sm text-pepper-90 bg-white"
-          onClick={(e) => (isActive ? setDndSettings(null) : handleSubmit(e))}
-        >
-          {isActive ? 'Turn off' : 'Done'}
-        </button>
-      </div>
+      {isActive ? turnedOnContent : turnedOffContent}
+      {isActive ? null : (
+        <div className="flex flex-row justify-center items-center w-full py-5 border-t border-salt-90 border-opacity-40">
+          <button
+            className="flex flex-row justify-center items-center w-40 h-10 rounded-xl py-5 font-bold text-sm text-pepper-90 bg-white"
+            onClick={handleSubmit}
+          >
+            Done
+          </button>
+        </div>
+      )}
     </StyledModal>
   );
 };
