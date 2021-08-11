aa1c59d312d8be25c3c1aa63f406ba3f1227caf7
Ido Shamun <idoesh1@gmail.com>
2021-08-11 14:23:06 +0300

    refactor: add facebook and twitter pixels

--- packages/shared/src/lib/analytics.ts
+++ packages/shared/src/lib/analytics.ts
@@ -5,8 +5,60 @@ declare global {
   interface Window {
     GoogleAnalyticsObject: string;
     ga: UniversalAnalytics.ga;
+    /* eslint-disable */
+    fbq: any;
+    _fbq: any;
+    twq: any;
+    /* eslint-enable */
   }
 }
+
+/* eslint-disable */
+const initFbPixel = (): void => {
+  (function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(
+    window,
+    document,
+    'script',
+    'https://connect.facebook.net/en_US/fbevents.js',
+  );
+  window.fbq('init', process.env.NEXT_PUBLIC_FB_PIXEL);
+  window.fbq('track', 'PageView');
+};
+
+const initTwPixel = (): void => {
+  (function (e, t, n, s, u, a) {
+    e.twq ||
+      ((s = e.twq =
+        function () {
+          s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+        }),
+      (s.version = '1.1'),
+      (s.queue = []),
+      (u = t.createElement(n)),
+      (u.async = !0),
+      (u.src = '//static.ads-twitter.com/uwt.js'),
+      (a = t.getElementsByTagName(n)[0]),
+      a.parentNode.insertBefore(u, a));
+  })(window, document, 'script');
+  window.twq('init', process.env.NEXT_PUBLIC_TW_PIXEL);
+  window.twq('track', 'PageView');
+};
+/* eslint-enable */
 
 export const initializeAnalyticsQueue = (clientId: string): void => {
   window.GoogleAnalyticsObject = 'ga';
@@ -92,6 +144,13 @@ export const loadAnalyticsScript = (): void => {
     }) as UniversalAnalytics.ga;
 
     queue.forEach((args) => window.ga(...(args as [string, ...unknown[]])));
+  }
+
+  if (process.env.NEXT_PUBLIC_FB_PIXEL) {
+    initFbPixel();
+  }
+  if (process.env.NEXT_PUBLIC_TW_PIXEL) {
+    initTwPixel();
   }
 };
 
