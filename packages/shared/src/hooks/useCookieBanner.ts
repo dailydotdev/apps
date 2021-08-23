0d765f51c0308a489baaad1bab6594e931740da7
Ido Shamun <idoesh1@gmail.com>
2021-08-23 10:55:12 +0300

    refactor: remove fb pixel

--- packages/shared/src/hooks/useCookieBanner.ts
+++ packages/shared/src/hooks/useCookieBanner.ts
@@ -25,9 +25,6 @@ export function useCookieBanner(): [
     ) {
       return;
     }
-    if (window?.location?.search.indexOf('heroic') > -1) {
-      return;
-    }
     if (user) {
       acceptCookies();
       return;
--- packages/shared/src/lib/analytics.ts
+++ packages/shared/src/lib/analytics.ts
@@ -14,31 +14,31 @@ declare global {
 }
 
 /* eslint-disable */
-const initFbPixel = (): void => {
-  (function (f, b, e, v, n, t, s) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = !0;
-    n.version = '2.0';
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = !0;
-    t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s);
-  })(
-    window,
-    document,
-    'script',
-    'https://connect.facebook.net/en_US/fbevents.js',
-  );
-  window.fbq('init', process.env.NEXT_PUBLIC_FB_PIXEL);
-  window.fbq('track', 'PageView');
-};
+// const initFbPixel = (): void => {
+//   (function (f, b, e, v, n, t, s) {
+//     if (f.fbq) return;
+//     n = f.fbq = function () {
+//       n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+//     };
+//     if (!f._fbq) f._fbq = n;
+//     n.push = n;
+//     n.loaded = !0;
+//     n.version = '2.0';
+//     n.queue = [];
+//     t = b.createElement(e);
+//     t.async = !0;
+//     t.src = v;
+//     s = b.getElementsByTagName(e)[0];
+//     s.parentNode.insertBefore(t, s);
+//   })(
+//     window,
+//     document,
+//     'script',
+//     'https://connect.facebook.net/en_US/fbevents.js',
+//   );
+//   window.fbq('init', process.env.NEXT_PUBLIC_FB_PIXEL);
+//   window.fbq('track', 'PageView');
+// };
 
 const initTwPixel = (): void => {
   (function (e, t, n, s, u, a) {
@@ -146,9 +146,9 @@ export const loadAnalyticsScript = (): void => {
     queue.forEach((args) => window.ga(...(args as [string, ...unknown[]])));
   }
 
-  if (process.env.NEXT_PUBLIC_FB_PIXEL) {
-    initFbPixel();
-  }
+  // if (process.env.NEXT_PUBLIC_FB_PIXEL) {
+  //   initFbPixel();
+  // }
   if (process.env.NEXT_PUBLIC_TW_PIXEL) {
     initTwPixel();
   }
