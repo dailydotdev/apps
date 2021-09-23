4042f09dda165c981bfadff3c1561a7bfd12ff1c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-09-23 17:37:40 +0800

    refactor(shared): create separate components for placeholder and upvotes list

---
+++ packages/shared/src/components/profile/UpvoterList.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement, memo } from 'react';
+import Link from 'next/link';
+import { Upvote } from '../../graphql/common';
+import { LazyImage } from '../LazyImage';
+import { Button } from '../buttons/Button';
+
+export interface UpvoterListProps {
+  upvotes: Upvote[];
+}
+
+export function UpvoterList({ upvotes }: UpvoterListProps): ReactElement {
+  return (
+    <div className="flex flex-col gap-3">
+      {upvotes.map(({ user }) => (
+        <Link key={user.username} href="/test">
+          <a className="flex flex-row hover:bg-theme-active px-6 py-3">
+            <LazyImage
+              imgSrc={user.image}
+              imgAlt={user.username}
+              className="w-12 h-12"
+            />
+            <div className="flex flex-col flex-1 ml-4">
+              <span className="typo-callout font-bold">{user.name}</span>
+              <span className="typo-callout text-theme-label-secondary">
+                @{user.username}
+              </span>
+              {user.bio && (
+                <span className="mt-1 typo-callout text-salt-90">
+                  {user.bio}
+                </span>
+              )}
+            </div>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default memo(UpvoterList);
---
+++ packages/shared/src/components/profile/UpvoterListPlaceholder.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement, memo } from 'react';
+import { PlaceholderList } from '../cards/PlaceholderList';
+
+export interface UpvoterListPlaceholderProps {
+  placeholderAmount?: number;
+}
+
+export function UpvoterListPlaceholder({
+  placeholderAmount = 5,
+}: UpvoterListPlaceholderProps): ReactElement {
+  return (
+    <div className="flex flex-col">
+      {Array(placeholderAmount)
+        .fill(0)
+        .map(() => (
+          <PlaceholderList key={Math.random()} />
+        ))}
+    </div>
+  );
+}
+
+export default memo(UpvoterListPlaceholder);
