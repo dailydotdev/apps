ee422555a4a0b583c989a0d312b6e8160ec40776
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-09-25 13:00:16 +0800

    fix(shared): upvoter ratio

--- packages/shared/src/components/profile/UpvoterList.tsx
+++ packages/shared/src/components/profile/UpvoterList.tsx
@@ -44,8 +44,8 @@ export function UpvoterList<
               <LazyImage
                 imgSrc={user.image}
                 imgAlt={user.username}
-                className="w-12 h-12 rounded-10"
-                ratio="1 / 1"
+                className="w-12 rounded-10"
+                ratio="100%"
               />
               <div className="flex flex-col flex-1 ml-4 typo-callout">
                 <span className="font-bold">{user.name}</span>
