7ebb8a201b62bd3577130e097dfb71be0cc39c98
Chris Bongers <chrisbongers@gmail.com>
2021-10-02 16:21:55 +0200

    Added account detail modal

--- packages/shared/src/components/ProfileMenu.tsx
+++ packages/shared/src/components/ProfileMenu.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { Item } from 'react-contexify';
 import Link from 'next/link';
@@ -8,60 +8,65 @@ const PortalMenu = dynamic(() => import('./fields/PortalMenu'), {
   ssr: false,
 });
 
+const AccountDetailsModal = dynamic(
+  () =>
+    import(
+      /* webpackChunkName: "accountDetailsModal" */ './modals/AccountDetailsModal'
+    ),
+);
+
 export type ProfileMenuProps = {};
 
 export default function ProfileMenu({}: ProfileMenuProps): ReactElement {
+  const [showAccountDetails, setShowAccountDetails] = useState(false);
   const { user, logout } = useContext(AuthContext);
-
   const shouldShowDnD = process.env.TARGET_BROWSER === 'chrome';
 
   return (
-    <PortalMenu
-      id="profile-context"
-      className="menu-primary"
-      animation="fade"
-      style={{ width: '9rem' }}
-    >
-      <Item>
-        <Link
-          href={`${process.env.NEXT_PUBLIC_WEBAPP_URL}${
-            user.username || user.id
-          }`}
-          passHref
-          prefetch={false}
-        >
-          <a className="w-full">Profile</a>
-        </Link>
-      </Item>
-      <Item>
-        <Link
-          href={`${process.env.NEXT_PUBLIC_WEBAPP_URL}`}
-          passHref
-          prefetch={false}
-        >
-          Account details
-        </Link>
-      </Item>
-      {shouldShowDnD && <Item>Do not disturb</Item>}
-      <Item>
-        <Link
-          href={`${process.env.NEXT_PUBLIC_WEBAPP_URL}devcard`}
-          passHref
-          prefetch={false}
-        >
-          <a className="w-full">Dev card</a>
-        </Link>
-      </Item>
-      <Item>
-        <a
-          href="https://it057218.typeform.com/to/S9p9SVNI"
-          target="_blank"
-          className="w-full"
-        >
-          Feedback
-        </a>
-      </Item>
-      <Item onClick={logout}>Logout</Item>
-    </PortalMenu>
+    <>
+      <AccountDetailsModal
+        isOpen={showAccountDetails}
+        onRequestClose={() => setShowAccountDetails(false)}
+      />
+      <PortalMenu
+        id="profile-context"
+        className="menu-primary"
+        animation="fade"
+        style={{ width: '9rem' }}
+      >
+        <Item>
+          <Link
+            href={`${process.env.NEXT_PUBLIC_WEBAPP_URL}${
+              user.username || user.id
+            }`}
+            passHref
+            prefetch={false}
+          >
+            <a className="w-full">Profile</a>
+          </Link>
+        </Item>
+        <Item onClick={() => setShowAccountDetails(true)}>Account details</Item>
+        {shouldShowDnD && <Item>Do not disturb</Item>}
+        <Item>
+          <Link
+            href={`${process.env.NEXT_PUBLIC_WEBAPP_URL}devcard`}
+            passHref
+            prefetch={false}
+          >
+            <a className="w-full">Dev card</a>
+          </Link>
+        </Item>
+        <Item>
+          <a
+            href="https://it057218.typeform.com/to/S9p9SVNI"
+            target="_blank"
+            className="w-full"
+          >
+            Feedback
+          </a>
+        </Item>
+        <Item onClick={logout}>Logout</Item>
+      </PortalMenu>
+    </>
   );
 }
