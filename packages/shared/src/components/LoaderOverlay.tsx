95da475d174393f79453e492440bb99af53d90e1
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-10-27 14:25:55 +0800

    refactor: using inset

--- packages/shared/src/components/LoaderOverlay.tsx
+++ packages/shared/src/components/LoaderOverlay.tsx
@@ -3,7 +3,7 @@ import { Loader, LoaderProps } from './Loader';
 
 export function LoaderOverlay({ ...props }: LoaderProps): ReactElement {
   return (
-    <div className="flex absolute top-0 right-0 bottom-0 left-0 bg-overlay-secondary-white">
+    <div className="flex absolute inset-0 bg-overlay-secondary-white">
       <Loader className="m-auto w-6 h-6" {...props} />
     </div>
   );
