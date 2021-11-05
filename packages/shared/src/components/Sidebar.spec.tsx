ca113b6033d394702c70efb38c34ce3ec0357e82
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-11-05 15:41:23 +0800

    test: remove red dot when any feedSettings exists

--- packages/shared/src/components/Sidebar.spec.tsx
+++ packages/shared/src/components/Sidebar.spec.tsx
@@ -1,32 +1,47 @@
 import React from 'react';
-import { render, RenderResult, screen } from '@testing-library/react';
+import nock from 'nock';
+import { render, RenderResult, screen, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import OnboardingContext from '../contexts/OnboardingContext';
 import AuthContext from '../contexts/AuthContext';
 import Sidebar from './Sidebar';
-import AlertContext from '../contexts/AlertContext';
+import defaultUser from '../../__tests__/fixture/loggedUser';
+import AlertContext, { AlertContextData } from '../contexts/AlertContext';
+import {
+  MockedGraphQLResponse,
+  mockGraphQL,
+} from '../../__tests__/helpers/graphql';
+import { FEED_SETTINGS_QUERY } from '../graphql/feedSettings';
+import { getFeedSettingsQueryKey } from '../hooks/useMutateFilters';
+import { LoggedUser } from '../lib/user';
 
+let client: QueryClient;
 const incrementOnboardingStep = jest.fn();
+const disableAlertFilterMock = jest.fn();
 
 beforeEach(() => {
   jest.clearAllMocks();
+  nock.cleanAll();
 });
 
-const renderComponent = (onboardingStep = -1): RenderResult => {
-  const client = new QueryClient();
+const renderComponent = (
+  mocks: MockedGraphQLResponse[] = [],
+  onboardingStep = -1,
+  user: LoggedUser = defaultUser,
+  alertsData: AlertContextData = {
+    alerts: { filter: true },
+    setAlerts: jest.fn(),
+    disableFilterAlert: disableAlertFilterMock,
+  },
+): RenderResult => {
+  client = new QueryClient();
+  mocks.forEach(mockGraphQL);
   return render(
     <QueryClientProvider client={client}>
-      <AlertContext.Provider
-        value={{
-          alerts: {
-            filter: true,
-          },
-          setAlerts: jest.fn(),
-        }}
-      >
+      <AlertContext.Provider value={alertsData}>
         <AuthContext.Provider
           value={{
-            user: null,
+            user,
             shouldShowLogin: false,
             showLogin: jest.fn(),
             logout: jest.fn(),
@@ -54,6 +69,25 @@ const renderComponent = (onboardingStep = -1): RenderResult => {
   );
 };
 
+it('should remove red dot for filter alert when there is a pre-configured feedSettings', async () => {
+  renderComponent([
+    {
+      request: { query: FEED_SETTINGS_QUERY, variables: { loggedIn: true } },
+      result: { data: { feedSettings: { blockedTags: ['javascript'] } } },
+    },
+  ]);
+  await waitFor(async () => {
+    const data = await client.getQueryData(
+      getFeedSettingsQueryKey(defaultUser),
+    );
+    expect(data).toBeTruthy();
+  });
+  const trigger = await screen.findByLabelText('Open sidebar');
+  trigger.click();
+  expect(disableAlertFilterMock).toBeCalled();
+  expect(incrementOnboardingStep).toBeCalledTimes(0);
+});
+
 it('should not increment onboarding step when it is done', async () => {
   renderComponent();
   const trigger = await screen.findByLabelText('Open sidebar');
@@ -62,7 +96,7 @@ it('should not increment onboarding step when it is done', async () => {
 });
 
 it('should increment onboarding step on trigger click', async () => {
-  renderComponent(2);
+  renderComponent([], 2);
   const trigger = await screen.findByLabelText('Open sidebar');
   trigger.click();
   expect(incrementOnboardingStep).toBeCalledTimes(1);
