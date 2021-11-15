db4ccbba54ecbd67f8194c5891eb43b19f940926
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-11-15 18:14:51 +0800

    refactor: next/link to anchor tag

--- packages/shared/src/components/history/ReadingHistoryItem.tsx
+++ packages/shared/src/components/history/ReadingHistoryItem.tsx
@@ -1,6 +1,5 @@
 import React, { MouseEvent, ReactElement } from 'react';
 import classNames from 'classnames';
-import Link from 'next/link';
 import { HideReadHistoryProps, ReadHistory } from '../../graphql/users';
 import XIcon from '../../../icons/x.svg';
 import classed from '../../lib/classed';
@@ -29,37 +28,38 @@ function ReadingHistoryItem({
   };
 
   return (
-    <Link href={post.url}>
-      <section
-        className={classNames(
-          'flex relative flex-row items-center py-3 pr-5 pl-9 hover:bg-theme-hover hover:cursor-pointer',
-          className,
-        )}
-      >
-        <LazyImage
-          imgSrc={post.image}
-          imgAlt={post.title}
-          className="w-16 laptop:w-24 h-16 rounded-16"
+    <a
+      className={classNames(
+        'flex relative flex-row items-center py-3 pr-5 pl-9 hover:bg-theme-hover hover:cursor-pointer',
+        className,
+      )}
+      href={post.url}
+      target="_blank"
+    >
+      <LazyImage
+        imgSrc={post.image}
+        imgAlt={post.title}
+        className="w-16 laptop:w-24 h-16 rounded-16"
+      />
+      <SourceShadow />
+      <LazyImage
+        imgSrc={post.source.image}
+        imgAlt={`source of ${post.title}`}
+        className="left-6 w-6 h-6 rounded-full"
+        absolute
+      />
+      <p className="flex flex-wrap flex-1 mr-6 ml-4 line-clamp-3 typo-callout">
+        {post.title}
+      </p>
+      {onHide && (
+        <Button
+          role="button"
+          className="btn-tertiary"
+          icon={<XIcon />}
+          onClick={onHideClick}
         />
-        <SourceShadow />
-        <LazyImage
-          imgSrc={post.source.image}
-          imgAlt={`source of ${post.title}`}
-          className="left-6 w-6 h-6 rounded-full"
-          absolute
-        />
-        <p className="flex flex-wrap flex-1 mr-6 ml-4 line-clamp-3 typo-callout">
-          {post.title}
-        </p>
-        {onHide && (
-          <Button
-            className="btn-tertiary"
-            icon={<XIcon />}
-            onClick={onHideClick}
-          />
-        )}
-      </section>
-    </Link>
+      )}
+    </a>
   );
 }
 
