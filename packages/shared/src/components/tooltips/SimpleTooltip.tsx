7e9fd1cf744eb91972b6d0b9f9dd22ae05bcead0
Chris Bongers <chrisbongers@gmail.com>
2021-12-01 12:40:36 +0400

    fix: :bug: Fix re focus state for tooltips

--- packages/shared/src/components/tooltips/SimpleTooltip.tsx
+++ packages/shared/src/components/tooltips/SimpleTooltip.tsx
@@ -20,6 +20,7 @@ export function SimpleTooltip({
   content,
   ...props
 }: SimpleTooltipProps): ReactElement {
+  let shouldShow = true;
   const component = useMemo(
     () =>
       React.cloneElement(children, {
@@ -29,11 +30,30 @@ export function SimpleTooltip({
     [children],
   );
 
+  const onTrigger = (_, event) => {
+    if (event.type !== 'focus') {
+      shouldShow = true;
+    }
+  };
+
+  const onUntrigger = (_, event) => {
+    const bodyPath = event.path.filter((path) => document.body === path)[0];
+    if (bodyPath.className === 'ReactModal__Body--open') {
+      shouldShow = false;
+      return;
+    }
+    shouldShow = true;
+  };
+
   return (
     <TippyTooltip
       shouldLoad={getShouldLoadTooltip()}
       {...props}
       content={content}
+      onClick
+      onTrigger={onTrigger}
+      onUntrigger={onUntrigger}
+      onShow={() => shouldShow}
     >
       {component}
     </TippyTooltip>
