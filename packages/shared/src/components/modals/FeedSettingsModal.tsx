3654183aec90c898ac0e28e31db9f5db207fcc6e
Chris Bongers <chrisbongers@gmail.com>
2021-12-01 10:01:06 +0400

    feat: move feed customisation to a modal (#708)
    Rework the feed settings to it's own modal

---
+++ packages/shared/src/components/modals/FeedSettingsModal.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react';
+import { ResponsiveModal } from './ResponsiveModal';
+import { ModalProps } from './StyledModal';
+import { ModalCloseButton } from './ModalCloseButton';
+import Settings from '../Settings';
+
+export function FeedSettingsModal({
+  onRequestClose,
+  ...modalProps
+}: ModalProps): ReactElement {
+  return (
+    <ResponsiveModal {...modalProps} onRequestClose={onRequestClose}>
+      <header className="py-4 px-6 w-full border-b border-theme-divider-tertiary">
+        <h3 className="font-bold typo-title3">Customize</h3>
+        <ModalCloseButton onClick={onRequestClose} />
+      </header>
+      <section className="overflow-auto relative flex-shrink w-full h-full max-h-full">
+        <Settings />
+      </section>
+    </ResponsiveModal>
+  );
+}
+
+export default FeedSettingsModal;
