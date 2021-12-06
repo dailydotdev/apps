4a6b9a9421872e50bccebddeccb70b883fd4d1bf
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-12-06 18:08:24 +0800

    fix: lint issue

--- packages/shared/src/components/tooltips/LinkWithTooltip.tsx
+++ packages/shared/src/components/tooltips/LinkWithTooltip.tsx
@@ -18,7 +18,8 @@ export function LinkWithTooltip({
     () =>
       React.cloneElement(children, {
         ...children.props,
-        'aria-label': typeof tooltip.content === 'string' ? tooltip.content : undefined,
+        'aria-label':
+          typeof tooltip.content === 'string' ? tooltip.content : undefined,
         ref: (el: Element) => setElement(el),
       }),
     [children],
