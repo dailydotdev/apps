8d8e4c98218b80ce52637e00b2bab91eb5fc641d
Chris Bongers <chrisbongers@gmail.com>
2021-12-06 14:04:03 +0400

    fix: :bug: Missing return type

--- packages/extension/src/newtab/dnd.ts
+++ packages/extension/src/newtab/dnd.ts
@@ -20,7 +20,7 @@ const BRAVE_DEFAULT_URL = 'chrome://new-tab-page';
 
 const browserTest = () =>
   process.env.TARGET_BROWSER === 'chrome' ? CHROME_DEFAULT_URL : DEFAULT_URL;
-export const getDefaultLink = () =>
+export const getDefaultLink = (): string =>
   isBrave() ? BRAVE_DEFAULT_URL : browserTest();
 interface DndOption<T extends TimeFormat> {
   value: number;
