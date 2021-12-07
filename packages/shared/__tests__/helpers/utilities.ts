85633900aa4ac00299560be512f87a8af9dccb57
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-12-07 19:52:24 +0800

    refactor: trim wait for rerender

--- packages/shared/__tests__/helpers/utilities.ts
+++ packages/shared/__tests__/helpers/utilities.ts
@@ -6,5 +6,5 @@ export const expectNockDone = (): void => expect(nock.isDone()).toBeTruthy();
 
 export const waitForNock = (): Promise<void> => waitFor(expectNockDone);
 
-export const waitForRerender = (ms = 100): Promise<void> =>
+export const waitForRerender = (ms = 1): Promise<void> =>
   act(() => new Promise((resolve) => setTimeout(resolve, ms)));
