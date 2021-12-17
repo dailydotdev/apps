65434b8a28cd9537302a8f03b23cb885f13df8a3
Chris Bongers <chrisbongers@gmail.com>
2021-12-17 13:53:28 +0400

    fix: :bug: Fix mobile header logo flicker

--- packages/shared/src/components/MobileHeaderRankProgress.tsx
+++ packages/shared/src/components/MobileHeaderRankProgress.tsx
@@ -1,18 +1,33 @@
-import React, { CSSProperties, ReactElement } from 'react';
+import React, {
+  CSSProperties,
+  HTMLAttributes,
+  ReactElement,
+  ReactNode,
+} from 'react';
 import useReadingRank from '../hooks/useReadingRank';
 import RankProgressWrapper from './RankProgressWrapper';
 import { rankToColor } from '../lib/rank';
+
+interface WrapperProps extends HTMLAttributes<HTMLDivElement> {
+  children?: ReactNode;
+}
+const Wrapper = ({ children, ...props }: WrapperProps) => {
+  return (
+    <div className="w-8 h-8" {...props}>
+      {children}
+    </div>
+  );
+};
 
 export default function MobileHeaderRankProgress(): ReactElement {
   const { isLoading, rank, nextRank } = useReadingRank();
 
   if (isLoading) {
-    return <></>;
+    return <Wrapper>&nbsp;</Wrapper>;
   }
 
   return (
-    <div
-      className=""
+    <Wrapper
       style={
         {
           '--rank-color': rankToColor(nextRank || rank),
@@ -20,6 +35,6 @@ export default function MobileHeaderRankProgress(): ReactElement {
       }
     >
       <RankProgressWrapper />
-    </div>
+    </Wrapper>
   );
 }
