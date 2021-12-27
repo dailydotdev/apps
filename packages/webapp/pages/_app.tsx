94dcbe00c619fe57482c59c7eb336891e09a7ed0
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-12-27 23:06:26 +0800

    fix: typo in the comment

--- packages/webapp/pages/_app.tsx
+++ packages/webapp/pages/_app.tsx
@@ -101,7 +101,7 @@ function InternalApp({ Component, pageProps, router }: AppProps): ReactElement {
     ) {
       /*  this will check if the user came from our registration
           if the user has clicked the logout button in the registration - it should go through here because the user will be redirected
-          and since the window.location.replace did not clear all the local state and data like window.location.replace - we will have to force a reload
+          and since the window.location.replace did not clear all the local state and data like window.location.reload - we will have to force a reload
       */
       const referrer = document.referrer && new URL(document.referrer);
       if (referrer.pathname?.indexOf(REGISTRATION_PATH) !== -1) {
