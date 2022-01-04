31a67a6bb5287b2998a2858f41d804d461294adc
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-01-04 14:17:02 +0800

    refactor: unnecessary wait for animation

--- packages/shared/src/components/filters/FeedFilters.tsx
+++ packages/shared/src/components/filters/FeedFilters.tsx
@@ -12,6 +12,7 @@ import XIcon from '../../../icons/x.svg';
 import { menuItemClassNames } from '../multiLevelMenu/MultiLevelMenuMaster';
 import useFeedSettings from '../../hooks/useFeedSettings';
 import AlertContext from '../../contexts/AlertContext';
+import { useDynamicLoadedAnimation } from '../../hooks/useDynamicLoadAnimated';
 
 const asideWidth = sizeN(89);
 
@@ -26,31 +27,13 @@ export default function FeedFilters({
 }: FeedFiltersProps): ReactElement {
   const { alerts, updateAlerts } = useContext(AlertContext);
   const { hasAnyFilter } = useFeedSettings();
-  const [hidden, setHidden] = useState(true);
-  const timeoutRef = useRef<number>();
 
   useEffect(() => {
     if (isOpen) {
       if (alerts?.filter && hasAnyFilter) {
         updateAlerts({ filter: false });
       }
-
-      if (!hidden) {
-        return;
-      }
-
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-      setHidden(false);
-      return;
     }
-
-    if (hidden) {
-      return;
-    }
-
-    timeoutRef.current = window.setTimeout(() => setHidden(true), 300);
-    // eslint-disable-next-line consistent-return
-    return () => timeoutRef.current && clearTimeout(timeoutRef.current);
   }, [isOpen, alerts, hasAnyFilter]);
 
   return (
@@ -59,7 +42,6 @@ export default function FeedFilters({
         'fixed top-14 left-0 z-3 bottom-0 self-stretch bg-theme-bg-primary rounded-r-2xl border-t border-r border-theme-divider-primary overflow-y-auto',
         'transition-transform duration-200 ease-linear delay-100',
         isOpen ? 'translate-x-0' : '-translate-x-96 pointer-events-none',
-        hidden && 'invisible',
       )}
       style={{ width: asideWidth }}
     >
