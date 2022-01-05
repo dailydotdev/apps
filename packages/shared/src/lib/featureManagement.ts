642cabbc4cba08fd444f2cf7adeb393799bfbde6
Chris Bongers <chrisbongers@gmail.com>
2022-01-05 16:54:38 +0200

    feat: :sparkles: Added a features class to maintain default states in one place

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -1,21 +1,58 @@
 import { IFlags } from 'flagsmith';
 
-export enum Features {
-  SignupButtonCopy = 'signup_button_copy',
-  DevcardLimit = 'feat_limit_dev_card',
-  FeedVersion = 'feed_version',
-  HidePublicationDate = 'hide_publication_date',
-  LoginModalButtonCopyPrefix = 'login_modal_button_copy_prefix',
-  LoginModalDescriptionCopy = 'login_modal_description_copy',
+export class Features {
+  static readonly SignupButtonCopy = new Features(
+    'SignupButtonCopy',
+    'signup_button_copy',
+    'Access all features',
+  );
+
+  static readonly DevcardLimit = new Features(
+    'DevcardLimit',
+    'feat_limit_dev_card',
+  );
+
+  static readonly FeedVersion = new Features(
+    'FeedVersion',
+    'feed_version',
+    '1',
+  );
+
+  static readonly HidePublicationDate = new Features(
+    'HidePublicationDate',
+    'hide_publication_date',
+    '0',
+  );
+
+  static readonly LoginModalButtonCopyPrefix = new Features(
+    'LoginModalButtonCopyPrefix',
+    'login_modal_button_copy_prefix',
+    'Sign in with',
+  );
+
+  static readonly LoginModalDescriptionCopy = new Features(
+    'LoginModalDescriptionCopy',
+    'login_modal_description_copy',
+    'Unlock useful features by signing in. A bunch of cool stuff like content filters and bookmarks are waiting just for you.',
+  );
+
+  private constructor(
+    private readonly key: string,
+    public readonly id: string,
+    public readonly defaultValue?: string,
+  ) {}
+
+  toString(): string {
+    return this.key;
+  }
 }
 
 export const getFeatureValue = (
   key: Features,
   flags: IFlags,
-  defaultValue: string = undefined,
 ): string | undefined => {
-  if (flags[key]?.enabled) {
-    return flags[key].value;
+  if (flags[key?.id]?.enabled) {
+    return flags[key?.id].value;
   }
-  return defaultValue;
+  return key?.defaultValue ?? undefined;
 };
