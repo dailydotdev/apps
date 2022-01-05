9c70605dba2333320ab69509114c9fe8fc299e4b
Chris Bongers <chrisbongers@gmail.com>
2022-01-05 18:44:15 +0200

    fix: remove key type

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -2,7 +2,6 @@ import { IFlags } from 'flagsmith';
 
 export class Features {
   static readonly SignupButtonCopy = new Features(
-    'SignupButtonCopy',
     'signup_button_copy',
     'Access all features',
   );
@@ -12,39 +11,27 @@ export class Features {
     'feat_limit_dev_card',
   );
 
-  static readonly FeedVersion = new Features(
-    'FeedVersion',
-    'feed_version',
-    '1',
-  );
+  static readonly FeedVersion = new Features('feed_version', '1');
 
   static readonly HidePublicationDate = new Features(
-    'HidePublicationDate',
     'hide_publication_date',
     '0',
   );
 
   static readonly LoginModalButtonCopyPrefix = new Features(
-    'LoginModalButtonCopyPrefix',
     'login_modal_button_copy_prefix',
     'Sign in with',
   );
 
   static readonly LoginModalDescriptionCopy = new Features(
-    'LoginModalDescriptionCopy',
     'login_modal_description_copy',
     'Unlock useful features by signing in. A bunch of cool stuff like content filters and bookmarks are waiting just for you.',
   );
 
   private constructor(
-    private readonly key: string,
     public readonly id: string,
     public readonly defaultValue?: string,
   ) {}
-
-  toString(): string {
-    return this.key;
-  }
 }
 
 export const getFeatureValue = (
