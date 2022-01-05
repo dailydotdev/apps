21f04cbaf057443dd9146e62f44a6073e4ec4f8c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-01-05 16:16:09 +0800

    fix: report and hide post requires login

--- packages/shared/src/hooks/useReportPost.ts
+++ packages/shared/src/hooks/useReportPost.ts
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { useMutation } from 'react-query';
 import request from 'graphql-request';
 import {
@@ -6,28 +7,58 @@ import {
   ReportReason,
 } from '../graphql/posts';
 import { apiUrl } from '../lib/config';
+import AuthContext from '../contexts/AuthContext';
 
 type UseReportPostRet = {
   reportPost: (variables: {
     id: string;
     reason: ReportReason;
     comment?: string;
-  }) => Promise<void>;
-  hidePost: (id: string) => Promise<void>;
+  }) => Promise<boolean>;
+  hidePost: (id: string) => Promise<boolean>;
 };
 
+interface ReportPostProps {
+  id: string;
+  reason: ReportReason;
+  comment: string;
+}
+
 export default function useReportPost(): UseReportPostRet {
-  const { mutateAsync: reportPost } = useMutation<
+  const { user, showLogin } = useContext(AuthContext);
+  const { mutateAsync: reportPostAsync } = useMutation<
     void,
     unknown,
-    { id: string; reason: ReportReason; comment: string }
+    ReportPostProps
   >((variables) =>
     request(`${apiUrl}/graphql`, REPORT_POST_MUTATION, variables),
   );
 
-  const { mutateAsync: hidePost } = useMutation<void, unknown, string>((id) =>
-    request(`${apiUrl}/graphql`, HIDE_POST_MUTATION, { id }),
+  const { mutateAsync: hidePostAsync } = useMutation<void, unknown, string>(
+    (id) => request(`${apiUrl}/graphql`, HIDE_POST_MUTATION, { id }),
   );
+
+  const reportPost = async (params: ReportPostProps) => {
+    if (!user) {
+      showLogin('report post');
+      return false;
+    }
+
+    await reportPostAsync(params);
+
+    return true;
+  };
+
+  const hidePost = async (id: string) => {
+    if (!user) {
+      showLogin('hide post');
+      return false;
+    }
+
+    await hidePostAsync(id);
+
+    return true;
+  };
 
   return { reportPost, hidePost };
 }
