9843ceafa627537f367cf9c89b4576e43ae9dc58
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-01-06 18:32:26 +0800

    refactor: unnecessary seo on my feed

--- packages/webapp/pages/my-feed.tsx
+++ packages/webapp/pages/my-feed.tsx
@@ -1,26 +1,10 @@
-import React, { ReactElement } from 'react';
-import { NextSeoProps } from 'next-seo/lib/types';
-import { NextSeo } from 'next-seo';
+import React, { Fragment, ReactElement } from 'react';
 import {
   getMainFeedLayout,
   mainFeedLayoutProps,
 } from '../components/layouts/MainFeedPage';
-import { defaultOpenGraph, defaultSeo } from '../next-seo';
 
-const seo: NextSeoProps = {
-  title: 'My Feed posts on daily.dev',
-  titleTemplate: '%s',
-  openGraph: { ...defaultOpenGraph },
-  ...defaultSeo,
-};
-
-const MyFeed = (): ReactElement => {
-  return (
-    <>
-      <NextSeo {...seo} />
-    </>
-  );
-};
+const MyFeed = (): ReactElement => <></>;
 
 MyFeed.getLayout = getMainFeedLayout;
 MyFeed.layoutProps = mainFeedLayoutProps;
