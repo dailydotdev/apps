a90f29c7983a11dfd64dd99b911d9b81c0f50e76
Chris Bongers <chrisbongers@gmail.com>
2022-01-21 14:29:09 +0200

    fix: :bug: Switch the ad case around

--- packages/shared/src/hooks/useFeed.ts
+++ packages/shared/src/hooks/useFeed.ts
@@ -155,13 +155,14 @@ export default function useFeed<T>(
             page: pageIndex,
             index,
           }));
-          posts.splice(adSpot, 0, {
-            type: 'placeholder',
-          });
           if (adsQuery.data?.pages[pageIndex]) {
             posts.splice(adSpot, 0, {
               type: 'ad',
               ad: adsQuery.data?.pages[pageIndex],
+            });
+          } else {
+            posts.splice(adSpot, 0, {
+              type: 'placeholder',
             });
           }
           return posts;
