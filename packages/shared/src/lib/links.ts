5e3a52abaa284507f37b962706b1c22baac730c8
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-01-25 00:09:16 +0800

    refactor: unnecessary regex url validation

--- packages/shared/src/lib/links.ts
+++ packages/shared/src/lib/links.ts
@@ -1,12 +1,3 @@
-const isRegexValidURL = (url: string) => {
-  const res = url.match(
-    // eslint-disable-next-line no-useless-escape
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
-  );
-
-  return res !== null;
-};
-
 export function isValidHttpUrl(link: string): boolean {
   let url: URL;
 
@@ -16,8 +7,5 @@ export function isValidHttpUrl(link: string): boolean {
     return false;
   }
 
-  return (
-    (url.protocol === 'http:' || url.protocol === 'https:') &&
-    isRegexValidURL(url.href)
-  );
+  return url.protocol === 'http:' || url.protocol === 'https:';
 }
