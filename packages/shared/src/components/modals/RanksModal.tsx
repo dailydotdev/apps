4b5118daf78e7478af7849ffabd8ef9c7f7f2637
Chris Bongers <chrisbongers@gmail.com>
2022-01-31 16:08:08 +0200

    feat: :sparkles: First work on the new ranks modal

--- packages/shared/src/components/modals/RanksModal.tsx
+++ packages/shared/src/components/modals/RanksModal.tsx
@@ -19,6 +19,8 @@ import { Button } from '../buttons/Button';
 import { ClickableText } from '../buttons/ClickableText';
 import { SimpleTooltip } from '../tooltips/SimpleTooltip';
 import { useTrackModal } from '../../hooks/useTrackModal';
+import RadialProgress from '../RadialProgress';
+import styles2 from '../RankProgress.module.css';
 
 const RankItem = ({
   rank,
@@ -84,62 +86,29 @@ function DevCardFooter({
   reads,
   devCardLimit,
 }: Pick<RanksModalProps, 'rank' | 'reads' | 'devCardLimit'>): ReactElement {
-  const { user, showLogin } = useContext(AuthContext);
-  const reachedLimit = devCardLimit && reads >= devCardLimit;
-  let devCardText: string;
-  let devCardCTA: ReactNode;
-  if (!user) {
-    if (devCardLimit && !reachedLimit) {
-      devCardText = `To generate a personal dev card you must read at least ${devCardLimit} articles and sign in`;
-    } else {
-      devCardText = 'To generate a personal dev card you must sign in';
-    }
-    devCardCTA = (
-      <Button
-        className="self-start mt-4 btn-primary"
-        onClick={() => showLogin('ranks instructions')}
-      >
-        Sign in
-      </Button>
-    );
-  } else if (devCardLimit && !reachedLimit) {
-    devCardText = `To generate a personal dev card you must read at least ${devCardLimit} articles`;
-    devCardCTA = (
-      <div className="flex items-center mt-2">
-        <div className="font-bold typo-subhead">{`${reads}/${devCardLimit}`}</div>
-        <div className="overflow-hidden flex-1 ml-2 h-1 bg-theme-active rounded-2">
-          <div
-            className="h-full rounded-2 bg-theme-label-primary"
-            style={{ width: `${(reads * 100) / devCardLimit}%` }}
-          />
-        </div>
-      </div>
-    );
-  } else {
-    devCardText =
-      'Generate a personal dev card to show the world the topics you love';
-    devCardCTA = (
-      <GoToDevCardButton
-        origin="ranks instructions"
-        className="self-start mt-4"
-      >
-        Generate
-      </GoToDevCardButton>
-    );
-  }
+  const { user } = useContext(AuthContext);
+
   return (
     <>
-      <div className="-mx-2 mt-8 mb-5 w-full h-px bg-theme-divider-tertiary" />
-      <div className="flex items-start self-stretch px-4 mb-6">
-        <DevCardPlaceholder profileImage={user?.image} width={44} rank={rank} />
-        <div className="flex flex-col flex-1 ml-4">
-          <div className="font-bold typo-callout">Your Dev Card</div>
-          <div className="mt-0.5 text-theme-label-tertiary typo-footnote">
-            {devCardText}
+      <section className="flex flex-col p-6 mb-4">
+        <strong className="typo-headline mb-2">Generate your DevCard</strong>
+        <div className="mt-2 flex">
+          <DevCardPlaceholder
+            profileImage={user?.image}
+            height={80}
+            rank={rank}
+          />
+          <div className="ml-6 flex flex-col flex-1 items-start">
+            <p className="typo-footnote text-theme-label-tertiary">
+              DevCard is your developer ID. It showcases your achievements to
+              the world.
+            </p>
+            <GoToDevCardButton origin="ranks instructions" className="mt-3">
+              Generate
+            </GoToDevCardButton>
           </div>
-          {devCardCTA}
         </div>
-      </div>
+      </section>
     </>
   );
 }
@@ -147,20 +116,12 @@ function DevCardFooter({
 const TimezoneText = ({ onShowAccount }) => {
   const classes =
     'mt-4 mx-10 font-normal text-center  text-theme-label-secondary typo-callout';
-  const { user, showLogin } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const signIn = (
     <p className={classes}>
-      <SimpleTooltip content="Login">
-        <ClickableText
-          tag="a"
-          className="inline-flex text-theme-label-link"
-          onClick={() => showLogin('ranks instructions')}
-        >
-          Sign in
-        </ClickableText>
-      </SimpleTooltip>{' '}
-      to add your weekly goal achievements to your profile.
+      To fit the weekly goal to your time zone, please sign up and add it in the
+      account details.
     </p>
   );
 
@@ -173,10 +134,9 @@ const TimezoneText = ({ onShowAccount }) => {
           className="inline-flex text-theme-label-link"
           onClick={() => onShowAccount()}
         >
-          account details
+          account details.
         </ClickableText>
       </SimpleTooltip>
-      .
     </p>
   );
 
@@ -199,6 +159,179 @@ export default function RanksModal({
   ...props
 }: RanksModalProps): ReactElement {
   useTrackModal({ isOpen: props.isOpen, title: 'ranks modal' });
+
+  // Todo: Refactor these to load from API
+  const tags = [
+    { title: 'general-programming', count: 0 },
+    { title: 'carreer', count: 1 },
+    { title: 'startup', count: 2 },
+    { title: 'productivity', count: 3 },
+    { title: 'webdev', count: 4 },
+    { title: 'javascript', count: 5 },
+    { title: 'css', count: 6 },
+    { title: 'tailwind-css', count: 7 },
+  ].reverse();
+
+  const ranks = [
+    {
+      name: 'Starter',
+      steps: 0,
+      level: 1,
+      color: 'red',
+    },
+    {
+      name: 'Bronze',
+      steps: 0,
+      level: 2,
+      color: 'red',
+    },
+    {
+      name: 'Silver',
+      level: 3,
+      color: 'red',
+    },
+    {
+      name: 'Gold',
+      level: 4,
+      color: 'red',
+    },
+    {
+      name: 'Platinum',
+      level: 5,
+      color: 'red',
+    },
+    {
+      name: 'Diamond',
+      level: 6,
+      color: 'red',
+    },
+    {
+      name: 'Legendary',
+      level: 7,
+      color: 'red',
+    },
+  ];
+
+  const currentRank = 1;
+
+  const getRankOffset = {
+    0: '37.5%',
+    1: '37.5%',
+    2: '12.5%',
+    3: '-12.5%',
+    4: '-37.5%',
+    5: '-62.5%',
+    6: '-87.5%',
+    7: '-112.5%',
+  };
+
+  return (
+    <ResponsiveModal {...props} onRequestClose={onRequestClose} padding={false}>
+      <header className="py-4 px-6 w-full border-b border-theme-divider-tertiary">
+        <h3 className="font-bold typo-title3">Weekly reading goal</h3>
+        <ModalCloseButton onClick={onRequestClose} />
+      </header>
+      <section className="flex flex-col p-6 mb-4">
+        <strong className="typo-headline mb-2">Reading status</strong>
+        <p className="typo-callout text-theme-label-tertiary">
+          Read content you love to stay updated.{' '}
+          <ClickableText
+            tag="a"
+            target="_blank"
+            href="https://docs.daily.dev/docs/your-profile/weekly-goal"
+            rel="noopener"
+            className="inline-flex text-theme-label-link"
+          >
+            Learn more.
+          </ClickableText>
+        </p>
+        <TimezoneText onShowAccount={onShowAccount} />
+      </section>
+      <section className="overflow-hidden">
+        <ul
+          className="flex flex-nowrap"
+          style={{
+            transform: `translate(${getRankOffset[currentRank]})`,
+          }}
+        >
+          {ranks.map((rank, i) => (
+            <li className="flex flex-col items-center justify-center w-1/4 relative">
+              {rank.level !== ranks.length && (
+                <div
+                  className="absolute w-3/4 h-px bg-white"
+                  style={{ top: '35%', left: '60%' }}
+                />
+              )}
+              <div
+                className={classNames(
+                  'flex justify-center items-center relative bg-theme-bg-tertiary',
+                  currentRank === i + 1
+                    ? 'w-16 h-16'
+                    : 'w-10 h-10 border rounded-12',
+                )}
+              >
+                {currentRank === i + 1 && (
+                  <RadialProgress
+                    progress={1}
+                    steps={rank.level}
+                    className={classNames(
+                      styles2.radialProgress,
+                      'w-full h-full absolute inset-0',
+                    )}
+                  />
+                )}
+                <Rank
+                  rank={i + 1}
+                  className={classNames(
+                    currentRank === i + 1 ? 'w-10 h-10' : 'w-8 h-8',
+                  )}
+                  style={
+                    {
+                      '--stop-color1': 'var(--theme-label-disabled)',
+                      '--stop-color2': 'var(--theme-label-disabled)',
+                    } as CSSProperties
+                  }
+                />
+              </div>
+              <strong
+                className={classNames(
+                  'mt-2',
+                  currentRank === i + 1 ? 'typo-callout' : 'typo-footnote',
+                )}
+              >
+                {rank.name}
+              </strong>
+            </li>
+          ))}
+        </ul>
+        <p className="text-center text-theme-label-tertiary typo-footnote mt-1 mb-3">
+          Earn: 4/5 reading days
+        </p>
+      </section>
+      <section className="m-4 p-4 mb-0 rounded-16 border bg-theme-secondary border-theme-divider-tertiary">
+        <strong className="typo-headline block mb-6">
+          Reading status per tag
+        </strong>
+        <ul className="grid grid-cols-2 grid-rows-4 grid-flow-col gap-4">
+          {tags.map((tag) => (
+            <li className="flex flex-row items-center">
+              <RadialProgress
+                progress={tag.count}
+                steps={7}
+                className={classNames('w-5 h-5', styles2.radialProgress)}
+              />
+              <strong className="flex flex-shrink h-6 items-center ml-2 w-auto bg-theme-float rounded-8 px-3 typo-callout text-theme-label-tertiary truncate">
+                #{tag.title}
+              </strong>
+            </li>
+          ))}
+        </ul>
+      </section>
+      {!hideProgress && (
+        <DevCardFooter rank={rank} reads={reads} devCardLimit={devCardLimit} />
+      )}
+    </ResponsiveModal>
+  );
 
   return (
     <ResponsiveModal
