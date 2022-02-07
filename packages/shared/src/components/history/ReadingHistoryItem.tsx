621dcb0432da64766be4f8c65726cf05dfefbb50
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-02-07 23:52:58 +0800

    fix: fallback image for reading history item

--- packages/shared/src/components/history/ReadingHistoryItem.tsx
+++ packages/shared/src/components/history/ReadingHistoryItem.tsx
@@ -41,6 +41,7 @@ function ReadingHistoryItem({
           imgSrc={post.image}
           imgAlt={post.title}
           className="w-16 laptop:w-24 h-16 rounded-16"
+          fallbackSrc="https://res.cloudinary.com/daily-now/image/upload/f_auto/v1/placeholders/1"
         />
         <SourceShadow />
         <LazyImage
