3a6a9d21a127c29d76a2a9f9c1c4ea8e8d678c5c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-02-14 17:31:19 +0800

    refactor: reusable component

---
+++ packages/shared/src/components/profile/UserShortInfo.tsx
@@ -0,0 +1,31 @@
+import React, { HTMLAttributes, ReactElement } from 'react';
+import { ProfileImageSize, ProfilePicture } from '../ProfilePicture';
+
+interface UserShortInfoProps extends HTMLAttributes<React.ElementType> {
+  image: string;
+  name: string;
+  username: string;
+  bio?: string;
+  imageSize?: ProfileImageSize;
+  Tag?: React.ElementType;
+}
+
+export function UserShortInfo({
+  name,
+  username,
+  image,
+  bio,
+  Tag = 'a',
+  imageSize = 'xlarge',
+}: UserShortInfoProps): ReactElement {
+  return (
+    <Tag className="flex flex-row py-3 px-6 hover:bg-theme-hover">
+      <ProfilePicture user={{ username, image }} size={imageSize} />
+      <div className="flex flex-col flex-1 ml-4 typo-callout">
+        <span className="font-bold">{name}</span>
+        <span className="text-theme-label-secondary">@{username}</span>
+        {bio && <span className="mt-1 text-theme-label-tertiary">{bio}</span>}
+      </div>
+    </Tag>
+  );
+}
