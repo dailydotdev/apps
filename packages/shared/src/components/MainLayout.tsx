00de34e8b4ec8e3d6fdea98654d2b4e40e72f077
Chris Bongers <chrisbongers@gmail.com>
2022-02-14 16:18:24 +0200

    fix: :bug: Making sure the mobile rank only shows on mobile (#898)
     Making sure the mobile rank only shows on mobile

--- packages/shared/src/components/MainLayout.tsx
+++ packages/shared/src/components/MainLayout.tsx
@@ -156,7 +156,7 @@ export default function MainLayout({
                 )}
               </>
             )}
-            <MobileHeaderRankProgress />
+            <MobileHeaderRankProgress sidebarRendered={sidebarRendered} />
           </>
         )}
       </header>
