f1304990595b015afb2c87826c56797c5db8fa1f
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-03-18 23:03:43 +0800

    fix: widget container roundness

--- packages/shared/src/components/widgets/common.ts
+++ packages/shared/src/components/widgets/common.ts
@@ -2,5 +2,5 @@ import classed from '../../lib/classed';
 
 export const WidgetContainer = classed(
   'div',
-  'border border-theme-divider-quaternary',
+  'border border-theme-divider-quaternary rounded-16',
 );
