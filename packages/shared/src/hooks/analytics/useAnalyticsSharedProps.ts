4c328d7d6c3dd5951135d46b93de563684420bcc
Ido Shamun <idoesh1@gmail.com>
2022-03-21 14:26:48 +0200

    feat: add device id to analytics events
    Device ID is used to identify the specific device that is sending the events to our server.
    It is used to filter out bots, cookie blockers, and other users who might generate noise in our tracking system.
    It is generated by the client and saved locally in WebSQL so it doesn't rely on cookies.

--- packages/shared/src/hooks/analytics/useAnalyticsSharedProps.ts
+++ packages/shared/src/hooks/analytics/useAnalyticsSharedProps.ts
@@ -6,10 +6,24 @@ import {
   useState,
 } from 'react';
 import { useRouter } from 'next/router';
+import { get as getCache, set as setCache } from 'idb-keyval';
+import { v4 as uuidv4 } from 'uuid/dist/esm-browser/v4';
 import { AnalyticsEvent } from './useAnalyticsQueue';
 import FeaturesContext from '../../contexts/FeaturesContext';
 import SettingsContext from '../../contexts/SettingsContext';
 import AuthContext from '../../contexts/AuthContext';
+
+const DEVICE_ID_KEY = 'device_id';
+
+const getOrGenerateDeviceId = async (): Promise<string> => {
+  const deviceId = await getCache<string | undefined>(DEVICE_ID_KEY);
+  if (deviceId) {
+    return deviceId;
+  }
+  const newDeviceId = uuidv4();
+  await setCache(DEVICE_ID_KEY, newDeviceId);
+  return newDeviceId;
+};
 
 export default function useAnalyticsSharedProps(
   app: string,
@@ -35,29 +49,35 @@ export default function useAnalyticsSharedProps(
     if (!visitId) {
       return;
     }
-    setSharedPropsSet(true);
     const queryStr = JSON.stringify(query);
-    sharedPropsRef.current = {
-      app_platform: app,
-      app_theme: themeMode,
-      app_version: version,
-      feed_density: spaciness,
-      feed_layout: insaneMode ? 'list' : 'cards',
-      // By default query equals '{}'
-      query_params: queryStr.length > 2 ? queryStr : undefined,
-      session_id: visit?.sessionId,
-      user_first_visit: anonymous?.firstVisit,
-      user_id: anonymous?.id,
-      user_referrer: anonymous?.referrer,
-      user_registration_date: user?.createdAt,
-      utm_campaign: query?.utm_campaign,
-      utm_content: query?.utm_content,
-      utm_medium: query?.utm_medium,
-      utm_source: query?.utm_source,
-      utm_term: query?.utm_term,
-      visit_id: visitId,
-      feature_flags: flags ? JSON.stringify(flags) : null,
-    };
+    (sharedPropsRef.current?.device_id
+      ? Promise.resolve(sharedPropsRef.current.device_id)
+      : getOrGenerateDeviceId()
+    ).then((deviceId) => {
+      sharedPropsRef.current = {
+        app_platform: app,
+        app_theme: themeMode,
+        app_version: version,
+        feed_density: spaciness,
+        feed_layout: insaneMode ? 'list' : 'cards',
+        // By default query equals '{}'
+        query_params: queryStr.length > 2 ? queryStr : undefined,
+        session_id: visit?.sessionId,
+        user_first_visit: anonymous?.firstVisit,
+        user_id: anonymous?.id,
+        user_referrer: anonymous?.referrer,
+        user_registration_date: user?.createdAt,
+        utm_campaign: query?.utm_campaign,
+        utm_content: query?.utm_content,
+        utm_medium: query?.utm_medium,
+        utm_source: query?.utm_source,
+        utm_term: query?.utm_term,
+        visit_id: visitId,
+        feature_flags: flags ? JSON.stringify(flags) : null,
+        device_id: deviceId,
+      };
+      setSharedPropsSet(true);
+    });
   }, [
     sharedPropsRef,
     tokenRefreshed,
