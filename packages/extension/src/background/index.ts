6cc095fef2d7addbd3adaad630a5c34a4799f454
Chris Bongers <chrisbongers@gmail.com>
2022-03-24 15:44:52 +0100

    fix: remove amplitude cache key

--- packages/extension/src/background/index.ts
+++ packages/extension/src/background/index.ts
@@ -1,19 +1,4 @@
-import { browser, Runtime } from 'webextension-polyfill-ts';
-import { getBootData } from '@dailydotdev/shared/src/lib/boot';
-
-const cacheAmplitudeDeviceId = async ({
-  reason,
-}: Runtime.OnInstalledDetailsType): Promise<void> => {
-  if (reason === 'install') {
-    const boot = await getBootData('extension');
-    if (boot.visit.ampStorage) {
-      localStorage.setItem(
-        `amp_${process.env.NEXT_PUBLIC_AMPLITUDE.slice(0, 6)}`,
-        boot.visit.ampStorage,
-      );
-    }
-  }
-};
+import { browser } from 'webextension-polyfill-ts';
 
 browser.browserAction.onClicked.addListener(() => {
   const url = browser.extension.getURL('index.html?source=button');
@@ -22,7 +7,6 @@ browser.browserAction.onClicked.addListener(() => {
 
 browser.runtime.onInstalled.addListener(async (details) => {
   await Promise.all([
-    cacheAmplitudeDeviceId(details),
     browser.runtime.setUninstallURL('https://daily.dev/uninstall'),
   ]);
 });
