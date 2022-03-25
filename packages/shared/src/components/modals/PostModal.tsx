4300284ec539907f258a3ca3bc7247b8570135e9
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-03-25 20:44:20 +0800

    feat: post modal

---
+++ packages/shared/src/components/modals/PostModal.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement } from 'react';
+import classNames from 'classnames';
+import { StyledModal, ModalProps } from './StyledModal';
+import { useHideOnModal } from '../../hooks/useHideOnModal';
+import { useResetScrollForResponsiveModal } from '../../hooks/useResetScrollForResponsiveModal';
+import { PostContent, PostContentProps } from '../post/PostContent';
+import styles from './PostModal.module.css';
+
+interface PostModalProps
+  extends ModalProps,
+    Pick<PostContentProps, 'navigation'> {
+  id: string;
+}
+
+export function PostModal({
+  className,
+  children,
+  onRequestClose,
+  id,
+  navigation,
+  ...props
+}: PostModalProps): ReactElement {
+  useResetScrollForResponsiveModal();
+  useHideOnModal(props.isOpen);
+
+  return (
+    <StyledModal
+      {...props}
+      className={classNames(className, styles.postModal)}
+      onRequestClose={onRequestClose}
+    >
+      <PostContent
+        id={id}
+        navigation={navigation}
+        className="modal-post"
+        onClose={onRequestClose}
+      />
+    </StyledModal>
+  );
+}
