8e7fd71f3f6fd1a470bfecb1f158d6f2dcdf5b28
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-03-31 06:36:53 -0300

    fix: explicitly defining the start date of heatmap (#943)
    DD-573 #done
    
    - The core issue was when generating the heatmap - we have a function that creates an array, based on the week columns we will have to create.
    - The concern came from the way we generate the start date - we utilize the function new Date(0) and set its year. Unfortunately, it looks like from UTC-1 and lower, the value it returns is 31-12-1969 which is supposed to be 01-01-1970.
    - To avoid the incorrect start date, we now explicitly defined that the calculation should start at 01-01-YYYY based on the selected year.

--- packages/shared/src/components/history/ReadingHistory.spec.tsx
+++ packages/shared/src/components/history/ReadingHistory.spec.tsx
@@ -13,6 +13,7 @@ import ReadHistoryList, { ReadHistoryListProps } from './ReadingHistoryList';
 import { ReadHistoryInfiniteData } from '../../hooks/useInfiniteReadingHistory';
 import AuthContext from '../../contexts/AuthContext';
 import user from '../../../__tests__/fixture/loggedUser';
+import { getLabel } from '../../lib/dateFormat.spec';
 
 beforeEach(() => {
   nock.cleanAll();
@@ -43,6 +44,7 @@ describe('ReadingHistoryList component', () => {
     createdThisYear,
     createdAtDifferentYear,
   ];
+  const sorted = timestamps.sort((a, b) => a.getTime() - b.getTime());
   const post = {
     id: 'p1',
     title: 'Most Recent Post',
@@ -57,7 +59,7 @@ describe('ReadingHistoryList component', () => {
       {
         readHistory: {
           pageInfo: { hasNextPage: true, endCursor: '' },
-          edges: timestamps.map((timestamp, index) => ({
+          edges: sorted.map((timestamp, index) => ({
             node: { timestamp, post: { ...post, id: `p${index}` } },
           })),
         },
@@ -104,13 +106,13 @@ describe('ReadingHistoryList component', () => {
   });
 
   it('should show date section for reads of the same year', async () => {
-    const year = new Date().getFullYear();
-    const date = new Date(`${year}-03-31 07:15:51.247`);
     const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][
-      date.getDay()
+      createdThisYear.getDay()
     ];
     renderComponent();
-    await screen.findByText(`${weekday}, 31 Mar`);
+    const label = getLabel(createdThisYear) || `${weekday}, 31 Mar`;
+    expect(label).toEqual('Today');
+    await screen.findByText(label);
   });
 
   it('should show date section for reads of the different year', async () => {
