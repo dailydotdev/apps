13c13c7cd429a0e98f38f724151290412f397725
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-04-27 14:20:37 +0800

    fix: jumpy ui on modal close for mobile devices. (#954)
    On mobile devices, its scrollbars don't occupy space, so it is not necessary to apply some padding to shift the content. Also, on tablets, there's an unnecessary top margin from the overlay that should be removed.

--- packages/shared/src/components/Feed.tsx
+++ packages/shared/src/components/Feed.tsx
@@ -221,8 +221,7 @@ export default function Feed<T>({
     row: number,
     column: number,
   ): void => {
-    document.body.classList.add('overflow-hidden');
-    document.body.classList.add('pr-2');
+    document.body.classList.add('overflow-hidden', 'pr-0', 'laptop:pr-2');
     trackEvent(
       postAnalyticsEvent('comments click', post, {
         columns: virtualizedNumCards,
@@ -257,8 +256,7 @@ export default function Feed<T>({
 
   useEffect(() => {
     if (!selectedPost) {
-      document.body.classList.remove('overflow-hidden');
-      document.body.classList.remove('pr-2');
+      document.body.classList.remove('overflow-hidden', 'pr-0', 'laptop:pr-2');
     }
   }, [selectedPost]);
 
