74401b221f1209b9e4a64d330fe982fdc24ce6be
Chris Bongers <chrisbongers@gmail.com>
2022-04-29 16:53:27 +0200

    fix: linting issue

--- packages/shared/src/components/SidebarRankProgress.spec.tsx
+++ packages/shared/src/components/SidebarRankProgress.spec.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import {
-  act,
-  render,
-  RenderResult,
-  screen,
-  waitFor,
-} from '@testing-library/react';
+import { render, RenderResult, screen, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import nock from 'nock';
-import { set as setCache, clear } from 'idb-keyval';
+import { set as setCache } from 'idb-keyval';
 import {
   MockedGraphQLResponse,
   mockGraphQL,
