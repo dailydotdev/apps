c2fea08bb8efaad1786f171243550b9319265a3f
Aya <aya.berdyeva@gmail.com>
2022-05-16 16:42:17 +0200

    refactor: icon size

--- packages/shared/src/components/comments/ScoutBadge.tsx
+++ packages/shared/src/components/comments/ScoutBadge.tsx
@@ -4,7 +4,7 @@ import ScoutIcon from '../../../icons/filled/scout.svg';
 export default function ScoutBadge(): ReactElement {
   return (
     <span className="flex items-center ml-2 text-theme-color-bun typo-footnote">
-      <ScoutIcon className="mr-0.5 text-base icon" />
+      <ScoutIcon className="mr-1 w-3 h-3" />
       Scout
     </span>
   );
