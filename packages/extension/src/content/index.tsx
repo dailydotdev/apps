835a1aa936f8b68eb621736cfe68553c8465b3fb
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-06-07 21:59:19 +0800

    fix: companion state on spa site navigation (#1001)
    * refactor: applying of theme in companion
    
    * feat: mutation observer for checking url
    
    * refactor: unmounting companion

--- packages/extension/src/content/index.tsx
+++ packages/extension/src/content/index.tsx
@@ -14,3 +14,12 @@ wrapper.id = 'daily-companion-wrapper';
 shadow.appendChild(wrapper);
 
 browser.runtime.sendMessage({ type: 'CONTENT_LOADED' });
+
+let lastUrl = window.location.href;
+new MutationObserver(() => {
+  const current = window.location.href;
+  if (current !== lastUrl) {
+    lastUrl = current;
+    browser.runtime.sendMessage({ type: 'CONTENT_LOADED' });
+  }
+}).observe(document, { subtree: true, childList: true });
