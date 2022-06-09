eba6074d597e9b1f8852f5d55303c8a892600104
Chris Bongers <chrisbongers@gmail.com>
2022-06-09 12:00:54 +0200

    feat: fallback for profile images not loading

--- packages/shared/src/components/ProfilePicture.tsx
+++ packages/shared/src/components/ProfilePicture.tsx
@@ -13,28 +13,42 @@ export type ProfileImageSize =
   | 'xxlarge'
   | 'xxxlarge';
 
+type ProfileImageRoundSize = ProfileImageSize | 'full';
+
 export interface ProfilePictureProps
   extends Omit<LazyImageProps, 'imgSrc' | 'imgAlt'> {
   user: Pick<PublicProfile, 'image' | 'username'>;
   size?: ProfileImageSize;
+  rounded?: ProfileImageRoundSize;
   className?: string;
   nativeLazyLoading?: boolean;
 }
 
 const sizeClasses = {
-  xsmall: 'w-5 h-5 rounded-6',
-  small: 'w-6 h-6 rounded-8',
-  medium: 'w-8 h-8 rounded-10',
-  large: 'w-10 h-10 rounded-12',
-  xlarge: 'w-12 h-12 rounded-14',
-  xxlarge: 'w-14 h-14 rounded-16',
-  xxxlarge: 'w-24 h-24 rounded-26',
+  xsmall: 'w-5 h-5',
+  small: 'w-6 h-6',
+  medium: 'w-8 h-8',
+  large: 'w-10 h-10',
+  xlarge: 'w-12 h-12',
+  xxlarge: 'w-14 h-14',
+  xxxlarge: 'w-24 h-24',
+};
+const roundClasses = {
+  xsmall: 'rounded-6',
+  small: 'rounded-8',
+  medium: 'rounded-10',
+  large: 'rounded-12',
+  xlarge: 'rounded-14',
+  xxlarge: 'rounded-16',
+  xxxlarge: 'rounded-26',
+  full: 'rounded-full',
 };
 
 function ProfilePictureComponent(
   {
     user,
     size = 'xlarge',
+    rounded,
     className,
     nativeLazyLoading,
     ...props
@@ -48,7 +62,16 @@ function ProfilePictureComponent(
         ref={ref}
         src={user.image}
         alt={`${user.username}'s profile`}
-        className={classNames(sizeClasses[size], className)}
+        onError={(e) => {
+          const target = e.target as HTMLImageElement;
+          target.onerror = null;
+          target.src = fallbackImages.avatar_base;
+        }}
+        className={classNames(
+          sizeClasses[size],
+          roundClasses[rounded ?? size],
+          className,
+        )}
         loading="lazy"
       />
     );
