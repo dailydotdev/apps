0b7d732eb819f72a71a48c11f5d057126ccd92fa
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-06-11 18:34:16 +0800

    refactor: options button

--- packages/shared/src/components/buttons/OptionsButton.tsx
+++ packages/shared/src/components/buttons/OptionsButton.tsx
@@ -1,25 +1,17 @@
 import React, { ReactElement } from 'react';
-import { Button } from './Button';
+import { AllowedTags, Button, ButtonProps } from './Button';
 import MenuIcon from '../icons/Menu';
-import { Post } from '../../graphql/posts';
 import { SimpleTooltip } from '../tooltips/SimpleTooltip';
 
-export default function OptionsButton({
-  post,
-  onClick,
-}: {
-  post: Post;
-  onClick?: (event: React.MouseEvent, post: Post) => unknown;
-}): ReactElement {
-  return (
-    <SimpleTooltip placement="left" content="Options">
-      <Button
-        className="mouse:invisible mouse:group-hover:visible my-auto btn-tertiary"
-        style={{ marginLeft: 'auto', marginRight: '-0.125rem' }}
-        icon={<MenuIcon size="medium" />}
-        onClick={(event) => onClick?.(event, post)}
-        buttonSize="small"
-      />
-    </SimpleTooltip>
-  );
-}
+const OptionsButton = (props: ButtonProps<AllowedTags>): ReactElement => (
+  <SimpleTooltip placement="left" content="Options">
+    <Button
+      {...props}
+      className="my-auto btn-tertiary"
+      icon={<MenuIcon size="medium" />}
+      buttonSize="small"
+    />
+  </SimpleTooltip>
+);
+
+export default OptionsButton;
