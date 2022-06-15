3494097f2c0956dff52f884778d725f5516f42d9
Chris Bongers <chrisbongers@gmail.com>
2022-06-15 16:28:15 +0200

    feat: add scout to post users highlight (#1025)
    Add scout to post when available

--- packages/shared/src/graphql/posts.ts
+++ packages/shared/src/graphql/posts.ts
@@ -1,5 +1,5 @@
 import { gql } from 'graphql-request';
-import { Author, Comment } from './comments';
+import { Author, Comment, Scout } from './comments';
 import { Connection, Upvote } from './common';
 import { UPVOTER_FRAGMENT } from './users';
 import { Source } from './sources';
@@ -26,6 +26,7 @@ export interface Post {
   numUpvotes?: number;
   numComments?: number;
   author?: Author;
+  scout?: Scout;
   views?: number;
   placeholder?: string;
   read?: boolean;
@@ -124,6 +125,13 @@ export const POST_BY_ID_QUERY = gql`
         id
         name
         image
+      }
+      scout {
+        id
+        image
+        name
+        permalink
+        username
       }
       author {
         id
