3826ef9b1588d459837b23fec05eaef7449bcb56
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-06-16 14:18:30 +0800

    fix: preview tooltip getting cut-off (#1026)

--- packages/shared/src/components/Markdown.tsx
+++ packages/shared/src/components/Markdown.tsx
@@ -90,7 +90,7 @@ export default function Markdown({
         offset,
         visible: !!userId,
         onShow: cancelUserClearing,
-        appendTo: appendTooltipTo,
+        appendTo: appendTooltipTo || document?.body || 'parent',
       }}
       onMouseEnter={cancelUserClearing}
       onMouseLeave={clearUser}
