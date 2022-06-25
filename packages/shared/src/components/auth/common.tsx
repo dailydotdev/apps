59a11f174f1515f4521454d5e8824fd9f1a8d86b
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-06-25 20:05:54 +0800

    refactor: providers map

--- packages/shared/src/components/auth/common.tsx
+++ packages/shared/src/components/auth/common.tsx
@@ -15,35 +15,39 @@ export interface Provider {
   style?: CSSProperties;
 }
 
-export const providers: Provider[] = [
-  {
+export const providerMap: Record<string, Provider> = {
+  twitter: {
     icon: <TwitterIcon />,
     provider: 'Twitter',
     style: { backgroundColor: '#1D9BF0' },
   },
-  {
+  facebook: {
     icon: <FacebookIcon />,
     provider: 'Facebook',
     style: { backgroundColor: '#4363B6' },
   },
-  {
+  google: {
     icon: <GoogleIcon />,
     provider: 'Google',
     style: { backgroundColor: '#FFFFFF', color: '#0E1217' },
   },
-  {
+  gitHub: {
     icon: <GitHubIcon />,
     provider: 'GitHub',
     style: { backgroundColor: '#2D313A' },
   },
-  {
+  apple: {
     icon: <AppleIcon />,
     provider: 'Apple',
     style: { backgroundColor: '#0E1217' },
   },
-];
+};
+
+export const providers: Provider[] = Object.values(providerMap);
 
 export const ColumnContainer = classed(
   'div',
   'grid grid-cols-1 gap-4 self-center px-[3.75rem] mt-6 w-full',
 );
+
+export const AuthForm = classed('form', 'grid grid-cols-1 gap-2');
