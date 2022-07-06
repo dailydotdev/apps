44ea115a048ed9fb7b35cc44aca64fd9e56e55be
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-07-06 16:00:06 +0800

    fix: post parent comments limit from 100 to 500 (#1071)

--- packages/shared/src/components/post/PostComments.tsx
+++ packages/shared/src/components/post/PostComments.tsx
@@ -93,7 +93,7 @@ export function PostComments({
         requestMethod(
           `${apiUrl}/graphql`,
           POST_COMMENTS_QUERY,
-          { postId: id, [initialDataKey]: comments },
+          { postId: id, [initialDataKey]: comments, first: 500 },
           { requestKey: JSON.stringify(queryKey) },
         ),
       {
