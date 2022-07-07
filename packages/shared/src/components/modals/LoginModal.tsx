d157f44f22044deb0d7ff31c6125f6f84c1bcfb9
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-07-08 03:07:39 +0800

    tmp: login modal to switch new auth ui

--- packages/shared/src/components/modals/LoginModal.tsx
+++ packages/shared/src/components/modals/LoginModal.tsx
@@ -1,45 +1,11 @@
-import React, { ReactElement, useContext } from 'react';
-import classNames from 'classnames';
-import DailyDevLogo from '../../svg/DailyDevLogo';
-import { StyledModal, ModalProps } from './StyledModal';
-import { ModalCloseButton } from './ModalCloseButton';
-import LoginButtons from '../LoginButtons';
-import styles from './LoginModal.module.css';
-import { useTrackModal } from '../../hooks/useTrackModal';
-import FeaturesContext from '../../contexts/FeaturesContext';
-import { Features, getFeatureValue } from '../../lib/featureManagement';
+import React, { ReactElement } from 'react';
+import AuthModal from '../auth/AuthModal';
+import { ModalProps } from './StyledModal';
 
 export type LoginModalProps = {
   trigger: string;
 } & ModalProps;
 
-export default function LoginModal({
-  trigger,
-  className,
-  onRequestClose,
-  children,
-  ...props
-}: LoginModalProps): ReactElement {
-  const { flags } = useContext(FeaturesContext);
-  const loginModalDescriptionCopy = getFeatureValue(
-    Features.LoginModalDescriptionCopy,
-    flags,
-  );
-  useTrackModal({ isOpen: props.isOpen, title: 'signup', trigger });
-
-  return (
-    <StyledModal
-      {...props}
-      onRequestClose={onRequestClose}
-      className={classNames(styles.loginModal, className)}
-    >
-      <ModalCloseButton onClick={onRequestClose} />
-      <DailyDevLogo />
-      <div className="mt-6 mb-8 text-center text-theme-label-secondary typo-callout">
-        {loginModalDescriptionCopy}
-      </div>
-      <LoginButtons />
-      {children}
-    </StyledModal>
-  );
+export default function LoginModal(props: LoginModalProps): ReactElement {
+  return <AuthModal {...props} />;
 }
