d1dd05cfd8c7500f42b333270ad87fe1b827ec30
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-07-12 16:55:16 +0800

    feat: connect with email

--- packages/webapp/pages/[userId]/security.tsx
+++ packages/webapp/pages/[userId]/security.tsx
@@ -7,6 +7,7 @@ import { Button } from '@dailydotdev/shared/src/components/buttons/Button';
 import AccountDangerZone from '@dailydotdev/shared/src/components/profile/AccountDangerZone';
 import MailIcon from '@dailydotdev/shared/src/components/icons/Mail';
 import AuthContext from '@dailydotdev/shared/src/contexts/AuthContext';
+import LockIcon from '@dailydotdev/shared/src/components/icons/Lock';
 import React, { ReactElement, useContext, useState } from 'react';
 import {
   getStaticPaths as getProfileStaticPaths,
@@ -17,10 +18,10 @@ import {
   AccountContentHeading,
   AccountPageContainer,
   AccountTextField,
-  CommonTextField,
   ContentHeading,
   ContentText,
 } from '../../components/layouts/ProfileLayout/common';
+import EmailForm from '../../components/layouts/ProfileLayout/EmailForm';
 
 export const getStaticProps = getProfileStaticProps;
 export const getStaticPaths = getProfileStaticPaths;
@@ -33,12 +34,13 @@ const providers = Object.values(socialProvider);
 enum Display {
   Default = 'default',
   ChangeEmail = 'change_email',
+  ConnectEmail = 'connect_email',
 }
 
 const AccountSecurityPage = (): ReactElement => {
   const { user } = useContext(AuthContext);
   const [email, setEmail] = useState<string>();
-  const [resetEmailSent, setResetEmailSent] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
   const [activeDisplay, setActiveDisplay] = useState(Display.Default);
 
   if (!user) {
@@ -46,9 +48,45 @@ const AccountSecurityPage = (): ReactElement => {
   }
 
   const onDelete = () => {};
-  const onSaveEmail = () => {
-    setResetEmailSent(true);
+  const onChangeEmail = () => {
+    setEmailSent(true);
     setActiveDisplay(Display.Default);
+  };
+
+  const onConnectEmail = () => {
+    setEmailSent(true);
+    setActiveDisplay(Display.Default);
+  };
+
+  const renderEmailAction = () => {
+    if (emailSent) {
+      return (
+        <div className="relative p-4 mt-6 max-w-md rounded-8 border border-theme-status-warning">
+          <div className="absolute inset-0 -z-1 w-full h-full opacity-24 bg-overlay-quaternary-bun" />
+          <p className="typo-callout">
+            We sent an email to verify your account. Please check your spam
+            folder if you {`don't`} see the email.
+          </p>
+          <span className="flex flex-row gap-4 mt-4">
+            <Button buttonSize="xsmall" className="w-fit btn-primary">
+              Resend
+            </Button>
+            <Button buttonSize="xsmall" className="w-fit btn-secondary">
+              Cancel Request
+            </Button>
+          </span>
+        </div>
+      );
+    }
+
+    return user.password ? (
+      <Button
+        className="mt-6 w-fit btn-secondary"
+        onClick={() => setActiveDisplay(Display.ChangeEmail)}
+      >
+        Change email
+      </Button>
+    ) : null;
   };
 
   return (
@@ -66,45 +104,38 @@ const AccountSecurityPage = (): ReactElement => {
             label="Email"
             inputId="email"
             leftIcon={<MailIcon />}
+            rightIcon={<LockIcon className="text-theme-label-secondary" />}
+            readOnly
           />
-          {resetEmailSent ? (
-            <div className="relative p-4 mt-6 max-w-md rounded-8 border border-theme-status-warning">
-              <div className="absolute inset-0 -z-1 w-full h-full opacity-24 bg-overlay-quaternary-bun" />
-              <p className="typo-callout">
-                We sent an email to verify your account. Please check your spam
-                folder if you {`don't`} see the email.
-              </p>
-              <span className="flex flex-row gap-4 mt-4">
-                <Button buttonSize="xsmall" className="w-fit btn-primary">
-                  Resend
-                </Button>
-                <Button buttonSize="xsmall" className="w-fit btn-secondary">
-                  Cancel Request
-                </Button>
-              </span>
-            </div>
-          ) : (
-            <Button
-              className="mt-6 w-fit btn-secondary"
-              onClick={() => setActiveDisplay(Display.ChangeEmail)}
-            >
-              Change email
-            </Button>
-          )}
+          {renderEmailAction()}
           <AccountContentHeading>Add login account</AccountContentHeading>
           <ContentText>
             Add more accounts to ensure you never lose access to your daily.dev
             profile and to make login quick and easy cross device
           </ContentText>
           <div className="grid grid-cols-1 gap-4 mt-6 w-64">
-            {providers.map(({ provider, ...rest }) => (
+            {providers
+              .filter(
+                ({ provider }) =>
+                  !user.providers.includes(provider.toLowerCase()),
+              )
+              .map(({ provider, ...rest }) => (
+                <ProviderButton
+                  key={provider}
+                  label="Connect with"
+                  provider={provider}
+                  {...rest}
+                />
+              ))}
+            {!user.password && (
               <ProviderButton
-                key={provider}
                 label="Connect with"
-                provider={provider}
-                {...rest}
+                provider="email"
+                icon={<MailIcon secondary />}
+                onClick={() => setActiveDisplay(Display.ConnectEmail)}
+                style={socialProvider.gitHub.style}
               />
-            ))}
+            )}
           </div>
           <AccountContentHeading>Account Password</AccountContentHeading>
           <ContentText>Change your account password</ContentText>
@@ -124,18 +155,16 @@ const AccountSecurityPage = (): ReactElement => {
           onBack={() => setActiveDisplay(Display.Default)}
           className={{ section: 'max-w-sm' }}
         >
-          <CommonTextField type="email" inputId="new_email" label="New email" />
-          <AccountTextField
-            label="Confirm current password"
-            inputId="confirm_password"
-            type="password"
-          />
-          <Button
-            className="mt-6 ml-auto w-fit bg-theme-color-cabbage btn-quaternary"
-            onClick={onSaveEmail}
-          >
-            Save changes
-          </Button>
+          <EmailForm onSubmit={onChangeEmail} />
+        </AccountPageContainer>
+      </Tab>
+      <Tab label={Display.ConnectEmail}>
+        <AccountPageContainer
+          title="Change email"
+          onBack={() => setActiveDisplay(Display.Default)}
+          className={{ section: 'max-w-sm' }}
+        >
+          <EmailForm onSubmit={onConnectEmail} />
         </AccountPageContainer>
       </Tab>
     </TabContainer>
