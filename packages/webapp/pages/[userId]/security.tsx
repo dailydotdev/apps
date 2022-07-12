176d5900a59f86c337345d653c7f9c6a21982c1d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-07-12 16:27:44 +0800

    refactor: security page with changing of email

--- packages/webapp/pages/[userId]/security.tsx
+++ packages/webapp/pages/[userId]/security.tsx
@@ -1,4 +1,7 @@
 import { getProviderMapClone } from '@dailydotdev/shared/src/components/auth/common';
+import TabContainer, {
+  Tab,
+} from '@dailydotdev/shared/src/components/tabs/TabContainer';
 import ProviderButton from '@dailydotdev/shared/src/components/auth/ProviderButton';
 import { Button } from '@dailydotdev/shared/src/components/buttons/Button';
 import AccountDangerZone from '@dailydotdev/shared/src/components/profile/AccountDangerZone';
@@ -14,6 +17,7 @@ import {
   AccountContentHeading,
   AccountPageContainer,
   AccountTextField,
+  CommonTextField,
   ContentHeading,
   ContentText,
 } from '../../components/layouts/ProfileLayout/common';
@@ -26,57 +30,115 @@ socialProvider.gitHub.style = { backgroundColor: '#383C47' };
 socialProvider.apple.style = { backgroundColor: '#404551' };
 const providers = Object.values(socialProvider);
 
+enum Display {
+  Default = 'default',
+  ChangeEmail = 'change_email',
+}
+
 const AccountSecurityPage = (): ReactElement => {
   const { user } = useContext(AuthContext);
   const [email, setEmail] = useState<string>();
+  const [resetEmailSent, setResetEmailSent] = useState(false);
+  const [activeDisplay, setActiveDisplay] = useState(Display.Default);
 
   if (!user) {
     return null;
   }
 
   const onDelete = () => {};
+  const onSaveEmail = () => {
+    setResetEmailSent(true);
+    setActiveDisplay(Display.Default);
+  };
 
   return (
-    <AccountPageContainer title="Security">
-      <ContentHeading>Account email</ContentHeading>
-      <ContentText>
-        The email address associated with your daily.dev account
-      </ContentText>
-      <AccountTextField
-        fieldType="tertiary"
-        value={email ?? user.email}
-        onChange={(e) => setEmail(e.currentTarget.value)}
-        label="Email"
-        inputId="email"
-        leftIcon={<MailIcon />}
-      />
-      <Button className="mt-6 w-fit btn-secondary">Change email</Button>
-      <AccountContentHeading>Add login account</AccountContentHeading>
-      <ContentText>
-        Add more accounts to ensure you never lose access to your daily.dev
-        profile and to make login quick and easy cross device
-      </ContentText>
-      <div className="grid grid-cols-1 gap-4 mt-6 w-64">
-        {providers.map(({ provider, ...rest }) => (
-          <ProviderButton
-            key={provider}
-            label="Connect with"
-            provider={provider}
-            {...rest}
+    <TabContainer showHeader={false} controlledActive={activeDisplay}>
+      <Tab label={Display.Default}>
+        <AccountPageContainer title="Security">
+          <ContentHeading>Account email</ContentHeading>
+          <ContentText>
+            The email address associated with your daily.dev account
+          </ContentText>
+          <AccountTextField
+            fieldType="tertiary"
+            value={email ?? user.email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+            label="Email"
+            inputId="email"
+            leftIcon={<MailIcon />}
           />
-        ))}
-      </div>
-      <AccountContentHeading>Account Password</AccountContentHeading>
-      <ContentText>Change your account password</ContentText>
-      <Button className="mt-6 w-fit btn-secondary">Reset password</Button>
-      <AccountContentHeading>🚨 Danger Zone</AccountContentHeading>
-      <AccountDangerZone
-        onDelete={onDelete}
-        className="overflow-hidden relative py-4 px-6 mt-6 rounded-26 border border-theme-status-error"
-      >
-        <div className="absolute inset-0 w-full h-full opacity-24 bg-theme-color-ketchup" />
-      </AccountDangerZone>
-    </AccountPageContainer>
+          {resetEmailSent ? (
+            <div className="relative p-4 mt-6 max-w-md rounded-8 border border-theme-status-warning">
+              <div className="absolute inset-0 -z-1 w-full h-full opacity-24 bg-overlay-quaternary-bun" />
+              <p className="typo-callout">
+                We sent an email to verify your account. Please check your spam
+                folder if you {`don't`} see the email.
+              </p>
+              <span className="flex flex-row gap-4 mt-4">
+                <Button buttonSize="xsmall" className="w-fit btn-primary">
+                  Resend
+                </Button>
+                <Button buttonSize="xsmall" className="w-fit btn-secondary">
+                  Cancel Request
+                </Button>
+              </span>
+            </div>
+          ) : (
+            <Button
+              className="mt-6 w-fit btn-secondary"
+              onClick={() => setActiveDisplay(Display.ChangeEmail)}
+            >
+              Change email
+            </Button>
+          )}
+          <AccountContentHeading>Add login account</AccountContentHeading>
+          <ContentText>
+            Add more accounts to ensure you never lose access to your daily.dev
+            profile and to make login quick and easy cross device
+          </ContentText>
+          <div className="grid grid-cols-1 gap-4 mt-6 w-64">
+            {providers.map(({ provider, ...rest }) => (
+              <ProviderButton
+                key={provider}
+                label="Connect with"
+                provider={provider}
+                {...rest}
+              />
+            ))}
+          </div>
+          <AccountContentHeading>Account Password</AccountContentHeading>
+          <ContentText>Change your account password</ContentText>
+          <Button className="mt-6 w-fit btn-secondary">Reset password</Button>
+          <AccountContentHeading>🚨 Danger Zone</AccountContentHeading>
+          <AccountDangerZone
+            onDelete={onDelete}
+            className="overflow-hidden relative py-4 px-6 mt-6 rounded-26 border border-theme-status-error"
+          >
+            <div className="absolute inset-0 -z-1 w-full h-full opacity-24 bg-overlay-quaternary-ketchup" />
+          </AccountDangerZone>
+        </AccountPageContainer>
+      </Tab>
+      <Tab label={Display.ChangeEmail}>
+        <AccountPageContainer
+          title="Change email"
+          onBack={() => setActiveDisplay(Display.Default)}
+          className={{ section: 'max-w-sm' }}
+        >
+          <CommonTextField type="email" inputId="new_email" label="New email" />
+          <AccountTextField
+            label="Confirm current password"
+            inputId="confirm_password"
+            type="password"
+          />
+          <Button
+            className="mt-6 ml-auto w-fit bg-theme-color-cabbage btn-quaternary"
+            onClick={onSaveEmail}
+          >
+            Save changes
+          </Button>
+        </AccountPageContainer>
+      </Tab>
+    </TabContainer>
   );
 };
 
