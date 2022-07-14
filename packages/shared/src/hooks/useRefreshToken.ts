1a756a1812e53808659dc4823fa67b5c8059ddc4
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-07-14 17:13:57 +0800

    fix: refresh token causing to refetch

--- packages/shared/src/hooks/useRefreshToken.ts
+++ packages/shared/src/hooks/useRefreshToken.ts
@@ -1,23 +1,24 @@
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import { differenceInMilliseconds } from 'date-fns';
 import { AccessToken } from '../lib/boot';
 import useDebounce from './useDebounce';
+import { nextTick } from '../lib/func';
 
 export function useRefreshToken(
   accessToken: AccessToken,
   refresh: () => Promise<unknown>,
 ): void {
-  const timeout = useRef<number>();
-  const [useRefresh] = useDebounce(refresh, timeout.current - 1000 * 60 * 2);
+  const difference = differenceInMilliseconds(
+    new Date(accessToken?.expiresIn),
+    new Date(),
+  );
+  const [useRefresh] = useDebounce(refresh, difference - 1000 * 60 * 2);
 
   useEffect(() => {
     if (accessToken) {
-      timeout.current = differenceInMilliseconds(
-        new Date(accessToken.expiresIn),
-        new Date(),
-      );
-      useRefresh();
+      nextTick().then(() => {
+        useRefresh();
+      });
     }
-    return () => clearTimeout(timeout.current);
   }, [accessToken, refresh]);
 }
