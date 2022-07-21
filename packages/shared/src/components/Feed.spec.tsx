36d022688d5cb16a5b749a5156a77b0a43756dab
Ido Shamun <idoesh1@gmail.com>
2022-07-21 16:38:48 +0300

    test: update the ad server mock

--- packages/shared/src/components/Feed.spec.tsx
+++ packages/shared/src/components/Feed.spec.tsx
@@ -125,7 +125,7 @@ const renderComponent = (
   queryClient = new QueryClient();
 
   mocks.forEach(mockGraphQL);
-  nock('http://localhost:3000').get('/v1/a').reply(200, [ad]);
+  nock('http://localhost:3000').get('/v1/a?active=false').reply(200, [ad]);
   const settingsContext: SettingsContextData = {
     spaciness: 'eco',
     showOnlyUnreadPosts: false,
