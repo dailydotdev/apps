6cd3d9299ccdeb181a7cf41ad388ce404550986c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-08-12 00:53:11 +0800

    refactor: profile utilizing user info

--- packages/webapp/pages/account/profile.tsx
+++ packages/webapp/pages/account/profile.tsx
@@ -1,16 +1,20 @@
+import ImageInput from '@dailydotdev/shared/src/components/fields/ImageInput';
 import AtIcon from '@dailydotdev/shared/src/components/icons/At';
 import GitHubIcon from '@dailydotdev/shared/src/components/icons/GitHub';
 import HashnodeIcon from '@dailydotdev/shared/src/components/icons/Hashnode';
 import LinkIcon from '@dailydotdev/shared/src/components/icons/Link';
 import TwitterIcon from '@dailydotdev/shared/src/components/icons/Twitter';
 import UserIcon from '@dailydotdev/shared/src/components/icons/User';
-import React, { ReactElement } from 'react';
+import AuthContext from '@dailydotdev/shared/src/contexts/AuthContext';
+import React, { ReactElement, useContext } from 'react';
 import { getAccountLayout } from '../../components/layouts/AccountLayout';
 import AccountContentSection from '../../components/layouts/AccountLayout/AccountContentSection';
 import { AccountPageContainer } from '../../components/layouts/AccountLayout/AccountPageContainer';
 import { AccountTextField } from '../../components/layouts/AccountLayout/common';
 
 const AccountProfilePage = (): ReactElement => {
+  const { user } = useContext(AuthContext);
+
   return (
     <AccountPageContainer title="Profile">
       <AccountContentSection
@@ -19,24 +23,35 @@ const AccountProfilePage = (): ReactElement => {
         description="Upload a picture to make your profile stand out and let people recognise
         your comments and contributions easily!"
       >
-        {/* ImageInput component from auth ui PR */}
+        <ImageInput className="mt-6" initialValue={user.image} />
       </AccountContentSection>
       <AccountContentSection title="Account Information">
         <AccountTextField
           label="Full Name"
           inputId="fullname"
           leftIcon={<UserIcon />}
+          value={user.name}
         />
         <AccountTextField
           label="Username"
           inputId="username"
           leftIcon={<AtIcon />}
+          value={user.username}
         />
       </AccountContentSection>
       <AccountContentSection title="About">
-        <AccountTextField aria-multiline label="Bio" inputId="bio" />
-        <AccountTextField label="Company" inputId="company" />
-        <AccountTextField label="Job Title" inputId="job" />
+        <AccountTextField
+          aria-multiline
+          label="Bio"
+          inputId="bio"
+          value={user.bio}
+        />
+        <AccountTextField
+          label="Company"
+          inputId="company"
+          value={user.company}
+        />
+        <AccountTextField label="Job Title" inputId="job" value={user.title} />
       </AccountContentSection>
       <AccountContentSection
         title="Profile Social Links"
@@ -47,21 +62,25 @@ const AccountProfilePage = (): ReactElement => {
           leftIcon={<TwitterIcon />}
           label="Twitter"
           inputId="twitter"
+          value={user.twitter}
         />
         <AccountTextField
           leftIcon={<GitHubIcon />}
           label="GitHub"
           inputId="github"
+          value={user.github}
         />
         <AccountTextField
           leftIcon={<HashnodeIcon />}
           label="Hashnode"
           inputId="hashnode"
+          value={user.hashnode}
         />
         <AccountTextField
           leftIcon={<LinkIcon />}
           label="Your Website"
           inputId="website"
+          value={user.portfolio}
         />
       </AccountContentSection>
     </AccountPageContainer>
