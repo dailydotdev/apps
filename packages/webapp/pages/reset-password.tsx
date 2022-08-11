4b14100ab9b72a0cb5f72447f1e6a5f29deb3dda
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-08-11 11:46:02 +0800

    feat: account recovery changing of password

--- packages/webapp/pages/reset-password.tsx
+++ packages/webapp/pages/reset-password.tsx
@@ -1,16 +1,71 @@
-import React, { ReactElement } from 'react';
+import React, { FormEvent, ReactElement, useState } from 'react';
 import DailyCircle from '@dailydotdev/shared/src/components/DailyCircle';
 import Logo from '@dailydotdev/shared/src/components/Logo';
 import { TextField } from '@dailydotdev/shared/src/components/fields/TextField';
 import { Button } from '@dailydotdev/shared/src/components/buttons/Button';
+import { useQuery, useMutation } from 'react-query';
+import {
+  errorsToJson,
+  getNodeValue,
+  getSettingsFlow,
+  RegistrationParameters,
+  resetPassword,
+} from '@dailydotdev/shared/src/lib/auth';
+import { disabledRefetch } from '@dailydotdev/shared/src/lib/func';
+import { useRouter } from 'next/router';
+import { formToJson } from '@dailydotdev/shared/src/lib/form';
 
 function ResetPassword(): ReactElement {
+  const router = useRouter();
+  const [hint, setHint] = useState(null);
+  const { data } = useQuery(
+    'settings',
+    () => getSettingsFlow(router.query.flow as string),
+    {
+      ...disabledRefetch,
+      retry: false,
+      enabled: !!router.query.flow,
+    },
+  );
+  const { mutateAsync: reset } = useMutation(resetPassword, {
+    onSuccess: ({ error, data: success }) => {
+      if (success) {
+        return window.location.replace(process.env.NEXT_PUBLIC_WEBAPP_URL);
+      }
+
+      if (!error?.ui) {
+        return setHint('Session could have expired!');
+      }
+
+      const json = errorsToJson<keyof RegistrationParameters>(error);
+      return setHint(json?.password);
+    },
+  });
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { password } = formToJson<{ password: string }>(e.currentTarget);
+    const params: RegistrationParameters = {
+      method: 'password',
+      csrf_token: getNodeValue('csrf_token', data.ui.nodes),
+      'traits.email': getNodeValue('traits.email', data.ui.nodes),
+      'traits.fullname': getNodeValue('traits.fullname', data.ui.nodes),
+      'traits.username': getNodeValue('traits.username', data.ui.nodes),
+      'traits.image': getNodeValue('traits.image', data.ui.nodes),
+      password,
+    };
+    reset({ params, action: data.ui.action });
+  };
+
   return (
     // eslint-disable-next-line @dailydotdev/daily-dev-eslint-rules/no-custom-color
     <div className="flex overflow-hidden relative flex-col items-center w-screen max-w-full h-screen bg-gradient-to-bl from-[#EF43FD32] to-[#6451F332]">
       <DailyCircle className="absolute left-20" />
       <Logo className="mt-16 h-fit" />
-      <div className="relative z-1 p-7 mt-20 w-full text-center rounded-16 max-w-[23.25rem] bg-theme-bg-primary">
+      <form
+        className="relative z-1 p-7 mt-20 w-full text-center rounded-16 max-w-[23.25rem] bg-theme-bg-primary"
+        onSubmit={onSubmit}
+      >
         <DailyCircle className="absolute top-1/4 -right-60" size="small" />
         <DailyCircle className="absolute bottom-0 left-2/4 -translate-x-1/2 translate-y-[calc(100%+10rem)]" />
         <h2 className="font-bold typo-title1">Create new password</h2>
@@ -18,11 +73,18 @@ function ResetPassword(): ReactElement {
           Please enter your new password. A password strength meter will guide
           you if your password is strong enough.
         </p>
-        <TextField label="Create new password" inputId="password" />
-        <Button className="mt-7 w-full bg-theme-color-cabbage">
+        <TextField
+          label="Create new password"
+          inputId="password"
+          type="password"
+          name="password"
+          onChange={() => hint && setHint('')}
+          hint={hint}
+        />
+        <Button className="mt-7 w-full bg-theme-color-cabbage" type="submit">
           Change password
         </Button>
-      </div>
+      </form>
     </div>
   );
 }
