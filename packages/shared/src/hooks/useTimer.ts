3f7108f022f9a3ce84b55dda1cf32ca74b8e6535
Chris Bongers <chrisbongers@gmail.com>
2022-08-16 09:45:05 +0200

    fix: types

--- packages/shared/src/hooks/useTimer.ts
+++ packages/shared/src/hooks/useTimer.ts
@@ -1,7 +1,17 @@
-import { useMemo, useState } from 'react';
+import { Dispatch, SetStateAction, useMemo, useState } from 'react';
 import useDebounce from './useDebounce';
 
-export default function useTimer(callback, initialTimer: number) {
+type CallbackFn<T> = (params?: T) => void;
+export interface UseTimerReturnProps {
+  timer?: number;
+  setTimer: Dispatch<SetStateAction<number>>;
+  runTimer: () => void;
+  clearTimer: () => void;
+}
+export default function useTimer<T = unknown>(
+  callback: CallbackFn<T>,
+  initialTimer: number,
+): UseTimerReturnProps {
   const [timer, setTimer] = useState(initialTimer);
   const [runTimer, clearTimer] = useDebounce(() => {
     if (timer > 0) {
