957173526b3d12abdcd54642fff94a5e70af1fc8
Chris Bongers <chrisbongers@gmail.com>
2022-08-17 09:46:00 +0200

    fix: linting issue

--- packages/shared/src/components/auth/EmailVerificationSent.tsx
+++ packages/shared/src/components/auth/EmailVerificationSent.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import classed from '../../lib/classed';
 import { Button } from '../buttons/Button';
 import EmailSentIcon from '../../../icons/mail_sent.svg';
