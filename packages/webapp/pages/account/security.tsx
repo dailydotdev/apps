cbd36ef7d370d833b278b2dd6ef9624809affba9
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-08-23 21:24:19 +0800

    refactor: change password with privileged session

--- packages/webapp/pages/account/security.tsx
+++ packages/webapp/pages/account/security.tsx
@@ -2,16 +2,48 @@ import TabContainer, {
   Tab,
 } from '@dailydotdev/shared/src/components/tabs/TabContainer';
 import AuthContext from '@dailydotdev/shared/src/contexts/AuthContext';
-import React, { ReactElement, useContext, useState } from 'react';
+import { formToJson } from '@dailydotdev/shared/src/lib/form';
+import usePrivilegedSession from '@dailydotdev/shared/src/hooks/usePrivilegedSession';
+import VerifySessionModal from '@dailydotdev/shared/src/components/auth/VerifySessionModal';
+import { useToastNotification } from '@dailydotdev/shared/src/hooks/useToastNotification';
+import React, {
+  FormEvent,
+  ReactElement,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
 import { AccountSecurityDisplay as Display } from '../../components/layouts/AccountLayout/common';
 import { getAccountLayout } from '../../components/layouts/AccountLayout';
 import AccountSecurityDefault from '../../components/layouts/AccountLayout/Security';
 import EmailFormPage from '../../components/layouts/AccountLayout/Security/EmailFormPage';
+import ChangePasswordForm, {
+  ChangePasswordParams,
+} from '../../components/layouts/AccountLayout/Security/ChangePasswordForm';
 
 const AccountSecurityPage = (): ReactElement => {
   const { user } = useContext(AuthContext);
   const [isEmailSent, setIsEmailSent] = useState(false);
   const [activeDisplay, setActiveDisplay] = useState(Display.Default);
+  const { displayToast } = useToastNotification();
+  const formRef = useRef<HTMLFormElement>();
+  const onPasswordReset = () => {
+    displayToast('Password reset successful!');
+    setActiveDisplay(Display.Default);
+  };
+
+  const {
+    showVerifySession,
+    onUpdatePassword,
+    onPasswordLogin,
+    onCloseVerifySession,
+  } = usePrivilegedSession({
+    onResetSuccessful: onPasswordReset,
+    onSessionVerified: () => {
+      const form = formToJson<ChangePasswordParams>(formRef.current);
+      onUpdatePassword(form);
+    },
+  });
 
   if (!user) {
     return null;
@@ -22,9 +54,12 @@ const AccountSecurityPage = (): ReactElement => {
     setActiveDisplay(Display.Default);
   };
 
-  const onConnectEmail = () => {
-    setIsEmailSent(true);
-    setActiveDisplay(Display.Default);
+  const onConnectEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = formToJson<ChangePasswordParams>(e.currentTarget);
+    onUpdatePassword(form);
+    // setIsEmailSent(true);
+    // setActiveDisplay(Display.Default);
   };
 
   return (
@@ -34,6 +69,20 @@ const AccountSecurityPage = (): ReactElement => {
           isEmailSent={isEmailSent}
           onSwitchDisplay={setActiveDisplay}
         />
+      </Tab>
+      <Tab label={Display.ChangePassword}>
+        <ChangePasswordForm
+          onActiveDisplay={setActiveDisplay}
+          onUpdatePassword={onUpdatePassword}
+          formRef={formRef}
+        />
+        {showVerifySession && (
+          <VerifySessionModal
+            isOpen={showVerifySession}
+            onRequestClose={() => onCloseVerifySession()}
+            onPasswordLogin={onPasswordLogin}
+          />
+        )}
       </Tab>
       <Tab label={Display.ChangeEmail}>
         <EmailFormPage
