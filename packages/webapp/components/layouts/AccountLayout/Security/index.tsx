eeb18331dba7b44fd575037db8211ae75182ce86
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-08-25 13:05:50 +0800

    refactor: reset password ui and functionality

--- packages/webapp/components/layouts/AccountLayout/Security/index.tsx
+++ packages/webapp/components/layouts/AccountLayout/Security/index.tsx
@@ -6,7 +6,13 @@ import MailIcon from '@dailydotdev/shared/src/components/icons/Mail';
 import AccountDangerZone from '@dailydotdev/shared/src/components/profile/AccountDangerZone';
 import { AlertBackground } from '@dailydotdev/shared/src/components/alert/AlertContainer';
 import AuthContext from '@dailydotdev/shared/src/contexts/AuthContext';
-import React, { ReactElement, useContext, useState } from 'react';
+import React, {
+  FormEvent,
+  ReactElement,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
 import { useMutation, useQuery } from 'react-query';
 import {
   AuthFlow,
@@ -14,11 +20,13 @@ import {
   initializeKratosFlow,
   submitKratosFlow,
 } from '@dailydotdev/shared/src/lib/kratos';
-import { disabledRefetch } from '@dailydotdev/shared/src/lib/func';
 import UnlinkModal from '@dailydotdev/shared/src/components/modals/UnlinkModal';
 import { getNodeByKey, SettingsParams } from '@dailydotdev/shared/src/lib/auth';
 import DeleteAccountModal from '@dailydotdev/shared/src/components/modals/DeleteAccountModal';
 import DeletedAccountConfirmationModal from '@dailydotdev/shared/src/components/modals/DeletedAccountConfirmationModal';
+import { useToastNotification } from '@dailydotdev/shared/src/hooks/useToastNotification';
+import { PasswordField } from '@dailydotdev/shared/src/components/fields/PasswordField';
+import { formToJson } from '@dailydotdev/shared/src/lib/form';
 import AccountContentSection from '../AccountContentSection';
 import { AccountPageContainer } from '../AccountPageContainer';
 import {
@@ -34,10 +42,17 @@ socialProvider.gitHub.style = { backgroundColor: '#383C47' };
 socialProvider.apple.style = { backgroundColor: '#404551' };
 const providers = Object.values(socialProvider);
 
+export interface ChangePasswordParams {
+  password: string;
+  onPasswordReset: () => void;
+}
+
 interface AccountSecurityDefaultProps {
   isEmailSent?: boolean;
   onSwitchDisplay: (display: Display) => void;
+  onUpdatePassword: (form: ChangePasswordParams) => void;
 }
+
 export interface ManageSocialProvidersProps {
   type: ManageSocialProviderTypes;
   provider: string;
@@ -46,18 +61,16 @@ export interface ManageSocialProvidersProps {
 function AccountSecurityDefault({
   isEmailSent,
   onSwitchDisplay,
+  onUpdatePassword,
 }: AccountSecurityDefaultProps): ReactElement {
+  const resetPasswordFormRef = useRef<HTMLFormElement>();
+  const { displayToast } = useToastNotification();
   const { user, deleteAccount } = useContext(AuthContext);
   const [showDeleteAccount, setShowDeleteAccount] = useState(false);
   const [deletedAccount, setDeletedAccount] = useState(false);
   const [unlinkProvider, setUnlinkProvider] = useState(null);
   const [, setEmail] = useState<string>(null);
-  const { data: userProviders } = useQuery(
-    'providers',
-    () => getKratosProviders(),
-    { ...disabledRefetch },
-  );
-
+  const { data: userProviders } = useQuery('providers', getKratosProviders);
   const { data: settings } = useQuery('settings', () =>
     initializeKratosFlow(AuthFlow.Settings),
   );
@@ -85,6 +98,17 @@ function AccountSecurityDefault({
       [type]: provider,
     };
     await updateSettings({ action, params: postData });
+  };
+
+  const onPasswordReset = () => {
+    displayToast('Password reset successful!');
+    resetPasswordFormRef.current.reset();
+  };
+
+  const onChangePassword = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = formToJson<{ password: string }>(e.currentTarget);
+    onUpdatePassword({ ...form, onPasswordReset });
   };
 
   const emailAction = isEmailSent ? (
@@ -164,12 +188,22 @@ function AccountSecurityDefault({
         title="Account Password"
         description="Change your account password"
       >
-        <Button
-          className="mt-6 w-fit btn-secondary"
-          onClick={() => onSwitchDisplay(Display.ChangePassword)}
+        <form
+          ref={resetPasswordFormRef}
+          className="flex flex-col"
+          onSubmit={onChangePassword}
         >
-          Reset password
-        </Button>
+          <PasswordField
+            className="mt-6 max-w-sm"
+            inputId="new_password"
+            label="New password"
+            name="password"
+            showStrength={false}
+          />
+          <Button type="submit" className="mt-6 w-fit btn-secondary">
+            Reset password
+          </Button>
+        </form>
       </AccountContentSection>
       {/* )} */}
       <AccountContentSection title="🚨 Danger Zone">
