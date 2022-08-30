060bc0b8e5267b8b7ec0163034af8056a7f33fe5
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-08-30 14:56:18 +0800

    feat: use keyed window event

--- packages/shared/src/hooks/useWindowEvents.ts
+++ packages/shared/src/hooks/useWindowEvents.ts
@@ -1,14 +1,40 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
+import { AuthEvent } from '../lib/kratos';
 
-// to make intellisense work on the first parameter
-const useWindowEvents: typeof window.addEventListener = (
-  event: string,
-  func: (e: Event) => void,
+export interface EventData {
+  eventKey?: AuthEvent | string;
+}
+
+type EventParameter = keyof WindowEventMap;
+type KeyedWindowEventHandler<T = unknown> = (e: MessageEvent<T>) => void;
+
+const useWindowEvents = <T extends EventData = EventData>(
+  event: EventParameter,
+  func: KeyedWindowEventHandler<T>,
 ): void => {
   useEffect(() => {
     window.addEventListener(event, func);
     return () => window.removeEventListener(event, func);
   }, [event, func]);
+};
+
+export const useKeyedWindowEvent = <T extends EventData = EventData>(
+  event: EventParameter,
+  key: string,
+  func: KeyedWindowEventHandler<T>,
+): void => {
+  useWindowEvents(
+    event,
+    useCallback(
+      (e: MessageEvent<T>) => {
+        if (!e.data?.eventKey || e.data.eventKey !== key) {
+          return;
+        }
+        func(e);
+      },
+      [event, key, func],
+    ),
+  );
 };
 
 export default useWindowEvents;
