282a8ca32cbb15d31d5a60a0d3fbdece08a4655f
Chris Bongers <chrisbongers@gmail.com>
2022-09-02 11:13:00 +0200

    fix: tailwind hot reload issue for arbitrary values

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('@dailydotdev/shared/tailwind.config');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   ...config,
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@dailydotdev/shared/src/**/*.{js,ts,jsx,tsx}',
+    path.join(
+      fs.realpathSync('./node_modules/@dailydotdev/shared/src'),
+      '**/*.{js,ts,jsx,tsx}',
+    ),
   ],
 };
