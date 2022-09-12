062d0689571fb9c2db303e2113556c97ab6ffa6b
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-09-12 09:39:32 +0800

    revert: classname

--- packages/shared/src/components/modals/StyledModal.tsx
+++ packages/shared/src/components/modals/StyledModal.tsx
@@ -16,6 +16,7 @@ export function StyledModal({
 }: ModalProps): ReactElement {
   return (
     <Modal
+      portalClassName={className?.toString()}
       overlayClassName={classNames(
         'overlay flex fixed flex-col justify-center inset-0 max-h-[100vh] items-center px-5 bg-theme-overlay-quaternary z-[10]',
         overlayClassName,
@@ -23,7 +24,6 @@ export function StyledModal({
       className={classNames(
         'focus:outline-none modal flex flex-col relative w-full max-w-[26.25rem] max-h-[100%] overflow-y-auto items-center bg-theme-bg-tertiary shadow-2 border border-theme-divider-secondary rounded-16',
         contentClassName,
-        className,
       )}
       {...props}
     />
