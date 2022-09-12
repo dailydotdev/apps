136fc867106a5adb9ff74f4576cf8fcbca021374
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-09-12 09:29:15 +0800

    refactor: code cleanup

--- packages/shared/src/components/modals/StyledModal.tsx
+++ packages/shared/src/components/modals/StyledModal.tsx
@@ -16,15 +16,13 @@ export function StyledModal({
 }: ModalProps): ReactElement {
   return (
     <Modal
-      portalClassName={classNames(className.toString())}
+      portalClassName={className.toString()}
       overlayClassName={classNames(
-        'flex fixed flex-col justify-center inset-0 max-h-[100vh] items-center px-5 bg-theme-overlay-quaternary z-[10]',
-        'overlay',
+        'overlay flex fixed flex-col justify-center inset-0 max-h-[100vh] items-center px-5 bg-theme-overlay-quaternary z-[10]',
         overlayClassName,
       )}
       className={classNames(
-        'focus:outline-none modal',
-        'flex flex-col relative w-full max-w-[26.25rem] max-h-[100%] overflow-y-auto items-center bg-theme-bg-tertiary shadow-2 border border-theme-divider-secondary rounded-16',
+        'focus:outline-none modal flex flex-col relative w-full max-w-[26.25rem] max-h-[100%] overflow-y-auto items-center bg-theme-bg-tertiary shadow-2 border border-theme-divider-secondary rounded-16',
         contentClassName,
       )}
       {...props}
