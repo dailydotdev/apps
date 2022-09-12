626a133d5c2f7cbc83788ca1e8bcfec42b3d98d6
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-09-12 09:21:34 +0800

    refactor: css module to inline classes

--- packages/shared/src/components/modals/StyledModal.tsx
+++ packages/shared/src/components/modals/StyledModal.tsx
@@ -1,8 +1,6 @@
 import Modal from 'react-modal';
 import classNames from 'classnames';
 import React, { ReactElement, ReactNode } from 'react';
-import classed from '../../lib/classed';
-import styles from './StyledModal.module.css';
 
 export interface ModalProps extends Modal.Props {
   padding?: boolean;
@@ -18,10 +16,15 @@ export function ReactModalAdapter({
 }: ModalProps): ReactElement {
   return (
     <Modal
-      portalClassName={className.toString()}
-      overlayClassName={classNames('styled-overlay overlay', overlayClassName)}
+      portalClassName={classNames(className.toString())}
+      overlayClassName={classNames(
+        'flex fixed flex-col justify-center inset-0 max-h-[100vh] items-center px-5 bg-theme-overlay-quaternary z-[10]',
+        'overlay',
+        overlayClassName,
+      )}
       className={classNames(
-        'focus:outline-none styled-modal modal',
+        'focus:outline-none modal',
+        'flex flex-col relative w-full max-w-[26.25rem] max-h-[100%] overflow-y-auto items-center bg-theme-bg-tertiary shadow-2 border border-theme-divider-secondary rounded-16',
         contentClassName,
       )}
       {...props}
@@ -29,4 +32,4 @@ export function ReactModalAdapter({
   );
 }
 
-export const StyledModal = classed(ReactModalAdapter, styles.styledModal);
+export const StyledModal = ReactModalAdapter;
