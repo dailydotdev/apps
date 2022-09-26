4dc7205ce7c3dac149cce13a057773893eff3c8f
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-09-26 15:27:28 +0800

    fix: missed import

--- packages/webapp/components/layouts/AccountLayout/EmailForm.tsx
+++ packages/webapp/components/layouts/AccountLayout/EmailForm.tsx
@@ -3,7 +3,7 @@ import {
   PasswordField,
   PasswordFieldProps,
 } from '@dailydotdev/shared/src/components/fields/PasswordField';
-import { TextFieldProps } from '@dailydotdev/shared/src/components/fields/common';
+import { TextFieldProps } from '@dailydotdev/shared/src/components/fields/TextField';
 import classNames from 'classnames';
 import React, {
   Dispatch,
