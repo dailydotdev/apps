5c82097d9560a07f9990f30bfb071a12f63f5286
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-09-28 21:22:51 +0800

    feat: feed filters step 2

--- packages/shared/src/components/modals/FeedFiltersModal.tsx
+++ packages/shared/src/components/modals/FeedFiltersModal.tsx
@@ -4,17 +4,34 @@ import SteppedModal from './SteppedModal';
 import FeedTopicCard from '../containers/FeedTopicCard';
 import FeedFilterStep from '../containers/FeedFilterStep';
 import useFeedSettings from '../../hooks/useFeedSettings';
+import classed from '../../lib/classed';
+import { CustomSwitch } from '../fields/CustomSwitch';
 
 interface FeedFitlersModalProps extends ModalProps {
   trigger?: string;
 }
 
+const CardPreviewPlaceholder = classed(
+  'div',
+  'rounded-8 bg-theme-divider-secondary w-full',
+);
+
+const CardPreview = () => (
+  <div className="flex flex-col p-2 w-24 h-28 rounded-8 bg-theme-divider-tertiary">
+    <CardPreviewPlaceholder className="h-2.5" />
+    <CardPreviewPlaceholder className="mt-1 w-3/4 h-2.5" />
+    <CardPreviewPlaceholder className="mt-auto h-12" />
+  </div>
+);
+
 function FeedFitlersModal(props: FeedFitlersModalProps): ReactElement {
+  const [isInsaneMode, setIsInsaneMode] = useState(false);
   const [selected, setSelected] = useState({});
   const { tagsCategories } = useFeedSettings();
+  const onNext = () => {};
 
   return (
-    <SteppedModal {...props} isOpen isLastStepLogin>
+    <SteppedModal {...props} onNext={onNext} isLastStepLogin>
       <FeedFilterStep
         title="Make the feed, your feed."
         description="Devs with a personal feed get 11.5x more relevant articles!"
@@ -43,8 +60,39 @@ function FeedFitlersModal(props: FeedFitlersModalProps): ReactElement {
       <FeedFilterStep
         title="Cards or list?"
         description="daily content can be presented in cards or a list, choose what works for you the best"
+        className={{
+          container: 'items-center',
+          content: 'relative flex flex-col items-center mt-8',
+        }}
       >
-        Test Sample Aaa
+        <img
+          className="absolute -top-4 -left-2"
+          src="/recommended.png"
+          alt="Pointing at the recommended layout"
+        />
+        <CustomSwitch
+          inputId="feed_layout"
+          name="feed_layout"
+          leftContent="Cards"
+          rightContent="List"
+          checked={isInsaneMode}
+          onToggle={() => setIsInsaneMode(!isInsaneMode)}
+        />
+        <div className="grid relative grid-cols-3 gap-3 mt-11 w-fit">
+          <div
+            className="absolute inset-0 rounded-8"
+            style={{
+              background:
+                'linear-gradient(45deg, rgba(23, 25, 31, 1) 0%, rgba(23, 25, 31, 0) 100%)',
+            }}
+          />
+          <CardPreview />
+          <CardPreview />
+          <CardPreview />
+          <CardPreview />
+          <CardPreview />
+          <CardPreview />
+        </div>
       </FeedFilterStep>
       <FeedFilterStep
         title="Your eyes don’t lie"
