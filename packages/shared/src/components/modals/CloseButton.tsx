a7cf4cf72b7473ddbfc2ae2dfb0403bc4ce19480
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-10-07 17:59:54 +0800

    tmp: close button

---
+++ packages/shared/src/components/modals/CloseButton.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import React, { forwardRef, ReactElement, Ref } from 'react';
+import { Button, StyledButtonProps, ButtonProps } from '../buttons/Button';
+import CloseIcon from '../icons/Close';
+
+function CloseButtonComponent(
+  { className, ...props }: StyledButtonProps & ButtonProps<'button'>,
+  ref: Ref<HTMLButtonElement>,
+): ReactElement {
+  return (
+    <Button
+      buttonSize="small"
+      {...props}
+      ref={ref}
+      className={classNames('btn-tertiary', className)}
+      title="Close"
+      icon={<CloseIcon />}
+    />
+  );
+}
+
+const CloseButton = forwardRef(CloseButtonComponent);
+
+export default CloseButton;
