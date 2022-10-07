50b40dce994831f54ef88ea7a92741ccb19fc89d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-10-07 17:56:15 +0800

    refactor: base elements styling

--- packages/shared/src/components/filters/common.tsx
+++ packages/shared/src/components/filters/common.tsx
@@ -21,15 +21,22 @@ export interface UnblockModalType {
   action?: () => unknown;
 }
 
-export const TagCategoryDetails = classed(
+export const BaseTagCategoryDetails = classed(
   'details',
-  'border-t border-b border-theme-divider-tertiary right-icon cursor-pointer',
+  'right-icon cursor-pointer',
 );
 
-export const TagCategorySummary = classed(
-  'summary',
-  'flex justify-between items-center p-6 pl-4 outline-none',
+export const TagCategoryDetails = classed(
+  BaseTagCategoryDetails,
+  'border-t border-b border-theme-divider-tertiary',
 );
+
+export const BaseTagCategorySummary = classed(
+  'summary',
+  'flex justify-between items-center outline-none',
+);
+
+export const TagCategorySummary = classed(BaseTagCategorySummary, 'p-6 pl-4');
 
 export const TagCategoryDetailsContent = classed(
   'div',
