7ed48394813df3be4f447a22a034169a6b2bac6c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-10-13 21:17:13 +0800

    fix: typo issue

--- packages/shared/src/components/modals/FeedFiltersModal.tsx
+++ packages/shared/src/components/modals/FeedFiltersModal.tsx
@@ -111,7 +111,7 @@ function FeedFitlersModal(props: FeedFitlersModalProps): ReactElement {
       </FeedFilterStep>
       <FeedFilterStep
         title="Your eyes don’t lie"
-        description="Dark mode will less emit blue light from your screen - which can keep you awake if you use your device before you go to bed"
+        description="Dark mode will emit less blue light from your screen - which can keep you awake if you use your device before you go to bed"
         className={{ content: 'grid grid-cols-1 gap-6 mt-11 px-11' }}
       >
         {themes.map((theme) => (
