12d8d3bcae26262adc56eb279e37b8ca4474c2af
Eliz <elizkilic@gmail.com>
2022-10-14 14:09:21 +0000

    fix: handle white bg for dark theme

--- packages/shared/src/components/PromotionalBanner.tsx
+++ packages/shared/src/components/PromotionalBanner.tsx
@@ -14,7 +14,8 @@ interface PromotionalBannerProps {
 const classNamesByTheme: Record<BannerTheme, string[]> = {
   [BannerCustomTheme.CabbageOnion]: [
     'from-theme-color-cabbage to-theme-color-onion bg-gradient-to-r', 'text-white', 'bg-white text-pepper-90'],
-  [BannerCustomTheme.WhitePepper]: ['bg-white', 'text-pepper-90', 'bg-theme-color-cabbage'],
+  [BannerCustomTheme.WhitePepper]: [
+    'bg-theme-bg-reverse', 'text-theme-label-invert', 'text-white btn-primary-cabbage'],
   [Theme.Avocado]: ['bg-theme-color-avocado', 'text-pepper-90', 'bg-white text-pepper-90'],
   [Theme.Bacon]: ['bg-theme-color-bacon', 'text-white', 'bg-white text-pepper-90'],
   [Theme.BlueCheese]: ['bg-theme-color-blueCheese', 'text-pepper-90', 'bg-white text-pepper-90'],
