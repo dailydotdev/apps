8cbae74cb97a62028cbb62f9147d3768eeff1a04
Eliz <elizkilic@gmail.com>
2022-10-14 14:49:17 +0000

    fix: run the eslint fix

--- packages/shared/src/components/PromotionalBanner.tsx
+++ packages/shared/src/components/PromotionalBanner.tsx
@@ -13,19 +13,57 @@ interface PromotionalBannerProps {
 
 const classNamesByTheme: Record<BannerTheme, string[]> = {
   [BannerCustomTheme.CabbageOnion]: [
-    'from-theme-color-cabbage to-theme-color-onion bg-gradient-to-r', 'text-white', 'bg-white text-pepper-90'],
+    'from-theme-color-cabbage to-theme-color-onion bg-gradient-to-r',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
   [BannerCustomTheme.WhitePepper]: [
-    'bg-theme-bg-reverse', 'text-theme-label-invert', 'text-white btn-primary-cabbage'],
-  [Theme.Avocado]: ['bg-theme-color-avocado', 'text-pepper-90', 'bg-white text-pepper-90'],
-  [Theme.Bacon]: ['bg-theme-color-bacon', 'text-white', 'bg-white text-pepper-90'],
-  [Theme.BlueCheese]: ['bg-theme-color-blueCheese', 'text-pepper-90', 'bg-white text-pepper-90'],
+    'bg-theme-bg-reverse',
+    'text-theme-label-invert',
+    'text-white btn-primary-cabbage',
+  ],
+  [Theme.Avocado]: [
+    'bg-theme-color-avocado',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Bacon]: [
+    'bg-theme-color-bacon',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.BlueCheese]: [
+    'bg-theme-color-blueCheese',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
   [Theme.Bun]: ['bg-theme-color-bun', 'text-white', 'bg-white text-pepper-90'],
-  [Theme.Burger]: ['bg-theme-color-burger', 'text-white', 'bg-white text-pepper-90'],
-  [Theme.Cabbage]: ['bg-theme-color-cabbage', 'text-white', 'bg-white text-pepper-90'],
-  [Theme.Cheese]: ['bg-theme-color-cheese', 'text-pepper-90', 'bg-white text-pepper-90'],
-  [Theme.Ketchup]: ['bg-theme-color-ketchup', 'text-white', 'bg-white text-pepper-90'],
-  [Theme.Lettuce]: ['bg-theme-color-lettuce', 'text-pepper-90', 'bg-white text-pepper-90'],
-}
+  [Theme.Burger]: [
+    'bg-theme-color-burger',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Cabbage]: [
+    'bg-theme-color-cabbage',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Cheese]: [
+    'bg-theme-color-cheese',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Ketchup]: [
+    'bg-theme-color-ketchup',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Lettuce]: [
+    'bg-theme-color-lettuce',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+};
 
 export default function PromotionalBanner({
   bannerData,
@@ -41,12 +79,15 @@ export default function PromotionalBanner({
   }
 
   const { banner } = bannerData;
-  const [container, text, button] = classNamesByTheme[banner.theme] ?? classNamesByTheme[BannerCustomTheme.CabbageOnion];
+  const [container, text, button] =
+    classNamesByTheme[banner.theme] ??
+    classNamesByTheme[BannerCustomTheme.CabbageOnion];
   return (
     <div
       className={classNames(
         'relative z-3 laptop:fixed flex flex-col items-start py-3 pl-3 pr-12 typo-footnote laptop:h-8 laptop:flex-row laptop:items-center laptop:justify-center laptop:p-0 w-full',
-        container, text
+        container,
+        text,
       )}
     >
       <div>
@@ -57,10 +98,7 @@ export default function PromotionalBanner({
         tag="a"
         href={banner.url}
         buttonSize="xsmall"
-        className={classNames(
-          'mt-2 laptop:ml-4 laptop:mt-0',
-          button
-        )}
+        className={classNames('mt-2 laptop:ml-4 laptop:mt-0', button)}
       >
         {banner.cta}
       </Button>
