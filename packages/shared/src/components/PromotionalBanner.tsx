93003fea95eb287d0f45404ef9ccc379ec6e5b6b
Eliz <el@users.noreply.github.com>
2022-10-14 18:02:23 +0300

    feat: add extra theme colors (#1324)
    * Add extra theme colors
    
    * fix: theme colors
    
    * fix: gradient color of cabbage-onion
    
    * fix: update the enum casing to PascalCase
    
    * fix: handle white bg for dark theme
    
    * fix: run the eslint fix

--- packages/shared/src/components/PromotionalBanner.tsx
+++ packages/shared/src/components/PromotionalBanner.tsx
@@ -3,12 +3,68 @@ import classNames from 'classnames';
 import XIcon from './icons/Close';
 import { Button } from './buttons/Button';
 import { isTesting } from '../lib/constants';
-import { BannerData } from '../graphql/banner';
+import { BannerCustomTheme, BannerData, BannerTheme } from '../graphql/banner';
+import { Theme } from './utilities';
 
 interface PromotionalBannerProps {
   bannerData: BannerData;
   setLastSeen: (value: Date) => Promise<void>;
 }
+
+const classNamesByTheme: Record<BannerTheme, string[]> = {
+  [BannerCustomTheme.CabbageOnion]: [
+    'from-theme-color-cabbage to-theme-color-onion bg-gradient-to-r',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [BannerCustomTheme.WhitePepper]: [
+    'bg-theme-bg-reverse',
+    'text-theme-label-invert',
+    'text-white btn-primary-cabbage',
+  ],
+  [Theme.Avocado]: [
+    'bg-theme-color-avocado',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Bacon]: [
+    'bg-theme-color-bacon',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.BlueCheese]: [
+    'bg-theme-color-blueCheese',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Bun]: ['bg-theme-color-bun', 'text-white', 'bg-white text-pepper-90'],
+  [Theme.Burger]: [
+    'bg-theme-color-burger',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Cabbage]: [
+    'bg-theme-color-cabbage',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Cheese]: [
+    'bg-theme-color-cheese',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Ketchup]: [
+    'bg-theme-color-ketchup',
+    'text-white',
+    'bg-white text-pepper-90',
+  ],
+  [Theme.Lettuce]: [
+    'bg-theme-color-lettuce',
+    'text-pepper-90',
+    'bg-white text-pepper-90',
+  ],
+};
+
 export default function PromotionalBanner({
   bannerData,
   setLastSeen,
@@ -23,14 +79,15 @@ export default function PromotionalBanner({
   }
 
   const { banner } = bannerData;
+  const [container, text, button] =
+    classNamesByTheme[banner.theme] ??
+    classNamesByTheme[BannerCustomTheme.CabbageOnion];
   return (
     <div
       className={classNames(
         'relative z-3 laptop:fixed flex flex-col items-start py-3 pl-3 pr-12 typo-footnote laptop:h-8 laptop:flex-row laptop:items-center laptop:justify-center laptop:p-0 w-full',
-        banner.theme === 'title-bacon' && 'text-theme-label-bun',
-        banner.theme === 'gradient-bacon-onion'
-          ? 'bg-theme-bg-bun'
-          : 'bg-theme-bg-primary',
+        container,
+        text,
       )}
     >
       <div>
@@ -41,12 +98,7 @@ export default function PromotionalBanner({
         tag="a"
         href={banner.url}
         buttonSize="xsmall"
-        className={classNames(
-          'mt-2 laptop:ml-4 laptop:mt-0',
-          banner.theme === 'cta-bacon-onion'
-            ? 'btn-primary-bacon'
-            : 'btn-primary',
-        )}
+        className={classNames('mt-2 laptop:ml-4 laptop:mt-0', button)}
       >
         {banner.cta}
       </Button>
