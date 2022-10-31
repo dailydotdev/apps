b2321858fe0c534adeb37cdbe2f73ca6fc6b96e1
Chris Bongers <chrisbongers@gmail.com>
2022-10-31 15:26:46 +0200

    fix: set correct tag links (#1343)
    Hotfix for linking to the right tag
    We no longer support the tag.name type in this format (but it's supported as a interface elsewhere)
    
    WT-720 #done

--- packages/shared/src/components/filters/TagOptionsMenu.tsx
+++ packages/shared/src/components/filters/TagOptionsMenu.tsx
@@ -37,7 +37,11 @@ export default function TagOptionsMenu({
     >
       {tag && (
         <Item>
-          <Link href={getTagPageLink(tag.name)} passHref prefetch={false}>
+          <Link
+            href={getTagPageLink(tag.name ? tag.name : (tag as string))}
+            passHref
+            prefetch={false}
+          >
             <a className="w-full">View</a>
           </Link>
         </Item>
