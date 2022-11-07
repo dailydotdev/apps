c169898c8bba961e6d4d3d14ed2b36cd52e36831
Eliz Kilic <elizkilic@gmail.com>
2022-11-07 23:24:06 +0300

    fix: move classnames to the constant

--- packages/shared/src/components/modals/common/Modal.tsx
+++ packages/shared/src/components/modals/common/Modal.tsx
@@ -61,26 +61,22 @@ export function Modal({
   const modalOverlayClassName = classNames(
     'overlay flex fixed flex-col inset-0 items-center bg-gradient-to-r to-theme-overlay-to from-theme-overlay-from z-[10]',
     modalKindAndSizeToOverlayClassName[kind]?.[size],
+    modalKindToOverlayClassName[kind],
+    overlayClassName,
   );
   const modalClassName = classNames(
     'modal flex flex-col relative focus:outline-none max-w-full overflow-y-auto items-center bg-theme-bg-tertiary shadow-2 border border-theme-divider-secondary rounded-16',
+    modalKindToClassName[kind],
+    modalSizeToClassName[size],
     modalKindAndSizeToClassName[kind]?.[size],
+    className,
   );
 
   return (
     <ReactModal
       isOpen
-      overlayClassName={classNames(
-        overlayClassName,
-        modalKindToOverlayClassName[kind],
-        modalOverlayClassName,
-      )}
-      className={classNames(
-        className,
-        modalKindToClassName[kind],
-        modalSizeToClassName[size],
-        modalClassName,
-      )}
+      overlayClassName={modalOverlayClassName}
+      className={modalClassName}
     >
       <ModalPropsContext.Provider value={{ size, kind, onRequestClose }}>
         {children}
--- packages/shared/src/components/modals/common/ModalBody.tsx
+++ packages/shared/src/components/modals/common/ModalBody.tsx
@@ -9,16 +9,8 @@ export type ModalBodyProps = {
 export function ModalBody({ children }: ModalBodyProps): ReactElement {
   const { kind, size } = useContext(ModalPropsContext);
   const className = classNames(
+    'overflow-auto relative w-full h-full shrink max-h-full p-6',
     kind === ModalKind.FlexibleTop && size === ModalSize.Large && 'mobileL:p-8',
   );
-  return (
-    <section
-      className={classNames(
-        'overflow-auto relative w-full h-full shrink max-h-full p-6',
-        className,
-      )}
-    >
-      {children}
-    </section>
-  );
+  return <section className={className}>{children}</section>;
 }
