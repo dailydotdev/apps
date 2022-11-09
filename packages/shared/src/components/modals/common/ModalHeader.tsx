45be56ca3aa93234af54d37f541f76b08bc41b5e
Eliz Kilic <elizkilic@gmail.com>
2022-11-09 18:50:14 +0300

    feat: confirmation modal update for the new modal guidelines

--- packages/shared/src/components/modals/common/ModalHeader.tsx
+++ packages/shared/src/components/modals/common/ModalHeader.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, ReactNode, useContext } from 'react';
 import classed from '../../../lib/classed';
 import { ModalClose } from './ModalClose';
 import { ModalPropsContext } from './types';
+import classNames from 'classnames';
 
 export type ModalHeaderProps = {
   children?: ReactNode;
@@ -16,7 +17,10 @@ export function ModalHeader({
 }: ModalHeaderProps): ReactElement {
   const { onRequestClose } = useContext(ModalPropsContext);
   return (
-    <header className="flex justify-between items-center py-4 px-6 w-full h-14 border-b border-theme-divider-tertiary">
+    <header className={classNames(
+      'flex justify-between items-center py-4 px-6 w-full h-14',
+      (title || tabs || children) && 'border-b border-theme-divider-tertiary'
+    )}>
       {!!title && <ModalHeaderTitle>{title}</ModalHeaderTitle>}
       {children}
       {onRequestClose && <ModalClose onClick={onRequestClose} />}
