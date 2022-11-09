2fcb70bc75cf2b437371ff1a4bf0780afbce0c03
Eliz <el@users.noreply.github.com>
2022-11-09 12:31:51 +0300

    feat: report modal and modal footer (#1360)

--- packages/shared/src/components/modals/common/ModalFooter.tsx
+++ packages/shared/src/components/modals/common/ModalFooter.tsx
@@ -5,5 +5,9 @@ export type ModalFooterProps = {
 };
 
 export function ModalFooter({ children }: ModalFooterProps): ReactElement {
-  return <footer>{children}</footer>;
+  return (
+    <footer className="flex gap-3 justify-end items-center py-4 px-2 w-full h-14 border-t border-theme-divider-tertiary">
+      {children}
+    </footer>
+  );
 }
