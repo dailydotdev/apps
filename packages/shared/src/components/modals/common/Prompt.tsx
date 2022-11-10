2b736be2fc2fbb9187497d904f66f6f18b4568a0
Eliz <el@users.noreply.github.com>
2022-11-10 13:51:59 +0300

    feat: confirmation modal update for the new modal guidelines (#1363)
    * feat: confirmation modal update for the new modal guidelines
    
    * feat: confirmation modal update for the new modal guidelines

--- packages/shared/src/components/modals/common/Prompt.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-
-export type PromptProps = {
-  title: string;
-};
-
-export function Prompt({ title }: PromptProps): ReactElement {
-  return <span>{title}</span>;
-}
