95f58eb122aa35155b987734a322e6858e034b48
Chris Bongers <chrisbongers@gmail.com>
2022-11-14 09:22:37 +0200

    feat: new squad AB variants (#1366)
    Added new squads menu AB variants
    
    WT-738 #done

---
+++ packages/shared/src/components/sidebar/SquadSection.tsx
@@ -0,0 +1,42 @@
+import React, { ReactElement } from 'react';
+import { FeaturesData } from '../../contexts/FeaturesContext';
+import { ListIcon, SidebarMenuItem } from './common';
+import { Section, SectionCommonProps } from './Section';
+import SquadIcon from '../icons/Squad';
+
+interface SquadSectionProps
+  extends SectionCommonProps,
+    Pick<FeaturesData, 'popularFeedCopy'> {
+  squadButton: string;
+  squadForm: string;
+  isItemsButton?: boolean;
+  onSquadClick: () => void;
+}
+
+export function SquadSection({
+  isItemsButton,
+  popularFeedCopy,
+  squadButton,
+  squadForm,
+  onSquadClick,
+  ...defaultRenderSectionProps
+}: SquadSectionProps): ReactElement {
+  const squadMenuItems: SidebarMenuItem[] = [
+    {
+      icon: () => <ListIcon Icon={() => <SquadIcon secondary />} />,
+      title: squadButton,
+      path: squadForm,
+      className: { text: 'text-theme-status-cabbage' },
+      action: onSquadClick,
+    },
+  ];
+
+  return (
+    <Section
+      {...defaultRenderSectionProps}
+      title="Squads"
+      items={squadMenuItems}
+      isItemsButton={isItemsButton}
+    />
+  );
+}
