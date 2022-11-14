e765d6661145e423b4ac4af9147c3aac204acf9b
Ido Shamun <ido@daily.dev>
2022-11-14 16:36:26 +0200

    fix: show squad only for new users
    Somehow we missed that in the review but I noticed it while debugging

--- packages/shared/src/components/sidebar/Sidebar.tsx
+++ packages/shared/src/components/sidebar/Sidebar.tsx
@@ -73,7 +73,7 @@ export default function Sidebar({
     squadForm,
     squadButton,
   } = useContext(FeaturesContext);
-  const squadVisible = sidebarRendered && !user;
+  const squadVisible = sidebarRendered && user;
   const [trackedSquadImpression, setTrackedSquadImpression] = useState(false);
   const activePage =
     activePageProp === '/' ? `/${defaultFeed}` : activePageProp;
