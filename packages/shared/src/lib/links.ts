1cf5a9d91416860682130164150754899865303d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-11-16 01:20:42 +0800

    refactor: preview features available on dev

--- packages/shared/src/lib/links.ts
+++ packages/shared/src/lib/links.ts
@@ -1,8 +1,14 @@
+import { isDevelopment } from './constants';
+
 export const getTagPageLink = (tag: string): string =>
   `${process.env.NEXT_PUBLIC_WEBAPP_URL}tags/${encodeURIComponent(tag)}`;
 
 export const isPreviewDeployment = (() => {
   const value = process.env.NEXT_PUBLIC_PREVIEW_DEPLOYMENT;
+
+  if (isDevelopment) {
+    return true;
+  }
 
   if (!value) {
     return false;
