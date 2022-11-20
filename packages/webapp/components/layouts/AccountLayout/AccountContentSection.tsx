0da85de30e74a31b51c4371c511eff31ee19ee43
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-11-20 18:55:31 +0800

    refactor: object class names

--- packages/webapp/components/layouts/AccountLayout/AccountContentSection.tsx
+++ packages/webapp/components/layouts/AccountLayout/AccountContentSection.tsx
@@ -1,11 +1,16 @@
 import classed from '@dailydotdev/shared/src/lib/classed';
 import React, { ReactElement, ReactNode } from 'react';
 
+interface ClassName {
+  container?: string;
+  heading?: string;
+}
+
 interface AccountContentSectionProps {
   title: string;
   description?: string;
-  children: ReactNode;
-  headingClassName?: string;
+  children?: ReactNode;
+  className?: ClassName;
 }
 
 const ContentHeading = classed('h2', 'font-bold typo-headline');
@@ -15,15 +20,23 @@ function AccountContentSection({
   title,
   description,
   children,
-  headingClassName = 'mt-10',
+  className = {},
 }: AccountContentSectionProps): ReactElement {
-  return (
+  const { container, heading = 'mt-10' } = className;
+
+  const content = (
     <>
-      <ContentHeading className={headingClassName}>{title}</ContentHeading>
+      <ContentHeading className={heading}>{title}</ContentHeading>
       {description && <ContentText>{description}</ContentText>}
       {children}
     </>
   );
+
+  if (!container) {
+    return content;
+  }
+
+  return <div className={container}>{content}</div>;
 }
 
 export default AccountContentSection;
