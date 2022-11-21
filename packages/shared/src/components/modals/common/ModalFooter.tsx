9a8b507eda738d73872b64745bd204bf619fae59
Chris Bongers <chrisbongers@gmail.com>
2022-11-21 14:15:37 +0200

    fix: added fully qualified type for justify modal footers

--- packages/shared/src/components/modals/common/ModalFooter.tsx
+++ packages/shared/src/components/modals/common/ModalFooter.tsx
@@ -1,18 +1,19 @@
 import classNames from 'classnames';
 import React, { ReactElement, ReactNode, useContext } from 'react';
 import { ModalPropsContext } from './types';
+import { Justify } from '../../utilities';
 
 export type ModalFooterProps = {
   children?: ReactNode;
   className?: string;
-  justify?: 'end' | 'center' | 'between';
+  justify?: Justify;
   tab?: string;
 };
 
 export function ModalFooter({
   children,
   className,
-  justify = 'end',
+  justify = Justify.End,
   tab,
 }: ModalFooterProps): ReactElement {
   const { activeTab } = useContext(ModalPropsContext);
@@ -21,7 +22,7 @@ export function ModalFooter({
     <footer
       className={classNames(
         'flex gap-3 items-center p-3 w-full h-16 border-t border-theme-divider-tertiary',
-        `justify-${justify}`,
+        justify,
         className,
       )}
     >
