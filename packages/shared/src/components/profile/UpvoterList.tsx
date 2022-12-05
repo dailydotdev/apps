cf9044af2b628e653bbbe7e45143dfa2cb2e56df
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-12-05 21:43:55 +0800

    Revert "revert: infinite scroll implementation on upvote"
    This reverts commit ad66fa9d4732a4e99b3050c39f72c9931ee04c1c.

--- packages/shared/src/components/profile/UpvoterList.tsx
+++ packages/shared/src/components/profile/UpvoterList.tsx
@@ -1,33 +1,26 @@
 import React, { ReactElement } from 'react';
 import Link from 'next/link';
 import { UseInfiniteQueryResult } from 'react-query';
-import useFeedInfiniteScroll from '../../hooks/feed/useFeedInfiniteScroll';
 import { UpvotesData } from '../../graphql/common';
 import { UpvoterListPlaceholder } from './UpvoterListPlaceholder';
 import { UserShortInfo } from './UserShortInfo';
+import InfiniteScrolling from '../containers/InfiniteScrolling';
 
 export interface UpvoterListProps {
   queryResult: UseInfiniteQueryResult<UpvotesData>;
   scrollingContainer?: HTMLElement;
   appendTooltipTo?: HTMLElement;
 }
+
 export function UpvoterList({
   queryResult,
   ...props
 }: UpvoterListProps): ReactElement {
-  const canFetchMore =
-    !queryResult.isLoading &&
-    !queryResult.isFetchingNextPage &&
-    queryResult.hasNextPage &&
-    queryResult.data.pages.length > 0;
-
-  const infiniteScrollRef = useFeedInfiniteScroll({
-    fetchPage: queryResult.fetchNextPage,
-    canFetchMore,
-  });
-
   return (
-    <div className="flex relative flex-col">
+    <InfiniteScrolling
+      queryResult={queryResult}
+      placeholder={<UpvoterListPlaceholder placeholderAmount={1} />}
+    >
       {queryResult.data.pages.map(
         (page) =>
           page &&
@@ -42,13 +35,8 @@ export function UpvoterList({
             </Link>
           )),
       )}
-      {queryResult.isFetchingNextPage && (
-        <UpvoterListPlaceholder placeholderAmount={1} />
-      )}
-      <div
-        className="absolute bottom-0 left-0 w-px h-px opacity-0 pointer-events-none"
-        ref={infiniteScrollRef}
-      />
-    </div>
+    </InfiniteScrolling>
   );
 }
+
+export default UpvoterList;
