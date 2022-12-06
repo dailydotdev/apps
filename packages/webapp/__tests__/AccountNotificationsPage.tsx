bd0b1ed6f131821ee7e39ac2d53d77ecf95527cb
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-12-07 02:27:42 +0800

    test: account notification preferences page

--- packages/webapp/__tests__/AccountNotificationsPage.tsx
+++ packages/webapp/__tests__/AccountNotificationsPage.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import nock from 'nock';
 import { LoggedUser } from '@dailydotdev/shared/src/lib/user';
 import loggedUser from '@dailydotdev/shared/__tests__/fixture/loggedUser';
-import { mockSettingsFlow } from '@dailydotdev/shared/__tests__/fixture/auth';
 import { render, RenderResult, screen } from '@testing-library/preact';
 import { AuthContextProvider } from '@dailydotdev/shared/src/contexts/AuthContext';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { UPDATE_USER_PROFILE_MUTATION } from '@dailydotdev/shared/src/graphql/users';
-import { mockGraphQL } from '@dailydotdev/shared/__tests__/helpers/graphql';
-import { act } from 'preact/test-utils';
+import {
+  MockedGraphQLResponse,
+  mockGraphQL,
+} from '@dailydotdev/shared/__tests__/helpers/graphql';
+import { waitForNock } from '@dailydotdev/shared/__tests__/helpers/utilities';
+import {
+  DeviceNotificationPreference,
+  DevicePreferenceData,
+  DEVICE_PREFERENCE_QUERY,
+  GeneralNotificationPreference,
+  GeneralPreferenceData,
+  GENERAL_PREFERENCE_QUERY,
+  UPDATE_DEVICE_PREFERENCE_MUTATION,
+  UPDATE_GENERAL_PREFERENCE_MUTATION,
+} from '@dailydotdev/shared/src/graphql/notifications';
+import { Visit } from '@dailydotdev/shared/src/lib/boot';
 import ProfileNotificationsPage from '../pages/account/notifications';
 
 jest.mock('next/router', () => ({
@@ -18,8 +31,13 @@ jest.mock('next/router', () => ({
   },
 }));
 
+let client: QueryClient;
+
 beforeEach(() => {
   jest.clearAllMocks();
+  jest.restoreAllMocks();
+  nock.cleanAll();
+  client = new QueryClient();
 });
 
 const defaultLoggedUser: LoggedUser = {
@@ -32,12 +50,48 @@ const defaultLoggedUser: LoggedUser = {
   pushNotification: true,
   newActivityEmail: true,
 };
+const defaultVisit: Visit = {
+  sessionId: 'sample session id',
+  visitId: 'sample visit id',
+};
 
 const updateUser = jest.fn();
 
-const renderComponent = (): RenderResult => {
-  const client = new QueryClient();
-  mockSettingsFlow();
+const defaultDeviceData: Pick<
+  DeviceNotificationPreference,
+  'pushNotification'
+> = {
+  pushNotification: false,
+};
+const createDevicePreferenceMock = (
+  data: Pick<
+    DeviceNotificationPreference,
+    'pushNotification'
+  > = defaultDeviceData,
+  deviceId = defaultVisit.sessionId,
+): MockedGraphQLResponse<DevicePreferenceData> => ({
+  request: { query: DEVICE_PREFERENCE_QUERY, variables: { deviceId } },
+  result: { data: { preference: data } },
+});
+
+const defaultGeneralData: GeneralNotificationPreference = {
+  marketingEmail: false,
+  notificationEmail: false,
+};
+const createGeneralPreferenceMock = (
+  data: GeneralNotificationPreference = defaultGeneralData,
+): MockedGraphQLResponse<GeneralPreferenceData> => ({
+  request: { query: GENERAL_PREFERENCE_QUERY },
+  result: { data: { preference: data } },
+});
+
+const renderComponent = (
+  mocks: MockedGraphQLResponse[] = [
+    createDevicePreferenceMock(),
+    createGeneralPreferenceMock(),
+  ],
+): RenderResult => {
+  mocks.forEach(mockGraphQL);
 
   return render(
     <QueryClientProvider client={client}>
@@ -45,6 +99,7 @@ const renderComponent = (): RenderResult => {
         user={defaultLoggedUser}
         updateUser={updateUser}
         getRedirectUri={jest.fn()}
+        visit={defaultVisit}
         tokenRefreshed
       >
         <ProfileNotificationsPage />
@@ -53,32 +108,61 @@ const renderComponent = (): RenderResult => {
   );
 };
 
+it('should accurately show device preference', async () => {
+  renderComponent([
+    createDevicePreferenceMock({ pushNotification: true }),
+    createGeneralPreferenceMock(),
+  ]);
+  await waitForNock();
+  const subscription = await screen.findByTestId('push_notification-switch');
+  expect(subscription).toHaveAttribute('data-testValue', 'true');
+});
+
 it('should change user push notification', async () => {
+  renderComponent();
+  await waitForNock();
   let mutationCalled = false;
-  const pushNotification = false;
+  const pushNotification = true;
   mockGraphQL({
     request: {
-      query: UPDATE_USER_PROFILE_MUTATION,
-      variables: { data: { pushNotification } },
+      query: UPDATE_DEVICE_PREFERENCE_MUTATION,
+      variables: {
+        data: { pushNotification },
+        deviceId: defaultVisit.sessionId,
+      },
     },
     result: () => {
       mutationCalled = true;
       return { data: { id: '' } };
     },
   });
-  renderComponent();
+  mockGraphQL(createDevicePreferenceMock({ pushNotification: true }));
   const subscription = await screen.findByTestId('push_notification-switch');
+  expect(subscription).not.toHaveAttribute('data-testValue');
   await subscription.click();
-  await act(() => new Promise((resolve) => setTimeout(resolve, 100)));
+  await waitForNock();
+  const newSubscription = await screen.findByTestId('push_notification-switch');
+  expect(newSubscription).toHaveAttribute('data-testValue', 'true');
   expect(mutationCalled).toBeTruthy();
 });
 
 it('should change user all email subscription', async () => {
+  renderComponent([
+    createDevicePreferenceMock(),
+    createGeneralPreferenceMock({
+      marketingEmail: false,
+      notificationEmail: true,
+    }),
+  ]);
+  await waitForNock();
   let mutationCalled = false;
-  const data = { acceptedMarketing: false, newActivityEmail: false };
+  const data: GeneralNotificationPreference = {
+    marketingEmail: true,
+    notificationEmail: true,
+  };
   mockGraphQL({
     request: {
-      query: UPDATE_USER_PROFILE_MUTATION,
+      query: UPDATE_GENERAL_PREFERENCE_MUTATION,
       variables: { data },
     },
     result: () => {
@@ -86,51 +170,84 @@ it('should change user all email subscription', async () => {
       return { data: { id: '' } };
     },
   });
-  renderComponent();
+  mockGraphQL(
+    createGeneralPreferenceMock({
+      marketingEmail: true,
+      notificationEmail: true,
+    }),
+  );
   const subscription = await screen.findByTestId('email_notification-switch');
+  expect(subscription).not.toHaveAttribute('data-testValue');
   await subscription.click();
-  await act(() => new Promise((resolve) => setTimeout(resolve, 100)));
+  await waitForNock();
+  const newSubscription = await screen.findByTestId(
+    'email_notification-switch',
+  );
+  expect(newSubscription).toHaveAttribute('data-testValue', 'true');
+  const marketingSubscription = await screen.findByTestId('marketing-switch');
+  expect(marketingSubscription).toHaveAttribute('data-testValue', 'true');
+  const notificationEmail = await screen.findByTestId('new_activity-switch');
+  expect(notificationEmail).toHaveAttribute('data-testValue', 'true');
   expect(mutationCalled).toBeTruthy();
 });
 
+// when you comment all tests this work
 it('should change user email marketing subscription', async () => {
+  renderComponent();
+  await waitForNock();
   let mutationCalled = false;
-  const acceptedMarketing = false;
+  const marketingEmail = true;
   mockGraphQL({
     request: {
-      query: UPDATE_USER_PROFILE_MUTATION,
-      variables: { data: { acceptedMarketing } },
+      query: UPDATE_GENERAL_PREFERENCE_MUTATION,
+      variables: { data: { marketingEmail } },
     },
     result: () => {
       mutationCalled = true;
       return { data: { id: '' } };
     },
   });
-  renderComponent();
-  const text = 'Marketing updates';
-  const subscription = await screen.findByText(text);
+  mockGraphQL(
+    createGeneralPreferenceMock({
+      marketingEmail,
+      notificationEmail: true,
+    }),
+  );
+  const subscription = await screen.findByTestId('marketing-switch');
+  expect(subscription).not.toHaveAttribute('data-testValue');
   await subscription.click();
-  await act(() => new Promise((resolve) => setTimeout(resolve, 100)));
+  await waitForNock();
+  const newSubscription = await screen.findByTestId('marketing-switch');
+  expect(newSubscription).toHaveAttribute('data-testValue', 'true');
   expect(mutationCalled).toBeTruthy();
 });
 
-it('should change user email subscription', async () => {
+it('should change user notification email subscription', async () => {
+  renderComponent();
+  await waitForNock();
   let mutationCalled = false;
-  const newActivityEmail = false;
+  const notificationEmail = true;
   mockGraphQL({
     request: {
-      query: UPDATE_USER_PROFILE_MUTATION,
-      variables: { data: { newActivityEmail } },
+      query: UPDATE_GENERAL_PREFERENCE_MUTATION,
+      variables: { data: { notificationEmail } },
     },
     result: () => {
       mutationCalled = true;
       return { data: { id: '' } };
     },
   });
-  renderComponent();
-  const text = 'New activity notifications (mentions, replies, etc.)';
-  const subscription = await screen.findByText(text);
+  mockGraphQL(
+    createGeneralPreferenceMock({
+      marketingEmail: false,
+      notificationEmail,
+    }),
+  );
+  const subscription = await screen.findByTestId('new_activity-switch');
+  expect(subscription).not.toHaveAttribute('data-testValue');
   await subscription.click();
-  await act(() => new Promise((resolve) => setTimeout(resolve, 100)));
+  await waitForNock();
+  const newSubscription = await screen.findByTestId('new_activity-switch');
+  expect(newSubscription).toHaveAttribute('data-testValue', 'true');
   expect(mutationCalled).toBeTruthy();
 });
