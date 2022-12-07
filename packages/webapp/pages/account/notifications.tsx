33310d38ad9f25045529a71ebad22477c290f1dd
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-12-07 15:38:17 +0800

    refactor: reusable hook for preference related api

--- packages/webapp/pages/account/notifications.tsx
+++ packages/webapp/pages/account/notifications.tsx
@@ -1,22 +1,9 @@
 import { Checkbox } from '@dailydotdev/shared/src/components/fields/Checkbox';
 import { Switch } from '@dailydotdev/shared/src/components/fields/Switch';
-import AuthContext from '@dailydotdev/shared/src/contexts/AuthContext';
-import React, { ReactElement, useContext, useState } from 'react';
-import { useQuery, useMutation } from 'react-query';
-import request from 'graphql-request';
+import React, { ReactElement, useState } from 'react';
 import { cloudinary } from '@dailydotdev/shared/src/lib/image';
 import CloseButton from '@dailydotdev/shared/src/components/CloseButton';
-import { apiUrl } from '@dailydotdev/shared/src/lib/config';
-import {
-  DeviceNotificationPreference,
-  DevicePreferenceData,
-  DEVICE_PREFERENCE_QUERY,
-  GeneralNotificationPreference,
-  GeneralPreferenceData,
-  GENERAL_PREFERENCE_QUERY,
-  UPDATE_DEVICE_PREFERENCE_MUTATION,
-  UPDATE_GENERAL_PREFERENCE_MUTATION,
-} from '@dailydotdev/shared/src/graphql/notifications';
+import { useNotificationPreferences } from '@dailydotdev/shared/src/hooks/useNotificationPreferences';
 import Pointer, {
   PointerColor,
 } from '@dailydotdev/shared/src/components/alert/Pointer';
@@ -28,49 +15,18 @@ import AccountContentSection from '../../components/layouts/AccountLayout/Accoun
 const ALERT_PUSH_KEY = 'alert_push_key';
 
 const AccountNotificationsPage = (): ReactElement => {
-  const { user, visit } = useContext(AuthContext);
   const [isAlertShown, setIsAlertShown] = usePersistentContext(
     ALERT_PUSH_KEY,
     true,
   );
-  const { data: generalData, refetch: refetchGeneralData } = useQuery(
-    ['general_preferences', user.id],
-    () =>
-      request<GeneralPreferenceData>(
-        `${apiUrl}/graphql`,
-        GENERAL_PREFERENCE_QUERY,
-      ),
-  );
-  const { mutateAsync: updateGeneralPreference } = useMutation(
-    (data: GeneralNotificationPreference) =>
-      request(`${apiUrl}/graphql`, UPDATE_GENERAL_PREFERENCE_MUTATION, {
-        data,
-      }),
-    { onSuccess: refetchGeneralData },
-  );
-
-  const { data: deviceData, refetch: refetchDeviceData } = useQuery(
-    ['device_preferences', visit.sessionId],
-    () =>
-      request<DevicePreferenceData>(
-        `${apiUrl}/graphql`,
-        DEVICE_PREFERENCE_QUERY,
-        { deviceId: visit.sessionId },
-      ),
-  );
-  const { mutateAsync: updateDevicePreference } = useMutation(
-    (data: Omit<DeviceNotificationPreference, 'deviceId' | 'description'>) =>
-      request(`${apiUrl}/graphql`, UPDATE_DEVICE_PREFERENCE_MUTATION, {
-        data,
-        deviceId: visit.sessionId,
-      }),
-    { onSuccess: refetchDeviceData },
-  );
-
-  const { preference: generalPreference } = generalData ?? {};
-  const { marketingEmail, notificationEmail } = generalPreference ?? {};
-  const { preference: devicePreference } = deviceData ?? {};
-  const { pushNotification } = devicePreference ?? {};
+  const {
+    generalPreference,
+    devicePreference,
+    updateGeneralPreference,
+    updateDevicePreference,
+  } = useNotificationPreferences();
+  const { marketingEmail, notificationEmail } = generalPreference;
+  const { pushNotification } = devicePreference;
   const [emailNotification, setEmailNotification] = useState(
     marketingEmail || notificationEmail,
   );
