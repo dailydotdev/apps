6e340e24ebb5432244d6be2a4ff86b0d66775944
Chris Bongers <chrisbongers@gmail.com>
2022-12-08 10:09:39 +0200

    fix: refactor to new modal

--- packages/shared/src/components/modals/PostModal.tsx
+++ packages/shared/src/components/modals/PostModal.tsx
@@ -5,10 +5,10 @@ import React, {
   useEffect,
   useState,
 } from 'react';
-import Modal from 'react-modal';
 import classNames from 'classnames';
 import { useQuery } from 'react-query';
 import request from 'graphql-request';
+import { Modal } from './common/Modal';
 import { ModalProps } from './StyledModal';
 import { useHideOnModal } from '../../hooks/useHideOnModal';
 import { useResetScrollForResponsiveModal } from '../../hooks/useResetScrollForResponsiveModal';
@@ -99,12 +99,14 @@ export default function PostModal({
 
   return (
     <Modal
+      size={Modal.Size.Large}
+      kind={Modal.Kind.FlexibleTop}
       {...props}
       portalClassName={styles.postModal}
       className={classNames(className, 'post-modal focus:outline-none')}
       overlayClassName="post-modal-overlay bg-overlay-quaternary-onion"
-      id="post-modal"
       onRequestClose={onRequestClose}
+      id="post-modal"
     >
       <PostContent
         position={position}
