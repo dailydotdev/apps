332b408818690040978891bfbde0060feeff56c1
Eliz Kilic <elizkilic@gmail.com>
2023-01-04 11:32:57 +0300

    feat: use stepped modal in create squad

--- packages/shared/src/components/modals/NewSquadModal.tsx
+++ packages/shared/src/components/modals/NewSquadModal.tsx
@@ -1,7 +1,5 @@
 import React, { ReactElement, useState } from 'react';
 import { Modal } from './common/Modal';
-import ArrowIcon from '../icons/Arrow';
-import { Button } from '../buttons/Button';
 import { Squad } from '../../graphql/squads';
 import { SquadDetails } from '../squads/Details';
 import { SquadSelectArticle } from '../squads/SelectArticle';
@@ -14,6 +12,7 @@ import {
 } from '../squads/utils';
 import { SquadComment } from '../squads/Comment';
 import { SquadsConfirmation } from './SquadsConfirmationModal';
+import { ModalStep } from './common/types';
 
 export const modalStateOrder = [
   ModalState.Details,
@@ -28,41 +27,51 @@ export type NewSquadModalProps = {
   isOpen: boolean;
 };
 
+let activeView;
 function NewSquadModal({
   onPreviousState,
   onRequestClose,
   isOpen,
 }: NewSquadModalProps): ReactElement {
-  const [modalState, setModalState] = useState(ModalState.Details);
   const [squad, setSquad] = useState<Squad>();
   const [showConfirmation, setShowConfirmation] = useState(false);
-
-  const changeState = (prev: boolean) => {
-    const index = modalStateOrder.findIndex((state) => state === modalState);
-    const changeTo = modalStateOrder[index + (prev ? -1 : 1)];
-    if (changeTo) setModalState(changeTo);
-    else if (prev) onPreviousState();
-    else onRequestClose();
-  };
-  const stepperWidth = () => {
-    const index =
-      modalStateOrder.findIndex((state) => state === modalState) + 1;
-    return (index / modalStateOrder.length) * 100;
-  };
   const [form, setForm] = useState<Partial<SquadForm>>({});
   const onNext = async (squadForm?: SquadForm) => {
-    const currentState = modalState;
-    changeState(false);
     if (squadForm) setForm(squadForm);
-    if (ModalState.WriteComment !== currentState) return;
+    if (!squadForm.commentary) return;
     const newSquad = await createSquad(squadForm);
     setSquad(newSquad);
   };
   const stateProps: SquadStateProps = {
-    modalState,
     form,
     setForm,
     onNext,
+  };
+
+  const modalSteps: ModalStep[] = [
+    {
+      key: ModalState.Details,
+      title: (
+        <>
+          <Modal.Header.StepsButton onClick={onPreviousState} />
+          <Modal.Header.Subtitle>{ModalState.Details}</Modal.Header.Subtitle>
+        </>
+      ),
+    },
+    {
+      key: ModalState.SelectArticle,
+    },
+    {
+      key: ModalState.WriteComment,
+    },
+    {
+      key: ModalState.Ready,
+      title: <Modal.Header.Title>{ModalState.Ready}</Modal.Header.Title>,
+    },
+  ];
+  const handleClose = () => {
+    if (activeView === ModalState.Ready) onRequestClose();
+    else setShowConfirmation(true);
   };
   return (
     <>
@@ -70,23 +79,13 @@ function NewSquadModal({
         isOpen={isOpen}
         kind={Modal.Kind.FixedCenter}
         size={Modal.Size.Small}
-        onRequestClose={() => setShowConfirmation(true)}
+        onRequestClose={handleClose}
+        onViewChange={(view) => {
+          activeView = view;
+        }}
+        steps={modalSteps}
       >
-        {modalState === ModalState.Ready && <Modal.Header title={modalState} />}
-        {modalState !== ModalState.Ready && (
-          <Modal.Header>
-            <Button
-              icon={<ArrowIcon className="-rotate-90" />}
-              className="flex justify-center items-center mr-2 -ml-2 btn btn-tertiary iconOnly"
-              onClick={() => changeState(true)}
-            />
-            <h3 className="font-bold typo-callout">{modalState}</h3>
-          </Modal.Header>
-        )}
-        <div
-          className="absolute left-0 h-1 top-[3.3rem] bg-theme-color-cabbage transition-[width]"
-          style={{ width: `${stepperWidth()}%` }}
-        />
+        <Modal.Header.Steps />
         <SquadDetails {...stateProps} />
         <SquadSelectArticle {...stateProps} />
         <SquadComment {...stateProps} />
