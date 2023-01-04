fa1c35320d2616b506f60e0fc135bf332a2a82dc
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-01-04 20:03:38 +0800

    refactor: feed filters to utilize generic modal

--- packages/shared/src/components/filters/FeedFilters.tsx
+++ packages/shared/src/components/filters/FeedFilters.tsx
@@ -1,7 +1,5 @@
 import React, { ReactElement, useState } from 'react';
-import classNames from 'classnames';
-import { ModalProps, StyledModal } from '../modals/StyledModal';
-import SidebarList from '../sidebar/SidebarList';
+import { ModalProps } from '../modals/StyledModal';
 import HashtagIcon from '../icons/Hashtag';
 import FilterIcon from '../icons/Filter';
 import BlockIcon from '../icons/Block';
@@ -9,13 +7,11 @@ import TagsFilter from './TagsFilter';
 import { TagCategoryLayout } from './TagCategoryDropdown';
 import AdvancedSettingsFilter from './AdvancedSettings';
 import BlockedFilter from './BlockedFilter';
-import CloseButton from '../CloseButton';
-import styles from './FeedFilters.module.css';
 import ArrowIcon from '../icons/Arrow';
 import { Button } from '../buttons/Button';
 import { UnblockItem } from './FilterMenu';
 import UnblockModal from '../modals/UnblockModal';
-// import { Modal } from '../modals/common/Modal';
+import { Modal } from '../modals/common/Modal';
 
 enum FilterMenuTitle {
   Tags = 'Manage tags',
@@ -25,135 +21,59 @@ enum FilterMenuTitle {
 
 export const filterAlertMessage = 'Edit your personal feed preferences here';
 
-export default function FeedFilters({
-  isOpen,
-  onRequestClose,
-  ...props
-}: ModalProps): ReactElement {
+export default function FeedFilters(props: ModalProps): ReactElement {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [display, setDisplay] = useState<string>(FilterMenuTitle.Tags);
   const [unblockItem, setUnblockItem] = useState<UnblockItem>();
-  const items = [
+  const tabs = [
     {
       title: FilterMenuTitle.Tags,
-      icon: <HashtagIcon />,
-      component: <TagsFilter tagCategoryLayout={TagCategoryLayout.Settings} />,
+      options: { icon: <HashtagIcon /> },
     },
     {
       title: FilterMenuTitle.Advanced,
-      icon: <FilterIcon />,
-      component: <AdvancedSettingsFilter />,
+      options: { icon: <FilterIcon /> },
     },
     {
       title: FilterMenuTitle.Blocked,
-      icon: <BlockIcon />,
-      component: <BlockedFilter onUnblockItem={setUnblockItem} />,
+      options: { icon: <BlockIcon /> },
     },
   ];
-  const onNavClick = (active: string) => {
-    setDisplay(active);
-    if (isNavOpen) {
-      setIsNavOpen(false);
-    }
-  };
-
-  //   const tabs = [
-  //     {
-  //       title: FilterMenuTitle.Tags,
-  //       options: { icon: <HashtagIcon /> },
-  //     },
-  //     {
-  //       title: FilterMenuTitle.Advanced,
-  //       options: { icon: <FilterIcon /> },
-  //     },
-  //     {
-  //       title: FilterMenuTitle.Blocked,
-  //       options: { icon: <BlockIcon /> },
-  //     },
-  //   ];
-  //   return (
-  //     <Modal
-  //       className="h-full mb-8 flex-1"
-  //       isOpen
-  //       kind={Modal.Kind.FlexibleTop}
-  //       size={Modal.Size.Large}
-  //       onRequestClose={onRequestClose}
-  //       tabs={tabs}
-  //     >
-  //       <Modal.Sidebar>
-  //         <Modal.Sidebar.List
-  //           className='w-74'
-  //           title="Feed filters"
-  //           isNavOpen={isNavOpen}
-  //           onViewChange={onNavClick}
-  //           setIsNavOpen={setIsNavOpen}
-  //         />
-  //         <Modal.Sidebar.Inner>
-  //           <Modal.Header>
-  //             <Button
-  //               buttonSize="small"
-  //               className="flex tablet:hidden mr-2 -rotate-90"
-  //               icon={<ArrowIcon />}
-  //               onClick={() => setIsNavOpen(true)}
-  //             />
-  //           </Modal.Header>
-  //           <Modal.Body view={FilterMenuTitle.Tags}>
-  //             <TagsFilter tagCategoryLayout={TagCategoryLayout.Settings} />
-  //           </Modal.Body>
-  //           <Modal.Body view={FilterMenuTitle.Advanced}>
-  //             <AdvancedSettingsFilter />
-  //           </Modal.Body>
-  //           <Modal.Body view={FilterMenuTitle.Blocked}>
-  //             <BlockedFilter onUnblockItem={setUnblockItem} />
-  //           </Modal.Body>
-  //         </Modal.Sidebar.Inner>
-  //       </Modal.Sidebar>
-  //     </Modal>
-  //   );
-
   return (
-    <StyledModal
+    <Modal
       {...props}
-      overlayClassName={classNames('py-12', styles.feedFiltersOverlay)}
-      contentClassName={classNames(
-        'w-full h-full flex flex-row',
-        isNavOpen && 'opened-nav',
-        styles.feedFilters,
-      )}
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
+      className="overflow-auto flex-1 h-full"
+      kind={Modal.Kind.FixedCenter}
+      size={Modal.Size.Large}
+      tabs={tabs}
     >
-      <div className="flex overflow-hidden relative flex-col tablet:flex-row flex-1 bg-theme-bg-inherit">
-        <SidebarList
-          className="z-1"
-          active={display}
+      <Modal.Sidebar>
+        <Modal.Sidebar.List
+          className="w-74"
           title="Feed filters"
-          onItemClick={onNavClick}
-          items={items}
-          isOpen={isNavOpen}
-          onClose={() => setIsNavOpen(false)}
+          isNavOpen={isNavOpen}
+          setIsNavOpen={setIsNavOpen}
+          onViewChange={() => setIsNavOpen(false)}
         />
-        <div className="flex flex-col flex-1 border-l border-theme-divider-tertiary">
-          <h2 className="flex sticky flex-row items-center py-4 px-6 w-full font-bold border-b border-theme-divider-tertiary typo-title3">
+        <Modal.Sidebar.Inner>
+          <Modal.Header>
             <Button
               buttonSize="small"
               className="flex tablet:hidden mr-2 -rotate-90"
               icon={<ArrowIcon />}
               onClick={() => setIsNavOpen(true)}
             />
-            {display}
-            <CloseButton
-              style={{ position: 'absolute' }}
-              className="right-2"
-              buttonSize="medium"
-              onClick={onRequestClose}
-            />
-          </h2>
-          <div className="flex overflow-auto flex-col py-6 h-full max-h-[calc(100vh-4.125rem)] tablet:max-h-[calc(100%-3.875rem)]">
-            {items.find(({ title }) => title === display)?.component}
-          </div>
-        </div>
-      </div>
+          </Modal.Header>
+          <Modal.Body view={FilterMenuTitle.Tags}>
+            <TagsFilter tagCategoryLayout={TagCategoryLayout.Settings} />
+          </Modal.Body>
+          <Modal.Body view={FilterMenuTitle.Advanced}>
+            <AdvancedSettingsFilter />
+          </Modal.Body>
+          <Modal.Body view={FilterMenuTitle.Blocked}>
+            <BlockedFilter onUnblockItem={setUnblockItem} />
+          </Modal.Body>
+        </Modal.Sidebar.Inner>
+      </Modal.Sidebar>
       {unblockItem && (
         <UnblockModal
           item={unblockItem}
@@ -162,6 +82,6 @@ export default function FeedFilters({
           onRequestClose={() => setUnblockItem(null)}
         />
       )}
-    </StyledModal>
+    </Modal>
   );
 }
