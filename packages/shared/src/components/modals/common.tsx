1e2a5fbf77dd772bb66688bfcc5734524ec7ddec
Eliz Kilic <elizkilic@gmail.com>
2023-01-11 02:08:11 +0300

    better type safety for modals

--- packages/shared/src/components/modals/common.tsx
+++ packages/shared/src/components/modals/common.tsx
@@ -22,10 +22,35 @@ export const modals = {
   [LazyModal.SquadsBeta]: SquadsBetaModal,
 };
 
+type GetComponentProps<T> = T extends
+  | React.ComponentType<infer P>
+  | React.Component<infer P>
+  ? P
+  : never;
+
 type ModalsType = typeof modals;
+
+type LazyModalComponentType<K extends LazyModal> = Omit<
+  GetComponentProps<ModalsType[K]>,
+  'isOpen' | 'onRequestClose'
+>;
+
+type RequiredKeys<T> = {
+  [K in keyof T]: Record<string, never> extends { [P in K]: T[K] } ? never : K;
+}[keyof T];
+
+type NonOptional<T> = Pick<T, RequiredKeys<T>>;
+
 export type LazyModalType = {
-  [K in keyof ModalsType]: {
-    type: K;
-    props?: Partial<React.ComponentProps<ModalsType[K]>>;
-  };
+  [K in keyof ModalsType]: NonOptional<
+    LazyModalComponentType<K>
+  > extends Record<string, never>
+    ? {
+        type: K;
+        props?: LazyModalComponentType<K>;
+      }
+    : {
+        type: K;
+        props: LazyModalComponentType<K>;
+      };
 }[keyof ModalsType];
