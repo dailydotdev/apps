49c0043475b49edf9d02df6f3fdad70f2245f5ab
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-01-11 18:46:14 +0800

    fix: top margin

--- packages/shared/src/components/modals/common/Modal.tsx
+++ packages/shared/src/components/modals/common/Modal.tsx
@@ -51,7 +51,7 @@ const modalKindAndSizeToClassName: Partial<
 > = {
   [ModalKind.FlexibleTop]: {
     [ModalSize.Medium]:
-      'mobileL:max-h-[calc(100vh-7.5rem)] max-h-[calc(100vh-2.5rem)] h-auto min-h-[25rem]',
+      'mobileL:max-h-[calc(100vh-7.5rem)] max-h-[calc(100vh-2.5rem)] h-auto min-h-[25rem] mt-6',
     [ModalSize.Large]: 'laptop:mt-14 laptop:mb-10',
   },
 };
