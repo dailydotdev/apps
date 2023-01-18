0b3dcf1052212365a2ad75b9cd3175dea5e8bf62
Eliz <elizkilic@gmail.com>
2023-01-18 12:13:39 +0300

    fix: hover bg

--- packages/shared/src/components/cards/NewSquadPostCard.tsx
+++ packages/shared/src/components/cards/NewSquadPostCard.tsx
@@ -11,13 +11,14 @@ export default function NewSquadPostCard({
 }: AdCardProps): ReactElement {
   return (
     <Button
-      className="flex flex-col justify-center items-center h-full rounded-14 border border-theme-color-cabbage"
+      className="flex flex-col justify-center items-center h-full rounded-14 border border-theme-color-cabbage hover:shadow-2-cabbage"
       onClick={onClick}
     >
       <Button
         className="mx-auto mb-4 btn-primary-cabbage"
         buttonSize="xlarge"
         icon={<PlusIcon />}
+        tag="span"
         iconOnly
       />
       <span className="typo-title1">Create new post</span>
