66f8132db366a5f0e852de0e5a073bbaaac37ecc
Eliz <elizkilic@gmail.com>
2023-01-18 17:08:58 +0300

    feat: squads notifications strip

--- packages/shared/src/components/squads/SquadPageHeader.tsx
+++ packages/shared/src/components/squads/SquadPageHeader.tsx
@@ -5,6 +5,8 @@ import FeedbackIcon from '../icons/Feedback';
 import { SquadHeaderBar } from './SquadHeaderBar';
 import { SquadImage } from './SquadImage';
 import { squadFeedback } from '../../lib/constants';
+import EnableNotification from '../notifications/EnableNotification';
+import { NotificationPromptSource } from '../../hooks/useEnableNotification';
 
 type SquadPageHeaderProps = {
   userId: string;
@@ -18,7 +20,7 @@ export function SquadPageHeader({
   members,
 }: SquadPageHeaderProps): ReactElement {
   return (
-    <section className="flex relative flex-col mobileL:items-center w-full laptop:border-b min-h-20 border-theme-divider-tertiary">
+    <section className="flex relative flex-col mobileL:items-center w-full laptop:border-b min-h-20 border-theme-divider-tertiary pb-4 mobileL:pb-8 mobileL:mb-8">
       <Button
         tag="a"
         target="_blank"
@@ -47,11 +49,12 @@ export function SquadPageHeader({
         {squad.description}
       </h4>
       <SquadHeaderBar
-        className="mb-8 mobileL:mb-12"
+        className='mb-4'
         squad={squad}
         members={members}
         memberCount={squad.membersCount}
       />
+      <EnableNotification contentName={squad.name} source={NotificationPromptSource.SquadPage}/>
     </section>
   );
 }
