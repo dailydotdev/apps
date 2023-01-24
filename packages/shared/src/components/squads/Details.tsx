135215bc6eaf9a297f5eb400436a8148423252e0
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-01-24 16:11:04 +0800

    refactor: usage of new query to check handle availability (#1507)
    * refactor: usage of new query to check handle availability
    
    * refactor: taken to exists

--- packages/shared/src/components/squads/Details.tsx
+++ packages/shared/src/components/squads/Details.tsx
@@ -11,7 +11,7 @@ import { cloudinary } from '../../lib/image';
 import CameraIcon from '../icons/Camera';
 import { formToJson } from '../../lib/form';
 import { Modal } from '../modals/common/Modal';
-import { checkSourceExists, SquadForm } from '../../graphql/squads';
+import { checkExistingHandle, SquadForm } from '../../graphql/squads';
 
 const squadImageId = 'squad_image_file';
 export function SquadDetails({
@@ -33,7 +33,7 @@ export function SquadDetails({
       return;
     }
     const handleExists = createMode
-      ? await checkSourceExists(formJson.handle)
+      ? await checkExistingHandle(formJson.handle)
       : false;
     setHandleValid(!handleExists);
     if (!handleExists) {
