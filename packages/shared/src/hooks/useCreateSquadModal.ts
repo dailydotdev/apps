d2f7bcb618969f0c95b72987c2b463d475c353b4
Chris Bongers <chrisbongers@gmail.com>
2023-01-25 11:00:04 +0100

    feat: introduce squad onboarding

--- packages/shared/src/hooks/useCreateSquadModal.ts
+++ packages/shared/src/hooks/useCreateSquadModal.ts
@@ -2,16 +2,21 @@ import { useRouter } from 'next/router';
 import { useEffect, useMemo, useRef } from 'react';
 import { LazyModal } from '../components/modals/common/types';
 import { useLazyModal } from './useLazyModal';
+import usePersistentContext from './usePersistentContext';
 
 interface UseCreateSquadModal {
   openNewSquadModal: () => void;
   openSquadBetaModal: () => void;
 }
 
+const SQUAD_ONBOARDING = 'hasTriedSquadOnboarding';
+
 export const useCreateSquadModal = (): UseCreateSquadModal => {
   const router = useRouter();
   const { openModal } = useLazyModal();
   const previousRef = useRef(null);
+  const [hasTriedOnboarding, setHasTriedOnboarding, isLoaded] =
+    usePersistentContext<boolean>(SQUAD_ONBOARDING, false);
 
   const openNewSquadModal = () =>
     openModal({
@@ -40,6 +45,15 @@ export const useCreateSquadModal = (): UseCreateSquadModal => {
     openSquadBetaModal();
     router.replace(origin + pathname);
   }, [router.pathname]);
+
+  useEffect(() => {
+    if (!isLoaded || hasTriedOnboarding) {
+      return;
+    }
+
+    openSquadBetaModal();
+    setHasTriedOnboarding(true);
+  }, [hasTriedOnboarding, isLoaded]);
 
   return useMemo(
     () => ({ openNewSquadModal, openSquadBetaModal }),
