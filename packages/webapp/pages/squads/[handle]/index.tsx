9761558a7cd930ba7bf7404ee36f61d05b596bdd
Eliz <elizkilic@gmail.com>
2023-01-26 15:45:09 +0300

    fix path and boolean

--- packages/webapp/pages/squads/[handle]/index.tsx
+++ packages/webapp/pages/squads/[handle]/index.tsx
@@ -92,7 +92,7 @@ const SquadPage = ({ handle }: SourcePageProps): ReactElement => {
           ]}
           query={SOURCE_FEED_QUERY}
           variables={queryVariables}
-          forceCardMode={true}
+          forceCardMode
         />
       </FeedPage>
     </ProtectedPage>
