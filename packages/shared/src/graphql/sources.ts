226e0f37a2c367e7088fab9af247d43eb0153faa
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-01-27 19:10:26 +0800

    refactor: legacy implementations

--- packages/shared/src/graphql/sources.ts
+++ packages/shared/src/graphql/sources.ts
@@ -15,17 +15,6 @@ export interface Source {
   permalink: string;
 }
 
-const SOURCE_PATH: Record<SourceType, string> = {
-  machine: 'source',
-  squad: 'squads',
-};
-
-export const getSourcePermalink = (id: string, type: SourceType): string => {
-  const path = SOURCE_PATH[type];
-
-  return `${process.env.NEXT_PUBLIC_WEBAPP_URL}${path}/${id}`;
-};
-
 export type SourceData = { source: Source };
 
 export const SOURCE_BASE_FRAGMENT = gql`
