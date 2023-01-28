7e3bef56bd11e2e340cb7b19749e0603e32253d7
Chris Bongers <chrisbongers@gmail.com>
2023-01-28 09:45:42 +0100

    fix: squad member list scroll

--- packages/shared/src/graphql/squads.ts
+++ packages/shared/src/graphql/squads.ts
@@ -191,8 +191,12 @@ export const SQUAD_HANDE_AVAILABILITY_QUERY = gql`
 `;
 
 export const SQUAD_MEMBERS_QUERY = gql`
-  query Source($id: ID!) {
-    sourceMembers(first: 5, sourceId: $id) {
+  query Source($id: ID!, $after: String, $first: Int) {
+    sourceMembers(sourceId: $id, after: $after, first: $first) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
       edges {
         node {
           role
@@ -271,7 +275,7 @@ export async function getSquadMembers(id: string): Promise<SquadMember[]> {
   const res = await request<SquadEdgesData>(
     `${apiUrl}/graphql`,
     SQUAD_MEMBERS_QUERY,
-    { id },
+    { id, first: 5 },
   );
   return res.sourceMembers.edges?.map((edge) => edge.node);
 }
