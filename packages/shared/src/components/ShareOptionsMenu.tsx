dfa91f03bae01dc7ddd236abbec430c368290260
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-02-07 15:31:06 +0800

    fix: share options menu icon size consistency (#1589)

--- packages/shared/src/components/ShareOptionsMenu.tsx
+++ packages/shared/src/components/ShareOptionsMenu.tsx
@@ -104,7 +104,7 @@ export default function ShareOptionsMenu({
         squad.active &&
         shareOptions.push({
           icon: squad.image ? (
-            <SquadImage className="mr-2.5 w-5 h-5 text-2xl" {...squad} />
+            <SquadImage className="mr-2.5 w-6 h-6" {...squad} />
           ) : (
             <MenuIcon Icon={DefaultSquadIcon} />
           ),
