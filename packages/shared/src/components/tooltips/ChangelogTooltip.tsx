50520cde790b60c4d4980b42db90c31097128e3e
capJavert <ante@kickass.website>
2023-02-10 17:51:13 +0100

    feat: fetch post and show basic info

--- packages/shared/src/components/tooltips/ChangelogTooltip.tsx
+++ packages/shared/src/components/tooltips/ChangelogTooltip.tsx
@@ -1,32 +1,28 @@
-import React, { MutableRefObject, ReactElement } from 'react';
+import React, {
+  MutableRefObject,
+  ReactElement,
+  useCallback,
+  useContext,
+} from 'react';
+import { useQuery } from 'react-query';
+import request from 'graphql-request';
 import { BaseTooltip } from './BaseTooltip';
 import { Button } from '../buttons/Button';
 import { ModalClose } from '../modals/common/ModalClose';
-import classed from '../../lib/classed';
 import styles from './ChangelogTooltip.module.css';
+import { FeedData, Post } from '../../graphql/posts';
+import { SOURCE_FEED_QUERY } from '../../graphql/feed';
+import { graphqlUrl } from '../../lib/config';
+import AuthContext from '../../contexts/AuthContext';
+import { cloudinary } from '../../lib/image';
+import { postDateFormat } from '../../lib/dateFormat';
+import { Image } from '../image/Image';
 
 interface ChangelogTooltipProps<TRef> {
   visible: boolean;
   elementRef: MutableRefObject<TRef>;
   onRequestClose?: (e?: React.MouseEvent | React.KeyboardEvent) => void;
 }
-
-const ChangelogTooltipContainer = classed(
-  'div',
-  'changelog flex flex-col relative focus:outline-none items-center bg-theme-bg-tertiary shadow-2 border border-theme-color-cabbage rounded-16',
-);
-const ChangelogTooltipHeader = classed(
-  'header',
-  'flex items-center px-4 py-3 w-full ',
-);
-const ChangelogTooltipBody = classed(
-  'section',
-  'overflow-auto relative w-full h-full shrink max-h-full p-5',
-);
-const ChangelogTooltipFooter = classed(
-  'footer',
-  'flex gap-3 items-center px-4 py-3 w-full h-16 border-t border-theme-divider-tertiary',
-);
 
 function ChangelogTooltip<TRef extends HTMLElement>({
   visible,
@@ -35,27 +31,72 @@ function ChangelogTooltip<TRef extends HTMLElement>({
 }: ChangelogTooltipProps<TRef>): ReactElement {
   // TODO WT-1054-changelog test extension
   const isExtension = true; // !!process.env.TARGET_BROWSER;
+  const { user } = useContext(AuthContext);
+  const { data: post } = useQuery<FeedData, unknown, Post>(
+    [
+      'changelog',
+      'latest-post',
+      {
+        source: 'daily_updates',
+        first: 1,
+        loggedIn: !!user?.id,
+        unreadOnly: false,
+      },
+    ],
+    async ({ queryKey }) => {
+      const [, , variables] = queryKey;
+
+      return await request(graphqlUrl, SOURCE_FEED_QUERY, variables);
+    },
+    {
+      select: useCallback((data) => {
+        return data?.page.edges[0]?.node;
+      }, []),
+    },
+  );
 
   return (
     <BaseTooltip
       content={
-        <ChangelogTooltipContainer>
-          <ChangelogTooltipHeader className="border-b border-theme-divider-tertiary">
-            <Button className="bg-water-40 btn-primary small text-theme-label-secondary">
-              New release
-            </Button>
-            {onRequestClose && <ModalClose onClick={onRequestClose} />}
-          </ChangelogTooltipHeader>
-          <ChangelogTooltipBody>Release article here</ChangelogTooltipBody>
-          <ChangelogTooltipFooter>
-            <Button className="btn-tertiary">Release notes</Button>
-            {isExtension && (
-              <Button className="bg-cabbage-40 btn-primary">
-                Update extension
+        !!post && (
+          <div className="flex flex-col w-96 rounded-16 border shadow-2 focus:outline-none changelog bg-theme-bg-tertiary border-theme-color-cabbage">
+            <header className="flex flex-1 items-center py-3 px-4 border-b border-theme-divider-tertiary">
+              <Button className="bg-water-40 btn-primary small text-theme-label-secondary">
+                New release
               </Button>
-            )}
-          </ChangelogTooltipFooter>
-        </ChangelogTooltipContainer>
+              {onRequestClose && <ModalClose onClick={onRequestClose} />}
+            </header>
+            <section className="flex flex-col flex-1 p-5 h-full shrink max-h-full">
+              <Image
+                className="object-cover w-44 h-28 rounded-lg"
+                alt="Post Cover image"
+                src={post.image}
+                fallbackSrc={cloudinary.post.imageCoverPlaceholder}
+                loading="lazy"
+              />
+              <h3 className="mt-2 font-bold text-theme-label-primary typo-title3">
+                {post.title}
+              </h3>
+              <time
+                className="text-theme-label-quaternary typo-callout"
+                dateTime={post.createdAt}
+              >
+                {postDateFormat(post.createdAt)}
+              </time>
+              <div className="mt-2 w text-theme-label-tertiary typo-callout">
+                {post.summary}
+              </div>
+            </section>
+            <footer className="flex gap-3 items-center py-3 px-4 w-full h-16 border-t border-theme-divider-tertiary">
+              <Button className="btn-tertiary">Release notes</Button>
+              {isExtension && (
+                <Button className="bg-cabbage-40 btn-primary">
+                  Update extension
+                </Button>
+              )}
+            </footer>
+          </div>
+        )
       }
       offset={[6 * 16, 2.5 * 16]}
       interactive
@@ -69,7 +110,7 @@ function ChangelogTooltip<TRef extends HTMLElement>({
       reference={elementRef}
       arrow
       placement="right-end"
-      visible={visible}
+      visible={visible && !!post}
     />
   );
 }
