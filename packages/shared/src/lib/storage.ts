db993564f10a9d110d5d44f45ddaca2e11c6c08e
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-02-20 21:18:24 +0800

    refactor: default indentifier value

--- packages/shared/src/lib/storage.ts
+++ packages/shared/src/lib/storage.ts
@@ -7,5 +7,5 @@ export const APP_KEY_PREFIX = 'dailydev';
 export const generateStorageKey = (
   topic: StorageTopic,
   key: string,
-  identifier = 'anonymous',
+  identifier = 'global',
 ): string => `${APP_KEY_PREFIX}:${topic}:${key}:${identifier}`;
