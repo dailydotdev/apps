eb284ebc68ef7a417302188618d4b2ec66931607
capJavert <ante@kickass.website>
2023-03-03 13:13:56 +0100

    feat: add controls

--- packages/shared/src/components/containers/Slider.tsx
+++ packages/shared/src/components/containers/Slider.tsx
@@ -5,11 +5,13 @@ import React, {
   useContext,
   useState,
 } from 'react';
-import { SwipeCallback, useSwipeable } from 'react-swipeable';
+import { SwipeEventData, useSwipeable } from 'react-swipeable';
 import SettingsContext from '../../contexts/SettingsContext';
 import { gridGaps, gapClass } from '../../lib/feed';
+import { Button } from '../buttons/Button';
+import ArrowIcon from '../icons/Arrow';
 
-type CanSwipeFn = <TSliderItem extends { id: string }>(
+type SliderCanSwipeFn = <TSliderItem extends { id: string }>(
   index: number,
   items: TSliderItem[],
 ) => boolean;
@@ -18,13 +20,45 @@ interface SliderProps<TSliderItem extends { id: string }> {
   items: TSliderItem[];
   itemWidth: number;
   Item: FunctionComponent<TSliderItem>;
-  canSwipeRight?: CanSwipeFn;
-  canSwipeLeft?: CanSwipeFn;
+  canSwipeRight?: SliderCanSwipeFn;
+  canSwipeLeft?: SliderCanSwipeFn;
 }
 
-const defaultCanSwipeRight: CanSwipeFn = (index) => index > 0;
+const defaultCanSwipeRight: SliderCanSwipeFn = (index) => index > 0;
 
-const defaultCanSwipeLeft: CanSwipeFn = (index, items) => index < items.length;
+const defaultCanSwipeLeft: SliderCanSwipeFn = (index, items) =>
+  index < items.length;
+
+type SliderControlPosition = 'left' | 'right';
+
+const SliderControlButton = ({
+  onClick,
+  position,
+}: {
+  onClick: () => void;
+  position: SliderControlPosition;
+}) => {
+  return (
+    <div
+      className={classNames(
+        'absolute top-1/2 my-auto mx-0 -translate-y-1/2',
+        position === 'left' ? 'left-3' : 'right-3',
+      )}
+    >
+      <Button
+        className="w-12 h-12 btn-primary"
+        onClick={() => {
+          onClick();
+        }}
+      >
+        <ArrowIcon
+          className={position === 'left' ? '-rotate-90' : 'rotate-90'}
+          size="xlarge"
+        />
+      </Button>
+    </div>
+  );
+};
 
 function Slider<TSliderItem extends { id: string }>({
   items,
@@ -36,8 +70,8 @@ function Slider<TSliderItem extends { id: string }>({
   const [index, setIndex] = useState(0);
   const { spaciness } = useContext(SettingsContext);
 
-  const onSwipedRight: SwipeCallback = (swipeEvent) => {
-    swipeEvent.event.stopPropagation();
+  const onSwipedRight = (swipeEvent?: SwipeEventData) => {
+    swipeEvent?.event.stopPropagation();
 
     setIndex((current) => {
       if (canSwipeRight(current, items)) {
@@ -48,8 +82,8 @@ function Slider<TSliderItem extends { id: string }>({
     });
   };
 
-  const onSwipedLeft: SwipeCallback = (swipeEvent) => {
-    swipeEvent.event.stopPropagation();
+  const onSwipedLeft = (swipeEvent?: SwipeEventData) => {
+    swipeEvent?.event.stopPropagation();
 
     setIndex((current) => {
       if (canSwipeLeft(current, items)) {
@@ -66,37 +100,28 @@ function Slider<TSliderItem extends { id: string }>({
     trackMouse: true,
   });
 
-  // TODO WT-1109-personal-digest remove
-  if (typeof window !== 'undefined') {
-    const swipeEvent = {
-      event: {
-        stopPropagation: () => undefined,
-      },
-    };
-    // @ts-expect-error globalThis
-    window.swipeRight = () => onSwipedRight(swipeEvent);
-    // @ts-expect-error globalThis
-    window.swipeLeft = () => onSwipedLeft(swipeEvent);
-  }
-
   const itemGapPx = +(gridGaps[spaciness] ?? 'gap-8').replace('gap-', '') * 4;
   const itemWidthWithGap = itemWidth + 1 * itemGapPx;
 
   return (
-    <div {...swipeable} className="overflow-hidden">
-      <div
-        className={classNames(
-          'flex justify-start transform ease-in-out duration-500',
-          gapClass(false, spaciness),
-        )}
-        style={{
-          transform: `translateX(-${index * itemWidthWithGap}px)`,
-        }}
-      >
-        {items.map((item) => (
-          <Item key={item.id} {...item} />
-        ))}
+    <div {...swipeable} className="relative">
+      <div className="overflow-hidden">
+        <div
+          className={classNames(
+            'flex justify-start transform ease-in-out duration-500',
+            gapClass(false, spaciness),
+          )}
+          style={{
+            transform: `translateX(-${index * itemWidthWithGap}px)`,
+          }}
+        >
+          {items.map((item) => (
+            <Item key={item.id} {...item} />
+          ))}
+        </div>
       </div>
+      <SliderControlButton position="left" onClick={onSwipedRight} />
+      <SliderControlButton position="right" onClick={onSwipedLeft} />
     </div>
   );
 }
