5bbb3651bfa6c56438ff379cf02f4bfe5db7508b
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-03-22 18:14:14 +0800

    fix: lint error

--- packages/shared/src/components/squads/SquadPageHeader.tsx
+++ packages/shared/src/components/squads/SquadPageHeader.tsx
@@ -68,7 +68,6 @@ export function SquadPageHeader({
           className={classNames('w-full', MAX_WIDTH)}
         />
       )}
-      
       <div
         className={classNames(
           'absolute bottom-0 w-full translate-y-1/2 px-6 laptop:px-0 bg-theme-bg-primary',
