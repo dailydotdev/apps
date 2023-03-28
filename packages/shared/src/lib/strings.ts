c8000b36341022dbe35b95bfaff4c2a54f18c3b8
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-03-28 17:43:19 +0800

    refactor: user mention allowing hyphens to be used (#1699)

--- packages/shared/src/lib/strings.ts
+++ packages/shared/src/lib/strings.ts
@@ -1,10 +1,10 @@
 export const isAlphaNumeric = (key: string): boolean => {
-  const match = key.match(/^[a-z0-9]+$/i);
+  const match = key.match(/^[a-z0-9-]+$/i);
 
   return match && match[0].length === 1;
 };
 
-const specialCharsFormat = /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
+const specialCharsFormat = /[ `!@#$%^&*()_+=[\]{};':"\\|,.<>/?~]/;
 export const isSpecialCharacter = (key: string): boolean =>
   specialCharsFormat.test(key);
 
