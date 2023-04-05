2c882425acf5432e3508d1728d48f71648d7747b
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-04-05 19:28:33 +0800

    refactor: unused fragment

--- packages/shared/src/graphql/posts.ts
+++ packages/shared/src/graphql/posts.ts
@@ -5,7 +5,6 @@ import { Source, Squad } from './sources';
 import { EmptyResponse } from './emptyResponse';
 import { graphqlUrl } from '../lib/config';
 import {
-  CURRENT_MEMBER_FRAGMENT,
   SHARED_POST_INFO_FRAGMENT,
   SOURCE_SHORT_INFO_FRAGMENT,
   USER_SHORT_INFO_FRAGMENT,
@@ -144,7 +143,6 @@ export const POST_BY_ID_QUERY = gql`
     }
   }
   ${SHARED_POST_INFO_FRAGMENT}
-  ${CURRENT_MEMBER_FRAGMENT}
 `;
 
 export const POST_UPVOTES_BY_ID_QUERY = gql`
