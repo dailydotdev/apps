1e0192ab02a8a7ca9b640e197fac2d8f8278a8d0
capJavert <ante@kickass.website>
2023-04-06 15:22:51 +0200

    revert: custom permission resolving logic

--- packages/shared/src/graphql/squads.ts
+++ packages/shared/src/graphql/squads.ts
@@ -378,22 +378,4 @@ export async function editSquad(
 export const verifyPermission = (
   squad: Squad,
   permission: SourcePermissions,
-): boolean => {
-  const hasPermission =
-    !!squad?.currentMember?.permissions?.includes(permission);
-
-  // TODO WT-1224 find better way to do this
-  if (hasPermission && permission === SourcePermissions.Post) {
-    const membersCanPost = squad.memberPostingRole === SourceMemberRole.Member;
-
-    if (membersCanPost) {
-      return true;
-    }
-
-    return [SourceMemberRole.Owner, SourceMemberRole.Moderator].includes(
-      squad.currentMember.role,
-    );
-  }
-
-  return hasPermission;
-};
+): boolean => !!squad?.currentMember?.permissions?.includes(permission);
