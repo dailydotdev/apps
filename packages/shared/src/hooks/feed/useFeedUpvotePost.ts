1f138569372163fac9d8005a9ecd80c086a8947c
capJavert <ante@kickass.website>
2023-04-07 17:05:14 +0200

    fix: feed upvote counter now syncs with single post counter

--- packages/shared/src/hooks/feed/useFeedUpvotePost.ts
+++ packages/shared/src/hooks/feed/useFeedUpvotePost.ts
@@ -1,6 +1,10 @@
 import { requestIdleCallback } from 'next/dist/client/request-idle-callback';
-import { useContext } from 'react';
-import useUpvotePost from '../useUpvotePost';
+import { useContext, useEffect } from 'react';
+import { useQueryClient } from 'react-query';
+import useUpvotePost, {
+  cancelUpvotePostMutationKey,
+  upvotePostMutationKey,
+} from '../useUpvotePost';
 import { FeedItem } from '../useFeed';
 import { Post } from '../../graphql/posts';
 import AuthContext from '../../contexts/AuthContext';
@@ -12,6 +16,24 @@ import {
 import AnalyticsContext from '../../contexts/AnalyticsContext';
 import { postEventName } from '../../components/utilities';
 import { AuthTriggers } from '../../lib/auth';
+
+export type UseFeedUpvotePostVariables = {
+  id: string;
+  index: number;
+};
+
+const upvotePostKey = upvotePostMutationKey.toString();
+const cancelUpvotePostKey = cancelUpvotePostMutationKey.toString();
+const mutationHandlers = {
+  [upvotePostKey]: (post: Post) => ({
+    upvoted: true,
+    numUpvotes: post.numUpvotes + 1,
+  }),
+  [cancelUpvotePostKey]: (post: Post) => ({
+    upvoted: false,
+    numUpvotes: post.numUpvotes - 1,
+  }),
+};
 
 export default function useFeedUpvotePost(
   items: FeedItem[],
@@ -27,24 +49,72 @@ export default function useFeedUpvotePost(
   column: number,
   upvoted: boolean,
 ) => Promise<void> {
+  const queryClient = useQueryClient();
   const { user, showLogin } = useContext(AuthContext);
   const { trackEvent } = useContext(AnalyticsContext);
 
-  const { upvotePost, cancelPostUpvote } = useUpvotePost<{
-    id: string;
-    index: number;
-  }>({
-    onUpvotePostMutate: optimisticPostUpdateInFeed(
-      items,
-      updatePost,
-      (post) => ({ upvoted: true, numUpvotes: post.numUpvotes + 1 }),
-    ),
-    onCancelPostUpvoteMutate: optimisticPostUpdateInFeed(
-      items,
-      updatePost,
-      (post) => ({ upvoted: false, numUpvotes: post.numUpvotes - 1 }),
-    ),
-  });
+  const { upvotePost, cancelPostUpvote } =
+    useUpvotePost<UseFeedUpvotePostVariables>({
+      onUpvotePostMutate: optimisticPostUpdateInFeed(
+        items,
+        updatePost,
+        mutationHandlers[upvotePostKey],
+      ),
+      onCancelPostUpvoteMutate: optimisticPostUpdateInFeed(
+        items,
+        updatePost,
+        mutationHandlers[cancelUpvotePostKey],
+      ),
+    });
+
+  useEffect(() => {
+    const matchedMutations = [
+      upvotePostMutationKey,
+      cancelUpvotePostMutationKey,
+    ].map((item) => item.toString());
+
+    const unsubscribe = queryClient.getMutationCache().subscribe((event) => {
+      if (event.state.status !== 'success') {
+        return;
+      }
+
+      const mutationKey = event.options.mutationKey?.toString();
+
+      if (matchedMutations.includes(mutationKey)) {
+        const { id, index: indexFromEvent } = event.options
+          .variables as unknown as UseFeedUpvotePostVariables;
+        const mutationFromFeedHook = typeof indexFromEvent !== 'undefined';
+
+        if (mutationFromFeedHook) {
+          return;
+        }
+
+        const index = items.findIndex(
+          (item) => item.type === 'post' && item.post.id === id,
+        );
+
+        if (index === -1) {
+          return;
+        }
+
+        const mutationHandler = mutationHandlers[mutationKey];
+
+        if (!mutationHandler) {
+          return;
+        }
+
+        optimisticPostUpdateInFeed(
+          items,
+          updatePost,
+          mutationHandler,
+        )({ index });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [items, updatePost]);
 
   return async (post, index, row, column, upvoted): Promise<void> => {
     if (!user) {
