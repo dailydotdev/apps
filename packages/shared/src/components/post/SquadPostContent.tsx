99c22739e70e80fbce6e468728fc39a35238b37f
Chris Bongers <chrisbongers@gmail.com>
2023-04-11 15:52:14 +0200

    fix: optional data for when modal loading

--- packages/shared/src/components/post/SquadPostContent.tsx
+++ packages/shared/src/components/post/SquadPostContent.tsx
@@ -57,8 +57,8 @@ function SquadPostContent({
   const engagementActions = usePostContent({ origin, post });
   const { onReadArticle, onSharePost, onToggleBookmark } = engagementActions;
   const { role } = useMemberRoleForSource({
-    source: post.source,
-    user: post.author,
+    source: post?.source,
+    user: post?.author,
   });
 
   const navigationProps: PostNavigationProps = {
