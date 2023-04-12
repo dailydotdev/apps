a04da5a2c68ebe3bf55c2e76fc093a4f35f0edfd
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-04-12 15:14:05 +0800

    refactor: copy updates (#1734)
    * refactor: remove member prompt to block member
    
    * fix: remove member alert
    
    * fix: unblock toast
    
    ---------
    
    Co-authored-by: Chris Bongers <chrisbongers@gmail.com>

--- packages/shared/src/components/squads/SquadMemberMenu.tsx
+++ packages/shared/src/components/squads/SquadMemberMenu.tsx
@@ -19,6 +19,7 @@ import { ModalSize } from '../modals/common/types';
 import { ContextMenu, ContextMenuItemProps } from '../fields/PortalMenu';
 import { UseSquadActions } from '../../hooks/squads/useSquadActions';
 import { verifyPermission } from '../../graphql/squads';
+import { useToastNotification } from '../../hooks/useToastNotification';
 
 interface SquadMemberMenuProps extends Pick<UseSquadActions, 'onUpdateRole'> {
   squad: Squad;
@@ -30,7 +31,7 @@ enum MenuItemTitle {
   PromoteToModerator = 'Promote to moderator',
   DemoteToModerator = 'Demote to moderator',
   DemoteToMember = 'Demote to member',
-  RemoveMember = 'Remove member',
+  BlockMember = 'Block member',
 }
 
 const promptDescription: Record<
@@ -45,8 +46,12 @@ const promptDescription: Record<
     `${memberName} will no longer have owner permissions. You can always reverse this decision later.`,
   [MenuItemTitle.DemoteToMember]: (memberName) =>
     `${memberName} will lose the moderator permissions and become a regular member.`,
-  [MenuItemTitle.RemoveMember]: (memberName, squadName) =>
-    `${memberName} will be a Blocked members and will no longer have access to ${squadName}. They will not be able to rejoin unless you unblock them.`,
+  [MenuItemTitle.BlockMember]: (memberName, squadName) =>
+    `${memberName} will be blocked and will no longer have access to ${squadName}. They will not be able to rejoin unless you unblock them.`,
+};
+
+const toastDescription: Partial<Record<MenuItemTitle, string>> = {
+  [MenuItemTitle.BlockMember]: 'Member has been blocked',
 };
 
 type UpdateRoleFn = (
@@ -109,6 +114,7 @@ export default function SquadMemberMenu({
 }: SquadMemberMenuProps): ReactElement {
   const { user } = useContext(AuthContext);
   const { showPrompt } = usePrompt();
+  const { displayToast } = useToastNotification();
   const onUpdateMember = async (
     role: SourceMemberRole,
     title: MenuItemTitle,
@@ -128,13 +134,15 @@ export default function SquadMemberMenu({
       className: { buttons: 'mt-6' },
     });
 
-    if (hasConfirmed) {
-      await onUpdateRole({
-        sourceId: squad.id,
-        memberId: member.user.id,
-        role,
-      });
-    }
+    if (!hasConfirmed) return;
+
+    await onUpdateRole({
+      sourceId: squad.id,
+      memberId: member.user.id,
+      role,
+    });
+
+    if (toastDescription[title]) displayToast(toastDescription[title]);
   };
 
   const options: ContextMenuItemProps[] = useMemo(() => {
@@ -170,11 +178,11 @@ export default function SquadMemberMenu({
 
     if (canRemoveMember) {
       menu.push({
-        text: MenuItemTitle.RemoveMember,
+        text: MenuItemTitle.BlockMember,
         icon: <BlockIcon size={IconSize.Small} />,
         action: getUpdateRoleFn(
           SourceMemberRole.Blocked,
-          MenuItemTitle.RemoveMember,
+          MenuItemTitle.BlockMember,
         ),
       });
     }
