2835f335d8473c7b5d8000949702aeadef734a0a
Chris Bongers <chrisbongers@gmail.com>
2023-04-20 14:27:19 +0200

    fix: move analytics context to be available

--- packages/webapp/pages/popup/notifications/enable.tsx
+++ packages/webapp/pages/popup/notifications/enable.tsx
@@ -1,18 +1,13 @@
 import classNames from 'classnames';
 import BrowserPermissionIcon from '@dailydotdev/shared/src/components/icons/BrowserPermission/primary.svg';
 import NotificationToggleIcon from '@dailydotdev/shared/src/components/icons/NotificationToggle/primary.svg';
-import { BootDataProvider } from '@dailydotdev/shared/src/contexts/BootProvider';
 import { useNotificationContext } from '@dailydotdev/shared/src/contexts/NotificationsContext';
 import classed from '@dailydotdev/shared/src/lib/classed';
 import { postWindowMessage } from '@dailydotdev/shared/src/lib/func';
 import React, { useEffect } from 'react';
 import { ENABLE_NOTIFICATION_WINDOW_KEY } from '@dailydotdev/shared/src/hooks/useNotificationPermissionPopup';
-import { BootApp } from '@dailydotdev/shared/src/lib/boot';
 import { useRouter } from 'next/router';
 import { NotificationPromptSource } from '@dailydotdev/shared/src/lib/analytics';
-
-const getRedirectUri = () =>
-  `${window.location.origin}${window.location.pathname}`;
 
 const InstructionContainer = classed(
   'div',
@@ -61,39 +56,35 @@ function Enable(): React.ReactElement {
   }
 
   return (
-    <BootDataProvider app={BootApp.Webapp} getRedirectUri={getRedirectUri}>
-      <main className="flex overflow-hidden flex-col justify-center items-center px-20 w-screen h-screen text-center max-h-[100%] max-w-[100%] bg-theme-overlay-float-cabbage">
-        <h1 className="relative font-bold bg-transparent typo-mega3">
-          <div className="absolute inset-0 -z-1 w-52 h-52 opacity-64 bg-theme-bg-cabbage-blur blur-[3.125rem] rounded-[3.125rem] -translate-y-[25%]" />
-          Click allow
-        </h1>
-        <p className="mt-6 text-theme-label-secondary typo-body">
-          Enable web push notifications to be notified of important events such
-          as replies, mentions, updates, etc.
-        </p>
-        <Description className="mt-28 font-bold">
-          Didn&apos;t get the popup?
-        </Description>
-        <div>
-          <InstructionContainer className="mb-1">
-            <Description>
-              1. Click on the lock icon in the search bar
-            </Description>
-            <BrowserPermissionIcon
-              className={classNames(iconClasses, 'w-full h-8 max-w-[18.75rem]')}
-            />
-          </InstructionContainer>
-          <InstructionContainer>
-            <Description>
-              2. Enable the toggle button under the notifications section
-            </Description>
-            <NotificationToggleIcon
-              className={classNames(iconClasses, 'w-52 h-8')}
-            />
-          </InstructionContainer>
-        </div>
-      </main>
-    </BootDataProvider>
+    <main className="flex overflow-hidden flex-col justify-center items-center px-20 w-screen h-screen text-center max-h-[100%] max-w-[100%] bg-theme-overlay-float-cabbage">
+      <h1 className="relative font-bold bg-transparent typo-mega3">
+        <div className="absolute inset-0 -z-1 w-52 h-52 opacity-64 bg-theme-bg-cabbage-blur blur-[3.125rem] rounded-[3.125rem] -translate-y-[25%]" />
+        Click allow
+      </h1>
+      <p className="mt-6 text-theme-label-secondary typo-body">
+        Enable web push notifications to be notified of important events such as
+        replies, mentions, updates, etc.
+      </p>
+      <Description className="mt-28 font-bold">
+        Didn&apos;t get the popup?
+      </Description>
+      <div>
+        <InstructionContainer className="mb-1">
+          <Description>1. Click on the lock icon in the search bar</Description>
+          <BrowserPermissionIcon
+            className={classNames(iconClasses, 'w-full h-8 max-w-[18.75rem]')}
+          />
+        </InstructionContainer>
+        <InstructionContainer>
+          <Description>
+            2. Enable the toggle button under the notifications section
+          </Description>
+          <NotificationToggleIcon
+            className={classNames(iconClasses, 'w-52 h-8')}
+          />
+        </InstructionContainer>
+      </div>
+    </main>
   );
 }
 
