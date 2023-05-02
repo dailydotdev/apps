4dc5f2c57509390b4378a60de0494064b32e9e2b
capJavert <ante@kickass.website>
2023-05-02 15:54:13 +0200

    feat: browsers icon

--- packages/shared/src/components/checklist/InstallExtensionChecklistStep.tsx
+++ packages/shared/src/components/checklist/InstallExtensionChecklistStep.tsx
@@ -2,15 +2,22 @@ import React, { ReactElement } from 'react';
 import { ChecklistStepProps } from '../../lib/checklist';
 import { ChecklistStep } from './ChecklistStep';
 import { Button } from '../buttons/Button';
+import BrowsersIcon from '../icons/Browsers';
+import { FlexCentered } from '../utilities';
 
 const InstallExtensionChecklistStep = (
   props: ChecklistStepProps,
 ): ReactElement => {
-  // TODO WT-1293-checklist-components add browsers icon
+  // TODO WT-1293-checklist-components add business logic
 
   return (
     <ChecklistStep {...props}>
-      <Button className="btn-primary">Add to browser - it&apos;s free! </Button>
+      <Button className="btn-primary">
+        <FlexCentered className="gap-2">
+          <BrowsersIcon className="text-theme-label-primary !w-12" /> Add to
+          browser - it&apos;s free!
+        </FlexCentered>
+      </Button>
     </ChecklistStep>
   );
 };
