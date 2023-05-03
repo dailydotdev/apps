fba81752337961d2445673eac2cc51025041a924
capJavert <ante@kickass.website>
2023-05-03 13:23:06 +0200

    feat: mobile checklist is modal

--- packages/shared/src/components/checklist/SquadChecklistCard.tsx
+++ packages/shared/src/components/checklist/SquadChecklistCard.tsx
@@ -4,6 +4,12 @@ import { useSquadChecklistSteps } from '../../hooks/useSquadChecklistSteps';
 import { Squad } from '../../graphql/sources';
 import usePersistentContext from '../../hooks/usePersistentContext';
 import { SQUAD_CHECKLIST_VISIBLE_KEY } from '../../lib/checklist';
+import InteractivePopup, {
+  InteractivePopupPosition,
+} from '../tooltips/InteractivePopup';
+import useSidebarRendered from '../../hooks/useSidebarRendered';
+import { Modal } from '../modals/common/Modal';
+import { ModalKind } from '../modals/common/types';
 
 const SquadChecklistCard = ({ squad }: { squad: Squad }): ReactElement => {
   const { steps } = useSquadChecklistSteps({ squad });
@@ -11,14 +17,14 @@ const SquadChecklistCard = ({ squad }: { squad: Squad }): ReactElement => {
     SQUAD_CHECKLIST_VISIBLE_KEY,
     false,
   );
+  const { sidebarRendered } = useSidebarRendered();
 
   if (!isChecklistVisible) {
     return null;
   }
 
-  return (
+  const checklistElement = (
     <ChecklistCard
-      className="fixed right-5 bottom-5 z-3"
       title="Squads v5"
       description="Use all of the new features"
       steps={steps}
@@ -26,6 +32,27 @@ const SquadChecklistCard = ({ squad }: { squad: Squad }): ReactElement => {
         setChecklistVisible(false);
       }}
     />
+  );
+
+  if (!sidebarRendered) {
+    return (
+      <Modal
+        className="border-none !bg-transparent shadow-none"
+        isOpen={isChecklistVisible}
+        kind={ModalKind.FlexibleCenter}
+      >
+        {checklistElement}
+      </Modal>
+    );
+  }
+
+  return (
+    <InteractivePopup
+      position={InteractivePopupPosition.RightEnd}
+      className="rounded-none"
+    >
+      {checklistElement}
+    </InteractivePopup>
   );
 };
 
