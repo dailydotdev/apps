d269d324c6e77b380f1d9caa22821239ef5b2205
Chris Bongers <chrisbongers@gmail.com>
2023-05-12 14:40:12 +0200

    fix: add v5 tracking events

--- packages/shared/src/lib/analytics.ts
+++ packages/shared/src/lib/analytics.ts
@@ -56,6 +56,7 @@ export enum AnalyticsEvent {
   CompleteSquadCreation = 'complete squad creation',
   StartShareToSquad = 'start share to squad',
   ShareToSquad = 'share to squad',
+  ChecklistClose = 'checklist close',
   // squads - end
   EligibleScrollBlock = 'eligible scroll block',
 }
@@ -65,6 +66,7 @@ export enum TargetType {
   ArticleAnonymousCTA = 'article anonymous cta',
   EnableNotifications = 'enable notifications',
   CreateSquadPopup = 'create squad popup',
+  OnboardingChecklist = 'onboarding checklist',
 }
 
 export enum NotificationChannel {
