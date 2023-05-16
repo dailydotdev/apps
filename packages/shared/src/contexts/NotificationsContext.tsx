3d5e12d13e37c37cc88b2c9c11bf1e322c8032b4
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-05-16 21:57:39 +0800

    refactor: unnecessary use effect

--- packages/shared/src/contexts/NotificationsContext.tsx
+++ packages/shared/src/contexts/NotificationsContext.tsx
@@ -17,8 +17,6 @@ import { checkIsExtension } from '../lib/func';
 import AuthContext from './AuthContext';
 import { AnalyticsEvent, NotificationPromptSource } from '../lib/analytics';
 import { useAnalyticsContext } from './AnalyticsContext';
-import { useActions } from '../hooks/useActions';
-import { ActionType } from '../graphql/actions';
 
 export interface NotificationsContextData
   extends UseNotificationPermissionPopup {
@@ -59,7 +57,6 @@ export const NotificationsContextProvider = ({
   const isExtension = checkIsExtension();
   const { trackEvent } = useAnalyticsContext();
   const { user } = useContext(AuthContext);
-  const { actions, completeAction, checkHasCompleted } = useActions();
   const [OneSignal, setOneSignal] = useState(null);
   const [isInitializing, setIsInitializing] = useState(false);
   const [isInitialized, setIsInitialized] = useState(false);
@@ -208,14 +205,6 @@ export const NotificationsContextProvider = ({
     // @NOTE see https://dailydotdev.atlassian.net/l/cp/dK9h1zoM
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isInitialized, isInitializing, user]);
-
-  // we could have make this trigger when the permission is once accepted
-  // but did this instead so we won't have to do retroactive checks in the BE
-  useEffect(() => {
-    if (!actions || !isSubscribed) return;
-
-    completeAction(ActionType.EnableNotification);
-  }, [actions, isSubscribed, completeAction, checkHasCompleted]);
 
   const data: NotificationsContextData = useMemo(
     () => ({
