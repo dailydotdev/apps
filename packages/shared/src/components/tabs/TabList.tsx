0d2093a0c682bb057f06bda32b524ed3cbf86d84
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-05-31 19:41:52 +0800

    refactor: tablist to have active bg

--- packages/shared/src/components/tabs/TabList.tsx
+++ packages/shared/src/components/tabs/TabList.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 
 interface TabListProps {
   items: string[];
@@ -8,28 +8,41 @@ interface TabListProps {
 }
 
 function TabList({ items, active, onClick }: TabListProps): ReactElement {
+  const [offset, setOffset] = useState<number>(undefined);
+
   return (
-    <ul className="flex flex-row">
+    <ul className="flex relative flex-row">
       {items.map((tab) => (
         <button
           key={tab}
+          ref={(el) => {
+            if (!el || offset === el?.offsetLeft || tab !== active) return;
+
+            const value = el.getBoundingClientRect().width / 2 + el.offsetLeft;
+            setOffset(value);
+          }}
           className={classNames(
-            'relative p-4 text-center typo-callout',
-            tab === active ? 'font-bold' : 'text-theme-label-tertiary',
+            'relative p-4 text-center typo-callout font-bold',
+            tab === active ? '' : 'text-theme-label-tertiary',
           )}
           onClick={() => onClick(tab)}
           type="button"
           role="menuitem"
         >
-          {tab}
-          {tab === active && (
-            <div
-              className="absolute bottom-0 mx-auto w-4 h-px bg-theme-label-primary"
-              style={{ left: 'calc(50% - 0.5rem)' }}
-            />
-          )}
+          <span
+            className={classNames(
+              'py-1.5 px-3 rounded-10',
+              tab === active && 'bg-theme-active',
+            )}
+          >
+            {tab}
+          </span>
         </button>
       ))}
+      <div
+        className="absolute bottom-0 mx-auto w-16 h-0.5 rounded ease-linear -translate-x-1/2 transition-[left] bg-theme-label-primary"
+        style={{ left: offset }}
+      />
     </ul>
   );
 }
