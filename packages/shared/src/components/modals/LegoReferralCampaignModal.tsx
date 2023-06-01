7f2083ec187719bbbcd257d00375601604b7349a
capJavert <ante@kickass.website>
2023-06-01 23:39:15 +0200

    feat: mobile and tablet resolution

--- packages/shared/src/components/modals/LegoReferralCampaignModal.tsx
+++ packages/shared/src/components/modals/LegoReferralCampaignModal.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @dailydotdev/daily-dev-eslint-rules/no-custom-color */
 import React, { ReactElement } from 'react';
+import classNames from 'classnames';
 import { LazyModalCommonProps, Modal } from './common/Modal';
 import { ReferralCampaignKey, useReferralCampaign } from '../../hooks';
 import { FlexCol, FlexRow } from '../utilities';
@@ -13,6 +14,7 @@ import CloseButton from '../CloseButton';
 import { cloudinary } from '../../lib/image';
 import usePersistentContext from '../../hooks/usePersistentContext';
 import { LEGO_REFERRAL_CAMPAIGN_MAY_2023_KEY } from '../../lib/storage';
+import useSidebarRendered from '../../hooks/useSidebarRendered';
 
 type LegoReferralCampaignModalProps = {
   campaignKey: ReferralCampaignKey;
@@ -37,6 +39,7 @@ function LegoReferralCampaignModal({
     false,
   );
   const [, copyReferralLink] = useCopyLink(() => referralUrl);
+  const { sidebarRendered } = useSidebarRendered();
 
   if (!isReady) {
     return null;
@@ -46,8 +49,8 @@ function LegoReferralCampaignModal({
     <Modal
       {...props}
       className="border-none"
-      kind={Modal.Kind.FixedCenter}
-      size={Modal.Size.XLarge}
+      kind={Modal.Kind.FlexibleCenter}
+      size={sidebarRendered ? Modal.Size.XLarge : undefined}
       onRequestClose={onRequestClose}
     >
       <div className="bg-gradient-to-br rounded-14 p-[3px] from-[#6DE8BE] via-[#DD3DFC] to-[#F9DD53]">
@@ -56,15 +59,27 @@ function LegoReferralCampaignModal({
             backgroundColor: '#1b174b',
             backgroundImage: `url(${cloudinary.referralCampaign.backgroundDark})`,
           }}
-          className="flex-1 gap-6 justify-center items-center p-6 pr-0 bg-cover rounded-14 border-0"
+          className="flex-1 gap-6 justify-center items-center p-6 pt-0 laptop:pt-6 laptop:pr-0 bg-cover rounded-14 border-0"
         >
           <CloseButton
             buttonSize={ButtonSize.Small}
             className="top-3 right-3 text-white border-white !absolute !btn-secondary"
             onClick={onRequestClose}
           />
-          <FlexCol className="justify-center basis-[23.25rem]">
-            <h3 className="mb-3 font-bold text-transparent bg-clip-text bg-gradient-to-l typo-mega2 from-[#6DE8BE] via-[#DD3DFC] to-[#F9DD53]">
+          <FlexCol
+            className={classNames(
+              'justify-center w-full text-center laptop:text-left',
+              sidebarRendered && 'basis-[23.25rem]',
+            )}
+          >
+            {!sidebarRendered && (
+              <Image
+                className="mx-auto h-[16.25rem]"
+                src="https://daily-now-res.cloudinary.com/image/upload/s--D-MLNY69--/f_auto,q_auto/v1685624124/public/lego-preview"
+                alt="Image showing lego set consisting of little chair and table with programmer setup."
+              />
+            )}
+            <h3 className="mb-3 font-bold text-transparent bg-clip-text bg-gradient-to-l typo-mega3 laptop:typo-mega2 from-[#6DE8BE] via-[#DD3DFC] to-[#F9DD53]">
               Win a limited edition lego set!
             </h3>
             <p className="mb-6 text-salt-50 typo-title3">
@@ -91,13 +106,15 @@ function LegoReferralCampaignModal({
                 {referralCurrentCount} out of {referralTargetCount}
               </div>
             </div>
-            <Image
-              className="mb-6 shadow-3-black"
-              width={372}
-              height={186}
-              src={cloudinary.referralCampaign.lego.francesco}
-              alt="Image showing lego set box held by a person."
-            />
+            {sidebarRendered && (
+              <Image
+                className="mb-6 shadow-3-black"
+                width={372}
+                height={186}
+                src={cloudinary.referralCampaign.lego.francesco}
+                alt="Image showing lego set box held by a person."
+              />
+            )}
             <FlexRow className="justify-center items-center">
               <Checkbox
                 name="lego-referral-hide"
@@ -120,27 +137,29 @@ function LegoReferralCampaignModal({
               </a>
             </FlexRow>
           </FlexCol>
-          <FlexCol className="flex-1 justify-center items-center">
-            <Image
-              className="-m-10 mb-3"
-              width={604}
-              height={512}
-              src="https://daily-now-res.cloudinary.com/image/upload/s--D-MLNY69--/f_auto,q_auto/v1685624124/public/lego-preview"
-              alt="Image showing lego set consisting of little chair and table with programmer setup."
-            />
-            <p className="mb-4 max-w-md italic text-center text-salt-50 typo-callout">
-              P.S. Inviting more than 5 friends will not affect your chance to
-              win but it will make my manager very happy if you do{' '}
-              <span className="not-italic">😉</span>
-            </p>
-            <Image
-              className="mb-3"
-              width={248}
-              height={27}
-              src={cloudinary.referralCampaign.lego.madeWithLove}
-              alt="Made with love by daily.dev"
-            />
-          </FlexCol>
+          {sidebarRendered && (
+            <FlexCol className="flex-1 justify-center items-center">
+              <Image
+                className="-m-10 mb-3"
+                width={604}
+                height={512}
+                src="https://daily-now-res.cloudinary.com/image/upload/s--D-MLNY69--/f_auto,q_auto/v1685624124/public/lego-preview"
+                alt="Image showing lego set consisting of little chair and table with programmer setup."
+              />
+              <p className="mb-4 max-w-md italic text-center text-salt-50 typo-callout">
+                P.S. Inviting more than 5 friends will not affect your chance to
+                win but it will make my manager very happy if you do{' '}
+                <span className="not-italic">😉</span>
+              </p>
+              <Image
+                className="mb-3"
+                width={248}
+                height={27}
+                src={cloudinary.referralCampaign.lego.madeWithLove}
+                alt="Made with love by daily.dev"
+              />
+            </FlexCol>
+          )}
         </FlexRow>
       </div>
     </Modal>
