3f2acba1262e095e26ed27a47f8f1688c6c5c179
capJavert <ante@kickass.website>
2023-06-02 15:34:51 +0200

    feat: add video modal

--- packages/webapp/pages/join.tsx
+++ packages/webapp/pages/join.tsx
@@ -19,16 +19,36 @@ import {
 import { PublicProfile } from '@dailydotdev/shared/src/lib/user';
 import request from 'graphql-request';
 import { GetServerSideProps } from 'next';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import BrowsersIcon from '@dailydotdev/shared/icons/browsers.svg';
 import { Image } from '@dailydotdev/shared/src/components/image/Image';
 import { cloudinary } from '@dailydotdev/shared/src/lib/image';
+import PlayIcon from '@dailydotdev/shared/src/components/icons/Play';
+import { IconSize } from '@dailydotdev/shared/src/components/Icon';
+import CloseButton from '@dailydotdev/shared/src/components/CloseButton';
+import {
+  ModalKind,
+  ModalSize,
+} from '@dailydotdev/shared/src/components/modals/common/types';
+import dynamic from 'next/dynamic';
 
 type PageProps = {
   referringUser: Pick<PublicProfile, 'id' | 'name' | 'image'>;
 };
 
+const Modal = dynamic(() =>
+  import(
+    /* webpackChunkName: "modal" */ '@dailydotdev/shared/src/components/modals/common/Modal'
+  ).then((mod) => mod.Modal),
+);
+
 const Page = ({ referringUser }: PageProps): ReactElement => {
+  const [isVideoOpen, setVideoOpen] = useState(false);
+
+  const onVideoModalClose = () => {
+    setVideoOpen(false);
+  };
+
   return (
     <div className="flex p-8 pr-0 h-[100vh]">
       <Logo className="h-6" logoClassName="h-6" />
@@ -71,15 +91,52 @@ const Page = ({ referringUser }: PageProps): ReactElement => {
             </FlexCentered>
           </Button>
         </FlexCol>
-        <FlexCol>
+        <FlexCol className="relative">
           <Image
+            style={{
+              width: '100%',
+              maxWidth: '1015px',
+              height: '633px',
+              objectFit: 'contain',
+            }}
             src={cloudinary.referralCampaign.appScreenshot}
             alt="Image showing the daily.dev app"
-            width={1015}
-            height={633}
           />
+          <button
+            className="absolute top-1/2 left-1/2 bg-pepper-90 rounded-full transform -translate-x-1/2 -translate-y-1/2"
+            type="button"
+            onClick={() => {
+              setVideoOpen(true);
+            }}
+          >
+            <PlayIcon className="w-28 h-28" size={IconSize.XXXLarge} />
+          </button>
         </FlexCol>
       </FlexRow>
+      {isVideoOpen && (
+        <Modal
+          // eslint-disable-next-line @dailydotdev/daily-dev-eslint-rules/no-custom-color
+          className="px-8 bg-[black]"
+          kind={ModalKind.FlexibleCenter}
+          size={ModalSize.XLarge}
+          isOpen={isVideoOpen}
+          onRequestClose={onVideoModalClose}
+        >
+          <CloseButton
+            buttonSize={ButtonSize.Small}
+            className="top-3 right-3 text-white border-white !absolute !btn-secondary"
+            onClick={onVideoModalClose}
+          />
+          <iframe
+            className="w-full border-none aspect-video"
+            // TODO WT-1412-lego-landing-page r.daily.dev link
+            src="https://www.youtube.com/embed/igZCEr3HwCg"
+            title="YouTube video player"
+            allow="encrypted-media;web-share"
+            allowFullScreen
+          />
+        </Modal>
+      )}
     </div>
   );
 };
