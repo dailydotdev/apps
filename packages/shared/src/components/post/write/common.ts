5158620a8e7dba58ccd0e814c601d711765a6f27
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-06-07 16:44:21 +0800

    refactor: improved readability

---
+++ packages/shared/src/components/post/write/common.ts
@@ -0,0 +1,14 @@
+import classed from '../../../lib/classed';
+import { FlexCol, FlexRow } from '../../utilities';
+
+export const previewImageClass = 'w-28 h-16 rounded-12 bg-theme-label-disabled';
+
+export const WritePreviewContainer = classed(
+  FlexCol,
+  'rounded-16 border border-theme-divider-tertiary',
+);
+
+export const WritePreviewContent = classed(
+  FlexRow,
+  'relative gap-3 items-center py-5 px-4',
+);
