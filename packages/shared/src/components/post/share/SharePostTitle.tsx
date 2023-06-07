35b643c02bacc523a50757ab30acf5b4467770b6
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-06-07 15:48:29 +0800

    refactor: share post modal to display mentions (#1857)
    * refactor: share post modal to display mentions
    
    * refactor: usage of built-in markdown

---
+++ packages/shared/src/components/post/share/SharePostTitle.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react';
+import { Post } from '../../../graphql/posts';
+import Markdown from '../../Markdown';
+
+interface SharePostTitleProps {
+  post: Post;
+}
+
+export function SharePostTitle({ post }: SharePostTitleProps): ReactElement {
+  if (!post?.title) return null;
+
+  if (!post?.titleHtml) {
+    return <p className="mt-6 whitespace-pre-line typo-title3">{post.title}</p>;
+  }
+
+  return <Markdown className="mt-6" content={post.titleHtml} />;
+}
