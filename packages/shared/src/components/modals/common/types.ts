242ffce690256480a3f59f1e12849c5cc376bf57
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-06-15 18:02:47 +0800

    removal of post to squad modal

--- packages/shared/src/components/modals/common/types.ts
+++ packages/shared/src/components/modals/common/types.ts
@@ -25,7 +25,6 @@ export enum ModalSize {
 export enum LazyModal {
   EditSquad = 'editSquad',
   NewSquad = 'newSquad',
-  PostToSquad = 'postToSquad',
   SquadMember = 'squadMember',
   SquadTour = 'squadTour',
   UpvotedPopup = 'upvotedPopup',
