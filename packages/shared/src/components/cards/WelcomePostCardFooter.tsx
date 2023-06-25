579063775e17f57ec123bc8554a6696f81319170
Tom Dowling <tom@digital-space.io>
2023-06-25 23:17:54 +0100

    feat: fetch the first image from the content if no banner image for a squad post

--- packages/shared/src/components/cards/WelcomePostCardFooter.tsx
+++ packages/shared/src/components/cards/WelcomePostCardFooter.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement, useMemo, useState } from 'react';
 import { sanitize } from 'dompurify';
 import { CardImage } from './Card';
 import { Post } from '../../graphql/posts';
@@ -11,17 +11,32 @@ type WelcomePostCardFooterProps = {
 export const WelcomePostCardFooter = ({
   post,
 }: WelcomePostCardFooterProps): ReactElement => {
+  const [image, setImage] = useState(post?.image);
   const content = useMemo(
     () =>
       post?.contentHtml ? sanitize(post.contentHtml, { ALLOWED_TAGS: [] }) : '',
     [post?.contentHtml],
   );
 
-  if (post.image) {
+  const imageCheck = useMemo(() => {
+    if (image) return true;
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(post?.contentHtml, 'text/html');
+    const imgTag = doc.querySelector('img');
+    if (imgTag) {
+      setImage(imgTag.getAttribute('src') || null);
+      return true;
+    }
+
+    return false;
+  }, [post.image]);
+
+  if (imageCheck) {
     return (
       <CardImage
         alt="Post Cover image"
-        src={post.image}
+        src={image}
         fallbackSrc={cloudinary.post.imageCoverPlaceholder}
         className="object-cover my-2"
         loading="lazy"
