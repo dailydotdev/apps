eccd48290aac44d60fb0a52f457ad8a0c26543b1
Tom Dowling <tom@digital-space.io>
2023-06-26 07:14:39 +0100

    feat: build error fixes

--- packages/webapp/pages/posts/[id]/index.tsx
+++ packages/webapp/pages/posts/[id]/index.tsx
@@ -109,8 +109,12 @@ const PostPage = ({ id, initialData }: Props): ReactElement => {
       modalSizeToClassName[ModalSize.Large],
   );
   const seoTitle = () => {
-    if (post?.type !== PostType.Share || (post?.type === PostType.Share && post?.title !== null)) return post?.title;
-    return `Shared post at ${post?.source?.name}`
+    if (
+      post?.type !== PostType.Share ||
+      (post?.type === PostType.Share && post?.title !== null)
+    )
+      return post?.title;
+    return `Shared post at ${post?.source?.name}`;
   };
   const seo: NextSeoProps = {
     title: getTemplatedTitle(seoTitle()),
