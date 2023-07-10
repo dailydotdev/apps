96e3c2c66244fbb170454d5e904f36861204c356
Chris Bongers <chrisbongers@gmail.com>
2023-07-10 16:11:03 +0200

    fix: Refactor to focus on registration hook first

--- packages/shared/src/hooks/useRegistration.ts
+++ packages/shared/src/hooks/useRegistration.ts
@@ -30,7 +30,6 @@ interface UseRegistrationProps {
   onRedirectFail?: () => void;
   onValidRegistration?: (params: ValidateRegistrationParams) => void;
   onInvalidRegistration?: (errors: RegistrationError) => void;
-  disabled?: boolean;
 }
 
 interface UseRegistration {
@@ -52,7 +51,6 @@ const useRegistration = ({
   onRedirectFail,
   onValidRegistration,
   onInvalidRegistration,
-  disabled,
 }: UseRegistrationProps): UseRegistration => {
   const { trackEvent } = useContext(AnalyticsContext);
   const { displayToast } = useToastNotification();
@@ -61,7 +59,7 @@ const useRegistration = ({
   const { data: registration, isLoading: isQueryLoading } = useQuery(
     key,
     () => initializeKratosFlow(AuthFlow.Registration),
-    { refetchOnWindowFocus: false, enabled: !disabled },
+    { refetchOnWindowFocus: false },
   );
 
   const referralTraits = useMemo(() => {
