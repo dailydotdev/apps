a49d2a56c46419d9e94766432bed15f77fb62aa8
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-07-11 21:23:41 +0800

    fix: excess margin

--- packages/shared/src/components/onboarding/MemberAlready.tsx
+++ packages/shared/src/components/onboarding/MemberAlready.tsx
@@ -26,7 +26,7 @@ export function MemberAlready({
     <span className={classNames('flex', className?.container)}>
       Already a member?
       <ClickableText
-        className={classNames('ml-1.5', className?.login)}
+        className={classNames('ml-1', className?.login)}
         onClick={onClick}
         inverseUnderline
       >
