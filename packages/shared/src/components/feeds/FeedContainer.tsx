79ba346bacae00e1299fc122406f2bb36eb94229
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-08-02 17:10:59 +0800

    refactor: taken out const

--- packages/shared/src/components/feeds/FeedContainer.tsx
+++ packages/shared/src/components/feeds/FeedContainer.tsx
@@ -23,6 +23,50 @@ export interface FeedContainerProps {
   inlineHeader?: boolean;
 }
 
+const listGaps = {
+  cozy: 'gap-5',
+  roomy: 'gap-3',
+};
+
+const gridGaps = {
+  cozy: 'gap-14',
+  roomy: 'gap-12',
+};
+
+const cardListClass = {
+  1: 'grid-cols-1',
+  2: 'grid-cols-2',
+  3: 'grid-cols-3',
+  4: 'grid-cols-4',
+  5: 'grid-cols-5',
+  6: 'grid-cols-6',
+  7: 'grid-cols-7',
+};
+
+const getFeedGapPx = {
+  'gap-2': 8,
+  'gap-3': 12,
+  'gap-5': 20,
+  'gap-8': 32,
+  'gap-12': 48,
+  'gap-14': 56,
+};
+
+const gapClass = (isList: boolean, space: Spaciness) =>
+  isList ? listGaps[space] ?? 'gap-2' : gridGaps[space] ?? 'gap-8';
+
+const cardClass = (isList: boolean, numberOfCards: number): string =>
+  isList ? 'grid-cols-1' : cardListClass[numberOfCards];
+
+const getStyle = (isList: boolean, space: Spaciness): CSSProperties => {
+  if (isList && space !== 'eco') {
+    return space === 'cozy'
+      ? { maxWidth: '48.75rem' }
+      : { maxWidth: '63.75rem' };
+  }
+  return {};
+};
+
 export const FeedContainer = ({
   children,
   forceCardMode,
@@ -39,50 +83,13 @@ export const FeedContainer = ({
   } = useContext(SettingsContext);
   const numCards = currentSettings.numCards[spaciness ?? 'eco'];
   const insaneMode = !forceCardMode && listMode;
-  const useList = insaneMode && numCards > 1;
-  const listGaps = {
-    cozy: 'gap-5',
-    roomy: 'gap-3',
-  };
-  const gridGaps = {
-    cozy: 'gap-14',
-    roomy: 'gap-12',
-  };
-  const cardListClass = {
-    1: 'grid-cols-1',
-    2: 'grid-cols-2',
-    3: 'grid-cols-3',
-    4: 'grid-cols-4',
-    5: 'grid-cols-5',
-    6: 'grid-cols-6',
-    7: 'grid-cols-7',
-  };
-  const getFeedGapPx = {
-    'gap-2': 8,
-    'gap-3': 12,
-    'gap-5': 20,
-    'gap-8': 32,
-    'gap-12': 48,
-    'gap-14': 56,
-  };
-  const gapClass = (isList: boolean, space: Spaciness) =>
-    isList ? listGaps[space] ?? 'gap-2' : gridGaps[space] ?? 'gap-8';
-  const cardClass = (isList: boolean, numberOfCards: number): string =>
-    isList ? 'grid-cols-1' : cardListClass[numberOfCards];
-  const feedGapPx = getFeedGapPx[gapClass(useList, spaciness)];
+  const isList = insaneMode && numCards > 1;
+  const feedGapPx = getFeedGapPx[gapClass(isList, spaciness)];
   const style = {
     '--num-cards': numCards,
     '--feed-gap': `${feedGapPx / 16}rem`,
   } as CSSProperties;
-  const getStyle = (isList: boolean, space: Spaciness): CSSProperties => {
-    if (isList && space !== 'eco') {
-      return space === 'cozy'
-        ? { maxWidth: '48.75rem' }
-        : { maxWidth: '63.75rem' };
-    }
-    return {};
-  };
-  const cardContainerStyle = { ...getStyle(useList, spaciness) };
+  const cardContainerStyle = { ...getStyle(isList, spaciness) };
 
   if (!loadedSettings) {
     return <></>;
@@ -97,27 +104,24 @@ export const FeedContainer = ({
       )}
     >
       <ScrollToTopButton />
-
       <div className="flex flex-col pt-2 laptopL:mx-auto w-full" style={style}>
         {!inlineHeader && header}
-
         <div
           className={classNames(
             'relative mx-auto w-full',
             styles.feed,
-            !useList && styles.cards,
+            !isList && styles.cards,
           )}
           style={cardContainerStyle}
           aria-live={subject === ToastSubject.Feed ? 'assertive' : 'off'}
           data-testid="posts-feed"
         >
           {inlineHeader && header}
-
           <div
             className={classNames(
               'grid',
-              gapClass(useList, spaciness),
-              cardClass(useList, numCards),
+              gapClass(isList, spaciness),
+              cardClass(isList, numCards),
             )}
           >
             {children}
