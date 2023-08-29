8fb40a598c5b578ac7049bf05931d6def856c573
Iorgulescu Dragos <dragos@creativepill.com>
2023-08-29 14:02:35 +0300

    Default to `code` in code snippets when language is missing

--- packages/shared/src/components/RenderMarkdown.tsx
+++ packages/shared/src/components/RenderMarkdown.tsx
@@ -146,7 +146,7 @@ const RenderMarkdown = ({
               {!inline && (
                 <div className="flex justify-between items-center py-2 px-5 bg-theme-float">
                   <span className="inline leading-8 text-theme-label-tertiary">
-                    {language}
+                    {language || 'code'}
                   </span>
 
                   <Button
