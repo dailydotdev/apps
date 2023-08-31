bbe05cacaa35213b6feefcd5a4c1765f4492c5b4
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-08-31 21:37:51 +0800

    fix: control page layout (#2144)
    Introduce a layout to make search page work on both versions.
    Have verified locally to work on both ends.
    
    WT-1763 #done

--- packages/webapp/__tests__/SearchPage.tsx
+++ packages/webapp/__tests__/SearchPage.tsx
@@ -8,6 +8,8 @@ import { mocked } from 'ts-jest/utils';
 import { NextRouter, useRouter } from 'next/router';
 import { TestBootProvider } from '../../shared/__tests__/helpers/boot';
 import SearchPage from '../pages/search';
+import { getLayout } from '../components/layouts/MainLayout';
+import { getMainFeedLayout } from '../components/layouts/MainFeedPage';
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn(),
@@ -17,7 +19,7 @@ beforeEach(() => {
   mocked(useRouter).mockImplementation(
     () =>
       ({
-        pathname: '/bookmarks',
+        pathname: '/search',
         query: {},
         push: jest.fn(),
         isReady: true,
@@ -25,18 +27,25 @@ beforeEach(() => {
   );
 });
 
-const renderComponent = (): RenderResult => {
+const renderComponent = (layout = getLayout): RenderResult => {
   const client = new QueryClient();
   const user = defaultUser;
 
   return render(
     <TestBootProvider client={client} auth={{ user }}>
-      {SearchPage.getLayout(<SearchPage />, {}, SearchPage.layoutProps)}
+      {layout(<SearchPage />, {}, {})}
     </TestBootProvider>,
   );
 };
 
-it('should render the search page', async () => {
+it('should render the search page control', async () => {
+  Features.Search.defaultValue = SearchExperiment.Control;
+  renderComponent(getMainFeedLayout);
+  const text = screen.queryByTestId('searchBar');
+  expect(text).not.toBeInTheDocument();
+});
+
+it('should render the search page v1', async () => {
   Features.Search.defaultValue = SearchExperiment.V1;
   renderComponent();
   const text = screen.queryByTestId('searchBar');
