91177ac4a7589fde38f6f0ad326c326ddb37b4a3
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-08-31 20:41:13 +0800

    fix: control page layout

---
+++ packages/webapp/components/layouts/SearchLayout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { MainLayoutProps } from '@dailydotdev/shared/src/components/MainLayout';
+import { useFeature } from '@dailydotdev/shared/src/components/GrowthBookProvider';
+import { Features } from '@dailydotdev/shared/src/lib/featureManagement';
+import { SearchExperiment } from '@dailydotdev/shared/src/lib/featureValues';
+import { getLayout } from './MainLayout';
+import { getMainFeedLayout, mainFeedLayoutProps } from './MainFeedPage';
+
+const props: Record<SearchExperiment, Record<string, unknown>> = {
+  control: { ...mainFeedLayoutProps, mobileTitle: 'Search' },
+  v1: { screenCentered: false },
+};
+
+export const GetSearchLayout = (
+  page: ReactNode,
+  pageProps?: Record<string, unknown>,
+  layoutProps: MainLayoutProps = {},
+): ReactNode => {
+  const searchVersion = useFeature(Features.Search);
+  const finalProps = { ...props[searchVersion], ...layoutProps };
+
+  if (searchVersion === SearchExperiment.V1) {
+    return getLayout(page, pageProps, finalProps);
+  }
+
+  return getMainFeedLayout(page, pageProps, finalProps);
+};
--- packages/webapp/pages/search/index.tsx
+++ packages/webapp/pages/search/index.tsx
@@ -2,9 +2,9 @@ import React, { ReactElement } from 'react';
 import { useFeature } from '@dailydotdev/shared/src/components/GrowthBookProvider';
 import { Features } from '@dailydotdev/shared/src/lib/featureManagement';
 import { SearchExperiment } from '@dailydotdev/shared/src/lib/featureValues';
-import { getLayout as getMainLayout } from '../../components/layouts/MainLayout';
 import SearchV1Page from '../../components/search/SearchV1Page';
 import SearchControlPage from '../../components/search/SearchControlPage';
+import { GetSearchLayout } from '../../components/layouts/SearchLayout';
 
 const SearchPage = (): ReactElement => {
   const searchVersion = useFeature(Features.Search);
@@ -16,7 +16,6 @@ const SearchPage = (): ReactElement => {
   return <SearchControlPage />;
 };
 
-SearchPage.getLayout = getMainLayout;
-SearchPage.layoutProps = { screenCentered: false };
+SearchPage.getLayout = GetSearchLayout;
 
 export default SearchPage;
