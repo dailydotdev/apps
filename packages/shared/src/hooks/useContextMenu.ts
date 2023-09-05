f9a8722b785ee987a993f8f62cee0316b68126c1
Ashraf Chowdury <87828904+ashrafchowdury@users.noreply.github.com>
2023-09-05 11:21:39 +0800

    fix: context menu state management (#2033)

--- packages/shared/src/hooks/useContextMenu.ts
+++ packages/shared/src/hooks/useContextMenu.ts
@@ -1,5 +1,7 @@
-import { MouseEventHandler, useCallback } from 'react';
+import { MouseEventHandler, useCallback, useMemo } from 'react';
 import { useContextMenu as useContexifyContextMenu } from '@dailydotdev/react-contexify';
+import { useQuery, useQueryClient } from 'react-query';
+import { generateQueryKey, RequestKey } from '../lib/query';
 
 interface UseContextMenuProps {
   id: string;
@@ -13,20 +15,38 @@ interface UseContextMenu {
 export default function useContextMenu({
   id,
 }: UseContextMenuProps): UseContextMenu {
+  const key = useMemo(
+    () => generateQueryKey(RequestKey.ContextMenu, null, id),
+    [id],
+  );
+  const client = useQueryClient();
   const { show, hideAll } = useContexifyContextMenu({ id });
+  const { data: isOpen } = useQuery(key, () => client.getQueryData(key));
+
+  const onIsOpen = useCallback(
+    (value: boolean) => client.setQueryData(key, value),
+    [client, key],
+  );
+
+  const onHide = useCallback(() => {
+    onIsOpen(false);
+    hideAll();
+  }, [onIsOpen, hideAll]);
 
   const onMenuClick: MouseEventHandler = useCallback(
     (e) => {
       const { right, bottom } = e.currentTarget.getBoundingClientRect();
-      show(e, {
-        position: { x: right, y: bottom + 4 },
-      });
+      const state = !isOpen;
+
+      if (state) {
+        show(e, {
+          position: { x: right, y: bottom + 4 },
+        });
+      }
+      onIsOpen(state);
     },
-    [show],
+    [show, isOpen, onIsOpen],
   );
 
-  return {
-    onMenuClick,
-    onHide: hideAll,
-  };
+  return { onMenuClick, onHide };
 }
