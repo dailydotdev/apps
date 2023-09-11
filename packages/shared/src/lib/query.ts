5fd724c03a58d9866b63f732fa20592c453a7a76
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-09-11 21:32:50 +0800

    revert: unnecessary change

--- packages/shared/src/lib/query.ts
+++ packages/shared/src/lib/query.ts
@@ -13,8 +13,8 @@ export type MutateFunc<T> = (variables: T) => Promise<(() => void) | undefined>;
 export const generateQueryKey = (
   name: string | RequestKey,
   user: Pick<LoggedUser, 'id'> | null,
-  ...additional: QueryKey[]
-): QueryKey => {
+  ...additional: unknown[]
+): unknown => {
   return [name, user?.id ?? 'anonymous', ...additional];
 };
 
