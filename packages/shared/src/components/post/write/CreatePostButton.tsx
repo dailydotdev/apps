a61cc1235b9929c4d6dfea2d79fbc19f8e8dc3b4
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-09-29 14:57:20 +0200

    fix: pre-select squad on new post button

--- packages/shared/src/components/post/write/CreatePostButton.tsx
+++ packages/shared/src/components/post/write/CreatePostButton.tsx
@@ -1,10 +1,14 @@
 import React, { ReactElement } from 'react';
 import classNames from 'classnames';
+import { useRouter } from 'next/router';
 import { Button, ButtonSize } from '../../buttons/Button';
 import { link } from '../../../lib/links';
 import { useAuthContext } from '../../../contexts/AuthContext';
 import useMedia from '../../../hooks/useMedia';
 import { laptop } from '../../../styles/media';
+import { useSquad } from '../../../hooks';
+import { verifyPermission } from '../../../graphql/squads';
+import { SourcePermissions } from '../../../graphql/sources';
 
 interface CreatePostButtonProps {
   className?: string;
@@ -14,7 +18,12 @@ export function CreatePostButton({
   className,
 }: CreatePostButtonProps): ReactElement {
   const { user, isAuthReady, squads } = useAuthContext();
+  const { route, query } = useRouter();
   const isTablet = !useMedia([laptop.replace('@media ', '')], [true], false);
+
+  const handle = route === '/squads/[handle]' ? (query.handle as string) : '';
+  const { squad } = useSquad({ handle });
+  const allowedToPost = verifyPermission(squad, SourcePermissions.Post);
 
   if (!user || !isAuthReady || !squads?.length) {
     return null;
@@ -24,7 +33,10 @@ export function CreatePostButton({
     <Button
       className={classNames('btn-secondary', className)}
       tag="a"
-      href={link.post.create}
+      href={
+        link.post.create +
+        (squad && allowedToPost ? `?sid=${squad.handle}` : '')
+      }
       buttonSize={isTablet ? ButtonSize.Small : ButtonSize.Medium}
     >
       New post
