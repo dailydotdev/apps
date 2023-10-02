eb5240a9b3a7480c5e22a07d0bc284978b39c10e
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-10-02 23:39:32 +0800

    fix: import

--- packages/webapp/pages/posts/finder.tsx
+++ packages/webapp/pages/posts/finder.tsx
@@ -1,7 +1,3 @@
-import SearchControlPage from '../../../components/search/SearchControlPage';
+import SearchControlPage from '../../components/search/SearchControlPage';
 
-const PostFinder = () => {
-  return <h1>Post Finder</h1>;
-};
-
-export default PostFinder;
+export default SearchControlPage;
