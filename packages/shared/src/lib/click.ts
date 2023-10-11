1d90e16e121ccec1596a08b520c1a06f0bd78962
Chris Bongers <chrisbongers@gmail.com>
2023-10-11 15:35:03 +0200

    fix: replace mouseUp with auxClick (#2270)
    Replace mouseUp with auxClick
    Verified locally on events, will do extra check once deployed as well

--- packages/shared/src/lib/click.ts
+++ packages/shared/src/lib/click.ts
@@ -1,14 +1,14 @@
 import React from 'react';
 
 type CombinedClicks = {
-  onMouseUp: React.MouseEventHandler<HTMLAnchorElement>;
+  onAuxClick: React.MouseEventHandler<HTMLAnchorElement>;
   onClick: React.MouseEventHandler<HTMLAnchorElement>;
 };
 export const combinedClicks = (
   func: React.MouseEventHandler | ((e: React.MouseEvent, any?) => void),
 ): CombinedClicks => {
   return {
-    onMouseUp: (event) => event.button === 1 && func(event),
+    onAuxClick: (event) => event.button === 1 && func(event),
     onClick: func,
   };
 };
