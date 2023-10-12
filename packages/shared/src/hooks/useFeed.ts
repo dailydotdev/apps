e485262cb6de115306dd02911b8a6578136e2958
Chris Bongers <chrisbongers@gmail.com>
2023-10-12 10:40:27 +0200

    fix: refactor ad placement for squads

--- packages/shared/src/hooks/useFeed.ts
+++ packages/shared/src/hooks/useFeed.ts
@@ -56,10 +56,15 @@ const findIndexOfPostInData = (
   return { pageIndex: -1, index: -1 };
 };
 
+type UseFeedSettingParams = {
+  adPostLength?: number;
+};
+
 export interface UseFeedOptionalParams<T> {
   query?: string;
   variables?: T;
   options?: UseInfiniteQueryOptions<FeedData>;
+  settings?: UseFeedSettingParams;
 }
 
 export default function useFeed<T>(
@@ -69,7 +74,7 @@ export default function useFeed<T>(
   placeholdersPerPage: number,
   params: UseFeedOptionalParams<T> = {},
 ): FeedReturnType {
-  const { query, variables, options = {} } = params;
+  const { query, variables, options = {}, settings } = params;
   const { user, tokenRefreshed } = useContext(AuthContext);
   const queryClient = useQueryClient();
 
@@ -131,15 +136,21 @@ export default function useFeed<T>(
             page: pageIndex,
             index,
           }));
-          if (adsQuery.data?.pages[pageIndex]) {
-            posts.splice(adSpot, 0, {
-              type: 'ad',
-              ad: adsQuery.data?.pages[pageIndex],
-            });
-          } else {
-            posts.splice(adSpot, 0, {
-              type: 'placeholder',
-            });
+
+          if (
+            !settings?.adPostLength ||
+            posts.length > settings?.adPostLength
+          ) {
+            if (adsQuery.data?.pages[pageIndex]) {
+              posts.splice(adSpot, 0, {
+                type: 'ad',
+                ad: adsQuery.data?.pages[pageIndex],
+              });
+            } else {
+              posts.splice(adSpot, 0, {
+                type: 'placeholder',
+              });
+            }
           }
           return posts;
         },
