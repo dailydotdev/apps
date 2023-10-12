c0cae34e55abcc61907c5182094bf630040c0dcf
capJavert <ante@kickass.website>
2023-10-12 15:18:09 +0200

    feat: add fallback option

--- packages/shared/src/components/withFeaturesBoundary.tsx
+++ packages/shared/src/components/withFeaturesBoundary.tsx
@@ -3,12 +3,13 @@ import { FeaturesReadyContext } from './GrowthBookProvider';
 
 const withFeaturesBoundary = <Props,>(
   WrappedComponent: ComponentType<Props>,
+  options?: { fallback?: ReactElement },
 ): ComponentType<Props> => {
   const WithFeaturesBoundary = (props: Props): ReactElement => {
     const { ready: areFeaturesReady } = useContext(FeaturesReadyContext);
 
     if (!areFeaturesReady) {
-      return null;
+      return options?.fallback ?? null;
     }
 
     return <WrappedComponent {...props} />;
