f16e7b5a2d21b4cf39cb9d3e806126b30785ae01
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-10-20 00:05:02 +0800

    fix: unused import

--- packages/shared/src/lib/links.ts
+++ packages/shared/src/lib/links.ts
@@ -1,5 +1,4 @@
 import { webappUrl } from './constants';
-import { EmptyObjectLiteral } from './kratos';
 
 export const getTagPageLink = (tag: string): string =>
   `${process.env.NEXT_PUBLIC_WEBAPP_URL}tags/${encodeURIComponent(tag)}`;
