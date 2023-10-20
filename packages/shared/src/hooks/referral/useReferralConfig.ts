5a158e36b8c889bc2792e8c6ca131be03eecd51a
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-10-20 18:41:45 +0800

    refactor: function to constants

--- packages/shared/src/hooks/referral/useReferralConfig.ts
+++ packages/shared/src/hooks/referral/useReferralConfig.ts
@@ -11,27 +11,25 @@ export const useReferralConfig = ({
   campaign,
   referringUser,
 }: UseReferralConfigProps): CampaignConfig => {
-  const getDefaultTitle = () =>
-    `${referringUser.name} invites you to use daily.dev`;
-  const getDefaultDescription = () =>
-    `daily dev is a professional network for developers to learn, collaborate, and grow together. Developers come to daily.dev to discover a wide variety of professional knowledge, create groups where they can collaborate with other developers they appreciate, and discuss the latest trends in the developer ecosystem.`;
-  const getDefaultImages = () => [
+  const defaultTitle = `${referringUser.name} invites you to use daily.dev`;
+  const defaultDescription = `daily dev is a professional network for developers to learn, collaborate, and grow together. Developers come to daily.dev to discover a wide variety of professional knowledge, create groups where they can collaborate with other developers they appreciate, and discuss the latest trends in the developer ecosystem.`;
+  const defaultImages = [
     { url: `https://og.daily.dev/api/refs/${referringUser.id}` },
   ];
 
   if (campaign === ReferralCampaignKey.Search) {
     return {
-      title: getDefaultTitle(),
-      description: getDefaultDescription(),
-      images: getDefaultImages(),
+      title: defaultTitle,
+      description: defaultDescription,
+      images: defaultImages,
       redirectTo: '/my-feed',
     };
   }
 
   return {
-    title: getDefaultTitle(),
-    description: getDefaultDescription(),
-    images: getDefaultImages(),
+    title: defaultTitle,
+    description: defaultDescription,
+    images: defaultImages,
     redirectTo: '/',
   };
 };
