1713bd609a57601a503a56582dadeb05a9223464
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-10-23 18:55:15 +0800

    fix: source feed

--- packages/shared/src/lib/query.ts
+++ packages/shared/src/lib/query.ts
@@ -14,6 +14,7 @@ import { MainFeedPage } from '../components/utilities';
 
 export enum OtherFeedPage {
   Squad = 'squad',
+  Source = 'source',
   Bookmarks = 'bookmarks',
   SearchBookmarks = 'search-bookmarks',
 }
