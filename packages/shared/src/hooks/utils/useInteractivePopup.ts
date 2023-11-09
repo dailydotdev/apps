64b0a7e6bc88b1d047ca755b2a91cdc204b904f3
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-11-09 22:36:07 +0800

    chore: comment fopr understanding

--- packages/shared/src/hooks/utils/useInteractivePopup.ts
+++ packages/shared/src/hooks/utils/useInteractivePopup.ts
@@ -8,6 +8,9 @@ interface UseInteractivePopup {
   wrapHandler: (callback: Fn) => Fn;
 }
 
+// We introduced this hook to wrap the issue where, if you click the trigger to open the popup
+// And your popup closes on click outside, the processing time would be so fast the listener is ready even before you release your mouse
+// It would result to the popup being closed. To prevent it, we stop the propagation instead of setting a timeout before adding the window event listeners
 export const useInteractivePopup = (): UseInteractivePopup => {
   const [isOpen, setIsOpen] = useState(false);
   const handleTrigger = useCallback((callback: Fn): Fn => {
