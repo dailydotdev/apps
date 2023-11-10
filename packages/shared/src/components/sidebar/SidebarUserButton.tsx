34d95d697453f32201ea7aae49140bfa9003f82a
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2023-11-10 17:07:58 +0800

    refactor: profile menu (#2334)

--- packages/shared/src/components/sidebar/SidebarUserButton.tsx
+++ packages/shared/src/components/sidebar/SidebarUserButton.tsx
@@ -1,21 +1,10 @@
 import React, { ReactElement, useContext } from 'react';
-import dynamic from 'next/dynamic';
 import { ProfileLink } from '../profile/ProfileLink';
 import { ProfilePicture } from '../ProfilePicture';
-import { Button } from '../buttons/Button';
-import useProfileMenu from '../../hooks/useProfileMenu';
 import AuthContext from '../../contexts/AuthContext';
-import SettingsIcon from '../icons/Settings';
 import { SidebarUserButtonProps } from './common';
 import LoginButton from '../LoginButton';
-
-// @NOTE see https://dailydotdev.atlassian.net/l/cp/dK9h1zoM
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const { onMenuClick } = useProfileMenu();
-
-const ProfileMenu = dynamic(
-  () => import(/* webpackChunkName: "profileMenu" */ '../ProfileMenu'),
-);
+import ProfileButton from '../profile/ProfileButton';
 
 export function SidebarUserButton({
   sidebarRendered,
@@ -36,18 +25,12 @@ export function SidebarUserButton({
                   <ProfilePicture user={user} size="medium" />
                   <span className="mr-3 ml-2">{user.reputation ?? 0}</span>
                 </ProfileLink>
-                <Button
-                  iconOnly
-                  className="btn btn-tertiary"
-                  onClick={onMenuClick}
-                  icon={<SettingsIcon />}
-                />
+                <ProfileButton atMobileSidebar />
               </div>
               <strong className="mb-0.5 typo-callout">{user.name}</strong>
               <p className="typo-footnote text-theme-label-secondary">
                 @{user.username}
               </p>
-              <ProfileMenu />
             </>
           ) : (
             <LoginButton />
