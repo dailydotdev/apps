742139d5f2c651ed0495e8ac4b890abdf57531eb
nensidosari <36197304+nensidosari@users.noreply.github.com>
2023-11-14 08:37:09 +0100

    feat: referral profile widget (#2343)
    * feat: new widget for referra invite
    
    * fix: make widget responsive in all screens
    
    * feat: added functionality to copy and share buttons
    
    * fix: remove unneccesasry changes
    
    * fix: add right analytics target id
    
    * fix: added disabled state to referral widget
    
    * fix: replacing card with div
    
    * fix: add disabled at input field for url
    
    * fix: replace named colors
    
    * fix: referral widget not query if user not viewing own profile
    
    * refactor: moved social share buttons on referral on separate component
    
    * fix: adding missing analytics from merge
    
    * fix: review feedback adjustments
    
    * feat: adding analytics to social share icons
    
    * fix: small feedback form review

---
+++ packages/shared/src/components/widgets/ReferralWidget.tsx
@@ -0,0 +1,69 @@
+import React, { ReactElement } from 'react';
+import { Button, ButtonSize } from '../buttons/Button';
+import { TextField } from '../fields/TextField';
+import { link } from '../../lib/links';
+import { useShareOrCopyLink } from '../../hooks/useShareOrCopyLink';
+import { labels } from '../../lib';
+import { AnalyticsEvent, TargetId, TargetType } from '../../lib/analytics';
+import ReferralSocialShareButtons from './ReferralSocialShareButtons';
+
+const ReferralWidget = ({ url }: { url: string }): ReactElement => {
+  const inviteLink = url || link.referral.defaultUrl;
+  const [, onShareOrCopyLink] = useShareOrCopyLink({
+    text: labels.referral.generic.inviteText,
+    link: inviteLink,
+    trackObject: () => ({
+      event_name: AnalyticsEvent.CopyReferralLink,
+      target_id: TargetId.ProfilePage,
+    }),
+  });
+
+  return (
+    <div
+      data-testid="referral-widget"
+      className="flex laptopL:absolute laptopL:left-full flex-col tablet:flex-row laptopL:flex-col flex-wrap justify-between p-4 laptopL:m-6 mt-6 laptopL:mt-0 mb-4 laptopL:max-w-widget laptopL:h-auto rounded-2xl border border-theme-divider-tertiary max-w-fit bg-theme-bg-primary"
+    >
+      <h3 className="mb-2 font-bold typo-title3">Invite friends</h3>
+      <p className="text-theme-label-secondary typo-callout">
+        Tell your dev friends how easy is it to learn, collaborate, and grow
+        together
+      </p>
+      <TextField
+        name="inviteURL"
+        inputId="inviteURL"
+        label="Your unique invite URL"
+        autoComplete="off"
+        type="url"
+        value={inviteLink}
+        fieldType="tertiary"
+        className={{
+          input: 'typo-footnote',
+          container: 'flex flex-col my-5 w-auto tablet:w-70',
+        }}
+        actionButton={
+          <Button
+            buttonSize={ButtonSize.XSmall}
+            className="btn-primary"
+            onClick={() => onShareOrCopyLink()}
+          >
+            Copy link
+          </Button>
+        }
+        readOnly
+      />
+      <div className="flex justify-between items-center">
+        <p className="mr-3 typo-callout text-theme-label-tertiary">
+          Invite with
+        </p>
+        <span className="flex gap-2">
+          <ReferralSocialShareButtons
+            url={url}
+            targetType={TargetType.ProfilePage}
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ReferralWidget;
