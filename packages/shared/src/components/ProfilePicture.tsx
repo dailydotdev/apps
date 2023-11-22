270e1fa31e5da6e713a6078d0a670696be54d179
dragosiorgulescu <dragos.m.iorgulescu@gmail.com>
2023-11-22 10:30:08 +0200

    chore: add `+x` to profile picture group

--- packages/shared/src/components/ProfilePicture.tsx
+++ packages/shared/src/components/ProfilePicture.tsx
@@ -128,14 +128,30 @@ const Group: React.FC<{
   total?: number;
 }> = ({ children, limit = 3, total }) => {
   const childrenMap = React.Children.toArray(children).slice(0, limit);
+  const remainingCount = total ? total - childrenMap.length : 0;
 
+  if (remainingCount) {
+    childrenMap.push(
+      <div
+        className={classNames(
+          'flex items-center justify-center',
+          'rounded-full',
+          'w-8 h-8',
+          'font-bold',
+          'bg-theme-bg-secondary',
+        )}
+      >
+        +{remainingCount}
+      </div>,
+    );
+  }
   return (
     <div className="flex">
       {childrenMap.map(
         (child: React.ReactElement<ProfilePictureProps>, index) =>
           React.cloneElement(child, {
             className: classNames(
-              index === 0 && 'mr-[-20%]',
+              index !== 0 && 'ml-[-10%]',
               'border-2 border-theme-bg-secondary',
               `z-${childrenMap.length - index}`,
               child.props.className,
