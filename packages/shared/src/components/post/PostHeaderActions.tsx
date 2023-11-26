dd0114a30e28d1435e96af35b3f4ee830a882f2d
NensiDosari <nensidosari97@gmail.com>
2023-11-26 01:22:59 +0100

    refactor: extracted post menu options in own component for reusability

--- packages/shared/src/components/post/PostHeaderActions.tsx
+++ packages/shared/src/components/post/PostHeaderActions.tsx
@@ -1,44 +1,14 @@
-import React, {
-  CSSProperties,
-  KeyboardEventHandler,
-  MouseEventHandler,
-  ReactElement,
-  useContext,
-} from 'react';
+import React, { ReactElement, useContext } from 'react';
 import classNames from 'classnames';
-import MenuIcon from '../icons/Menu';
-import CloseIcon from '../icons/MiniClose';
 import OpenLinkIcon from '../icons/OpenLink';
-import { Roles } from '../../lib/user';
-import AuthContext from '../../contexts/AuthContext';
-import {
-  banPost,
-  demotePost,
-  internalReadTypes,
-  Post,
-  promotePost,
-} from '../../graphql/posts';
+import { internalReadTypes } from '../../graphql/posts';
 import classed from '../../lib/classed';
 import { SimpleTooltip } from '../tooltips/SimpleTooltip';
 import { Button } from '../buttons/Button';
-import PostOptionsMenu, { PostOptionsMenuProps } from '../PostOptionsMenu';
-import { ShareBookmarkProps } from './PostActions';
-import { PromptOptions, usePrompt } from '../../hooks/usePrompt';
 import SettingsContext from '../../contexts/SettingsContext';
+import { PostHeaderActionsProps } from './common';
+import { PostMenuOptions } from './PostMenuOptions';
 import { Origin } from '../../lib/analytics';
-import useContextMenu from '../../hooks/useContextMenu';
-
-export interface PostHeaderActionsProps extends ShareBookmarkProps {
-  post: Post;
-  onReadArticle?: () => void;
-  onClose?: MouseEventHandler | KeyboardEventHandler;
-  className?: string;
-  style?: CSSProperties;
-  inlineActions?: boolean;
-  notificationClassName?: string;
-  contextMenuId: string;
-  onRemovePost?: PostOptionsMenuProps['onRemovePost'];
-}
 
 const Container = classed('div', 'flex flex-row items-center');
 
@@ -55,47 +25,8 @@ export function PostHeaderActions({
   ...props
 }: PostHeaderActionsProps): ReactElement {
   const { openNewTab } = useContext(SettingsContext);
-  const { user } = useContext(AuthContext);
-  const { showPrompt } = usePrompt();
-  const { onMenuClick, isOpen } = useContextMenu({ id: contextMenuId });
 
   const isInternalReadType = internalReadTypes.includes(post?.type);
-  const isModerator = user?.roles?.includes(Roles.Moderator);
-
-  const banPostPrompt = async () => {
-    const options: PromptOptions = {
-      title: 'Ban post 💩',
-      description: 'Are you sure you want to ban this post?',
-      okButton: {
-        title: 'Ban',
-        className: 'btn-primary-ketchup',
-      },
-    };
-    if (await showPrompt(options)) {
-      await banPost(post.id);
-    }
-  };
-
-  const promotePostPrompt = async () => {
-    const promoteFlag = post.flags?.promoteToPublic;
-
-    const options: PromptOptions = {
-      title: promoteFlag ? 'Demote post' : 'Promote post',
-      description: `Do you want to ${
-        promoteFlag ? 'demote' : 'promote'
-      } this post ${promoteFlag ? 'from' : 'to'} the public?`,
-      okButton: {
-        title: promoteFlag ? 'Demote' : 'Promote',
-      },
-    };
-    if (await showPrompt(options)) {
-      if (promoteFlag) {
-        await demotePost(post.id);
-      } else {
-        await promotePost(post.id);
-      }
-    }
-  };
 
   return (
     <Container {...props} className={classNames('gap-2', className)}>
@@ -118,31 +49,14 @@ export function PostHeaderActions({
           </Button>
         </SimpleTooltip>
       )}
-      <SimpleTooltip placement="bottom" content="Options">
-        <Button
-          className={classNames('btn-tertiary', !inlineActions && 'ml-auto')}
-          icon={<MenuIcon />}
-          onClick={onMenuClick}
-        />
-      </SimpleTooltip>
-      {onClose && (
-        <SimpleTooltip placement="bottom" content="Close">
-          <Button
-            className="btn-tertiary"
-            icon={<CloseIcon />}
-            onClick={(e) => onClose(e)}
-          />
-        </SimpleTooltip>
-      )}
-      <PostOptionsMenu
+      <PostMenuOptions
         onShare={onShare}
         post={post}
+        onClose={onClose}
+        inlineActions={inlineActions}
+        contextMenuId={contextMenuId}
         onRemovePost={onRemovePost}
-        setShowBanPost={isModerator ? () => banPostPrompt() : null}
-        setShowPromotePost={isModerator ? () => promotePostPrompt() : null}
-        contextId={contextMenuId}
         origin={Origin.ArticleModal}
-        isOpen={isOpen}
       />
     </Container>
   );
--- packages/shared/src/components/post/collection/CollectionPostHeaderActions.tsx
+++ packages/shared/src/components/post/collection/CollectionPostHeaderActions.tsx
@@ -1,37 +1,13 @@
-import React, {
-  CSSProperties,
-  KeyboardEventHandler,
-  MouseEventHandler,
-  ReactElement,
-  useContext,
-} from 'react';
+import React, { ReactElement, useContext } from 'react';
 import classNames from 'classnames';
-import MenuIcon from '../../icons/Menu';
-import CloseIcon from '../../icons/MiniClose';
-import { Roles } from '../../../lib/user';
-import AuthContext from '../../../contexts/AuthContext';
-import { banPost, demotePost, Post, promotePost } from '../../../graphql/posts';
 import classed from '../../../lib/classed';
 import { SimpleTooltip } from '../../tooltips/SimpleTooltip';
 import { Button } from '../../buttons/Button';
-import PostOptionsMenu, { PostOptionsMenuProps } from '../../PostOptionsMenu';
-import { ShareBookmarkProps } from '../PostActions';
-import { PromptOptions, usePrompt } from '../../../hooks/usePrompt';
 import SettingsContext from '../../../contexts/SettingsContext';
 import { Origin } from '../../../lib/analytics';
-import useContextMenu from '../../../hooks/useContextMenu';
 import BellIcon from '../../icons/Bell';
-
-export interface PostHeaderActionsProps extends ShareBookmarkProps {
-  post: Post;
-  onClose?: MouseEventHandler | KeyboardEventHandler;
-  className?: string;
-  style?: CSSProperties;
-  inlineActions?: boolean;
-  notificationClassName?: string;
-  contextMenuId: string;
-  onRemovePost?: PostOptionsMenuProps['onRemovePost'];
-}
+import { PostHeaderActionsProps } from '../common';
+import { PostMenuOptions } from '../PostMenuOptions';
 
 const Container = classed('div', 'flex flex-row items-center');
 
@@ -47,46 +23,6 @@ const CollectionPostHeaderActions = ({
   ...props
 }: PostHeaderActionsProps): ReactElement => {
   const { openNewTab } = useContext(SettingsContext);
-  const { user } = useContext(AuthContext);
-  const { showPrompt } = usePrompt();
-  const { onMenuClick, isOpen } = useContextMenu({ id: contextMenuId });
-
-  const isModerator = user?.roles?.includes(Roles.Moderator);
-
-  const banPostPrompt = async () => {
-    const options: PromptOptions = {
-      title: 'Ban post 💩',
-      description: 'Are you sure you want to ban this post?',
-      okButton: {
-        title: 'Ban',
-        className: 'btn-primary-ketchup',
-      },
-    };
-    if (await showPrompt(options)) {
-      await banPost(post.id);
-    }
-  };
-
-  const promotePostPrompt = async () => {
-    const promoteFlag = post.flags?.promoteToPublic;
-
-    const options: PromptOptions = {
-      title: promoteFlag ? 'Demote post' : 'Promote post',
-      description: `Do you want to ${
-        promoteFlag ? 'demote' : 'promote'
-      } this post ${promoteFlag ? 'from' : 'to'} the public?`,
-      okButton: {
-        title: promoteFlag ? 'Demote' : 'Promote',
-      },
-    };
-    if (await showPrompt(options)) {
-      if (promoteFlag) {
-        await demotePost(post.id);
-      } else {
-        await promotePost(post.id);
-      }
-    }
-  };
 
   return (
     <Container {...props} className={classNames('gap-2', className)}>
@@ -110,31 +46,14 @@ const CollectionPostHeaderActions = ({
       </SimpleTooltip>
 
       {/* TODO: check if menu is the same */}
-      <SimpleTooltip placement="bottom" content="Options">
-        <Button
-          className={classNames('btn-tertiary', !inlineActions && 'ml-auto')}
-          icon={<MenuIcon />}
-          onClick={onMenuClick}
-        />
-      </SimpleTooltip>
-      {onClose && (
-        <SimpleTooltip placement="bottom" content="Close">
-          <Button
-            className="btn-tertiary"
-            icon={<CloseIcon />}
-            onClick={(e) => onClose(e)}
-          />
-        </SimpleTooltip>
-      )}
-      <PostOptionsMenu
+      <PostMenuOptions
         onShare={onShare}
         post={post}
+        onClose={onClose}
+        inlineActions={inlineActions}
+        contextMenuId={contextMenuId}
         onRemovePost={onRemovePost}
-        setShowBanPost={isModerator ? () => banPostPrompt() : null}
-        setShowPromotePost={isModerator ? () => promotePostPrompt() : null}
-        contextId={contextMenuId}
         origin={Origin.CollectionModal}
-        isOpen={isOpen}
       />
     </Container>
   );
