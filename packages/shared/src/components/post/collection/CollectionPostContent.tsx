003d61e0a07ad99418b9b34bb7dfa87ef2dbc5a8
Ante Barić <ante@kickass.website>
2023-11-29 09:05:16 +0100

    Update packages/shared/src/components/post/collection/CollectionPostContent.tsx
    Co-authored-by: Chris Bongers <chrisbongers@gmail.com>

--- packages/shared/src/components/post/collection/CollectionPostContent.tsx
+++ packages/shared/src/components/post/collection/CollectionPostContent.tsx
@@ -121,7 +121,7 @@ const CollectionPostContent = ({
           />
 
           <h1
-            className="font-bold break-words typo-large-title"
+            className="font-bold break-words typo-headline"
             data-testid="post-modal-title"
           >
             {post.title}
