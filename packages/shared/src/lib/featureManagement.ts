ab347173fcab2e3cb578afff77b0cd0a02a24a63
Chris Bongers <chrisbongers@gmail.com>
2023-11-29 13:38:56 +0200

    fix: set onboarding v4 as the new default (#2376)
    Removal of the onboarding v3 on onboarding page and just run v4 as default
    Allowed removal of experiment code.
    WT-2012

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -1,9 +1,5 @@
 import { JSONValue } from '@growthbook/growthbook';
-import {
-  OnboardingFilteringTitle,
-  SearchExperiment,
-  OnboardingV4,
-} from './featureValues';
+import { SearchExperiment } from './featureValues';
 
 export class Feature<T extends JSONValue> {
   readonly id: string;
@@ -18,12 +14,7 @@ export class Feature<T extends JSONValue> {
 
 const feature = {
   feedVersion: new Feature('feed_version', 15),
-  onboardingV4: new Feature('onboarding_v4', OnboardingV4.Control),
   search: new Feature('search', SearchExperiment.Control),
-  onboardingFilterTitle: new Feature(
-    'onboarding_filtering_title',
-    OnboardingFilteringTitle.Control,
-  ),
   lowImps: new Feature('feed_low_imps'),
 };
 
