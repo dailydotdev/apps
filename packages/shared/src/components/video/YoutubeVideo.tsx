9796aafef0b4815b64527a49fed8ba3bf75e9eee
NensiDosari <nensidosari97@gmail.com>
2023-12-01 12:55:27 +0100

    feat: create video component wrapper

---
+++ packages/shared/src/components/video/YoutubeVideo.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement } from 'react';
+import classNames from 'classnames';
+
+interface YoutubeVideoProps {
+  videoId: string;
+  className?: string;
+  title: string;
+}
+
+const YoutubeVideo = ({
+  videoId,
+  className,
+  title,
+  ...props
+}: YoutubeVideoProps): ReactElement => (
+  <div
+    className={classNames(
+      'overflow-hidden relative pt-[56.25%] w-full',
+      className,
+    )}
+  >
+    <iframe
+      title={title}
+      src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      allowFullScreen
+      className="absolute inset-0 w-full h-full border-0 aspect-video"
+      {...props}
+    />
+  </div>
+);
+
+export default YoutubeVideo;
