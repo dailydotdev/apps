53e39ee58836344a2ad1e6a631530c0580968efc
Milan Freml <milan@daily.dev>
2023-12-01 10:01:05 +0100

    feat: button V2 implementation

---
+++ packages/shared/src/components/buttons/ButtonV2.spec.tsx
@@ -0,0 +1,95 @@
+import { render, RenderResult, screen } from '@testing-library/react';
+import React, { HTMLAttributes } from 'react';
+import { Button, BaseButtonProps, ButtonKind, ButtonVariant } from './ButtonV2';
+import UpvoteIcon from '../icons/Upvote';
+
+const renderComponent = <
+  C extends HTMLElement = HTMLButtonElement,
+  P = HTMLAttributes<C>,
+>(
+  props: Partial<BaseButtonProps & P> = {},
+): RenderResult => {
+  return render(<Button {...props} />);
+};
+
+it('should render children', async () => {
+  renderComponent({ children: 'Button' });
+  expect(await screen.findByText('Button')).toBeInTheDocument();
+});
+
+it('should render primary button', async () => {
+  renderComponent({ variant: ButtonVariant.Primary, children: 'Primary' });
+  expect(await screen.findByRole('button')).toBeInTheDocument();
+  expect(await screen.findByRole('button')).toHaveClass('btn-primary');
+});
+
+it('should render secondary button with color', async () => {
+  renderComponent({
+    variant: ButtonVariant.Secondary,
+    color: 'burger',
+    children: 'Secondary burger',
+  });
+  expect(await screen.findByRole('button')).toBeInTheDocument();
+  expect(await screen.findByRole('button')).toHaveClass('btn-secondary-burger');
+});
+
+it('color prop can be overriden with className', async () => {
+  renderComponent({
+    variant: ButtonVariant.Secondary,
+    color: 'burger',
+    children: 'Secondary burger',
+    className: 'btn-secondary-bacon',
+  });
+
+  const button = await screen.findByRole('button');
+  expect(button).toBeInTheDocument();
+
+  expect(button).toHaveClass('btn-secondary-burger');
+  expect(button).toHaveClass('btn-secondary-bacon');
+
+  expect(button.className.indexOf('btn-secondary-burger')).toBeLessThan(
+    button.className.indexOf('btn-secondary-bacon'),
+  );
+});
+
+it('providing color without variant does not do anything', async () => {
+  renderComponent({ color: 'burger', children: 'Button' });
+  expect(await screen.findByRole('button')).toBeInTheDocument();
+  expect((await screen.findByRole('button')).className).not.toContain('burger');
+});
+
+it('should render icon', async () => {
+  renderComponent({ icon: <UpvoteIcon data-testid="icon" /> });
+  expect(await screen.findByTestId('icon')).toBeInTheDocument();
+});
+
+it('should render right icon', async () => {
+  renderComponent({ rightIcon: <UpvoteIcon data-testid="right-icon" /> });
+  expect(await screen.findByTestId('right-icon')).toBeInTheDocument();
+});
+
+it('should render loader and set aria-busy when loading', async () => {
+  renderComponent({ children: 'Button', loading: true });
+  expect(await screen.findByRole('button')).toHaveAttribute(
+    'aria-busy',
+    'true',
+  );
+  expect(await screen.findByTestId('buttonLoader')).toBeInTheDocument();
+});
+
+it('should set aria-pressed when pressed is true', async () => {
+  renderComponent({ children: 'Button', pressed: true });
+  expect(await screen.findByRole('button')).toHaveAttribute(
+    'aria-pressed',
+    'true',
+  );
+});
+
+it('should render the button as an anchor element', async () => {
+  renderComponent({
+    children: 'Button',
+    kind: ButtonKind.Link,
+    href: 'https://daily.dev',
+  });
+  expect(await screen.findByRole('link')).toBeInTheDocument();
+});
---
+++ packages/shared/src/components/buttons/ButtonV2.tsx
@@ -0,0 +1,134 @@
+import React, {
+  forwardRef,
+  HTMLAttributes,
+  ReactElement,
+  ReactNode,
+  Ref,
+} from 'react';
+import classNames from 'classnames';
+import { IconProps, IconSize } from '../Icon';
+import { Loader } from '../Loader';
+import { combinedClicks } from '../../lib/click';
+
+import { ColorName } from '../../styles/colors';
+
+export enum ButtonSize {
+  XXSmall = 'xxsmall',
+  XSmall = 'xsmall',
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+  XLarge = 'xlarge',
+}
+
+const buttonSizeToIconSize: Record<ButtonSize, IconSize> = {
+  [ButtonSize.XXSmall]: IconSize.XXSmall,
+  [ButtonSize.XSmall]: IconSize.XSmall,
+  [ButtonSize.Small]: IconSize.Small,
+  [ButtonSize.Medium]: IconSize.Medium,
+  [ButtonSize.Large]: IconSize.Large,
+  [ButtonSize.XLarge]: IconSize.XLarge,
+};
+
+export type IconType = React.ReactElement<IconProps>;
+
+export type ButtonColor = ColorName;
+
+export enum ButtonKind {
+  Button = 'button',
+  Link = 'link',
+}
+
+export enum ButtonVariant {
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Tertiary = 'tertiary',
+}
+
+export interface BaseButtonProps {
+  kind?: ButtonKind;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  icon?: IconType;
+  rightIcon?: IconType;
+  loading?: boolean;
+  pressed?: boolean;
+  disabled?: boolean;
+  children?: ReactNode;
+}
+
+const useGetIconWithSize = (size: ButtonSize, iconOnly: boolean) => {
+  return (icon: React.ReactElement<IconProps>) =>
+    React.cloneElement(icon, {
+      size: icon.props?.size ?? buttonSizeToIconSize[size],
+      className: classNames(icon.props.className, !iconOnly && 'icon'),
+    });
+};
+
+export type AllowedTags = keyof Pick<JSX.IntrinsicElements, 'a' | 'button'>;
+export type AllowedElements = HTMLButtonElement | HTMLAnchorElement;
+export type ButtonElementType<Tag extends AllowedTags> = Tag extends 'a'
+  ? HTMLAnchorElement
+  : HTMLButtonElement;
+
+export type ButtonProps<Tag extends AllowedTags> = BaseButtonProps &
+  HTMLAttributes<Tag> &
+  JSX.IntrinsicElements[Tag] & {
+    ref?: Ref<ButtonElementType<Tag>>;
+  };
+
+function ButtonComponent<T extends AllowedTags>(
+  {
+    kind = ButtonKind.Button,
+    variant,
+    size,
+    color,
+    className,
+    icon,
+    rightIcon,
+    loading,
+    pressed,
+    children,
+    onClick,
+    ...props
+  }: ButtonProps<T>,
+  ref?: Ref<ButtonElementType<T>>,
+): ReactElement {
+  const iconOnly = icon && !children && !rightIcon;
+  const getIconWithSize = useGetIconWithSize(size, iconOnly);
+  const isAnchor = kind === ButtonKind.Link;
+  const Tag = isAnchor ? 'a' : 'button';
+
+  return (
+    <Tag
+      {...props}
+      {...(isAnchor ? combinedClicks(onClick) : { onClick })}
+      aria-busy={loading}
+      aria-pressed={pressed}
+      ref={ref}
+      className={classNames(
+        { iconOnly },
+        size,
+        'btn flex flex-row items-center justify-center border typo-callout font-bold no-underline shadow-none cursor-pointer select-none focus-outline relative',
+        {
+          [`btn-${variant}`]: variant,
+          [`btn-${variant}-${color}`]: color && variant,
+        },
+        className,
+      )}
+    >
+      {icon && getIconWithSize(icon)}
+      {children && <span>{children}</span>}
+      {rightIcon && getIconWithSize(rightIcon)}
+      {loading && (
+        <Loader
+          data-testid="buttonLoader"
+          className="hidden absolute top-0 right-0 bottom-0 left-0 m-auto btn-loader"
+        />
+      )}
+    </Tag>
+  );
+}
+
+export const Button = forwardRef(ButtonComponent);
