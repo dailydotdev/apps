d0fa00cc91c20319f8f561784a6ee2dc797992e3
nensidosari <36197304+nensidosari@users.noreply.github.com>
2023-12-01 14:58:58 +0100

    feat: create video component wrapper (#2382)
    * feat: create video component wrapper
    
    * fix:removing h-full from iframe

---
+++ packages/storybook/stories/video.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import YoutubeVideo from '@dailydotdev/shared/src/components/video/YoutubeVideo';
+
+const meta: Meta<typeof YoutubeVideo> = {
+  component: YoutubeVideo,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof YoutubeVideo>;
+
+export const Primary: Story = {
+  render: (props) => (
+      <YoutubeVideo {...props} />
+  ),
+  name: "YoutubeVideo",
+  args: {
+    videoId: "igZCEr3HwCg",
+    title: "Daily dev introduction video",
+    className: ''
+  },
+};
