bb8b39bef31e2ed54158b7900bd52ec50d141a23
capJavert <ante@kickass.website>
2023-12-01 11:39:32 +0100

    feat: implement mobile subscribe button

--- packages/shared/src/components/post/collection/CollectionPostHeaderActions.tsx
+++ packages/shared/src/components/post/collection/CollectionPostHeaderActions.tsx
@@ -1,18 +1,10 @@
 import React, { ReactElement } from 'react';
 import classNames from 'classnames';
 import classed from '../../../lib/classed';
-import { Button } from '../../buttons/Button';
 import { Origin } from '../../../lib/analytics';
-import BellIcon from '../../icons/Bell';
 import { PostHeaderActionsProps } from '../common';
 import { PostMenuOptions } from '../PostMenuOptions';
-import {
-  checkHasStatusPreference,
-  useNotificationPreference,
-} from '../../../hooks/notifications';
-import { NotificationType } from '../../notifications/utils';
-import { NotificationPreferenceStatus } from '../../../graphql/notifications';
-import BellDisabledIcon from '../../icons/Bell/Disabled';
+import { CollectionSubscribeButton } from './CollectionSubscribeButton';
 
 const Container = classed('div', 'flex flex-row items-center');
 
@@ -27,54 +19,9 @@ export const CollectionPostHeaderActions = ({
   onRemovePost,
   ...props
 }: PostHeaderActionsProps): ReactElement => {
-  const {
-    preferences,
-    subscribeNotification,
-    clearNotificationPreference,
-    isFetching,
-  } = useNotificationPreference({
-    params: post?.id
-      ? [
-          {
-            notificationType: NotificationType.CollectionUpdated,
-            referenceId: post.id,
-          },
-        ]
-      : undefined,
-  });
-  const isSubscribed = !!preferences?.some((item) =>
-    checkHasStatusPreference(
-      item,
-      NotificationType.CollectionUpdated,
-      post?.id,
-      [NotificationPreferenceStatus.Subscribed],
-    ),
-  );
-
   return (
     <Container {...props} className={classNames('gap-2', className)}>
-      <Button
-        className={classNames(
-          'btn',
-          isSubscribed ? 'btn-secondary' : 'btn-primary',
-        )}
-        icon={isSubscribed ? <BellDisabledIcon /> : <BellIcon />}
-        disabled={isFetching}
-        onClick={() => {
-          const notificationPreferenceParams = {
-            type: NotificationType.CollectionUpdated,
-            referenceId: post.id,
-          };
-
-          if (isSubscribed) {
-            clearNotificationPreference(notificationPreferenceParams);
-          } else {
-            subscribeNotification(notificationPreferenceParams);
-          }
-        }}
-      >
-        {isSubscribed ? 'Unsubscribe' : 'Subscribe'}
-      </Button>
+      <CollectionSubscribeButton post={post} />
       <PostMenuOptions
         onShare={onShare}
         post={post}
