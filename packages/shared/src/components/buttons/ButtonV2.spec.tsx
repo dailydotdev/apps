178eef81a825a50432f4179e05227017c8c592e2
Milan Freml <milan@daily.dev>
2023-12-05 08:50:12 +0100

    group tests in describe statement

--- packages/shared/src/components/buttons/ButtonV2.spec.tsx
+++ packages/shared/src/components/buttons/ButtonV2.spec.tsx
@@ -17,104 +17,123 @@ const renderComponent = <Tag extends AllowedTags>(
   return render(<Button {...props} />);
 };
 
-it('should render children', async () => {
-  renderComponent({ children: 'Button' });
-  expect(await screen.findByText('Button')).toBeInTheDocument();
-});
-
-it('should render primary button', async () => {
-  renderComponent({ variant: ButtonVariant.Primary, children: 'Primary' });
-  expect(await screen.findByRole('button')).toBeInTheDocument();
-  expect(await screen.findByRole('button')).toHaveClass('btn-primary');
-});
-
-it('should render secondary button with color', async () => {
-  renderComponent({
-    variant: ButtonVariant.Secondary,
-    color: ButtonColor.Burger,
-    children: 'Secondary burger',
-  });
-  expect(await screen.findByRole('button')).toBeInTheDocument();
-  expect(await screen.findByRole('button')).toHaveClass('btn-secondary-burger');
-});
-
-it('color prop can be overriden with className', async () => {
-  renderComponent({
-    variant: ButtonVariant.Secondary,
-    color: ButtonColor.Burger,
-    children: 'Secondary burger',
-    className: 'btn-secondary-bacon',
+describe('Button', () => {
+  it('should render children', async () => {
+    renderComponent({ children: 'Button' });
+    expect(await screen.findByText('Button')).toBeInTheDocument();
   });
 
-  const button = await screen.findByRole('button');
-  expect(button).toBeInTheDocument();
+  describe('variants', () => {
+    it('should render primary button', async () => {
+      renderComponent({ variant: ButtonVariant.Primary, children: 'Primary' });
+      expect(await screen.findByRole('button')).toBeInTheDocument();
+      expect(await screen.findByRole('button')).toHaveClass('btn-primary');
+    });
 
-  expect(button).toHaveClass('btn-secondary-burger');
-  expect(button).toHaveClass('btn-secondary-bacon');
-
-  expect(button.className.indexOf('btn-secondary-burger')).toBeLessThan(
-    button.className.indexOf('btn-secondary-bacon'),
-  );
-});
-
-it('should render icon button', async () => {
-  renderComponent({
-    icon: <UpvoteIcon data-testid="icon" />,
-    children: 'Upvote',
-  });
-  expect(await screen.findByTestId('icon')).toBeInTheDocument();
-  expect(await screen.findByRole('button')).not.toHaveClass('iconOnly');
-});
-
-it('should render right icon', async () => {
-  renderComponent({
-    icon: <UpvoteIcon data-testid="right-icon" />,
-    iconPosition: ButtonIconPosition.Right,
-    children: 'Upvote',
+    it('should render tertiary button', async () => {
+      renderComponent({
+        variant: ButtonVariant.Tertiary,
+        children: 'Tertiary',
+      });
+      expect(await screen.findByRole('button')).toBeInTheDocument();
+      expect(await screen.findByRole('button')).toHaveClass('btn-tertiary');
+    });
   });
 
-  const button = await screen.findByRole('button');
-  const rightIcon = await screen.findByTestId('right-icon');
+  describe('colors', () => {
+    it('should render secondary button with color', async () => {
+      renderComponent({
+        variant: ButtonVariant.Secondary,
+        color: ButtonColor.Burger,
+        children: 'Secondary burger',
+      });
+      expect(await screen.findByRole('button')).toBeInTheDocument();
+      expect(await screen.findByRole('button')).toHaveClass(
+        'btn-secondary-burger',
+      );
+    });
 
-  expect(button).toBeInTheDocument();
-  expect(rightIcon).toBeInTheDocument();
+    it('color prop can be overriden with className', async () => {
+      renderComponent({
+        variant: ButtonVariant.Secondary,
+        color: ButtonColor.Burger,
+        children: 'Secondary burger',
+        className: 'btn-secondary-bacon',
+      });
 
-  // check if icon appears AFTER the label
-  expect(button.innerHTML.indexOf('Upvote')).toBeLessThan(
-    button.innerHTML.indexOf(rightIcon.outerHTML),
-  );
-});
+      const button = await screen.findByRole('button');
+      expect(button).toBeInTheDocument();
 
-it('should render iconOnly button', async () => {
-  renderComponent({
-    icon: <UpvoteIcon data-testid="icon" />,
+      expect(button).toHaveClass('btn-secondary-burger');
+      expect(button).toHaveClass('btn-secondary-bacon');
+
+      expect(button.className.indexOf('btn-secondary-burger')).toBeLessThan(
+        button.className.indexOf('btn-secondary-bacon'),
+      );
+    });
   });
-  expect(await screen.findByTestId('icon')).toBeInTheDocument();
-  expect(await screen.findByRole('button')).toHaveClass('iconOnly');
-});
 
-it('should render loader and set aria-busy when loading', async () => {
-  renderComponent({ children: 'Button', loading: true });
-  expect(await screen.findByRole('button')).toHaveAttribute(
-    'aria-busy',
-    'true',
-  );
-  expect(await screen.findByTestId('buttonLoader')).toBeInTheDocument();
-});
+  describe('icons', () => {
+    it('should render icon button', async () => {
+      renderComponent({
+        icon: <UpvoteIcon data-testid="icon" />,
+        children: 'Upvote',
+      });
+      expect(await screen.findByTestId('icon')).toBeInTheDocument();
+      expect(await screen.findByRole('button')).not.toHaveClass('iconOnly');
+    });
 
-it('should set aria-pressed when pressed is true', async () => {
-  renderComponent({ children: 'Button', pressed: true });
-  expect(await screen.findByRole('button')).toHaveAttribute(
-    'aria-pressed',
-    'true',
-  );
-});
+    it('should render right icon', async () => {
+      renderComponent({
+        icon: <UpvoteIcon data-testid="right-icon" />,
+        iconPosition: ButtonIconPosition.Right,
+        children: 'Upvote',
+      });
 
-it('should render the button as an anchor element', async () => {
-  renderComponent({
-    children: 'Button',
-    kind: ButtonKind.Link,
-    href: 'https://daily.dev',
+      const button = await screen.findByRole('button');
+      const rightIcon = await screen.findByTestId('right-icon');
+
+      expect(button).toBeInTheDocument();
+      expect(rightIcon).toBeInTheDocument();
+
+      // check if icon appears AFTER the label
+      expect(button.innerHTML.indexOf('Upvote')).toBeLessThan(
+        button.innerHTML.indexOf(rightIcon.outerHTML),
+      );
+    });
+
+    it('should render iconOnly button', async () => {
+      renderComponent({
+        icon: <UpvoteIcon data-testid="icon" />,
+      });
+      expect(await screen.findByTestId('icon')).toBeInTheDocument();
+      expect(await screen.findByRole('button')).toHaveClass('iconOnly');
+    });
   });
-  expect(await screen.findByRole('link')).toBeInTheDocument();
+
+  it('should render loader and set aria-busy when loading', async () => {
+    renderComponent({ children: 'Button', loading: true });
+    expect(await screen.findByRole('button')).toHaveAttribute(
+      'aria-busy',
+      'true',
+    );
+    expect(await screen.findByTestId('buttonLoader')).toBeInTheDocument();
+  });
+
+  it('should set aria-pressed when pressed is true', async () => {
+    renderComponent({ children: 'Button', pressed: true });
+    expect(await screen.findByRole('button')).toHaveAttribute(
+      'aria-pressed',
+      'true',
+    );
+  });
+
+  it('should render the button as an anchor element', async () => {
+    renderComponent({
+      children: 'Button',
+      kind: ButtonKind.Link,
+      href: 'https://daily.dev',
+    });
+    expect(await screen.findByRole('link')).toBeInTheDocument();
+  });
 });
