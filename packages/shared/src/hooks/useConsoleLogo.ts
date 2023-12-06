fa60e5ab9e1f4ca360474e24cd2756200ef198a6
Chris Bongers <chrisbongers@gmail.com>
2023-12-06 15:11:34 +0000

    fix: only show on production

--- packages/shared/src/hooks/useConsoleLogo.ts
+++ packages/shared/src/hooks/useConsoleLogo.ts
@@ -1,7 +1,12 @@
 import { useEffect } from 'react';
+import { isProduction } from '../lib/constants';
 
 export function useConsoleLogo(): void {
   useEffect(() => {
+    if (!isProduction) {
+      return;
+    }
+
     // eslint-disable-next-line no-console
     console.log(`
 
