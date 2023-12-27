f725ffde54777162c36e1246b00c245d380bac4f
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2023-12-27 18:56:54 +0800

    fix: card height (#2459)

---
+++ packages/shared/src/components/cards/WelcomePostCardHeader.tsx
@@ -0,0 +1,45 @@
+import React, { ReactElement } from 'react';
+import { Post } from '../../graphql/posts';
+import { ProfilePicture } from '../ProfilePicture';
+import SourceButton from './SourceButton';
+import PostMetadata from './PostMetadata';
+
+type WelcomePostCardHeaderProps = Pick<
+  Post,
+  'author' | 'source' | 'createdAt'
+> & { enableSourceHeader?: boolean };
+
+export const WelcomePostCardHeader = ({
+  author,
+  createdAt,
+  source,
+  enableSourceHeader = false,
+}: WelcomePostCardHeaderProps): ReactElement => {
+  return (
+    <div className="flex relative flex-row gap-2 m-2">
+      <div className="relative">
+        <ProfilePicture
+          user={author}
+          size={enableSourceHeader ? 'xsmall' : 'xlarge'}
+          className={enableSourceHeader && 'top-7 -right-2.5'}
+          absolute={enableSourceHeader}
+        />
+        <SourceButton
+          source={source}
+          className={!enableSourceHeader && 'absolute -right-2 -bottom-2'}
+          size={enableSourceHeader ? 'large' : 'xsmall'}
+        />
+      </div>
+      <div className="flex flex-col flex-1 flex-grow mr-6 ml-2 typo-footnote">
+        <span className="font-bold line-clamp-2">
+          {enableSourceHeader ? source.name : author.name}
+        </span>
+        <PostMetadata
+          className="break-words line-clamp-1"
+          createdAt={createdAt}
+          description={enableSourceHeader ? author.name : `@${author.username}`}
+        />
+      </div>
+    </div>
+  );
+};
