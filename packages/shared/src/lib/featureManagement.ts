266fc7f4f7cf723d03ff9ebe3ddaa3baf47c6ab6
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-01-03 21:32:02 +0800

    revert: feature default value

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -14,14 +14,14 @@ export class Feature<T extends JSONValue> {
 
 const feature = {
   feedVersion: new Feature('feed_version', 15),
-  search: new Feature('search', SearchExperiment.V1),
+  search: new Feature('search', SearchExperiment.Control),
   lowImps: new Feature('feed_low_imps'),
   bookmarkOnCard: new Feature('bookmark_on_card', false),
   onboardingCopy: new Feature('onboarding_copy', {
     title: 'Where developers grow together',
     description: 'Get one personalized feed for all the knowledge you need.',
   }),
-  feedLayout: new Feature('feed_layout', FeedLayout.V1), // TODO: revert after testing
+  feedLayout: new Feature('feed_layout', FeedLayout.Control), // TODO: revert after testing
   cardFeedbackCopy: new Feature('card_feedback_copy', 'Did you like the post?'),
 };
 
