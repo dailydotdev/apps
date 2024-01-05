8849e97f85172a8acae943ee2b3c1f261b1ac523
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-01-05 10:58:16 +0800

    fix: gradient bg

--- packages/shared/src/components/feeds/FeedGradientBg.tsx
+++ packages/shared/src/components/feeds/FeedGradientBg.tsx
@@ -1,23 +1,27 @@
 import React, { ReactElement } from 'react';
 import classNames from 'classnames';
-import { useViewSize, ViewSize } from '../../hooks';
+import { useFeedLayout, useViewSize, ViewSize } from '../../hooks';
 import { cloudinary } from '../../lib/image';
 
 export function FeedGradientBg(): ReactElement {
   const isMobile = useViewSize(ViewSize.MobileL);
   const isLaptop = useViewSize(ViewSize.Laptop);
+  const { shouldUseFeedLayoutV1 } = useFeedLayout();
   const getImage = () => {
     if (isMobile) {
       return cloudinary.feed.bg.mobile;
     }
 
-    return isLaptop ? cloudinary.feed.bg.laptop : cloudinary.feed.bg.tablet;
+    return isLaptop || shouldUseFeedLayoutV1
+      ? cloudinary.feed.bg.laptop
+      : cloudinary.feed.bg.tablet;
   };
 
   return (
     <img
       className={classNames(
         'absolute left-0 top-0 w-full laptop:max-w-[58.75rem]',
+        shouldUseFeedLayoutV1 && 'laptop:left-1/2 laptop:-translate-x-1/2',
       )}
       src={getImage()}
       alt="Gradient background"
