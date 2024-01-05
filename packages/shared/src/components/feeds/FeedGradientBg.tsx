8de8febde948036475302a6c7e6a77270f9fabe8
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-01-05 11:09:00 +0800

    refactor: get image to src set

--- packages/shared/src/components/feeds/FeedGradientBg.tsx
+++ packages/shared/src/components/feeds/FeedGradientBg.tsx
@@ -1,30 +1,30 @@
 import React, { ReactElement } from 'react';
 import classNames from 'classnames';
-import { useFeedLayout, useViewSize, ViewSize } from '../../hooks';
+import { useFeedLayout } from '../../hooks';
 import { cloudinary } from '../../lib/image';
 
 export function FeedGradientBg(): ReactElement {
-  const isMobile = useViewSize(ViewSize.MobileL);
-  const isLaptop = useViewSize(ViewSize.Laptop);
   const { shouldUseFeedLayoutV1 } = useFeedLayout();
-  const getImage = () => {
-    if (isMobile) {
-      return cloudinary.feed.bg.mobile;
-    }
-
-    return isLaptop || shouldUseFeedLayoutV1
-      ? cloudinary.feed.bg.laptop
-      : cloudinary.feed.bg.tablet;
-  };
 
   return (
-    <img
-      className={classNames(
-        'absolute left-0 top-0 w-full laptop:max-w-[58.75rem]',
-        shouldUseFeedLayoutV1 && 'laptop:left-1/2 laptop:-translate-x-1/2',
-      )}
-      src={getImage()}
-      alt="Gradient background"
-    />
+    <picture>
+      <source media="(min-width: 1020px)" srcSet={cloudinary.feed.bg.laptop} />
+      <source
+        media="(min-width: 656px)"
+        srcSet={
+          shouldUseFeedLayoutV1
+            ? cloudinary.feed.bg.laptop
+            : cloudinary.feed.bg.tablet
+        }
+      />
+      <img
+        className={classNames(
+          'absolute left-0 top-0 w-full laptop:max-w-[58.75rem]',
+          shouldUseFeedLayoutV1 && 'laptop:left-1/2 laptop:-translate-x-1/2',
+        )}
+        src={cloudinary.feed.bg.mobile}
+        alt="Gradient background"
+      />
+    </picture>
   );
 }
