ea1d88c1cc45ba5a4436359f3df343f60a3b9ccc
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-01-16 17:01:01 +0800

    fix: modal usage

--- packages/shared/src/components/modals/streaks/NewStreakModal.tsx
+++ packages/shared/src/components/modals/streaks/NewStreakModal.tsx
@@ -43,79 +43,83 @@ export default function FirstStreakModal({
   return (
     <Modal
       {...props}
-      kind={Modal.Kind.FlexibleCenter}
+      kind={Modal.Kind.FlexibleTop}
       size={Modal.Size.XSmall}
-      className="relative !h-[auto] p-6"
       onRequestClose={onClose}
     >
       <ModalClose onClick={onRequestClose} className="right-2 top-2" />
-      <span className="relative flex flex-col items-center justify-center">
-        {shouldShowSplash ? (
-          <SplashIcon className="ml-2 h-[10rem] w-[15rem] text-theme-color-bacon" />
-        ) : (
-          <FireIcon className="h-[10rem] w-[10rem] text-theme-color-bacon" />
-        )}
-        <strong className="typo-tera absolute">{currentStreak}</strong>
-        {shouldShowSplash && (
-          <span className="typo-tera absolute mt-44">🏆</span>
-        )}
-      </span>
-      <strong
-        className={classNames(
-          'typo-title1',
-          shouldShowSplash ? 'mt-20' : 'mt-10',
-        )}
-      >
-        {shouldShowSplash
-          ? 'New streak record!'
-          : `${currentStreak} days streak`}
-      </strong>
-      <Paragraph
-        className={classNames('typo-body', shouldShowSplash ? 'mt-3' : 'mt-5')}
-      >
-        {shouldShowSplash
-          ? 'Epic win! You are on a league of your own'
-          : `New milestone reached! You are unstoppable. Your previous record was
+      <Modal.Body className="items-center">
+        <span className="relative flex flex-col items-center justify-center">
+          {shouldShowSplash ? (
+            <SplashIcon className="ml-2 h-[10rem] w-[15rem] text-theme-color-bacon" />
+          ) : (
+            <FireIcon className="h-[10rem] w-[10rem] text-theme-color-bacon" />
+          )}
+          <strong className="typo-tera absolute">{currentStreak}</strong>
+          {shouldShowSplash && (
+            <span className="typo-tera absolute mt-44">🏆</span>
+          )}
+        </span>
+        <strong
+          className={classNames(
+            'typo-title1',
+            shouldShowSplash ? 'mt-20' : 'mt-10',
+          )}
+        >
+          {shouldShowSplash
+            ? 'New streak record!'
+            : `${currentStreak} days streak`}
+        </strong>
+        <Paragraph
+          className={classNames(
+            'typo-body',
+            shouldShowSplash ? 'mt-3' : 'mt-5',
+          )}
+        >
+          {shouldShowSplash
+            ? 'Epic win! You are on a league of your own'
+            : `New milestone reached! You are unstoppable. Your previous record was
         ${previousStreak} days.`}
-      </Paragraph>
-      <Paragraph className="mt-10 typo-callout">
-        <strong>Brag about it via...</strong>
-      </Paragraph>
-      <span className="mt-3 flex flex-row gap-2">
-        <SocialShareButton
-          href="" // what should be the link
-          icon={<TwitterIcon />}
-          variant={ButtonVariant.Primary}
-          color={ButtonColor.Twitter}
-          label="X"
-        />
-        <SocialShareButton
-          href="" // what should be the link
-          icon={<WhatsappIcon />}
-          variant={ButtonVariant.Primary}
-          color={ButtonColor.WhatsApp}
-          label="WhatsApp"
-        />
-        <SocialShareButton
-          href="" // what should be the link
-          icon={
-            <StraightArrowIcon
-              className="text-theme-label-tertiary"
-              size={IconSize.Small}
-            />
-          }
-          variant={ButtonVariant.Float}
-          label="WhatsApp"
-        />
-      </span>
-      <Checkbox
-        name="show_streaks"
-        className="mt-10"
-        checked={isChecked}
-        onToggle={() => setIsChecked((state) => !state)}
-      >
-        Never show this again
-      </Checkbox>
+        </Paragraph>
+        <Paragraph className="mt-10 typo-callout">
+          <strong>Brag about it via...</strong>
+        </Paragraph>
+        <span className="mt-3 flex flex-row gap-2">
+          <SocialShareButton
+            href="" // what should be the link
+            icon={<TwitterIcon />}
+            variant={ButtonVariant.Primary}
+            color={ButtonColor.Twitter}
+            label="X"
+          />
+          <SocialShareButton
+            href="" // what should be the link
+            icon={<WhatsappIcon />}
+            variant={ButtonVariant.Primary}
+            color={ButtonColor.WhatsApp}
+            label="WhatsApp"
+          />
+          <SocialShareButton
+            href="" // what should be the link
+            icon={
+              <StraightArrowIcon
+                className="text-theme-label-tertiary"
+                size={IconSize.Small}
+              />
+            }
+            variant={ButtonVariant.Float}
+            label="WhatsApp"
+          />
+        </span>
+        <Checkbox
+          name="show_streaks"
+          className="mt-10"
+          checked={isChecked}
+          onToggle={() => setIsChecked((state) => !state)}
+        >
+          Never show this again
+        </Checkbox>
+      </Modal.Body>
     </Modal>
   );
 }
