001f48963964d6ca6846849f9dec80ac6912a8fa
capJavert <ante@kickass.website>
2024-01-19 11:16:15 +0100

    feat: add todo

--- packages/shared/src/components/search/SearchPanel/SearchPanelContext.ts
+++ packages/shared/src/components/search/SearchPanel/SearchPanelContext.ts
@@ -9,6 +9,7 @@ export type SearchPanelContextValue = {
   setActive: (isActive: boolean) => void;
 };
 
+// TODO AS-3-search-merge see if default values are needed for context and functions used
 const noop = () => undefined;
 
 export const SearchPanelContext = createContext<SearchPanelContextValue>({
