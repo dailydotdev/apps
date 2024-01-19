fcf0bcd5a9c06a33ff536089d3e1c15aa2039a77
Milan Freml <milan@daily.dev>
2024-01-19 14:58:37 +0100

    fix: onboarding visuals

--- packages/shared/src/components/feeds/FeedContainer.tsx
+++ packages/shared/src/components/feeds/FeedContainer.tsx
@@ -176,6 +176,7 @@ export const FeedContainer = ({
 
     completeAction(ActionType.UsedSearch);
   };
+  const showFeedReadyMessage = router.query?.welcome === 'true';
 
   return (
     <div
@@ -199,8 +200,22 @@ export const FeedContainer = ({
           aria-live={subject === ToastSubject.Feed ? 'assertive' : 'off'}
           data-testid="posts-feed"
         >
-          {router.query?.welcome === 'true' && (
-            <FeedReadyMessage className="mb-10" />
+          {showFeedReadyMessage && (
+            <FeedReadyMessage
+              className={{
+                main: classNames(
+                  shouldUseFeedLayoutV1
+                    ? 'mt-8 mb-8 w-full laptop:gap-4 [@media(width<=680px)]:px-6'
+                    : 'mb-10 max-w-xl laptop:gap-6',
+                ),
+                textContainer: classNames(
+                  shouldUseFeedLayoutV1 ? 'laptop:flex-1' : 'flex flex-col',
+                ),
+                header: classNames(
+                  shouldUseFeedLayoutV1 ? 'mb-0.5' : 'mb-2 laptop:mb-1',
+                ),
+              }}
+            />
           )}
           {inlineHeader && header}
           {isV1Search && (
