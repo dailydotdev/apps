d592351842aa1a290dd655720df11e8f3af6d0eb
Milan Freml <kopancek@users.noreply.github.com>
2024-01-22 11:15:42 +0100

    feat: align top border radius with latest design (#2532)
    Co-authored-by: nensidosari <36197304+nensidosari@users.noreply.github.com>

--- packages/shared/src/components/cards/v1/Card.tsx
+++ packages/shared/src/components/cards/v1/Card.tsx
@@ -55,7 +55,7 @@ export const CardVideoImage = classed(
 export const CardSpace = classed('div', 'flex-1');
 
 const clickableCardClasses = classNames(
-  'focus-outline absolute inset-0 block h-full w-full',
+  'focus-outline absolute inset-0 block h-full w-full rounded-16',
 );
 
 export const CardButton = classed('button', clickableCardClasses);
@@ -64,7 +64,7 @@ export const CardLink = classed('a', clickableCardClasses);
 
 export const Card = classed(
   'article',
-  `group relative w-full flex flex-col py-6 px-4 border-t border-theme-divider-tertiary
+  `group relative w-full flex flex-col py-6 px-4 border-t border-theme-divider-tertiary rounded-16
    hover:bg-theme-float
   `,
 );
