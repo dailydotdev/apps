ab07aafa1b1c6b6af73561c1c3681eae5a942df7
Milan Freml <milan@daily.dev>
2024-01-23 15:22:47 +0100

    fix: set card image to object-cover and keep max 1:1 aspect ratio

--- packages/shared/src/components/cards/v1/CollectionCard.tsx
+++ packages/shared/src/components/cards/v1/CollectionCard.tsx
@@ -91,7 +91,7 @@ export const CollectionCard = forwardRef(function CollectionCard(
               alt="Post Cover image"
               src={image}
               fallbackSrc={cloudinary.post.imageCoverPlaceholder}
-              className="my-2 object-cover mobileXXL:self-start mobileXXL:object-fill"
+              className="my-2 object-cover mobileXXL:self-start"
               loading="lazy"
             />
           )}
