276d100b32177968edef8e8f7a1d122b3c2d37f8
NensiDosari <nensidosari97@gmail.com>
2024-01-24 08:10:28 +0100

    fix: toastOnSuccess onDownvote

--- packages/shared/src/components/cards/v1/ActionButtons.tsx
+++ packages/shared/src/components/cards/v1/ActionButtons.tsx
@@ -87,7 +87,7 @@ export default function ActionButtons({
           <PostTagsPanel
             post={post}
             className="pointer-events-auto mt-4"
-            toastOnSuccess={false}
+            toastOnSuccess
           />
         </div>
       )}
