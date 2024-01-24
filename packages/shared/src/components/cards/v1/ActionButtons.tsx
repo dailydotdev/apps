8a325b0fba6517e5949f3e5f8272cfc5d1c769ae
Milan Freml <milan@daily.dev>
2024-01-24 09:28:04 +0100

    remove temporary import needed to create loom video

--- packages/shared/src/components/cards/v1/ActionButtons.tsx
+++ packages/shared/src/components/cards/v1/ActionButtons.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import classNames from 'classnames';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
