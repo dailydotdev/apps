d651c9af173dea7bf6de9601da1d0a322fea3c6d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-01-25 20:30:28 +0800

    fix: wrappers

--- packages/shared/src/components/post/ShareYouTubeContent.tsx
+++ packages/shared/src/components/post/ShareYouTubeContent.tsx
@@ -8,6 +8,7 @@ import { SharedPostLink } from './common/SharedPostLink';
 import YoutubeVideo from '../video/YoutubeVideo';
 import { formatReadTime } from '../utilities';
 import { combinedClicks } from '../../lib/click';
+import ConditionalWrapper from '../ConditionalWrapper';
 
 interface ShareYouTubeContentProps {
   post: Post;
@@ -18,6 +19,7 @@ function ShareYouTubeContent({
   post,
   onReadArticle,
 }: ShareYouTubeContentProps): ReactElement {
+  const isUnknownSource = post.sharedPost.source.id === 'unknown';
   const onLinkClick = (e: React.MouseEvent) => {
     const selection = globalThis?.window?.getSelection();
     const hasSelection = selection?.anchorOffset !== selection?.focusOffset;
@@ -27,6 +29,15 @@ function ShareYouTubeContent({
     }
   };
 
+  const linkToPost = post.sharedPost.commentsPermalink;
+  const wrappedComponent = (wrapped) => (
+    <Link href={linkToPost}>
+      <a href={linkToPost} draggable="false" onClick={onLinkClick}>
+        {wrapped}
+      </a>
+    </Link>
+  );
+
   return (
     <>
       <SharePostTitle post={post} />
@@ -34,32 +45,29 @@ function ShareYouTubeContent({
         className="my-5"
         summary={post?.sharedPost?.summary}
         wrapper={(node) => (
-          <Link href={post.commentsPermalink}>
-            <a
-              href={post.commentsPermalink}
-              draggable="false"
-              onClick={onLinkClick}
+          <ConditionalWrapper
+            condition={!isUnknownSource}
+            wrapper={wrappedComponent}
+          >
+            <SharedPostLink
+              post={post}
+              onGoToLinkProps={combinedClicks(onReadArticle)}
+              className="m-4 flex flex-wrap font-bold typo-body"
             >
-              <SharedPostLink
-                post={post}
-                onGoToLinkProps={combinedClicks(onReadArticle)}
-                className="m-4 flex flex-wrap font-bold typo-body"
-              >
-                {post.sharedPost.title}
-              </SharedPostLink>
-              <PostSourceInfo
-                date={
-                  post.sharedPost.readTime
-                    ? `${formatReadTime(post.sharedPost.readTime)} watch time`
-                    : undefined
-                }
-                source={post.sharedPost.source}
-                className="mx-4 mb-4"
-                size="small"
-              />
-              {node}
-            </a>
-          </Link>
+              {post.sharedPost.title}
+            </SharedPostLink>
+            <PostSourceInfo
+              date={
+                post.sharedPost.readTime
+                  ? `${formatReadTime(post.sharedPost.readTime)} watch time`
+                  : undefined
+              }
+              source={post.sharedPost.source}
+              className="mx-4 mb-4"
+              size="small"
+            />
+            {node}
+          </ConditionalWrapper>
         )}
       >
         <YoutubeVideo
