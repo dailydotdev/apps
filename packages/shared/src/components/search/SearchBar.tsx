de55aff7cc31bfc80a034ae778ff9814610f4709
capJavert <ante@kickass.website>
2024-01-31 11:12:48 +0100

    fix: show chat progress bar

--- packages/shared/src/components/search/SearchBar.tsx
+++ packages/shared/src/components/search/SearchBar.tsx
@@ -7,6 +7,7 @@ import { useSearchQuestionRecommendations } from '../../hooks/search';
 import { Origin } from '../../lib/analytics';
 import { labels } from '../../lib';
 import { isNullOrUndefined } from '../../lib/func';
+import { SearchProgressBar } from './SearchProgressBar';
 
 export type SearchBarProps = Pick<
   SearchBarInputProps,
@@ -15,16 +16,30 @@ export type SearchBarProps = Pick<
   isLoading?: boolean;
 };
 
-export function SearchBar({ className, chunk }: SearchBarProps): ReactElement {
+export function SearchBar({
+  className,
+  showProgress,
+  chunk,
+}: SearchBarProps): ReactElement {
   const suggestionsProps = useSearchQuestionRecommendations({
     origin: Origin.SearchPage,
   });
 
   return (
     <div className={classNames('w-full', className?.container)}>
+      {showProgress && (
+        <>
+          <SearchProgressBar max={chunk?.steps} progress={chunk?.progress} />
+          {!!chunk?.status && (
+            <div className="mt-2 text-theme-label-tertiary typo-callout">
+              {chunk?.status}
+            </div>
+          )}
+        </>
+      )}
       {!isNullOrUndefined(chunk?.error?.code) && (
         <Alert
-          className="my-4"
+          className="mb-4"
           type={AlertType.Error}
           title={chunk?.error?.message || labels.error.generic}
         />
