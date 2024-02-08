ae03dc7023646a619c0b90511775234614f9a9d7
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-02-08 09:46:48 +0100

    fix: bg

--- packages/shared/src/components/profile/devcard/DevCard.tsx
+++ packages/shared/src/components/profile/devcard/DevCard.tsx
@@ -1,11 +1,10 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement } from 'react';
 import classNames from 'classnames';
 import { useQuery } from '@tanstack/react-query';
 import { Separator } from '../../cards/common';
 import { postDateFormat } from '../../../lib/dateFormat';
 import { Divider } from '../../utilities';
 import { TagLinks } from '../../TagLinks';
-import { UserRank } from '../../../lib/rank';
 import classed from '../../../lib/classed';
 import { generateQueryKey, RequestKey } from '../../../lib/query';
 import { useRequestProtocol } from '../../../hooks/useRequestProtocol';
@@ -24,6 +23,7 @@ import { IconSize } from '../../Icon';
 import { largeNumberFormat } from '../../../lib/numberFormat';
 import ConditionalWrapper from '../../ConditionalWrapper';
 import { Author } from '../../../graphql/comments';
+import colors from '../../../styles/colors';
 
 export enum DevCardType {
   Vertical = 'vertical',
@@ -35,18 +35,8 @@ interface DevCardProps {
   type?: DevCardType;
   user: Author;
   tags?: string[];
-  theme?: UserRank;
+  theme?: DevCardTheme;
 }
-
-const rankToThemeClass: Record<UserRank, string> = {
-  Iron: '',
-  Bronze: '',
-  Silver: '',
-  Gold: '',
-  Platinum: '',
-  Diamond: '',
-  Legendary: '',
-};
 
 const RoundedContainer = classed('div', 'rounded-[32px]');
 
@@ -76,6 +66,42 @@ const StatsSection = ({
   );
 };
 
+enum DevCardTheme {
+  Default = 'default',
+  Iron = 'iron',
+  Bronze = 'bronze',
+  Silver = 'silver',
+  Gold = 'gold',
+  Platinum = 'platinum',
+  Diamond = 'diamond',
+  Legendary = 'legendary',
+}
+
+const themeToLinearGradient = {
+  [DevCardTheme.Default]: `linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(152.83deg, #FFFFFF 51.92%, ${colors.salt['20']} 85.8%)`,
+  [DevCardTheme.Iron]:
+    `linear-gradient(135.48deg, ${colors.pepper['10']} 0%, ${colors.pepper['90']} 20%, ${colors.pepper['10']} 47.5%, #2C303A 67%, ${colors.pepper['90']} 83%, ${colors.pepper['10']} 100%),\n` +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+  [DevCardTheme.Bronze]:
+    `linear-gradient(135.48deg, #C98463 0%, ${colors.burger['40']} 20%, #FFB760 47.5%, #C98463 67%, ${colors.burger['40']} 83%, #C98463 100%),\n` +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+  [DevCardTheme.Silver]:
+    'linear-gradient(135.11deg, #A8B3CE 0%, #F6F8FC 18%, #CFD6E5 31.5%, #FFFFFF 49.19%, #CFD6E5 61.5%, #A8B3CE 78.5%, #CFD6E5 95.5%),\n' +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+  [DevCardTheme.Gold]:
+    `linear-gradient(135deg, ${colors.bun['10']} 0%, #FFF86E 27.5%, ${colors.bun['10']} 50%, #FFF86E 75%, ${colors.bun['10']} 100%),\n` +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+  [DevCardTheme.Platinum]:
+    'linear-gradient(135.18deg, #75F3BB 0%, #95EEF4 24.16%, #77A6F5 40%, #75F3BB 71.33%, #77A6F5 100%),\n' +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+  [DevCardTheme.Diamond]:
+    'linear-gradient(135.18deg, #E769FB 0%, #9E70F8 24.16%, #68A6FD 40%, #9E70F8 71.33%, #D473F4 100%),\n' +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+  [DevCardTheme.Legendary]:
+    `linear-gradient(135.18deg, ${colors.ketchup['10']} 0%, ${colors.bacon['10']} 24.16%, ${colors.bun['10']} 40%, ${colors.bacon['10']} 71.33%, ${colors.ketchup['10']} 100%),\n` +
+    `linear-gradient(180deg, ${colors.salt['20']} 0%, ${colors.salt['10']} 85.42%)`,
+};
+
 interface DevCardQueryData {
   sources: Source[];
   totalReads: number;
@@ -85,7 +111,7 @@ export function DevCard({
   type = DevCardType.Vertical,
   user,
   tags,
-  theme,
+  theme = DevCardTheme.Default,
 }: DevCardProps): ReactElement {
   const { requestMethod } = useRequestProtocol();
   const { data, isLoading } = useQuery<DevCardQueryData>(
@@ -106,7 +132,6 @@ export function DevCard({
       return { sources: res.sources, totalReads };
     },
   );
-  const bg = rankToThemeClass[theme] ?? 'bg-white';
   const favorites = (
     <>
       <TagLinks
@@ -134,6 +159,8 @@ export function DevCard({
     </>
   );
 
+  const bg = themeToLinearGradient[theme] ?? themeToLinearGradient.default;
+
   if (isLoading) {
     return null;
   }
@@ -142,11 +169,11 @@ export function DevCard({
     <RoundedContainer
       className={classNames(
         'm-4 flex h-fit p-2',
-        bg,
         type === DevCardType.Horizontal
           ? 'w-[37.75rem] flex-row-reverse'
           : 'w-[20.25rem] flex-col',
       )}
+      style={{ background: bg }}
     >
       <ConditionalWrapper
         condition={type === DevCardType.Horizontal}
