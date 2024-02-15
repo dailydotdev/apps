e2773f24f02dfdf1d7c56af5cb1cb777b4e06535
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-02-15 23:36:21 +0800

    fix: placement of experiment allocation (#2641)

--- packages/webapp/pages/posts/[id]/index.tsx
+++ packages/webapp/pages/posts/[id]/index.tsx
@@ -49,9 +49,13 @@ import { useFeature } from '@dailydotdev/shared/src/components/GrowthBookProvide
 import { feature } from '@dailydotdev/shared/src/lib/featureManagement';
 import { FeedLayout } from '@dailydotdev/shared/src/lib/featureValues';
 import { PostBackButton } from '@dailydotdev/shared/src/components/post/common/PostBackButton';
-import { AuthenticationBanner } from '@dailydotdev/shared/src/components/auth';
+import {
+  AuthenticationBanner,
+  authGradientBg,
+} from '@dailydotdev/shared/src/components/auth';
 import { useOnboarding } from '@dailydotdev/shared/src/hooks/auth/useOnboarding';
 import { useViewSize, ViewSize } from '@dailydotdev/shared/src/hooks';
+import LoginButton from '@dailydotdev/shared/src/components/LoginButton';
 import { getTemplatedTitle } from '../../../components/layouts/utils';
 import { getLayout as getMainLayout } from '../../../components/layouts/MainLayout';
 
@@ -187,7 +191,8 @@ const PostPage = ({ id, initialData }: Props): ReactElement => {
   if (!Content || isError) {
     return <Custom404 />;
   }
-  return (
+
+  return getMainLayout(
     <>
       <Head>
         <link rel="preload" as="image" href={post?.image} />
@@ -214,12 +219,24 @@ const PostPage = ({ id, initialData }: Props): ReactElement => {
         }}
       />
       {shouldShowAuthBanner && isLaptop && <AuthenticationBanner />}
-    </>
-  );
+    </>,
+    {},
+    {
+      screenCentered: false,
+      customBanner: shouldShowAuthBanner && !isLaptop && (
+        <LoginButton
+          className={{
+            container: classNames(
+              authGradientBg,
+              'sticky left-0 top-0 z-max w-full justify-center gap-2 border-b border-theme-color-cabbage px-4 py-2',
+            ),
+            button: 'flex-1 tablet:max-w-[9rem]',
+          }}
+        />
+      ),
+    },
+  ) as ReactElement;
 };
-
-PostPage.getLayout = getMainLayout;
-PostPage.layoutProps = { screenCentered: false, isValidOnboardingPage: true };
 
 export default PostPage;
 
