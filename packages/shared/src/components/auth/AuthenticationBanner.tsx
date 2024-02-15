9dc93f9bcfc1e7df8adffffd67166bdb2a0a5404
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-02-15 19:32:50 +0800

    refactor: tailwind usage over inline style

--- packages/shared/src/components/auth/AuthenticationBanner.tsx
+++ packages/shared/src/components/auth/AuthenticationBanner.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import classNames from 'classnames';
 import classed from '../../lib/classed';
 import { OnboardingHeadline } from './OnboardingHeadline';
@@ -11,9 +11,8 @@ import LoginButton from '../LoginButton';
 import { bottomBannerBaseClasses, BottomBannerContainer } from '../banners';
 
 const Section = classed('div', 'flex flex-col w-[23.25rem]');
-const style: CSSProperties = {
-  background: `linear-gradient(270deg, var(--theme-overlay-active-cabbage) 0%, var(--theme-overlay-active-onion) 100%), var(--theme-background-primary)`,
-};
+const gradientBg =
+  'bg-theme-bg-primary bg-gradient-to-l from-theme-overlay-active-cabbage from-0% to-theme-overlay-active-onion to-100%';
 
 export function AuthenticationBanner(): ReactElement {
   const { showLogin } = useAuthContext();
@@ -22,9 +21,12 @@ export function AuthenticationBanner(): ReactElement {
   if (!isLaptop) {
     return (
       <LoginButton
-        style={style}
         className={{
-          container: classNames(bottomBannerBaseClasses, 'gap-2 px-4 py-2'),
+          container: classNames(
+            bottomBannerBaseClasses,
+            gradientBg,
+            'gap-2 px-4 py-2',
+          ),
           button: 'flex-1 tablet:max-w-[9rem]',
         }}
       />
@@ -32,7 +34,9 @@ export function AuthenticationBanner(): ReactElement {
   }
 
   return (
-    <BottomBannerContainer className="gap-6 py-10" style={style}>
+    <BottomBannerContainer
+      className={classNames('gap-6 py-10 shadow-3', gradientBg)}
+    >
       <Section>
         <OnboardingHeadline
           className={{ title: 'typo-mega3', description: 'typo-title3' }}
