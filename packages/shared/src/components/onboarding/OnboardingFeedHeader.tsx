e7f0294b3ef589452f346f3c036278fc9bccb2f6
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-02-19 13:50:07 +0100

    fix: feed preview button with side dividers

--- packages/shared/src/components/onboarding/OnboardingFeedHeader.tsx
+++ packages/shared/src/components/onboarding/OnboardingFeedHeader.tsx
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { FilterOnboardingV4 } from './FilterOnboardingV4';
 import { Button, ButtonIconPosition, ButtonVariant } from '../buttons/ButtonV2';
 import { REQUIRED_TAGS_THRESHOLD } from './common';
-import useFeedSettings from '../../hooks/useFeedSettings';
 import { ArrowIcon } from '../icons';
 import { LayoutHeader } from '../layout/common';
 
@@ -23,38 +22,31 @@ export const OnboardingFeedHeader = ({
           <Button>Create my feed</Button>
         </div>
         <FilterOnboardingV4 />
-        <Button
-          className="mt-10"
-          variant={
-            isPreviewFeedVisible
-              ? ButtonVariant.Primary
-              : ButtonVariant.Secondary
-          }
-          disabled={!isFeedPreviewEnabled}
-          icon={
-            <ArrowIcon
-              className={classNames(!isPreviewFeedVisible && 'rotate-180')}
-            />
-          }
-          iconPosition={ButtonIconPosition.Right}
-          onClick={() => {
-            setPreviewFeedVisible((current) => {
-              const newValue = !current;
-
-              // trackEvent({
-              //   event_name: AnalyticsEvent.ToggleFeedPreview,
-              //   target_id: newValue,
-              //   extra: JSON.stringify({ origin: Origin.EditTag }),
-              // });
-
-              return newValue;
-            });
-          }}
-        >
-          {isFeedPreviewEnabled
-            ? `${isPreviewFeedVisible ? 'Hide' : 'Show'} feed preview`
-            : `${tagsCount}/${REQUIRED_TAGS_THRESHOLD} to show feed preview`}
-        </Button>
+        <div className="mt-10 flex items-center justify-center gap-10 text-theme-label-quaternary typo-callout">
+          <div className="h-px flex-1 bg-theme-divider-tertiary" />
+          <Button
+            variant={
+              isPreviewFeedVisible
+                ? ButtonVariant.Primary
+                : ButtonVariant.Secondary
+            }
+            disabled={!isFeedPreviewEnabled}
+            icon={
+              <ArrowIcon
+                className={classNames(!isPreviewFeedVisible && 'rotate-180')}
+              />
+            }
+            iconPosition={ButtonIconPosition.Right}
+            onClick={() => {
+              setPreviewFeedVisible((current) => !current);
+            }}
+          >
+            {isFeedPreviewEnabled
+              ? `${isPreviewFeedVisible ? 'Hide' : 'Show'} feed preview`
+              : `${tagsCount}/${REQUIRED_TAGS_THRESHOLD} to show feed preview`}
+          </Button>
+          <div className="h-px flex-1 bg-theme-divider-tertiary" />
+        </div>
       </div>
     </LayoutHeader>
   );
