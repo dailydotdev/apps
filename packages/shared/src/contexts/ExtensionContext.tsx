4385206fdd6a539e6906a26bc23cd17719b7c81e
Dragos Iorgulescu <dragos.m.iorgulescu@gmail.com>
2024-02-20 02:40:01 -0600

    chore: manifest migration to V3 (#2332)
    Introducing Manifest v3 for Chrome and v2 up for FF

---
+++ packages/shared/src/contexts/ExtensionContext.tsx
@@ -0,0 +1,25 @@
+import { Permissions } from 'webextension-polyfill';
+import React, { useContext } from 'react';
+
+export type RequestContentScripts = (data: {
+  origin: string;
+  skipRedirect?: boolean;
+}) => Promise<boolean>;
+
+export interface ExtensionContextData {
+  getContentScriptPermission?: () => Promise<boolean>;
+  requestContentScripts?: RequestContentScripts;
+  registerBrowserContentScripts?: () => Promise<void>;
+  getHostPermission?: () => Promise<boolean>;
+  requestHostPermissions?: Permissions.Static['request'];
+  origins?: string[];
+  currentPage?: string;
+  setCurrentPage?: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// This is an empty context when used outside the extension
+// in the extension, we are attaching data to the context via the ExtensionContextProvider
+export const ExtensionContext = React.createContext<ExtensionContextData>({});
+
+export const useExtensionContext = (): ExtensionContextData =>
+  useContext(ExtensionContext);
