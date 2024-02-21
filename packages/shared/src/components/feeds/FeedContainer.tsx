08b73f800e8197c0b5fd505315e70ad2873a4fd0
NensiDosari <nensidosari97@gmail.com>
2024-02-21 13:02:33 +0100

    fix: remove duplicate scroll to top usage

--- packages/shared/src/components/feeds/FeedContainer.tsx
+++ packages/shared/src/components/feeds/FeedContainer.tsx
@@ -9,7 +9,6 @@ import { useRouter } from 'next/router';
 import { Spaciness } from '../../graphql/settings';
 import SettingsContext from '../../contexts/SettingsContext';
 import FeedContext from '../../contexts/FeedContext';
-import ScrollToTopButton from '../ScrollToTopButton';
 import styles from '../Feed.module.css';
 import { useFeature } from '../GrowthBookProvider';
 import { feature } from '../../lib/featureManagement';
@@ -140,7 +139,6 @@ export const FeedContainer = ({
         className,
       )}
     >
-      <ScrollToTopButton />
       <div className="flex w-full flex-col laptopL:mx-auto" style={style}>
         {!inlineHeader && header}
         <div
