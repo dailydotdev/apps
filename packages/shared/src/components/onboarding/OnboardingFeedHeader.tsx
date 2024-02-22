deaa64124741471aa08b721e05ccfc32b81f685d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-02-22 10:12:03 +0100

    feat: move the tag selection from onboarding to my-feed page (#2656)
    Co-authored-by: Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
    Co-authored-by: Chris Bongers <chrisbongers@gmail.com>
    fix: added fixed header styling (#2664)
    fix: minor design adjustments (#2667)

---
+++ packages/shared/src/components/onboarding/OnboardingFeedHeader.tsx
@@ -0,0 +1,154 @@
+import React, { useCallback, useEffect } from 'react';
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import { FilterOnboardingV4 } from './FilterOnboardingV4';
+import {
+  Button,
+  ButtonColor,
+  ButtonIconPosition,
+  ButtonSize,
+  ButtonVariant,
+} from '../buttons/Button';
+import { REQUIRED_TAGS_THRESHOLD } from './common';
+import { ArrowIcon } from '../icons';
+import { LayoutHeader } from '../layout/common';
+import { usePrompt } from '../../hooks/usePrompt';
+import { useMyFeed } from '../../hooks/useMyFeed';
+import { useAlertsContext } from '../../contexts/AlertContext';
+import { useAnalyticsContext } from '../../contexts/AnalyticsContext';
+import { AnalyticsEvent } from '../../lib/analytics';
+
+const promptConfig = {
+  title: 'Discard tag selection?',
+  description: 'Your personalized feed is only a few tags away',
+  cancelButton: {
+    title: 'Stay',
+    variant: ButtonVariant.Primary,
+    color: ButtonColor.Cabbage,
+  },
+  okButton: {
+    title: 'Leave',
+    variant: ButtonVariant.Secondary,
+  },
+  className: {
+    buttons: 'flex-row-reverse',
+  },
+};
+
+type OnboardingFeedHeaderProps = {
+  isPreviewFeedVisible: boolean;
+  setPreviewFeedVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  isFeedPreviewEnabled: boolean;
+  tagsCount: number;
+};
+
+export const OnboardingFeedHeader = ({
+  isPreviewFeedVisible,
+  setPreviewFeedVisible,
+  isFeedPreviewEnabled,
+  tagsCount,
+}: OnboardingFeedHeaderProps): JSX.Element => {
+  const { updateAlerts } = useAlertsContext();
+  const { trackEvent } = useAnalyticsContext();
+  const { showPrompt } = usePrompt();
+  const router = useRouter();
+  const { registerLocalFilters } = useMyFeed();
+
+  const completeOnboarding = useCallback(() => {
+    registerLocalFilters();
+    updateAlerts({ filter: false, myFeed: 'created' });
+
+    trackEvent({
+      event_name: AnalyticsEvent.CreateFeed,
+    });
+
+    return router.replace({
+      pathname: router.route,
+      query: {
+        welcome: 'true',
+      },
+    });
+  }, [registerLocalFilters, router, trackEvent, updateAlerts]);
+
+  useEffect(() => {
+    let stopNav = true;
+
+    const routeHandler = (newRoute: string) => {
+      if (!stopNav || newRoute === '/my-feed') {
+        return;
+      }
+      router.events.emit('routeChangeError');
+
+      showPrompt(promptConfig).then((leave) => {
+        if (leave) {
+          stopNav = false;
+          trackEvent({
+            event_name: AnalyticsEvent.OnboardingSkip,
+          });
+          router.replace(newRoute);
+        }
+      });
+      throw new Error('Cancelling navigation');
+    };
+
+    router.events.on('routeChangeStart', routeHandler);
+    return () => {
+      router.events.off('routeChangeStart', routeHandler);
+    };
+  }, [router, showPrompt, trackEvent]);
+
+  return (
+    <LayoutHeader className="flex-col overflow-x-visible">
+      <div className="fixed z-1 flex w-full justify-center bg-gradient-to-b from-theme-surface-invert via-theme-surface-invert tablet:px-4 tablet:py-10">
+        <div className="flex h-44 w-full max-w-[40rem] flex-col items-center border-b-2 border-theme-color-cabbage bg-theme-bg-subtle p-6 tablet:h-auto tablet:flex-row tablet:rounded-16 tablet:border-b-0 tablet:border-l-2">
+          <div className="text-center tablet:text-left">
+            <p className="font-bold typo-title3">
+              Pick tags that are relevant to you
+            </p>
+            <p className="text-theme-label-tertiary typo-callout">
+              You can always modify your tags later
+            </p>
+          </div>
+          <div className="flex flex-1" />
+          <Button
+            size={ButtonSize.Large}
+            variant={ButtonVariant.Primary}
+            disabled={!isFeedPreviewEnabled}
+            onClick={completeOnboarding}
+          >
+            Create my feed
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex max-w-full flex-col">
+        <FilterOnboardingV4 className="mt-44 pt-6 tablet:pt-0" />
+        <div className="mt-10 flex items-center justify-center gap-10 text-theme-label-quaternary typo-callout">
+          <div className="h-px flex-1 bg-theme-divider-tertiary" />
+          <Button
+            variant={
+              isPreviewFeedVisible
+                ? ButtonVariant.Primary
+                : ButtonVariant.Secondary
+            }
+            disabled={!isFeedPreviewEnabled}
+            icon={
+              <ArrowIcon
+                className={classNames(!isPreviewFeedVisible && 'rotate-180')}
+              />
+            }
+            iconPosition={ButtonIconPosition.Right}
+            onClick={() => {
+              setPreviewFeedVisible((current) => !current);
+            }}
+          >
+            {isFeedPreviewEnabled
+              ? `${isPreviewFeedVisible ? 'Hide' : 'Show'} feed preview`
+              : `${tagsCount}/${REQUIRED_TAGS_THRESHOLD} to show feed preview`}
+          </Button>
+          <div className="h-px flex-1 bg-theme-divider-tertiary" />
+        </div>
+      </div>
+    </LayoutHeader>
+  );
+};
