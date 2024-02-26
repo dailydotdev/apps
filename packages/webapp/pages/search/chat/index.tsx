3e1c7e53260faf610ffe47db77ab7e08fd9873ac
Chris Bongers <chrisbongers@gmail.com>
2024-02-26 16:21:08 +0200

    fix: mionor cleanup

--- packages/webapp/pages/search/chat/index.tsx
+++ packages/webapp/pages/search/chat/index.tsx
@@ -1,11 +1,11 @@
 import React, { ReactElement } from 'react';
 import { withFeaturesBoundary } from '@dailydotdev/shared/src/components';
 import { MainLayoutProps } from '@dailydotdev/shared/src/components/MainLayout';
-import SearchV1Page from '../../../components/search/SearchV1Page';
+import SearchPage from '../../../components/search/SearchChatPage';
 import { GetSearchLayout } from '../../../components/layouts/SearchLayout';
 
 const SearchChatPage = (): ReactElement => {
-  return <SearchV1Page />;
+  return <SearchPage />;
 };
 
 SearchChatPage.getLayout = GetSearchLayout;
