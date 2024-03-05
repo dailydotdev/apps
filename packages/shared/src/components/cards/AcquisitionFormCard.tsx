14cd49b0c0628a721b897e5079e2cc2a34b47e69
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-03-05 21:15:53 +0800

    fix: acquisition form card on mobile layout

--- packages/shared/src/components/cards/AcquisitionFormCard.tsx
+++ packages/shared/src/components/cards/AcquisitionFormCard.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useContext, useEffect, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 import { Card } from './Card';
 import { Radio } from '../fields/Radio';
 import { Button, ButtonSize, ButtonVariant } from '../buttons/Button';
@@ -10,6 +11,7 @@ import { OnboardingTitleGradient } from '../onboarding/common';
 import { removeQueryParam } from '../../lib/links';
 import AnalyticsContext from '../../contexts/AnalyticsContext';
 import { AnalyticsEvent, UserAcquisitionEvent } from '../../lib/analytics';
+import { useFeedLayout } from '../../hooks';
 
 const options = [
   {
@@ -36,6 +38,7 @@ export function AcquisitionFormCard(): ReactElement {
   const { trackEvent } = useContext(AnalyticsContext);
   const [isDismissed, setIsDismissed] = useState(false);
   const [value, setValue] = useState<AcquisitionChannel>();
+  const { shouldUseMobileFeedLayout } = useFeedLayout();
   const router = useRouter();
   const onRemoveQueryParams = () => {
     const updatedUrl = removeQueryParam(window.location.href, acquisitionKey);
@@ -77,7 +80,13 @@ export function AcquisitionFormCard(): ReactElement {
   }
 
   return (
-    <Card data-testid="acquisitionFormCard" className="p-4">
+    <Card
+      data-testid="acquisitionFormCard"
+      className={classNames(
+        'p-4',
+        shouldUseMobileFeedLayout && '!bg-theme-bg-primary',
+      )}
+    >
       <OnboardingTitleGradient className="flex w-full flex-row items-center whitespace-nowrap pb-1 typo-body">
         How did you hear about us?
         <Button
@@ -96,7 +105,10 @@ export function AcquisitionFormCard(): ReactElement {
         value={value}
       />
       <Button
-        className="mt-auto w-full"
+        className={classNames(
+          'w-full',
+          shouldUseMobileFeedLayout ? 'mt-4' : 'mt-auto',
+        )}
         variant={ButtonVariant.Primary}
         size={ButtonSize.Small}
         loading={isLoading}
