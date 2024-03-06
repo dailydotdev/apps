ca520a1b3d473f4119cc598dbd618e06517e2fe2
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-03-06 20:56:21 +0800

    fix: reading streak staletime (#2745)

--- packages/shared/src/hooks/streaks/useReadingStreak.ts
+++ packages/shared/src/hooks/streaks/useReadingStreak.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { generateQueryKey, RequestKey } from '../../lib/query';
+import { generateQueryKey, RequestKey, StaleTime } from '../../lib/query';
 import { getReadingStreak, UserStreak } from '../../graphql/users';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { useStreakExperiment } from './useStreakExperiment';
@@ -16,7 +16,7 @@ export const useReadingStreak = (): UserReadingStreak => {
   const { data: streak, isLoading } = useQuery(
     generateQueryKey(RequestKey.UserStreak, user),
     getReadingStreak,
-    { enabled: shouldShowStreak },
+    { enabled: shouldShowStreak, staleTime: StaleTime.Default },
   );
 
   return {
