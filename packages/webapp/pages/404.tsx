3f635eacc6224678587e09c66cd77a559923c259
Chris Bongers <chrisbongers@gmail.com>
2024-03-06 10:55:24 +0200

    fix: add tracking event for 404 page

--- packages/webapp/pages/404.tsx
+++ packages/webapp/pages/404.tsx
@@ -1,8 +1,24 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext, useEffect, useState } from 'react';
 import Custom404 from '@dailydotdev/shared/src/components/Custom404';
 import { NextSeo } from 'next-seo';
+import { AnalyticsEvent } from '@dailydotdev/shared/src/lib/analytics';
+import AnalyticsContext from '@dailydotdev/shared/src/contexts/AnalyticsContext';
 
 export default function Custom404Seo(): ReactElement {
+  const { trackEvent } = useContext(AnalyticsContext);
+  const [trackedImpression, setTrackedImpression] = useState(false);
+
+  useEffect(() => {
+    if (trackedImpression) {
+      return;
+    }
+
+    trackEvent({
+      event_name: AnalyticsEvent.View404Page,
+    });
+    setTrackedImpression(true);
+  }, [trackEvent, trackedImpression]);
+
   return (
     <Custom404>
       <NextSeo title="Page not found" nofollow noindex />
