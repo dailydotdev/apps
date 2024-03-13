0a73c06902d8758229edd33714d9f1c8c9237c17
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-03-13 15:11:08 +0800

    fix: shared component

--- packages/shared/src/components/cards/v1/SharedPostCardFooter.tsx
+++ packages/shared/src/components/cards/v1/SharedPostCardFooter.tsx
@@ -23,12 +23,9 @@ export const SharedPostCardFooter = ({
         'mb-2 flex h-auto min-h-0 w-full flex-auto flex-col gap-3 rounded-12 border border-theme-divider-tertiary p-3 mobileXXL:flex-row',
       )}
     >
-      <div className={classNames('flex flex-col')}>
-        <p className="line-clamp-3 text-text-secondary typo-body">
-          {sharedPost.title}
-        </p>
+      <div className={classNames('flex flex-col mobileXL:flex-1')}>
+        <span className="line-clamp-1">{sharedPost.title}</span>
       </div>
-
       <CardCover
         data-testid="sharedPostImage"
         isVideoType={isVideoType}
@@ -38,7 +35,8 @@ export const SharedPostCardFooter = ({
           loading: 'lazy',
           alt: 'Shared Post Cover image',
           src: sharedPost.image,
-          className: 'min-h-0 w-full',
+          className:
+            'w-full mobileXXL:!w-40 mobileXXL:!h-20 mobileXXL:!min-h-0',
         }}
         videoProps={{ size: IconSize.XXXLarge }}
       />
