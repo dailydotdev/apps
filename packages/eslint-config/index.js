27aeb37f9beda8ca8b9a403014e313e29c40887a
Ido Shamun <idoesh1@gmail.com>
2024-03-26 18:40:20 +0200

    chore: disable import/no-cycle

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -33,6 +33,7 @@ module.exports = {
   },
   rules: {
     curly: ['error', 'all'],
+    'import/no-cycle': 0,
     'react/prop-types': 0,
     'react/require-default-props': 0,
     'react/no-unused-prop-types': 0,
