826e1afcd3e16d0fe4a6394e7666e2f6a09651c1
Chris Bongers <chrisbongers@gmail.com>
2024-03-26 16:10:02 +0200

    fix: initial feedback

--- packages/shared/src/hooks/useConditionalFeature.ts
+++ packages/shared/src/hooks/useConditionalFeature.ts
@@ -1,9 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { JSONValue, WidenPrimitives } from '@growthbook/growthbook';
 import { useGrowthBook } from '@growthbook/growthbook-react';
+import { useContext } from 'react';
 import { generateQueryKey, RequestKey } from '../lib/query';
 import { Feature } from '../lib/featureManagement';
 import { disabledRefetch } from '../lib/func';
+import AuthContext from '../contexts/AuthContext';
+import { useGrowthBookContext } from '../components/GrowthBookProvider';
 
 export const useConditionalFeature = <T extends JSONValue>({
   feature,
@@ -12,13 +15,9 @@ export const useConditionalFeature = <T extends JSONValue>({
   feature: Feature<T>;
   shouldEvaluate: boolean;
 }): WidenPrimitives<T> => {
-  const growthbook = useGrowthBook();
-  const queryKey = generateQueryKey(
-    RequestKey.Feature,
-    null,
-    feature.id,
-    shouldEvaluate,
-  );
+  const { user } = useContext(AuthContext);
+  const { growthbook } = useGrowthBookContext();
+  const queryKey = generateQueryKey(RequestKey.Feature, user, feature.id);
 
   const { data: featureValue } = useQuery(
     queryKey,
@@ -28,10 +27,11 @@ export const useConditionalFeature = <T extends JSONValue>({
         : (feature.defaultValue as WidenPrimitives<T>);
     },
     {
+      initialData: feature.defaultValue as WidenPrimitives<T>,
       enabled: shouldEvaluate,
       ...disabledRefetch,
     },
   );
 
-  return featureValue ?? (feature.defaultValue as WidenPrimitives<T>);
+  return featureValue;
 };
