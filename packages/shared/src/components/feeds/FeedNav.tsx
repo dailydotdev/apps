cf40ce765cb895073777be9666394c5700c2657a
dragosiorgulescu <dragos.m.iorgulescu@gmail.com>
2024-03-27 09:22:55 +0200

    fix: add more spacing to mobile feed nav bell icon
    - otherwise it's clipped when there is a counter badge on it

--- packages/shared/src/components/feeds/FeedNav.tsx
+++ packages/shared/src/components/feeds/FeedNav.tsx
@@ -42,7 +42,7 @@ function FeedNav(): ReactElement {
         <Tab label={FeedNavTab.History} url="/history" />
       </TabContainer>
 
-      <div className="fixed right-0 top-0 my-1 flex h-12 w-36 items-center justify-end bg-gradient-to-r from-transparent via-background-default via-60% to-background-default">
+      <div className="fixed right-0 top-0 my-1 mr-0.5 flex h-12 w-36 items-center justify-end bg-gradient-to-r from-transparent via-background-default via-60% to-background-default">
         <NotificationsBell />
       </div>
     </div>
