accf260ba3b780d10d7cbaea07d28248f6c58bf5
Chris Bongers <chrisbongers@gmail.com>
2024-03-28 16:51:13 +0200

    fix: comment tracking (#2880)

--- packages/shared/src/components/CommentFeed.tsx
+++ packages/shared/src/components/CommentFeed.tsx
@@ -106,7 +106,10 @@ export default function CommentFeed<T>({
               postScoutId={null}
               appendTooltipTo={() => document.body}
               linkToComment
+              lazy
               showContextHeader
+              trackImpression
+              trackClick
             />
           )),
         )}
