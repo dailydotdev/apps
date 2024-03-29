631f34ba301a93e27b3091e8477ad054467122d3
dragosiorgulescu <dragos.m.iorgulescu@gmail.com>
2024-03-29 10:04:42 +0200

    fix: feed action buttons positioning

--- packages/shared/src/components/feeds/FeedContainer.tsx
+++ packages/shared/src/components/feeds/FeedContainer.tsx
@@ -173,6 +173,7 @@ export const FeedContainer = ({
                 className={classNames(
                   'flex flex-col rounded-16 border border-theme-divider-tertiary tablet:mt-6',
                   isSearch && 'mt-6',
+                  isNewMobileLayout && 'border-0 mobileL:mt-2 tablet:mt-2',
                 )}
               >
                 <span className="flex w-full flex-row items-center justify-between px-6 py-4">
