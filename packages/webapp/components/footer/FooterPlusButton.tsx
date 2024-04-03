b70cfa78b6996d765d850e4a304e10f6b1db1081
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-04-04 01:07:52 +0800

    fix: footer create button

--- packages/webapp/components/footer/FooterPlusButton.tsx
+++ packages/webapp/components/footer/FooterPlusButton.tsx
@@ -1,31 +1,34 @@
 import React, { ReactElement, useState } from 'react';
-import { CreatePostButton } from '@dailydotdev/shared/src/components/post/write';
 import { Drawer } from '@dailydotdev/shared/src/components/drawers';
 import {
   Button,
   ButtonSize,
   ButtonVariant,
 } from '@dailydotdev/shared/src/components/buttons/Button';
-import { EditIcon, LinkIcon } from '@dailydotdev/shared/src/components/icons';
+import {
+  EditIcon,
+  LinkIcon,
+  PlusIcon,
+} from '@dailydotdev/shared/src/components/icons';
 import { link } from '@dailydotdev/shared/src/lib/links';
 import { RootPortal } from '@dailydotdev/shared/src/components/tooltips/Portal';
 
 export function FooterPlusButton(): ReactElement {
-  const [newPostOptsOpen, toggleNewPostOpts] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   return (
-    <div className="text-center">
-      <CreatePostButton
-        onClick={() => toggleNewPostOpts(true)}
-        compact
-        sidebar
-        className="h-8"
+    <>
+      <Button
+        icon={<PlusIcon />}
+        variant={ButtonVariant.Float}
+        onClick={() => setIsDrawerOpen(true)}
+        className="z-1 border border-border-subtlest-tertiary"
       />
       <RootPortal>
         <Drawer
           displayCloseButton
-          isOpen={newPostOptsOpen}
-          onClose={() => toggleNewPostOpts(false)}
+          isOpen={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
         >
           <div className="flex h-auto justify-around p-4">
             <div className="flex flex-col items-center gap-2">
@@ -57,6 +60,6 @@ export function FooterPlusButton(): ReactElement {
           </div>
         </Drawer>
       </RootPortal>
-    </div>
+    </>
   );
 }
