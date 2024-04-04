ef235849e3f5dda8a47dbea8736879eb41f9a441
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-04-05 00:45:50 +0800

    fix: flex orientation

--- packages/shared/src/components/MainLayout.tsx
+++ packages/shared/src/components/MainLayout.tsx
@@ -225,7 +225,7 @@ function MainLayoutComponent({
       />
       <main
         className={classNames(
-          'flex flex-col laptop:flex-row',
+          'flex flex-col',
           className,
           !isScreenCentered && sidebarExpanded
             ? 'laptop:pl-60'
