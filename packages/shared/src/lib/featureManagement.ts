a09fb2c18d1b8611d18634e89025a9c10c061294
NensiDosari <nensidosari97@gmail.com>
2024-04-05 14:57:31 +0200

    fix: remove true on mobile ux exp

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -41,7 +41,7 @@ const feature = {
   searchVersion: new Feature('search_version', 1),
   commentFeed: new Feature('comment_feed', false),
   forcedTagSelection: new Feature('forced_tag_selection', false),
-  mobileUxLayout: new Feature('mobile_ux_layout', true),
+  mobileUxLayout: new Feature('mobile_ux_layout', false),
   readingReminder: new Feature('reading_reminder', false),
 };
 
