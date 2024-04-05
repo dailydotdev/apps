c3537d5838413dea8d1c113c7cd57e3c3313a754
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-04-06 00:54:34 +0800

    Revert "fix: footer navbar spacing (#2944)"
    This reverts commit e7493f5e26506ab475a40ab7a27ba1b09fd00632.

--- packages/webapp/components/FooterNavBar.tsx
+++ packages/webapp/components/FooterNavBar.tsx
@@ -70,7 +70,7 @@ export default function FooterNavBar({
       className={classNames(
         'fixed !bottom-0 left-0 z-2 w-full',
         isNewMobileLayout
-          ? 'bg-gradient-to-t from-blur-baseline via-blur-bg via-70% to-transparent p-2 pb-6'
+          ? 'bg-gradient-to-t from-blur-baseline via-blur-bg via-70% to-transparent p-2'
           : post && 'bg-blur-bg backdrop-blur-20',
       )}
     >
@@ -92,12 +92,12 @@ export default function FooterNavBar({
         spring="veryGentle"
         element="nav"
         className={classNames(
-          'grid w-full grid-flow-col items-center justify-between px-3',
+          'grid h-14 w-full grid-flow-col items-center justify-between px-3',
           !showNav && 'hidden',
           styles.footerNavBar,
           isNewMobileLayout
             ? classNames('rounded-16', !post && activeClasses)
-            : 'h-14 rounded-t-24 bg-background-default',
+            : 'rounded-t-24 bg-background-default',
           !post && 'border-t border-theme-divider-tertiary',
         )}
       >
