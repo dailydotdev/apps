607c044f1e43004c05578b9652da1fa207bc213c
Chris Bongers <chrisbongers@gmail.com>
2024-04-08 16:52:18 +0200

    fix: linter

--- packages/shared/src/components/cards/PostTags.tsx
+++ packages/shared/src/components/cards/PostTags.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useMemo, useRef } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import { Post } from '../../graphql/posts';
 import Classed from '../../lib/classed';
 
