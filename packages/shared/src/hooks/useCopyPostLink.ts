af6a13c04283749670f3bcb237cf06797af81dab
Milan Freml <milan@daily.dev>
2024-04-12 09:02:07 +0200

    fix: share post link copy needs to be clicked twice

--- packages/shared/src/hooks/useCopyPostLink.ts
+++ packages/shared/src/hooks/useCopyPostLink.ts
@@ -1,7 +1,7 @@
 import { CopyNotifyFunction, useCopyLink } from './useCopy';
 
 export function useCopyPostLink(link?: string): [boolean, CopyNotifyFunction] {
-  const [copying, copy] = useCopyLink(() => link);
+  const [copying, copy] = useCopyLink(link ? () => link : undefined);
 
   return [copying, copy];
 }
