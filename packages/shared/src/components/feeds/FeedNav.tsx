2c981aab0095730dba96eaaa0e140addb65e4fe2
dragosiorgulescu <dragos.m.iorgulescu@gmail.com>
2024-04-15 10:22:14 +0300

    fix: spacing issue when sorting action is enabled on mobile

--- packages/shared/src/components/feeds/FeedNav.tsx
+++ packages/shared/src/components/feeds/FeedNav.tsx
@@ -83,7 +83,7 @@ function FeedNav(): ReactElement {
         ))}
       </TabContainer>
       {isMobile && sortingEnabled && isSortableFeed && (
-        <div className="sticky flex h-11 w-20 -translate-y-12 translate-x-[calc(100vw-100%)] items-center justify-end bg-gradient-to-r from-transparent via-background-default via-40% to-background-default pr-4">
+        <div className="fixed flex h-11 w-20 -translate-y-12 translate-x-[calc(100vw-100%)] items-center justify-end bg-gradient-to-r from-transparent via-background-default via-40% to-background-default pr-4">
           <Dropdown
             className={{ label: 'hidden', chevron: 'hidden', button: '!px-1' }}
             dynamicMenuWidth
