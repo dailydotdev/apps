68c60235a81ae472c61d488589d350feb1cce961
NensiDosari <nensidosari97@gmail.com>
2024-04-15 15:19:57 +0200

    feat: use lodash instead of own func

--- packages/shared/src/components/cards/AcquisitionFormCard.tsx
+++ packages/shared/src/components/cards/AcquisitionFormCard.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useContext, useEffect, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
+import shuffle from 'lodash.shuffle';
 import { Card } from './Card';
 import { Card as CardV1 } from './v1/Card';
 import { Radio } from '../fields/Radio';
@@ -13,7 +14,6 @@ import { removeQueryParam } from '../../lib/links';
 import AnalyticsContext from '../../contexts/AnalyticsContext';
 import { AnalyticsEvent, UserAcquisitionEvent } from '../../lib/analytics';
 import { useFeedLayout } from '../../hooks';
-import { shuffle } from '../../lib/func';
 
 const options = [
   {
@@ -41,6 +41,7 @@ export function AcquisitionFormCard(): ReactElement {
   const [isDismissed, setIsDismissed] = useState(false);
   const [value, setValue] = useState<AcquisitionChannel>();
   const { shouldUseMobileFeedLayout } = useFeedLayout();
+  const [shuffledOptions] = useState(() => shuffle(options));
   const router = useRouter();
   const onRemoveQueryParams = () => {
     const updatedUrl = removeQueryParam(window.location.href, acquisitionKey);
@@ -69,8 +70,6 @@ export function AcquisitionFormCard(): ReactElement {
       event_name: AnalyticsEvent.Impression,
       target_type: acquisitionKey,
     });
-
-    shuffle(options);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -101,7 +100,7 @@ export function AcquisitionFormCard(): ReactElement {
       <Radio<AcquisitionChannel>
         name="acquisition"
         onChange={setValue}
-        options={options}
+        options={shuffledOptions}
         value={value}
       />
       <Button
