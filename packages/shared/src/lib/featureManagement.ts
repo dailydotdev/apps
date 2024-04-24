030d8f0f24c1d118bdb8e40320d0a425c5b83652
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-04-24 11:16:20 +0800

    feat: experiment on sidebar state (#3039)

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -36,6 +36,7 @@ const feature = {
   onboardingMostVisited: new Feature('onboarding_most_visited', false),
   shareExperience: new Feature('share_experience', false),
   bookmarkLoops: new Feature('bookmark_loops', false),
+  sidebarClosed: new Feature('sidebar_closed', false),
 };
 
 export { feature };
