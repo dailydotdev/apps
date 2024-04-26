fe25fe1a574c24739b4e550a2b0719e57cf0eb25
Chris Bongers <chrisbongers@gmail.com>
2024-04-26 13:54:51 +0200

    fix: missed update

--- packages/shared/src/hooks/useActions.ts
+++ packages/shared/src/hooks/useActions.ts
@@ -64,7 +64,7 @@ export const useActions = (): UseActions => {
 
         return {
           actions: [...old?.actions, optimisticAction],
-          serverLoaded: old?.serverLoaded,
+          serverLoaded: !!old?.serverLoaded,
         };
       });
 
