ae387792799efb5f48c5247576f6dc7ee93a7bc3
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-04-30 14:20:42 +0800

    fix: squad page header and animation (#3066)

--- packages/shared/src/components/squads/SquadHeaderBar.tsx
+++ packages/shared/src/components/squads/SquadHeaderBar.tsx
@@ -48,12 +48,13 @@ export function SquadHeaderBar({
   const totalStepsCount = steps.length;
   const checklistTooltipText = `${completedStepsCount}/${totalStepsCount}`;
   const showJoinButton = squad.public && !squad.currentMember;
+  const firstItemClasses = 'order-5 tablet:order-1';
 
   return (
     <div
       {...props}
       className={classNames(
-        'no-scrollbar flex h-fit w-full flex-row justify-center gap-4 overflow-x-auto pr-4 tablet:w-auto tablet:pr-0',
+        'flex h-fit w-full flex-row flex-wrap justify-center gap-4 tablet:w-auto',
         className,
       )}
     >
@@ -61,7 +62,7 @@ export function SquadHeaderBar({
         <Button
           variant={ButtonVariant.Secondary}
           className={classNames(
-            'ml-14 tablet:ml-0',
+            firstItemClasses,
             tourIndex === TourScreenIndex.CopyInvitation && 'highlight-pulse',
           )}
           onClick={() => {
@@ -75,12 +76,16 @@ export function SquadHeaderBar({
       )}
       {showJoinButton && (
         <SquadJoinButton
-          className="flex w-full flex-1 tablet:ml-auto tablet:w-auto tablet:flex-initial"
+          className={{ wrapper: firstItemClasses }}
           squad={squad}
           origin={Origin.SquadPage}
         />
       )}
-      <SquadMemberShortList squad={squad} members={members} />
+      <SquadMemberShortList
+        className="order-1 tablet:order-2"
+        squad={squad}
+        members={members}
+      />
       {!!squad.currentMember && (
         <SimpleTooltip
           forceLoad={!isTesting}
@@ -94,6 +99,7 @@ export function SquadHeaderBar({
         >
           <Button
             data-testid="squad-checklist-button"
+            className="order-2 tablet:order-3"
             variant={ButtonVariant.Float}
             icon={<ChecklistBIcon secondary size={IconSize.Small} />}
             onClick={() => {
@@ -110,6 +116,7 @@ export function SquadHeaderBar({
         >
           <Button
             data-testid="squad-notification-button"
+            className="order-3 tablet:order-4"
             variant={ButtonVariant.Float}
             icon={
               <BellIcon
@@ -128,6 +135,7 @@ export function SquadHeaderBar({
       )}
       <SimpleTooltip placement="top" content="Squad options">
         <Button
+          className="order-4 tablet:order-5"
           variant={ButtonVariant.Float}
           icon={<MenuIcon size={IconSize.Small} />}
           onClick={onMenuClick}
