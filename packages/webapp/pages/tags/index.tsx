7a47a691acf8bb99dc39f262d5450133331343e2
Chris Bongers <chrisbongers@gmail.com>
2024-05-29 17:17:45 +0700

    fix: cleanup the page styling

--- packages/webapp/pages/tags/index.tsx
+++ packages/webapp/pages/tags/index.tsx
@@ -1,63 +1,151 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import { Card } from '@dailydotdev/shared/src/components/cards/Card';
 import { getTagPageLink } from '@dailydotdev/shared/src/lib/links';
 import Link from 'next/link';
-import { getLayout } from '../../components/layouts/MainLayout';
+import { useQuery } from '@tanstack/react-query';
+import { RequestKey, StaleTime } from '@dailydotdev/shared/src/lib/query';
+import request from 'graphql-request';
+import { Source } from '@dailydotdev/shared/src/graphql/sources';
+import { graphqlUrl } from '@dailydotdev/shared/src/lib/config';
+import { Keyword, TAGS_QUERY } from '@dailydotdev/shared/src/graphql/keywords';
+import { TagLink } from '@dailydotdev/shared/src/components/TagLinks';
+import { useViewSize, ViewSize } from '@dailydotdev/shared/src/hooks';
+import classed from '@dailydotdev/shared/src/lib/classed';
+import {
+  Button,
+  ButtonVariant,
+} from '@dailydotdev/shared/src/components/buttons/Button';
+import { HomeIcon } from '@dailydotdev/shared/src/components/icons';
 import { getLayout as getFooterNavBarLayout } from '../../components/layouts/FooterNavBarLayout';
+import { getLayout } from '../../components/layouts/MainLayout';
 
 const ListItem = ({
   index,
-  tag,
+  href,
+  children,
 }: {
   index: number;
-  tag: string;
+  href: string;
+  children: ReactElement;
 }): ReactElement => {
   return (
-    <li className="py-1.5 pr-2 text-text-tertiary">
-      <Link href={getTagPageLink(tag)} passHref key={tag} prefetch={false}>
-        <a className="inline-block w-full">
+    <li className="py-1.5 pr-2">
+      <Link href={href} passHref key={href} prefetch={false}>
+        <a className="flex w-full flex-row items-center">
           <span className="inline-flex w-6 pr-2 text-text-quaternary">
             {index}
           </span>
-          {tag}
+          {children}
         </a>
       </Link>
     </li>
   );
 };
 
-const TopList = ({ title }: { title: string }): ReactElement => {
+const TopList = ({
+  title,
+  items,
+  className,
+}: {
+  title: string;
+  items: Keyword[] | Source[];
+  className?: string;
+}): ReactElement => {
+  const isMobile = useViewSize(ViewSize.MobileL);
+  const MobileDiv = classed(
+    'div',
+    'flex flex-col border-b border-b-border-subtlest-tertiary p-4',
+    className,
+  );
+  const CardElement = classed(Card, '!p-4');
+  const Wrapper = isMobile ? MobileDiv : CardElement;
   return (
-    <Card className="!p-4">
+    <Wrapper>
       <h3 className="mb-2 font-bold typo-title3">{title}</h3>
       <ol className="typo-body">
-        {[...new Array(10)].map((_, i) => (
-          <ListItem key={i} index={i + 1} tag={i + 1} />
+        {items?.map((tag, i) => (
+          <ListItem key={i} index={i + 1} href={getTagPageLink(tag.value)}>
+            <p>{tag.value}</p>
+          </ListItem>
         ))}
       </ol>
-    </Card>
+    </Wrapper>
   );
 };
 
 const TagsPage = (): ReactElement => {
+  const { data } = useQuery(
+    [RequestKey.Tags, null, 'all'],
+    async () => await request<{ tags: Keyword[] }>(graphqlUrl, TAGS_QUERY),
+    {
+      staleTime: StaleTime.OneHour,
+    },
+  );
+
+  const recentlyAddedTags = useMemo(() => {
+    return data?.tags
+      ?.sort((a, b) => +new Date(b.createdAt) - +new Date(a.createdAt))
+      .slice(0, 10);
+  }, [data]);
+
+  const tagsByFirstLetter = useMemo(() => {
+    return data?.tags?.reduce((acc, cur) => {
+      const rawLetter = cur.value[0].toLowerCase();
+      const firstLetter = new RegExp(/^[a-zA-Z]+$/).test(rawLetter)
+        ? rawLetter
+        : '#';
+      acc[firstLetter] = (acc[firstLetter] || []).concat([cur]);
+      return acc;
+    }, []);
+  }, [data]);
+
   return (
-    <main className="px-10 py-6">
-      <div className="flex h-10 items-center">Breadcrumbs</div>
-      <div className="grid grid-cols-3 gap-6">
-        <TopList title="Trending tags" />
-        <TopList title="Popular tags" />
-        <TopList title="Recently added tags" />
+    <main className="flex flex-col gap-4 p-0 tablet:px-10 tablet:py-6">
+      <div className="hidden h-10 items-center p-1.5 text-border-subtlest-tertiary laptop:flex">
+        <Button
+          variant={ButtonVariant.Tertiary}
+          icon={<HomeIcon />}
+          tag="a"
+          href={process.env.NEXT_PUBLIC_WEBAPP_URL}
+        />
+        /
+        <Button variant={ButtonVariant.Tertiary} icon={<HomeIcon />} disabled>
+          Sources
+        </Button>
       </div>
-      <div>
-        <p>All tags</p>
-        <div className="grid grid-cols-5">
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-          <p>Item 4</p>
-          <p>Item 5</p>
-          <p>Item 6</p>
-        </div>
+      <div className="grid grid-cols-1 gap-0 tablet:grid-cols-2 tablet:gap-6 laptopL:grid-cols-3">
+        <TopList title="Trending tags" items={recentlyAddedTags} />
+        <TopList title="Popular tags" items={recentlyAddedTags} />
+        <TopList
+          className="col-span-1 tablet:col-span-2 laptopL:col-span-1"
+          title="Recently added tags"
+          items={recentlyAddedTags}
+        />
+      </div>
+      <div className="flex h-10 items-center justify-between px-4 tablet:px-0">
+        <p className="font-bold typo-body">All tags</p>
+      </div>
+      <div className="columns-[17rem] px-4 tablet:px-0">
+        {tagsByFirstLetter &&
+          Object.entries(tagsByFirstLetter).map(([letter, value]) => {
+            return (
+              <div
+                key={letter}
+                className="mt-3 flex flex-col items-baseline gap-3 px-4 first:mt-0"
+              >
+                <p className="flex h-8 items-center font-bold text-text-tertiary typo-callout">
+                  {letter}
+                </p>
+                {value.map((tag) => (
+                  <TagLink
+                    key={tag.value}
+                    tag={tag.value}
+                    className="!line-clamp-2 !h-auto"
+                  />
+                ))}
+              </div>
+            );
+          })}
       </div>
     </main>
   );
