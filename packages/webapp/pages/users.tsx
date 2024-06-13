bf024e8c7bf363421058c029275b538af5bc1097
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-06-13 13:18:42 +0200

    feat: user leaderboard page and query

---
+++ packages/webapp/pages/users.tsx
@@ -0,0 +1,199 @@
+import React from 'react';
+import { GetStaticPropsResult } from 'next';
+import { ApiError } from '@dailydotdev/shared/src/graphql/common';
+import request from 'graphql-request';
+import { graphqlUrl } from '@dailydotdev/shared/src/lib/config';
+import { LEADERBOARD_QUERY } from '@dailydotdev/shared/src/graphql/leaderboard';
+import { useRouter } from 'next/router';
+import { BreadCrumbs } from '@dailydotdev/shared/src/components/header';
+import { SquadIcon } from '@dailydotdev/shared/src/components/icons';
+import { IconSize } from '@dailydotdev/shared/src/components/Icon';
+import { UserHighlight } from '@dailydotdev/shared/src/components/widgets/PostUsersHighlights';
+import { ElementPlaceholder } from '@dailydotdev/shared/src/components/ElementPlaceholder';
+import classed from '@dailydotdev/shared/src/lib/classed';
+import { LoggedUser } from '@dailydotdev/shared/src/lib/user';
+import { getLayout as getFooterNavBarLayout } from '../components/layouts/FooterNavBarLayout';
+import { getLayout } from '../components/layouts/MainLayout';
+import { ListItem, TopList } from '../components/common';
+
+type UserLeaderboard = {
+  score: number;
+  user: LoggedUser;
+};
+
+type PageProps = {
+  highestReputation: UserLeaderboard[];
+  longestStreak: UserLeaderboard[];
+  highestPostViews: UserLeaderboard[];
+  mostUpvoted: UserLeaderboard[];
+  mostReferrals: UserLeaderboard[];
+  mostReadingDays: UserLeaderboard[];
+};
+
+const PlaceholderList = classed(
+  ElementPlaceholder,
+  'h-[1.6875rem] my-1.5 rounded-12',
+);
+
+const indexToEmoji = (index: number): string => {
+  switch (index) {
+    case 0:
+      return '🏆';
+    case 1:
+      return '🥈';
+    case 2:
+      return '🥉';
+    default:
+      return '';
+  }
+};
+
+const UserTopList = ({
+  items,
+  isLoading,
+  ...props
+}: {
+  title: string;
+  items: UserLeaderboard[];
+  isLoading: boolean;
+  className?: string;
+}): JSX.Element => {
+  return (
+    <TopList {...props}>
+      <>
+        {/* eslint-disable-next-line react/no-array-index-key */}
+        {isLoading && [...Array(10)].map((_, i) => <PlaceholderList key={i} />)}
+        {items?.map((item, i) => (
+          <ListItem
+            key={item.user.id}
+            index={item.score}
+            href={item.user.permalink}
+          >
+            <>
+              <span className="pl-1">{indexToEmoji(i)}</span>
+              <UserHighlight
+                {...item.user}
+                showReputation
+                className={{
+                  wrapper: 'min-w-0 flex-shrink !p-2',
+                  image: '!h-8 !w-8',
+                  textWrapper: '!ml-2',
+                  name: '!typo-caption1',
+                  reputation: '!typo-caption1',
+                  handle: '!typo-caption2',
+                }}
+                allowSubscribe={false}
+              />
+            </>
+          </ListItem>
+        ))}
+      </>
+    </TopList>
+  );
+};
+
+const LeaderboardPage = ({
+  highestReputation,
+  longestStreak,
+  highestPostViews,
+  mostUpvoted,
+  mostReferrals,
+  mostReadingDays,
+}: PageProps): JSX.Element => {
+  const { isFallback: isLoading } = useRouter();
+
+  if (isLoading) {
+    return <></>;
+  }
+
+  return (
+    <main className="py-6 tablet:px-4 laptop:px-10">
+      <div className="mb-6 flex justify-between">
+        <BreadCrumbs>
+          <SquadIcon size={IconSize.XSmall} secondary /> Leaderboard
+        </BreadCrumbs>
+      </div>
+      <div className="grid grid-cols-1 gap-6 tablet:grid-cols-2 laptopXL:grid-cols-3">
+        <UserTopList
+          title="Highest reputation"
+          items={highestReputation}
+          isLoading={isLoading}
+        />
+        <UserTopList
+          title="Longest streak"
+          items={longestStreak}
+          isLoading={isLoading}
+        />
+        <UserTopList
+          title="Highest post views"
+          items={highestPostViews}
+          isLoading={isLoading}
+        />
+        <UserTopList
+          title="Most upvoted"
+          items={mostUpvoted}
+          isLoading={isLoading}
+        />
+        <UserTopList
+          title="Most referrals"
+          items={mostReferrals}
+          isLoading={isLoading}
+        />
+        <UserTopList
+          title="Most reading days"
+          items={mostReadingDays}
+          isLoading={isLoading}
+        />
+      </div>
+    </main>
+  );
+};
+
+const getPageLayout: typeof getLayout = (...props) =>
+  getFooterNavBarLayout(getLayout(...props));
+
+LeaderboardPage.getLayout = getPageLayout;
+LeaderboardPage.layoutProps = {
+  screenCentered: false,
+};
+
+export default LeaderboardPage;
+
+export async function getStaticProps(): Promise<
+  GetStaticPropsResult<PageProps>
+> {
+  try {
+    const res = await request<PageProps>(graphqlUrl, LEADERBOARD_QUERY);
+
+    return {
+      props: {
+        highestReputation: res.highestReputation,
+        longestStreak: res.longestStreak,
+        highestPostViews: null,
+        mostUpvoted: null,
+        mostReferrals: null,
+        mostReadingDays: res.mostReadingDays,
+      },
+      revalidate: 60,
+    };
+  } catch (err) {
+    if (
+      [ApiError.NotFound, ApiError.Forbidden].includes(
+        err?.response?.errors?.[0]?.extensions?.code,
+      )
+    ) {
+      return {
+        props: {
+          highestReputation: null,
+          longestStreak: null,
+          highestPostViews: null,
+          mostUpvoted: null,
+          mostReferrals: null,
+          mostReadingDays: null,
+        },
+        revalidate: 60,
+      };
+    }
+    throw err;
+  }
+}
