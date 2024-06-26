23c50ea58783153839a1be067a99c283937e070d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-06-26 10:15:03 +0200

    feat: add notifications on footer on mobile (#3269)

--- packages/shared/src/components/feeds/MobileFeedActions.tsx
+++ packages/shared/src/components/feeds/MobileFeedActions.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import Logo, { LogoPosition } from '../Logo';
 import { ReadingStreakButton } from '../streak/ReadingStreakButton';
 import { Divider } from '../utilities';
@@ -13,13 +14,17 @@ import { useFeatureTheme } from '../../hooks/utils/useFeatureTheme';
 import { useFeature } from '../GrowthBookProvider';
 import { feature } from '../../lib/featureManagement';
 import { HypeButton } from '../referral';
+import { useAuthContext } from '../../contexts/AuthContext';
+import { ProfileImageSize, ProfilePicture } from '../ProfilePicture';
 
 export function MobileFeedActions(): ReactElement {
   const router = useRouter();
   const { openModal } = useLazyModal();
+  const { user } = useAuthContext();
   const { streak, isLoading, isStreaksEnabled } = useReadingStreak();
   const featureTheme = useFeatureTheme();
   const hypeCampaign = useFeature(feature.hypeCampaign);
+  const notificationsNavBar = useFeature(feature.notificationsNavBar);
 
   return (
     <div className="flex flex-row justify-between px-4 py-1">
@@ -48,7 +53,19 @@ export function MobileFeedActions(): ReactElement {
             })
           }
         />
-        <NotificationsBell compact />
+        {notificationsNavBar ? (
+          <Link href={user.permalink} passHref>
+            <a>
+              <ProfilePicture
+                user={user}
+                size={ProfileImageSize.Medium}
+                nativeLazyLoading
+              />
+            </a>
+          </Link>
+        ) : (
+          <NotificationsBell compact />
+        )}
       </span>
     </div>
   );
