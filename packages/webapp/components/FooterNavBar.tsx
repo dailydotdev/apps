10fab4dcefaf14850a751bd34d9ceb609e03acb1
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-06-27 10:55:47 +0200

    Update packages/webapp/components/FooterNavBar.tsx

--- packages/webapp/components/FooterNavBar.tsx
+++ packages/webapp/components/FooterNavBar.tsx
@@ -88,7 +88,7 @@ export default function FooterNavBar({
         spring="veryGentle"
         element="nav"
         className={classNames(
-          'grid w-full auto-cols-fr grid-flow-col items-center justify-between rounded-16 px-3',
+          'grid w-full auto-cols-fr grid-flow-col items-center justify-between rounded-16',
           !post && activeClasses,
           !showNav && 'hidden',
           !post && 'border-t border-border-subtlest-tertiary',
