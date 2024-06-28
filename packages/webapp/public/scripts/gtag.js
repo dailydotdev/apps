8b4b4957996b123de396df09f66f216c45b00d99
Ido Shamun <idoesh1@gmail.com>
2024-06-28 12:52:15 +0300

    fix: scope the pixels to avoid conflicts

--- packages/webapp/public/scripts/gtag.js
+++ packages/webapp/public/scripts/gtag.js
@@ -1,19 +1,21 @@
-const GA_ID = document.currentScript.getAttribute('data-ga-id');
-const userId = document.currentScript.getAttribute('data-user-id');
-const instanceId = document.currentScript.getAttribute('data-instance-id');
+(function() {
+  const GA_ID = document.currentScript.getAttribute('data-ga-id');
+  const userId = document.currentScript.getAttribute('data-user-id');
+  const instanceId = document.currentScript.getAttribute('data-instance-id');
 
-window.dataLayer = window.dataLayer || [];
+  window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-  dataLayer.push(arguments);
-}
+  function gtag() {
+    dataLayer.push(arguments);
+  }
 
-gtag('js', new Date());
+  gtag('js', new Date());
 
-const props = {
-  'user_id': userId,
-};
-if (instanceId) {
-  props.client_id = instanceId;
-}
-gtag('config', GA_ID, props);
+  const props = {
+    'user_id': userId,
+  };
+  if (instanceId) {
+    props.client_id = instanceId;
+  }
+  gtag('config', GA_ID, props);
+})();
