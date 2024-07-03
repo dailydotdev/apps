b8f0a3ef273080b0e57c8a3cb327f7145135650c
Luca Pagliaro <luca@sentiero.digital>
2024-07-03 14:32:30 +0200

    feat: default mock 'getFeatureValue' now is 'control';

--- packages/storybook/mock/gb.ts
+++ packages/storybook/mock/gb.ts
@@ -1,9 +1,15 @@
+// GrowthBook enables feature flagging and experimentation.
 export class GrowthBook {
   constructor() {
     return this;
   }
+
+  /*
+  * this is a mock implementation for the `GrowthBook.getFeatureValue` method
+  * will return the default value for all the features.
+  * */
   getFeatureValue(key: string, defaultValue: unknown) {
-    return true;
+    return 'control';
   };
 }
 
