30a5b323aab7b857f19576c3c0c390ff310f5aa7
Luca Pagliaro <luca@sentiero.digital>
2024-07-05 16:56:30 +0200

    feat: added ActionButtons.stories.tsx; feat: updated mocks and added GB provider mock;

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -34,8 +34,19 @@ const config: StorybookConfig = {
         alias: {
           '@growthbook/growthbook': path.resolve(__dirname, '../mock/gb.ts'),
           'node-fetch': path.resolve(__dirname, '../mock/node-fetch.ts'),
-          'webextension-polyfill': path.resolve(__dirname, '../mock/webextension-polyfill.ts'),
+          'webextension-polyfill': path.resolve(
+            __dirname,
+            '../mock/webextension-polyfill.ts',
+          ),
           'next/router': path.resolve(__dirname, '../mock/next-router.ts'),
+          '../../GrowthBookProvider': path.resolve(
+            __dirname,
+            '../mock/GrowthBookProvider.tsx',
+          ),
+          // '@dailydotdev/shared/src/components/GrowthBookProvider': path.resolve(
+          //   __dirname,
+          //   '../mock/GrowthBookProvider.tsx',
+          // ),
         },
       },
       define: {
--- packages/storybook/stories/components/ActionButtons.stories.tsx
+++ packages/storybook/stories/components/ActionButtons.stories.tsx
@@ -1,10 +1,13 @@
+import React, { useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import ActionButtons
-  from '@dailydotdev/shared/src/components/cards/list/ActionButtons';
-import { UserVote } from '@dailydotdev/shared/src/graphql/posts';
+import ActionButtons from '@dailydotdev/shared/src/components/cards/list/ActionButtons/ActionButtons';
+import { Post, UserVote } from '@dailydotdev/shared/src/graphql/posts';
 import post from '@dailydotdev/shared/__tests__/fixture/post';
 import ExtensionProviders from '../extension/_providers';
+import { feature } from '@dailydotdev/shared/src/lib/featureManagement';
+import { useFeature } from '../../mock/GrowthBookProvider';
+import { UpvoteExperiment } from '@dailydotdev/shared/src/lib/featureValues';
 
 const meta: Meta<typeof ActionButtons> = {
   title: 'components/ActionButtons',
@@ -24,8 +27,31 @@ const meta: Meta<typeof ActionButtons> = {
     onBookmarkClick: fn(),
     onCopyLinkClick: fn(),
   },
+  beforeEach: async () => {
+    useFeature.mockReturnValue(UpvoteExperiment.Control);
+  },
   render: (props) => {
-    return <ExtensionProviders><ActionButtons {...props} /></ExtensionProviders>;
+    const currentFeature = useFeature(feature.upvote);
+    const [post, setPost] = useState(props.post);
+    const onUpvoteClick = async (post: Post) => {
+      const isAdding = post.userState?.vote !== UserVote.Up;
+
+      const updatedPost = {
+        ...post,
+        numUpvotes: (post?.numUpvotes ?? 0) + (isAdding ? 1 : -1),
+        userState: { vote: isAdding ? UserVote.Up : UserVote.None },
+      };
+      setPost(updatedPost);
+      props.onUpvoteClick(updatedPost);
+    };
+
+    return (
+      <ExtensionProviders>
+        <>Feature: "{currentFeature}"</>
+        <hr className={'my-4 border-accent-salt-baseline'}/>
+        <ActionButtons {...props} post={post} onUpvoteClick={onUpvoteClick} />
+      </ExtensionProviders>
+    );
   },
 };
 
@@ -35,4 +61,8 @@ type Story = StoryObj<typeof ActionButtons>;
 
 export const Default: Story = {};
 
-export const AnimatedUpvote: Story = {};
+export const AnimatedUpvote: Story = {
+  beforeEach: async () => {
+    useFeature.mockReturnValue(UpvoteExperiment.Animated);
+  },
+};
