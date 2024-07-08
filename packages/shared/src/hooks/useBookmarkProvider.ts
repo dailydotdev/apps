dcc05eccba7f4bf3dc253b668b4b7101fa8f1558
Chris Bongers <chrisbongers@gmail.com>
2024-07-08 09:52:09 +0200

    chore: cleanup bookmark_provider experiment

--- packages/shared/src/hooks/useBookmarkProvider.ts
+++ packages/shared/src/hooks/useBookmarkProvider.ts
@@ -1,8 +1,6 @@
 import { useRef } from 'react';
-import { feature } from '../lib/featureManagement';
 import { useActiveFeedNameContext } from '../contexts';
 import { SharedFeedPage } from '../components/utilities';
-import { useConditionalFeature } from './useConditionalFeature';
 
 interface UseBookmarkProviderReturn {
   highlightBookmarkedPost: boolean;
@@ -20,17 +18,11 @@ const useBookmarkProvider = ({
   const justBookmarked = bookmarked !== isBookmarked.current && bookmarked;
 
   const isMyFeed = feedName === SharedFeedPage.MyFeed;
-  const isCardShownFromBookmarkProvider =
+  const highlightBookmarkedPost =
     isMyFeed && isBookmarked.current && !justBookmarked && bookmarked;
 
-  const { value: bookmarkProvider } = useConditionalFeature({
-    feature: feature.bookmark_provider,
-    shouldEvaluate: isCardShownFromBookmarkProvider,
-  });
-
   return {
-    highlightBookmarkedPost:
-      bookmarkProvider && isCardShownFromBookmarkProvider,
+    highlightBookmarkedPost,
   };
 };
 
