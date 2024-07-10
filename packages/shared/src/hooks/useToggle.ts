40a7fbac316323e51f47e9382a494cd71d78c9c6
Luca Pagliaro <luca@sentiero.digital>
2024-07-10 14:54:12 +0200

    refactor: removed unused import;

--- packages/shared/src/hooks/useToggle.ts
+++ packages/shared/src/hooks/useToggle.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 export const useToggle = (initialState = false) => {
   const [state, setState] = useState(initialState);
