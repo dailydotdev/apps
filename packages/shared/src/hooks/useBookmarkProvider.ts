ff4e25e6dcb6a556b28b113c6a9ca67efc18c3c7
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-10 20:50:11 +0800

    chore: comments for clearer understanding

--- packages/shared/src/hooks/useBookmarkProvider.ts
+++ packages/shared/src/hooks/useBookmarkProvider.ts
@@ -23,9 +23,12 @@ export const useJustBookmarked = ({
   const justBookmarked = bookmarked !== wasBookmarkedRef.current && bookmarked;
 
   useEffect(() => {
+    // on initial render, we only have few props (server side call) which does not include bookmark information. that would lead to inaccurate value of wasBookmarked
     const isInitialRender =
       isNullOrUndefined(wasBookmarkedRef.current) &&
       !isNullOrUndefined(bookmarked);
+
+    // if bookmarked was just turned off, we can then again consider to have been just bookmarked when the user bookmarks again
     const turnedOffBookmark = wasBookmarkedRef.current && !bookmarked;
 
     if (isInitialRender || turnedOffBookmark) {
