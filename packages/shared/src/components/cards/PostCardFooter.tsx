c96aca62a2885eaf093fb2fac2c575dca6ad31da
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-07-22 16:44:37 +0800

    feat: author image on feed card (#3375)

--- packages/shared/src/components/cards/PostCardFooter.tsx
+++ packages/shared/src/components/cards/PostCardFooter.tsx
@@ -5,6 +5,8 @@ import { ProfileImageSize, ProfilePicture } from '../ProfilePicture';
 import { isVideoPost, Post } from '../../graphql/posts';
 import { CommonCardCoverProps, visibleOnGroupHover } from './common';
 import { CardCover } from './common/CardCover';
+import { useConditionalFeature } from '../../hooks';
+import { feature } from '../../lib/featureManagement';
 
 interface PostCardFooterClassName {
   image?: string;
@@ -21,6 +23,10 @@ export const PostCardFooter = ({
   className,
   onShare,
 }: PostCardFooterProps): ReactElement => {
+  const { value: shouldShowImage } = useConditionalFeature({
+    shouldEvaluate: !!post?.author,
+    feature: feature.authorImage,
+  });
   const isVideoType = isVideoPost(post);
   return (
     <>
@@ -40,7 +46,8 @@ export const PostCardFooter = ({
         }}
         videoProps={{ className: 'my-2' }}
       />
-      {post.author && (
+      {/* hide the existing hover behavior */}
+      {!shouldShowImage && post.author && (
         <div
           className={classNames(
             'absolute z-1 mt-2 flex w-full items-center rounded-t-12 bg-background-default px-3 py-2 font-bold text-text-secondary typo-callout',
