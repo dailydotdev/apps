345e5a77dfa0f628c74a59f5d89f0c25f88d82cc
Luca Pagliaro <luca@sentiero.digital>
2024-07-22 09:59:17 +0200

    feat: unsubscribe to notifications before unfollowing;

--- packages/shared/src/hooks/source/useSourceActionsFollow.ts
+++ packages/shared/src/hooks/source/useSourceActionsFollow.ts
@@ -4,6 +4,7 @@ import { Origin } from '../../lib/log';
 import { useToastNotification } from '../useToastNotification';
 import useTagAndSource from '../useTagAndSource';
 import useFeedSettings from '../useFeedSettings';
+import { useSourceActionsNotify } from './useSourceActionsNotify';
 
 export type UseSourceActionsFollowProps = {
   source: Source;
@@ -25,12 +26,21 @@ export function useSourceActionsFollow(
     return !!feedSettings?.includeSources?.find(({ id }) => source?.id === id);
   }, [feedSettings, source]);
 
+  const { haveNotifications, isReady, onNotify } = useSourceActionsNotify({
+    source,
+  });
   const { onFollowSource, onUnfollowSource } = useTagAndSource({
     origin: Origin.SourcePage,
   });
 
   const toggleFollow = useCallback(async () => {
+    // Unfollow path
     if (isFollowing) {
+      // have to remove notifications first
+      if (haveNotifications && isReady) {
+        await onNotify();
+      }
+
       const { successful } = await onUnfollowSource({
         source,
         requireLogin: true,
@@ -41,11 +51,21 @@ export function useSourceActionsFollow(
       return;
     }
 
+    // follow path
     const { successful } = await onFollowSource({ source, requireLogin: true });
     if (successful) {
       displayToast(`✅ You are now subscribed to ${source.name}`);
     }
-  }, [displayToast, isFollowing, onFollowSource, onUnfollowSource, source]);
+  }, [
+    displayToast,
+    haveNotifications,
+    isFollowing,
+    isReady,
+    onFollowSource,
+    onNotify,
+    onUnfollowSource,
+    source,
+  ]);
 
   return {
     isFollowing,
