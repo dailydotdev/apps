8ffbe0687a507702747ad9e26efeec642a7e33a9
Luca Pagliaro <pagliaroluca@gmail.com>
2024-07-25 17:28:49 +0200

    feat: added dismiss action in go public squad checklist (#3392)
    * feat: wip added dismiss button;
    
    * feat: add dismiss for public squad step;
    
    * fix: revert unwanted changes;
    
    * fix: show 5th step if squad is already public;
    
    * fix: dependency cycle;
    
    * refactor: wait for actions load in checklist step filter;
    
    * fix: used already exist checkHasCompleted
    
    * fix: lint errors;

--- packages/shared/src/components/checklist/GoPublicStep.tsx
+++ packages/shared/src/components/checklist/GoPublicStep.tsx
@@ -3,6 +3,9 @@ import { ChecklistStepPropsWithSquad } from '../../lib/checklist';
 import { ChecklistStep } from './ChecklistStep';
 import PublicSquadSubmissionActions from '../squads/PublicSquadSubmissionActions';
 import { SquadPublicProgressBars } from '../squads/SquadPublicProgressBars';
+import { Button } from '../buttons/Button';
+import { ActionType } from '../../graphql/actions';
+import { useActions } from '../../hooks';
 
 export const GoPublicStep = ({
   squad,
@@ -10,11 +13,27 @@ export const GoPublicStep = ({
 }: ChecklistStepPropsWithSquad): ReactElement => {
   const postsCount = squad?.flags?.totalPosts ?? 0;
 
+  const { completeAction } = useActions();
+
+  const onDismiss = () => {
+    completeAction(ActionType.HidePublicSquadStep);
+  };
+
   return (
     <ChecklistStep {...props}>
       <div className="flex flex-col items-stretch gap-4">
         <SquadPublicProgressBars postsCount={postsCount} />
-        <PublicSquadSubmissionActions squad={squad} />
+        <div className="flex flex-row gap-2">
+          {!squad.public && (
+            <>
+              <PublicSquadSubmissionActions
+                squad={squad}
+                isDetailsVisible={false}
+              />
+              <Button onClick={onDismiss}>Dismiss</Button>
+            </>
+          )}
+        </div>
       </div>
     </ChecklistStep>
   );
