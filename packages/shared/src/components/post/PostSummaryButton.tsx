380cf59118c9c5bd2afb41424e6b73634713b81a
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-27 03:57:11 +0800

    fix: usage of raw

--- packages/shared/src/components/post/PostSummaryButton.tsx
+++ packages/shared/src/components/post/PostSummaryButton.tsx
@@ -22,7 +22,7 @@ export function PostSummaryButton({
 
   return (
     <>
-      <div className="from-pepper-90 absolute inset-0 bg-gradient-to-t from-60% to-overlay-primary-pepper" />
+      <div className="absolute inset-0 bg-gradient-to-t from-raw-pepper-90 from-60% to-overlay-primary-pepper" />
       <Button
         icon={isLoading ? <Loader /> : <MagicIcon secondary />}
         iconPosition={ButtonIconPosition.Right}
