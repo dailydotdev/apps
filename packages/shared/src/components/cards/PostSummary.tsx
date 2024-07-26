21b676fddfc1b870aab4629e978b6e8946ec1032
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-26 19:44:31 +0800

    feat: generate tldr button experiment

--- packages/shared/src/components/cards/PostSummary.tsx
+++ packages/shared/src/components/cards/PostSummary.tsx
@@ -4,8 +4,10 @@ import React, {
   ReactElement,
   RefObject,
 } from 'react';
+import classNames from 'classnames';
 import { SummaryContainer, TLDRText } from '../utilities';
 import ShowMoreContent from './ShowMoreContent';
+import { PostSummaryButton } from '../post/PostSummaryButton';
 
 interface SummaryProps extends HTMLAttributes<HTMLDivElement> {
   summary: string;
@@ -16,7 +18,12 @@ function PostSummary(
   ref: RefObject<HTMLDivElement>,
 ): ReactElement {
   return (
-    <SummaryContainer ref={ref} {...props}>
+    <SummaryContainer
+      ref={ref}
+      {...props}
+      className={classNames('relative', props.className)}
+    >
+      <PostSummaryButton summary={summary} />
       <ShowMoreContent
         className="overflow-hidden"
         content={summary}
