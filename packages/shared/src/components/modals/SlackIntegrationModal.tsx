d4f381382a910d562523d255c874a9bcea4f0117
capJavert <ante@kickass.website>
2024-08-02 16:46:50 +0200

    feat: adjust channel/workspace selection

--- packages/shared/src/components/modals/SlackIntegrationModal.tsx
+++ packages/shared/src/components/modals/SlackIntegrationModal.tsx
@@ -9,16 +9,25 @@ import {
   UserIntegration,
   UserIntegrationType,
 } from '../../graphql/users';
-import { SlackIcon } from '../icons';
 import { Dropdown } from '../fields/Dropdown';
 import { ButtonSize, ButtonVariant } from '../buttons/common';
 import { Button } from '../buttons/Button';
 import { useSlack } from '../../hooks';
-import { Source, SourceType } from '../../graphql/sources';
+import { Source } from '../../graphql/sources';
 import { Connection, gqlClient } from '../../graphql/common';
+import { ModalClose } from './common/ModalClose';
+import { SourceAvatar } from '../profile/source';
+import { ProfileImageSize } from '../ProfilePicture';
+import {
+  Typography,
+  TypographyColor,
+  TypographyTag,
+  TypographyType,
+} from '../typography/Typography';
+import { SlackIcon } from '../icons';
 
 export type SlackIntegrationModalProps = Omit<ModalProps, 'children'> & {
-  source: Pick<Source, 'id' | 'handle' | 'type'>;
+  source: Pick<Source, 'id' | 'handle' | 'type' | 'image' | 'name'>;
 };
 
 const SlackIntegrationModal = ({
@@ -78,53 +87,79 @@ const SlackIntegrationModal = ({
   const slack = useSlack();
 
   return (
-    <Modal kind={Modal.Kind.FlexibleCenter} size={Modal.Size.Medium} {...props}>
-      <Modal.Header title="Get source updates on Slack" />
-      <Modal.Body className="flex flex-col items-center gap-4">
-        {!!slackIntegrations?.length && (
-          <>
-            <p>Select slack integration</p>
-            <Dropdown
-              dynamicMenuWidth
-              shouldIndicateSelected
-              buttonSize={ButtonSize.Medium}
-              iconOnly={false}
-              key="feed"
-              selectedIndex={selectedIntegrationIndex}
-              renderItem={(_, index) => (
-                <span className="typo-callout">
-                  {slackIntegrations[index].name}
-                </span>
-              )}
-              options={slackIntegrations?.map((item) => item.name)}
-              onChange={(value, index) => {
-                setSelectedIntegrationIndex(index);
-              }}
-              scrollable
-            />
-            <p>or</p>
-          </>
-        )}
-        <Button
-          type="button"
-          variant={ButtonVariant.Primary}
-          size={ButtonSize.Medium}
-          onClick={() => {
-            slack.connect({
-              redirectPath: `/${
-                source.type === SourceType.Squad ? 'squads' : 'sources'
-              }/${source.handle}`,
-            });
-          }}
-          icon={<SlackIcon />}
+    <Modal kind={Modal.Kind.FlexibleCenter} size={Modal.Size.XSmall} {...props}>
+      <ModalClose className="top-4" onClick={props.onRequestClose} />
+      <Modal.Body className="flex flex-col gap-4">
+        <div className="mr-auto flex items-center justify-center">
+          <SourceAvatar source={source} size={ProfileImageSize.Small} />
+          <Typography
+            type={TypographyType.Callout}
+            color={TypographyColor.Primary}
+          >
+            <Typography
+              tag={TypographyTag.Span}
+              bold
+              color={TypographyColor.Primary}
+            >
+              {source.name}
+            </Typography>{' '}
+            <Typography
+              tag={TypographyTag.Span}
+              color={TypographyColor.Tertiary}
+            >
+              @{source.handle}
+            </Typography>
+          </Typography>
+        </div>
+        <Typography
+          tag={TypographyTag.H3}
+          type={TypographyType.Title2}
+          color={TypographyColor.Primary}
+          bold
         >
-          Connect Slack
-        </Button>
-        {!!channels?.length && (
-          <>
-            <p>Select channel</p>
+          Configure Integration
+        </Typography>
+        {!!slackIntegrations?.length && (
+          <Dropdown
+            shouldIndicateSelected
+            buttonSize={ButtonSize.Medium}
+            iconOnly={false}
+            key="feed"
+            selectedIndex={selectedIntegrationIndex}
+            renderItem={(_, index) => (
+              <span className="typo-callout">
+                {slackIntegrations[index].name}
+              </span>
+            )}
+            icon={<SlackIcon />}
+            options={slackIntegrations?.map((item) => item.name)}
+            onChange={(value, index) => {
+              setSelectedIntegrationIndex(index);
+            }}
+            scrollable
+          />
+        )}
+        <div>
+          <Typography
+            className="mb-2 flex items-center"
+            type={TypographyType.Body}
+            color={TypographyColor.Tertiary}
+          >
+            Select channels
+            {!!channels?.length && (
+              <Typography
+                className="ml-1 flex min-h-6 min-w-6 items-center justify-center rounded-full bg-surface-float p-0.5"
+                tag={TypographyTag.Span}
+                type={TypographyType.Callout}
+                color={TypographyColor.Tertiary}
+                bold={false}
+              >
+                {channels.length}
+              </Typography>
+            )}
+          </Typography>
+          {!!channels?.length && (
             <Dropdown
-              dynamicMenuWidth
               shouldIndicateSelected
               buttonSize={ButtonSize.Medium}
               iconOnly={false}
@@ -139,12 +174,12 @@ const SlackIntegrationModal = ({
               }}
               scrollable
             />
-          </>
-        )}
+          )}
+        </div>
         <Button
           type="button"
           variant={ButtonVariant.Primary}
-          size={ButtonSize.Medium}
+          size={ButtonSize.Large}
           onClick={() => {
             slack.connectSource({
               channelId: channels[selectedChannelIndex].id,
@@ -154,6 +189,20 @@ const SlackIntegrationModal = ({
           }}
         >
           Save
+        </Button>
+        <Button
+          type="button"
+          variant={ButtonVariant.Tertiary}
+          size={ButtonSize.Medium}
+          onClick={() => {
+            slack.connectSource({
+              channelId: channels[selectedChannelIndex].id,
+              integrationId: selectedIntegration.id,
+              sourceId: source.id,
+            });
+          }}
+        >
+          Remove integration
         </Button>
       </Modal.Body>
     </Modal>
