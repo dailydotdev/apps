c88d9bdb6991ea32c4d8178f128911e7ccfedf57
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-08-06 23:28:59 +0800

    fix: read it later on collections card (#3428)

--- packages/shared/src/components/post/common/PostReminderOptions.tsx
+++ packages/shared/src/components/post/common/PostReminderOptions.tsx
@@ -13,6 +13,7 @@ import {
 import { LazyModal } from '../../modals/common/types';
 import { Post } from '../../../graphql/posts';
 import { useLazyModal } from '../../../hooks/useLazyModal';
+import { wrapStopPropagation } from '../../../lib/func';
 
 interface PostReminderOptionsProps {
   post: Post;
@@ -39,21 +40,25 @@ export function PostReminderOptions({
     <span className={classNames('flex flex-row gap-3', className)}>
       <Button
         {...buttonProps}
-        onClick={() => runBookmarkReminder(ReminderPreference.OneHour)}
+        onClick={wrapStopPropagation(() =>
+          runBookmarkReminder(ReminderPreference.OneHour),
+        )}
       >
         1h
       </Button>
       <Button
         {...buttonProps}
-        onClick={() => runBookmarkReminder(ReminderPreference.Tomorrow)}
+        onClick={wrapStopPropagation(() =>
+          runBookmarkReminder(ReminderPreference.Tomorrow),
+        )}
       >
         24h
       </Button>
       <Button
         {...buttonProps}
-        onClick={() =>
-          openModal({ type: LazyModal.BookmarkReminder, props: { post } })
-        }
+        onClick={wrapStopPropagation(() =>
+          openModal({ type: LazyModal.BookmarkReminder, props: { post } }),
+        )}
       >
         Other
       </Button>
