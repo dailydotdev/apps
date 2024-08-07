51e4f5cec97128cf6e4077011854d2520fe92136
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-08-07 14:37:22 +0800

    fix: leaderboard containers

---
+++ packages/shared/src/components/cards/Leaderboard/LeaderboardList.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement, ReactNode } from 'react';
+import { LeaderboardListContainer } from './LeaderboardListContainer';
+import { LeaderboardListContainerProps } from './common';
+import classed from '../../../lib/classed';
+import { ElementPlaceholder } from '../../ElementPlaceholder';
+
+export interface LeaderboardListProps {
+  containerProps: Omit<LeaderboardListContainerProps, 'children'>;
+  isLoading: boolean;
+  children: ReactNode;
+}
+
+const PlaceholderList = classed(
+  ElementPlaceholder,
+  'h-[1.6875rem] my-1.5 rounded-12',
+);
+
+export function LeaderboardList({
+  containerProps,
+  isLoading,
+  children,
+}: LeaderboardListProps): ReactElement {
+  return (
+    <LeaderboardListContainer {...containerProps}>
+      {/* eslint-disable-next-line react/no-array-index-key */}
+      {isLoading && [...Array(10)].map((_, i) => <PlaceholderList key={i} />)}
+      {children}
+    </LeaderboardListContainer>
+  );
+}
