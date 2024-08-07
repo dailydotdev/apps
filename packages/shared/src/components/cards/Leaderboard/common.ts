c5ab1e08a926cef3962b5a325e5445a105ed50a2
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-08-07 19:23:19 +0800

    fix: leaderboards on safari (#3437)

---
+++ packages/shared/src/components/cards/Leaderboard/common.ts
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import classed from '../../../lib/classed';
+
+export interface LeaderboardListContainerProps {
+  title: string;
+  children: ReactNode;
+  className?: string;
+}
+
+export const LeaderboardCard = classed(
+  'div',
+  'flex flex-col border-b border-border-subtlest-tertiary p-4 tablet:rounded-12 tablet:border tablet:bg-surface-float',
+);
