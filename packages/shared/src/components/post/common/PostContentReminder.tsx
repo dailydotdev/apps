4dbf2f9efc40740a6ea9a10bd72863ec930e45bd
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-08-08 19:11:13 +0800

    fix: bookmark reminder on freeform without image (#3442)

--- packages/shared/src/components/post/common/PostContentReminder.tsx
+++ packages/shared/src/components/post/common/PostContentReminder.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import classNames from 'classnames';
 import { PostContentWidget } from './PostContentWidget';
 import { Post } from '../../../graphql/posts';
 import { BookmarkReminderIcon } from '../../icons/Bookmark/Reminder';
@@ -8,10 +9,12 @@ import { useBookmarkReminderCover } from '../../../hooks/bookmark/useBookmarkRem
 
 interface PostContentReminderProps {
   post: Post;
+  className?: string;
 }
 
 export function PostContentReminder({
   post,
+  className,
 }: PostContentReminderProps): ReactElement {
   const shouldShowReminder = useBookmarkReminderCover(post);
 
@@ -21,8 +24,8 @@ export function PostContentReminder({
 
   return (
     <PostContentWidget
-      className="mt-6 w-full"
-      icon={<BookmarkReminderIcon size={IconSize.Small} />}
+      className={classNames('mt-6 w-full', className)}
+      icon={<BookmarkReminderIcon size={IconSize.Small} secondary />}
       title="Remind about this post later?"
     >
       <PostReminderOptions post={post} className="laptop:ml-auto" />
