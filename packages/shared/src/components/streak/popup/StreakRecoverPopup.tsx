f0ae6d5706aabac7128b72ad5521c56998fcfdae
Luca Pagliaro <luca@sentiero.digital>
2024-08-09 10:14:48 +0200

    feat: init StreakRecoverPopup component;

--- packages/shared/src/components/streak/popup/StreakRecoverPopup.tsx
+++ packages/shared/src/components/streak/popup/StreakRecoverPopup.tsx
@@ -1,11 +1,27 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
+import { ModalSize } from '../../modals/common/types';
+import { ModalBody } from '../../modals/common/ModalBody';
+import { Modal } from '../../modals/common/Modal';
+import { useToggle } from '../../../hooks/useToggle';
 
 export interface StreakRecoverPopupProps {}
 
 export const StreakRecoverPopup = (
   props: StreakRecoverPopupProps,
 ): ReactElement => {
-  return null;
+  const [isOpen, toggle] = useToggle(true);
+  const onRequestClose = () => {};
+
+  return (
+    <Modal
+      isDrawerOnMobile={isOpen}
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      size={ModalSize.Small}
+    >
+      <ModalBody />
+    </Modal>
+  );
 };
 
 export default StreakRecoverPopup;
