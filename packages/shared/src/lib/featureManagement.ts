b32537490e087edef57adeb801adffd5411f5533
Chris Bongers <chrisbongers@gmail.com>
2024-08-12 15:11:43 +0200

    chore: cleanup card_downvote experiment

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -30,7 +30,6 @@ const feature = {
   animatedUpvote: new Feature('animated_upvote', false),
   authorImage: new Feature('author_image', false),
   sourceNotifyButton: new Feature('source_notify_button', false),
-  cardDownvote: new Feature('card_downvote', false),
   generateSummary: new Feature('generate_summary', false),
   searchUsers: new Feature('search_users', false),
 };
