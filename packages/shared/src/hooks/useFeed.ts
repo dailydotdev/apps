f1cc22790c1cc91d063ff4d0d027d2b0ca26d975
Chris Bongers <chrisbongers@gmail.com>
2024-09-03 14:27:55 +0200

    fix: handle garmr retry exclusion

--- packages/shared/src/hooks/useFeed.ts
+++ packages/shared/src/hooks/useFeed.ts
@@ -5,6 +5,7 @@ import {
   UseInfiniteQueryOptions,
   useQueryClient,
 } from '@tanstack/react-query';
+import { ClientError } from 'graphql-request';
 import {
   Ad,
   FeedData,
@@ -22,7 +23,7 @@ import {
 } from '../lib/query';
 import { MarketingCta } from '../components/marketingCta/common';
 import { FeedItemType } from '../components/cards/common';
-import { gqlClient } from '../graphql/common';
+import { GARMR_ERROR, gqlClient } from '../graphql/common';
 
 interface FeedItemBase<T extends FeedItemType> {
   type: T;
@@ -123,7 +124,10 @@ export default function useFeed<T>(
         first: pageSize,
         after: pageParam,
         loggedIn: !!user,
+        test: 'test',
       });
+
+      console.log('done loading???');
 
       if (
         !feedQuery?.data?.pages[0]?.page.edges.length &&
@@ -138,6 +142,15 @@ export default function useFeed<T>(
     {
       refetchOnMount: false,
       ...options,
+      retry: (failureCount, error) => {
+        const clientError = error as ClientError;
+        if (
+          clientError?.response?.errors?.[0]?.extensions?.code === GARMR_ERROR
+        ) {
+          return false;
+        }
+        return failureCount < 3;
+      },
       enabled: query && tokenRefreshed,
       refetchOnReconnect: false,
       refetchOnWindowFocus: false,
