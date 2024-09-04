b01f0a89228cd7bd14fae1255fdb08e8d31aac20
Chris Bongers <chrisbongers@gmail.com>
2024-09-04 10:42:52 +0200

    chore: cleanup search list mode (won)

--- packages/shared/src/hooks/useFeedLayout.ts
+++ packages/shared/src/hooks/useFeedLayout.ts
@@ -12,8 +12,6 @@ import { AllFeedPages, OtherFeedPage } from '../lib/query';
 import SettingsContext from '../contexts/SettingsContext';
 import { isNullOrUndefined } from '../lib/func';
 import { useSearchResultsLayout } from './search/useSearchResultsLayout';
-import { useConditionalFeature } from './useConditionalFeature';
-import { feature } from '../lib/featureManagement';
 
 interface UseFeedLayoutReturn {
   shouldUseListFeedLayout: boolean;
@@ -24,7 +22,6 @@ interface UseFeedLayoutReturn {
   shouldUseCommentFeedLayout: boolean;
   isListMode: boolean;
   shouldUseListMode: boolean;
-  isLoadingExperiment?: boolean;
 }
 
 interface UseFeedLayoutProps {
@@ -114,13 +111,7 @@ export const useFeedLayout = ({
   const { insaneMode } = useContext(SettingsContext);
   const { isSearchPageLaptop } = useSearchResultsLayout();
 
-  const { value: isSearchListModeExperiment, isLoading: isLoadingExperiment } =
-    useConditionalFeature({
-      feature: feature.searchListMode,
-      shouldEvaluate: isSearchPageLaptop,
-    });
-
-  const isListMode = isSearchListModeExperiment || insaneMode;
+  const isListMode = isSearchPageLaptop || insaneMode;
 
   const shouldUseListFeedLayoutOnProfilePages = UserProfileFeedPages.has(
     feedName as UserProfileFeedType,
@@ -160,6 +151,5 @@ export const useFeedLayout = ({
     screenCenteredOnMobileLayout:
       shouldUseListFeedLayoutOnMobileTablet &&
       !UserProfileFeedPages.has(feedName as UserProfileFeedType),
-    isLoadingExperiment,
   };
 };
