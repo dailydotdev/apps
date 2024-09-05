229e64919ac88253a8c033e7566f9bc683050a9d
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-09-05 17:55:43 +0800

    refactor: create Squad (#3490)
    * refactor: create Squad
    
    * fix: bg image on mobile
    
    * fix: stale time and flat query
    
    * fix: pr comments
    
    * refactor: updated the stale time
    
    * refactor: edit squad page (#3495)
    
    * refactor: common danger zone
    
    * refactor: reusable squad stats
    
    * refactor: edit squad changes
    
    * fix: lint
    
    * refactor: usage of default rel
    
    * refactor: create/edit squad form and its types (#3496)
    
    * fix: lint
    
    * refactor: squad create/edit form
    
    * fix: usage of partial

--- packages/shared/src/components/squads/Details.tsx
+++ packages/shared/src/components/squads/Details.tsx
@@ -1,4 +1,10 @@
-import React, { FormEvent, ReactElement, ReactNode, useState } from 'react';
+import React, {
+  FormEvent,
+  ReactElement,
+  ReactNode,
+  useCallback,
+  useState,
+} from 'react';
 import classNames from 'classnames';
 import { ClientError } from 'graphql-request';
 import { useMutation } from '@tanstack/react-query';
@@ -10,32 +16,26 @@ import Textarea from '../fields/Textarea';
 import ImageInput from '../fields/ImageInput';
 import { cloudinary } from '../../lib/image';
 import { formToJson } from '../../lib/form';
-import { blobToBase64 } from '../../lib/blob';
 import { checkExistingHandle, SquadForm } from '../../graphql/squads';
 import { capitalize } from '../../lib/strings';
 import { IconSize } from '../Icon';
-import { SourceMemberRole } from '../../graphql/sources';
-import { Radio } from '../fields/Radio';
 import { FormWrapper } from '../fields/form';
-import { SquadSettingsSection, SquadStatus } from './settings';
-import {
-  PrivacyOption,
-  useSquadPrivacyOptions,
-} from '../../hooks/squads/useSquadPrivacyOptions';
-import Alert, { AlertType } from '../widgets/Alert';
-import { Anchor } from '../text';
-import { usePublicSquadRequests } from '../../hooks';
-import { PUBLIC_SQUAD_REQUEST_REQUIREMENT } from '../../lib/config';
+import { SquadPrivacySection } from './settings/SquadPrivacySection';
+import { PermissionSection } from './settings/PermissionSection';
+import { SquadSettingsSection } from './settings';
+import { SquadStats } from './common/SquadStat';
+import { SquadPrivacyState } from './common/SquadPrivacyState';
+import { SquadDangerZone } from './settings/SquadDangerZone';
+import { Squad } from '../../graphql/sources';
 
 const squadImageId = 'squad_image_file';
 
 interface SquadDetailsProps {
-  onSubmit?: (e: FormEvent, formJson: SquadForm) => void;
-  form: Partial<SquadForm>;
-  createMode: boolean;
+  onSubmit: (e: FormEvent, formJson: SquadForm) => void;
   onRequestClose?: () => void;
   children?: ReactNode;
   isLoading?: boolean;
+  squad?: Squad;
 }
 
 const getFormData = async (
@@ -48,65 +48,34 @@ const getFormData = async (
 
   const input = document.getElementById(squadImageId) as HTMLInputElement;
   const file = input.files[0];
-  const base64 = await blobToBase64(file);
 
-  return { ...current, file: base64 };
+  return { ...current, file };
 };
-
-const memberRoleOptions = [
-  {
-    label: 'All members (recommended)',
-    value: SourceMemberRole.Member,
-  },
-  {
-    label: 'Only moderators',
-    value: SourceMemberRole.Moderator,
-  },
-];
 
 export function SquadDetails({
   onSubmit,
-  form,
-  createMode = true,
   children,
   isLoading,
+  squad,
 }: SquadDetailsProps): ReactElement {
+  const createMode = !squad;
   const {
     name,
     handle,
     description,
+    image,
+    category,
+    flags,
     memberPostingRole: initialMemberPostingRole,
     memberInviteRole: initialMemberInviteRole,
-  } = form;
-  const isRequestsEnabled =
-    !createMode &&
-    (form.flags?.totalPosts ?? 0) >= PUBLIC_SQUAD_REQUEST_REQUIREMENT &&
-    !!form?.id;
-  const { status, daysLeft } = usePublicSquadRequests({
-    isQueryEnabled: isRequestsEnabled,
-    sourceId: form?.id,
-    isPublic: form?.public,
-  });
+  } = squad ?? {};
   const [activeHandle, setActiveHandle] = useState(handle);
-  const [privacy, setPrivacy] = useState(
-    form.public ? PrivacyOption.Public : PrivacyOption.Private,
-  );
   const [imageChanged, setImageChanged] = useState(false);
   const [handleHint, setHandleHint] = useState<string>(null);
   const [canSubmit, setCanSubmit] = useState(!!name && !!activeHandle);
+  const [categoryHint, setCategoryHint] = useState('');
   const [isDescriptionOpen, setDescriptionOpen] = useState(!createMode);
-  const privacyOptions = useSquadPrivacyOptions({
-    totalPosts: form?.flags?.totalPosts,
-    status,
-    squadId: form?.id,
-  });
   const router = useRouter();
-  const [memberPostingRole, setMemberPostingRole] = useState(
-    () => initialMemberPostingRole || SourceMemberRole.Member,
-  );
-  const [memberInviteRole, setMemberInviteRole] = useState(
-    () => initialMemberInviteRole || SourceMemberRole.Member,
-  );
 
   const { mutateAsync: onValidateHandle } = useMutation(checkExistingHandle, {
     onError: (err) => {
@@ -126,11 +95,17 @@ export function SquadDetails({
       return setHandleHint(unknown as string);
     },
   });
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formJson = formToJson<SquadForm>(e.currentTarget);
 
     if (!formJson.name || !formJson.handle) {
+      return null;
+    }
+
+    if (formJson.status === 'public' && !formJson.categoryId) {
+      setCategoryHint('Please select a category');
       return null;
     }
 
@@ -158,6 +133,10 @@ export function SquadDetails({
       setActiveHandle(formJson.name.toLowerCase().replace(/[^a-zA-Z0-9]/g, ''));
     }
 
+    if (formJson.status === 'public' && formJson.categoryId) {
+      setCategoryHint('');
+    }
+
     setCanSubmit(!!formJson.name);
   };
 
@@ -182,134 +161,97 @@ export function SquadDetails({
     >
       {children}
       <form
-        className="flex w-full flex-col items-center justify-center gap-4 p-6 pt-0"
+        className="flex w-full flex-col justify-center gap-6 p-6"
         onSubmit={handleSubmit}
         onBlur={handleChange}
         id="squad-form"
       >
         {!createMode && (
-          <ImageInput
-            initialValue={form.image ?? form.file}
-            id={squadImageId}
-            fallbackImage={cloudinary.squads.imageFallback}
-            className={{
-              container: 'mt-4 !rounded-full border-0',
-              img: 'object-cover',
-            }}
-            hoverIcon={<CameraIcon size={IconSize.Large} />}
-            alwaysShowHover={!imageChanged}
-            onChange={() => setImageChanged(true)}
-            size="large"
-          />
+          <div className="flex flex-col items-center gap-5">
+            <ImageInput
+              initialValue={image}
+              id={squadImageId}
+              fallbackImage={cloudinary.squads.imageFallback}
+              className={{
+                container: 'mt-4 !rounded-full border-0',
+                img: 'object-cover',
+              }}
+              hoverIcon={<CameraIcon size={IconSize.Large} />}
+              alwaysShowHover={!imageChanged}
+              onChange={() => setImageChanged(true)}
+              size="medium"
+            />
+            <SquadPrivacyState
+              isPublic={squad?.public}
+              isFeatured={flags?.featured}
+            />
+            <SquadStats flags={flags} />
+          </div>
         )}
-        <TextField
-          label={createMode ? 'Name your Squad' : 'Squad name'}
-          inputId="name"
-          name="name"
-          valid={!!name}
-          leftIcon={<SquadIcon />}
-          value={name ?? ''}
-          className={{
-            container: 'w-full',
-          }}
-        />
-        <TextField
-          label="Squad handle"
-          inputId="handle"
-          hint={handleHint}
-          valid={!handleHint}
-          name="handle"
-          leftIcon={<AtIcon />}
-          value={activeHandle ?? ''}
-          onChange={() => !!handleHint && setHandleHint(null)}
-          className={{
-            hint: 'text-status-error',
-            container: classNames('w-full', !handleHint && 'mb-1'),
-          }}
-        />
-        {!isDescriptionOpen && (
-          <button
-            className="mr-auto text-text-tertiary typo-callout"
-            type="button"
-            onClick={() => {
-              setDescriptionOpen((current) => !current);
-            }}
-          >
-            + Add description
-          </button>
-        )}
-        {isDescriptionOpen && (
-          <Textarea
-            label="Squad description"
-            inputId="description"
-            name="description"
-            hint="(optional)"
-            rows={4}
-            value={description ?? ''}
-            maxLength={250}
+        <SquadSettingsSection title="Squad details" className="!gap-4">
+          <TextField
+            label={createMode ? 'Name your Squad' : 'Squad name'}
+            inputId="name"
+            name="name"
+            valid={!!name}
+            leftIcon={<SquadIcon />}
+            value={name ?? ''}
             className={{
-              hint: '-mt-8 py-2 pl-4',
               container: 'w-full',
             }}
           />
-        )}
-        {!createMode && (
-          <SquadSettingsSection title="Status" className="w-full">
-            <Radio
-              name="status"
-              options={privacyOptions}
-              value={privacy}
-              className={{ container: 'gap-4' }}
-              onChange={(value) => setPrivacy(value)}
+          <TextField
+            label="Squad handle"
+            inputId="handle"
+            hint={handleHint}
+            valid={!handleHint}
+            name="handle"
+            leftIcon={<AtIcon />}
+            value={activeHandle ?? ''}
+            onChange={() => !!handleHint && setHandleHint(null)}
+            className={{
+              hint: 'text-status-error',
+              container: classNames('w-full', !handleHint && 'mb-1'),
+            }}
+          />
+          {!isDescriptionOpen && (
+            <button
+              className="ml-4 mr-auto font-bold text-text-tertiary typo-callout"
+              type="button"
+              onClick={() => {
+                setDescriptionOpen((current) => !current);
+              }}
+            >
+              + Add description (recommended)
+            </button>
+          )}
+          {isDescriptionOpen && (
+            <Textarea
+              label="Squad description"
+              inputId="description"
+              name="description"
+              hint="(optional)"
+              rows={4}
+              value={description ?? ''}
+              maxLength={250}
+              className={{
+                hint: '-mt-8 py-2 pl-4',
+                container: 'w-full',
+              }}
             />
-            {status === SquadStatus.Rejected && (
-              <Alert
-                type={AlertType.Error}
-                flexDirection="flex-row"
-                className="mt-2"
-              >
-                <p className="flex-1">
-                  You did not pass the review process. You can try again
-                  in&nbsp;{daysLeft}
-                  &nbsp;days. To increase your chances to pass in the next round
-                  you can contact us at{' '}
-                  <Anchor href="mailto:support@daily.dev">
-                    support@daily.dev
-                  </Anchor>
-                  .
-                </p>
-              </Alert>
-            )}
-          </SquadSettingsSection>
-        )}
-        <div className="mt-2 flex flex-col gap-4 tablet:flex-row">
-          <SquadSettingsSection
-            title="Post permissions"
-            description="Choose who is allowed to post new content in this Squad."
-          >
-            <Radio
-              name="memberPostingRole"
-              options={memberRoleOptions}
-              value={memberPostingRole}
-              onChange={(value) =>
-                setMemberPostingRole(value as SourceMemberRole)
-              }
-            />
-          </SquadSettingsSection>
-          <SquadSettingsSection
-            title="Invitation permissions"
-            description="Choose who is allowed to invite new members to this Squad."
-          >
-            <Radio
-              name="memberInviteRole"
-              options={memberRoleOptions}
-              value={memberInviteRole}
-              onChange={(value) =>
-                setMemberInviteRole(value as SourceMemberRole)
-              }
-            />
-          </SquadSettingsSection>
-        </div>
+          )}
+        </SquadSettingsSection>
+        <SquadPrivacySection
+          initialCategory={category?.id}
+          isPublic={squad?.public}
+          categoryHint={categoryHint}
+          onCategoryChange={useCallback(() => setCategoryHint(''), [])}
+        />
+        <PermissionSection
+          initialMemberInviteRole={initialMemberInviteRole}
+          initialMemberPostingRole={initialMemberPostingRole}
+        />
+        {!createMode && <SquadDangerZone squad={squad} />}
       </form>
     </FormWrapper>
   );
---
+++ packages/shared/src/components/squads/settings/SquadCategoryDropdown.tsx
@@ -0,0 +1,90 @@
+import React, { ReactElement, useEffect, useMemo, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import {
+  Typography,
+  TypographyColor,
+  TypographyType,
+} from '../../typography/Typography';
+import { Dropdown } from '../../fields/Dropdown';
+import { gqlClient } from '../../../graphql/common';
+import {
+  SourceCategoryData,
+  SOURCE_CATEGORIES_QUERY,
+} from '../../../graphql/sources';
+import { generateQueryKey, RequestKey, StaleTime } from '../../../lib/query';
+
+interface SquadCategoryDropdownProps {
+  initialCategory: string;
+  isDisabled: boolean;
+  categoryHint?: string;
+  onCategoryChange?: (category: string) => void;
+}
+
+export function SquadCategoryDropdown({
+  initialCategory,
+  isDisabled,
+  categoryHint,
+  onCategoryChange,
+}: SquadCategoryDropdownProps): ReactElement {
+  const [selected, setSelected] = useState(-1);
+  const { data } = useQuery<SourceCategoryData>(
+    generateQueryKey(RequestKey.Source, null, 'categories'),
+    () => gqlClient.request(SOURCE_CATEGORIES_QUERY),
+    { staleTime: StaleTime.OneDay },
+  );
+  const list = useMemo(
+    () => data?.categories?.edges?.map(({ node }) => node),
+    [data],
+  );
+
+  useEffect(() => {
+    if (!list?.length) {
+      return;
+    }
+
+    const index = list.findIndex(({ id }) => id === initialCategory);
+
+    if (index === -1) {
+      return;
+    }
+
+    setSelected(index);
+  }, [initialCategory, list]);
+
+  return (
+    <div className="ml-9 flex flex-col gap-4">
+      <Typography
+        type={TypographyType.Callout}
+        color={TypographyColor.Secondary}
+      >
+        Squad is searchable, publicly listed in our squad directory, and open
+        for everyone to join. It’s ideal for building a community or for content
+        creators.
+      </Typography>
+      {list && (
+        <Dropdown
+          placeholder="Select category"
+          className={{
+            container: 'w-56',
+            button: categoryHint && 'border border-status-error',
+          }}
+          options={list.map(({ title }) => title)}
+          selectedIndex={selected}
+          onChange={(_, index) => {
+            setSelected(index);
+            onCategoryChange?.(list[index].id);
+          }}
+          disabled={isDisabled}
+        />
+      )}
+      {list && !isDisabled && (
+        <input
+          type="hidden"
+          id="categoryId"
+          name="categoryId"
+          value={list[selected]?.id}
+        />
+      )}
+    </div>
+  );
+}
