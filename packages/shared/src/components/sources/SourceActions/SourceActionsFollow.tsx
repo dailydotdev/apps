28a176b8b658d974ff0c9ab40b98aa7d52298aa5
capJavert <dev@kickass.website>
2024-09-19 11:54:38 +0200

    feat: move to css for hover

--- packages/shared/src/components/sources/SourceActions/SourceActionsFollow.tsx
+++ packages/shared/src/components/sources/SourceActions/SourceActionsFollow.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import classNames from 'classnames';
 import { ButtonSize, ButtonVariant } from '../../buttons/common';
 import { Button } from '../../buttons/Button';
@@ -13,34 +13,28 @@ interface SourceActionsFollowProps {
 
 const SourceActionsFollow = (props: SourceActionsFollowProps): ReactElement => {
   const { className, isSubscribed, isFetching, onClick, variant } = props;
-  const [isHovered, setIsHovered] = useState(false);
-
-  let label = isSubscribed ? 'Following' : 'Follow';
-
-  if (isHovered && isSubscribed) {
-    label = 'Unfollow';
-  }
 
   return (
     <Button
-      aria-label={label}
+      aria-label={`Toggle follow status, currently you are ${
+        isSubscribed ? 'following' : 'not following'
+      }`}
       className={classNames(
         isSubscribed &&
-          'min-w-24 hover:bg-overlay-float-ketchup hover:text-accent-ketchup-default',
+          'group min-w-24 hover:bg-overlay-float-ketchup hover:text-accent-ketchup-default',
         className,
       )}
       disabled={isFetching}
       onClick={onClick}
       size={ButtonSize.Small}
       variant={isSubscribed ? ButtonVariant.Subtle : variant}
-      onMouseEnter={() => {
-        setIsHovered(true);
-      }}
-      onMouseOut={() => {
-        setIsHovered(false);
-      }}
     >
-      {label}
+      <span className="group-hover:hidden">
+        {isSubscribed ? 'Following' : 'Follow'}
+      </span>
+      <span className="hidden group-hover:block">
+        {isSubscribed ? 'Unfollow' : 'Follow'}
+      </span>
     </Button>
   );
 };
