6c054fdf79ab1b1e03834532ce8e40aa3c24e328
nensidosari <36197304+nensidosari@users.noreply.github.com>
2024-09-19 22:23:05 +0200

    Mi 542 fix unfeatured squad card (#3561)
    * fix: card width
    
    * fix: remove max width
    
    * fix: max widths
    
    * fix: show featured squads with unfeatured squad card in the categories
    
    * fix: widths on directory feed
    
    * fix: place cards within feed container on unfeatured categories

--- packages/shared/src/components/cards/squad/SquadsDirectoryFeed.tsx
+++ packages/shared/src/components/cards/squad/SquadsDirectoryFeed.tsx
@@ -58,14 +58,10 @@ export function SquadsDirectoryFeed({
     >
       {children}
       {flatSources?.map(({ node }) =>
-        node.flags?.featured ? (
-          <SquadGrid key={node.id} source={node} className="max-w-80" />
+        node.flags?.featured && linkToSeeAll.includes('featured') ? (
+          <SquadGrid key={node.id} source={node} className="w-80" />
         ) : (
-          <UnfeaturedSquadGrid
-            key={node.id}
-            source={node}
-            className="max-w-76"
-          />
+          <UnfeaturedSquadGrid key={node.id} source={node} className="w-80" />
         ),
       )}
     </HorizontalScroll>
