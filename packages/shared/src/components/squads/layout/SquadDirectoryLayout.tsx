c54d688424549dba77dace2548844fb3653507ac
nensidosari <36197304+nensidosari@users.noreply.github.com>
2024-09-20 11:24:42 +0200

    fix: layout shifts (#3565)
    * fix: sidebar layout shift
    
    * fix: squad navbar layout shift

--- packages/shared/src/components/squads/layout/SquadDirectoryLayout.tsx
+++ packages/shared/src/components/squads/layout/SquadDirectoryLayout.tsx
@@ -2,6 +2,7 @@ import React, {
   ComponentProps,
   PropsWithChildren,
   ReactElement,
+  useContext,
   useEffect,
   useId,
 } from 'react';
@@ -23,6 +24,7 @@ import {
 import { PlusIcon } from '../../icons';
 import { useSquadDirectoryLayout } from './useSquadDirectoryLayout';
 import { squadCategoriesPaths } from '../../../lib/constants';
+import SettingsContext from '../../../contexts/SettingsContext';
 
 type SquadDirectoryLayoutProps = PropsWithChildren & ComponentProps<'section'>;
 
@@ -54,7 +56,7 @@ export const SquadDirectoryLayout = (
   props: SquadDirectoryLayoutProps,
 ): ReactElement => {
   const { children, className, ...attrs } = props;
-
+  const { loadedSettings } = useContext(SettingsContext);
   const id = useId();
   const { pathname, asPath } = useRouter();
   const { categoryPaths, isMobileLayout } = useSquadDirectoryLayout();
@@ -68,39 +70,42 @@ export const SquadDirectoryLayout = (
   const isDiscover = pathname === squadCategoriesPaths.discover;
 
   return (
-    <BaseFeedPage className="relative mb-4 flex-col px-4 pt-4 laptop:px-18 laptop:pt-8">
-      {isDiscover && (
-        <div className="absolute inset-0 -z-1 hidden h-[25rem] w-full bg-gradient-to-t from-accent-cabbage-default to-background-default tablet:flex" />
-      )}
-      <header className="flex w-full flex-col gap-2">
-        <section className="flex w-full flex-row items-center justify-between typo-body laptop:hidden">
-          <strong>Squads</strong>
-          <NewSquadButton icon={<PlusIcon />} variant={ButtonVariant.Float} />
-        </section>
-        <div className="flex max-w-full flex-row flex-nowrap items-center justify-between gap-6 laptop:gap-22">
-          <SquadDirectoryNavbar className="min-w-0 flex-1">
-            {Object.entries(categoryPaths ?? {}).map(([category, path]) => (
-              <SquadDirectoryNavbarItem
-                buttonSize={buttonSize}
-                elementProps={{ id: `squad-item-${category}-${id}` }}
-                isActive={path === pathname || path === asPath}
-                key={category}
-                label={category}
-                path={path}
-              />
-            ))}
-          </SquadDirectoryNavbar>
-          <div className="hidden laptop:block">
-            <NewSquadButton />
+    loadedSettings && (
+      <BaseFeedPage className="relative mb-4 flex-col px-4 pt-4 laptop:px-18 laptop:pt-8">
+        {isDiscover && (
+          <div className="absolute inset-0 -z-1 hidden h-[25rem] w-full bg-gradient-to-t from-accent-cabbage-default to-background-default tablet:flex" />
+        )}
+
+        <header className="flex w-full flex-col gap-2">
+          <section className="flex w-full flex-row items-center justify-between typo-body laptop:hidden">
+            <strong>Squads</strong>
+            <NewSquadButton icon={<PlusIcon />} variant={ButtonVariant.Float} />
+          </section>
+          <div className="flex max-w-full flex-row flex-nowrap items-center justify-between gap-6 laptop:gap-22">
+            <SquadDirectoryNavbar className="min-h-14 min-w-0 flex-1">
+              {Object.entries(categoryPaths ?? {}).map(([category, path]) => (
+                <SquadDirectoryNavbarItem
+                  buttonSize={buttonSize}
+                  elementProps={{ id: `squad-item-${category}-${id}` }}
+                  isActive={path === pathname || path === asPath}
+                  key={category}
+                  label={category}
+                  path={path}
+                />
+              ))}
+            </SquadDirectoryNavbar>
+            <div className="hidden laptop:block">
+              <NewSquadButton />
+            </div>
           </div>
-        </div>
-      </header>
-      <section
-        {...attrs}
-        className={classNames('flex w-full flex-col pt-5', className)}
-      >
-        {children}
-      </section>
-    </BaseFeedPage>
+        </header>
+        <section
+          {...attrs}
+          className={classNames('flex w-full flex-col pt-5', className)}
+        >
+          {children}
+        </section>
+      </BaseFeedPage>
+    )
   );
 };
