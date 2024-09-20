b7bf176e0bc88775205a7783a2a388cc9d2a0ac9
NensiDosari <nensidosari97@gmail.com>
2024-09-20 15:22:07 +0200

    fix: typo squad

--- packages/shared/src/components/squads/settings/SquadDangerZone.tsx
+++ packages/shared/src/components/squads/settings/SquadDangerZone.tsx
@@ -38,8 +38,8 @@ export function SquadDangerZone({ squad }: SquadDangerZoneProps): ReactElement {
       <DangerZone
         onClick={onDeleteSquad}
         className="mt-4"
-        cta="Delete account"
-        title="Deleting your account will:"
+        cta="Delete squad"
+        title="Deleting your squad will:"
         notes={[
           'Permanently delete your Squad.',
           'Permanently delete all Squad’s content, including your posts and others, comments, upvotes, etc',
