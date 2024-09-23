9e1990d62f73d409056317b503b52fb6c37aee74
Chris Bongers <chrisbongers@gmail.com>
2024-09-23 11:05:36 +0200

    fix: add logged user to query

--- packages/shared/src/hooks/profile/useProfile.ts
+++ packages/shared/src/hooks/profile/useProfile.ts
@@ -3,12 +3,16 @@ import { useMemo } from 'react';
 import { getProfile, PublicProfile } from '../../lib/user';
 import { generateQueryKey, RequestKey, StaleTime } from '../../lib/query';
 import { disabledRefetch } from '../../lib/func';
+import { useAuthContext } from '../../contexts/AuthContext';
 
 export function useProfile(initialUser?: PublicProfile): {
   user: PublicProfile;
   userQueryKey: unknown[];
 } {
-  const userQueryKey = generateQueryKey(RequestKey.Profile, initialUser);
+  const { user: loggedUser } = useAuthContext();
+  const userQueryKey = generateQueryKey(RequestKey.Profile, initialUser, {
+    id: loggedUser?.id,
+  });
   const { data: user } = useQuery(
     userQueryKey,
     () => getProfile(initialUser?.id),
