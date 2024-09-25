88bf69aed826111e1f80668f42f3dbe7b613906a
Amar Trebinjac <amartrebinjac@Amars-MacBook-Pro.local>
2024-09-25 15:16:23 +0200

    feat: Add ProfilePicture component to Storybook

---
+++ packages/storybook/stories/components/profile/ProfilePicture.stories.tsx
@@ -0,0 +1,33 @@
+import { ProfilePicture } from '@dailydotdev/shared/src/components/ProfilePicture';
+import { Meta, StoryObj } from '@storybook/react';
+import { QueryClientProvider } from '../../extension/_providers';
+
+const meta: Meta<typeof ProfilePicture> = {
+  title: 'Components/Profile/ProfilePicture',
+  component: ProfilePicture,
+  args: {
+    user: {
+      image:
+        'https://res.cloudinary.com/daily-now/image/upload/s--O0TOmw4y--/f_auto/v1715772965/public/noProfile',
+      id: '1337',
+      username: 'johndoe',
+    },
+  },
+  render: (args) => {
+    return (
+      <QueryClientProvider>
+        <ProfilePicture {...args} />
+      </QueryClientProvider>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProfilePicture>;
+
+export const Default: Story = {
+  args: {
+    rounded: 'full',
+  },
+};
