f566ca1f59e794b91f57e17149fa8da2cb858da6
Chris Bongers <chrisbongers@gmail.com>
2024-09-25 09:42:53 +0200

    feat: profile follow button (#3575)

--- packages/shared/src/hooks/profile/useProfile.ts
+++ packages/shared/src/hooks/profile/useProfile.ts
@@ -1,24 +1,29 @@
-import { useContext } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getProfile, PublicProfile } from '../../lib/user';
-import AuthContext from '../../contexts/AuthContext';
 import { generateQueryKey, RequestKey, StaleTime } from '../../lib/query';
 import { disabledRefetch } from '../../lib/func';
+import { useAuthContext } from '../../contexts/AuthContext';
 
-export function useProfile(initialUser?: PublicProfile): PublicProfile {
-  const { user: loggedUser } = useContext(AuthContext);
-  const isSameUser = loggedUser?.id === initialUser?.id;
-
+export function useProfile(initialUser?: PublicProfile): {
+  user: PublicProfile;
+  userQueryKey: unknown[];
+} {
+  const { user: loggedUser } = useAuthContext();
+  const userQueryKey = generateQueryKey(RequestKey.Profile, initialUser, {
+    id: loggedUser?.id,
+  });
   const { data: user } = useQuery(
-    generateQueryKey(RequestKey.Profile, initialUser),
+    userQueryKey,
     () => getProfile(initialUser?.id),
     {
       ...disabledRefetch,
       cacheTime: StaleTime.OneHour,
       initialData: initialUser,
-      enabled: !!initialUser && isSameUser,
     },
   );
 
-  return user;
+  return {
+    user,
+    userQueryKey,
+  };
 }
