8c937ceb92dae61df627d11d16b1439955b2a93d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-10-02 18:47:53 +0800

    fix: redirect when squads is empty

--- packages/webapp/pages/squads/discover/my.tsx
+++ packages/webapp/pages/squads/discover/my.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from 'react';
 import { NextSeo } from 'next-seo';
 import { useAuthContext } from '@dailydotdev/shared/src/contexts/AuthContext';
 import { SquadList } from '@dailydotdev/shared/src/components/cards/squad/SquadList';
+import { useRouter } from 'next/router';
+import { squadCategoriesPaths } from '@dailydotdev/shared/src/lib/constants';
 import { getLayout } from '../../../components/layouts/FeedLayout';
 import { mainFeedLayoutProps } from '../../../components/layouts/MainFeedPage';
 import { SquadDirectoryLayout } from '../../../../shared/src/components/squads/layout/SquadDirectoryLayout';
@@ -9,6 +11,12 @@ import { defaultSeo } from '../../../next-seo';
 
 function MySquadsPage(): ReactElement {
   const { squads } = useAuthContext();
+  const router = useRouter();
+
+  if (squads?.length === 0) {
+    router.push(squadCategoriesPaths.discover);
+    return null;
+  }
 
   return (
     <SquadDirectoryLayout className="gap-3">
