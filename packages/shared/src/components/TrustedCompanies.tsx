c25b2e463ce08243bb53d240652a2d51119a9dc3
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-10-06 14:53:06 +0200

    fix: 500K to 1M (#3632)

--- packages/shared/src/components/TrustedCompanies.tsx
+++ packages/shared/src/components/TrustedCompanies.tsx
@@ -72,7 +72,7 @@ const TrustedCompanies = ({
       )}
     >
       <p className="relative z-3 max-w-[15rem] text-center text-text-quaternary typo-callout tablet:max-w-full tablet:flex-1">
-        Trusted by 500K+ developers from the world&apos;s leading companies
+        Trusted by 1M+ developers from the world&apos;s leading companies
       </p>
 
       <div className="talet:flex-1 relative z-3 flex flex-wrap items-start justify-center gap-2 laptop:gap-6">
