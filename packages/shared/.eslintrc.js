f3bc7e359878772538f0f476e47cd988a9838516
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2024-10-13 18:00:54 +0000

    fix

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@dailydotdev'],
+  plugins: ['tailwindcss'],
+  rules: {
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-custom-classname': 'off', 
+  },
 };
