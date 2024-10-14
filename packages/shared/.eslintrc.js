390e1b52037c91511f799fe43038264763fcf6cc
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-10-14 10:46:19 +0400

    revert: unnecessary change

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -1,8 +1,3 @@
 module.exports = {
   extends: ['@dailydotdev'],
-  plugins: ['tailwindcss'],
-  rules: {
-    'tailwindcss/classnames-order': 'warn',
-    'tailwindcss/no-custom-classname': 'off', 
-  },
 };
