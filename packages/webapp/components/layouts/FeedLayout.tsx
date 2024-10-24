844af9c0e980f79567c9fc90198c0a62b151570c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-10-24 16:19:56 +0400

    revert: unnecessary changes on the layout

--- packages/webapp/components/layouts/FeedLayout.tsx
+++ packages/webapp/components/layouts/FeedLayout.tsx
@@ -4,7 +4,6 @@ import MainLayout, {
   MainLayoutProps,
 } from '@dailydotdev/shared/src/components/MainLayout';
 import FeedLayout from '@dailydotdev/shared/src/components/FeedLayout';
-import { NextSeo, NextSeoProps } from 'next-seo';
 import { getLayout as getFooterNavBarLayout } from './FooterNavBarLayout';
 
 export default FeedLayout;
@@ -12,17 +11,14 @@ export default FeedLayout;
 export const getLayout = (
   page: ReactNode,
   pageProps: Record<string, unknown>,
-  { seo, ...layoutProps }: MainLayoutProps & { seo?: NextSeoProps },
+  layoutProps: MainLayoutProps,
 ): ReactNode => {
   // @NOTE see https://dailydotdev.atlassian.net/l/cp/dK9h1zoM
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   return getFooterNavBarLayout(
-    <>
-      {seo && <NextSeo {...seo} />}
-      <MainLayout {...layoutProps} activePage={router?.asPath}>
-        <FeedLayout>{page}</FeedLayout>
-      </MainLayout>
-    </>,
+    <MainLayout {...layoutProps} activePage={router?.asPath}>
+      <FeedLayout>{page}</FeedLayout>
+    </MainLayout>,
   );
 };
--- packages/webapp/components/layouts/MainLayout.tsx
+++ packages/webapp/components/layouts/MainLayout.tsx
@@ -3,24 +3,20 @@ import { useRouter } from 'next/router';
 import MainLayout, {
   MainLayoutProps,
 } from '@dailydotdev/shared/src/components/MainLayout';
-import { NextSeo, NextSeoProps } from 'next-seo';
 
 export default MainLayout;
 
 export const getLayout = (
   page: ReactNode,
   pageProps?: Record<string, unknown>,
-  { seo, ...layoutProps }: MainLayoutProps & { seo?: NextSeoProps } = {},
+  layoutProps?: MainLayoutProps,
 ): ReactNode => {
   // @NOTE see https://dailydotdev.atlassian.net/l/cp/dK9h1zoM
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   return (
-    <>
-      {seo && <NextSeo {...seo} />}
-      <MainLayout {...layoutProps} activePage={router?.asPath}>
-        {page}
-      </MainLayout>
-    </>
+    <MainLayout {...layoutProps} activePage={router?.asPath}>
+      {page}
+    </MainLayout>
   );
 };
