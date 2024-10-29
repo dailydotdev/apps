75403a4970e01907e16783ee83638832a7a52863
capJavert <dev@kickass.website>
2024-10-29 16:04:44 +0100

    feat: error boundary

---
+++ packages/shared/src/ErrorBoundary.tsx
@@ -0,0 +1,48 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+import LogContext from './contexts/LogContext';
+import { LogEvent } from './lib/log';
+
+type ErrorBoundaryProps = {
+  children: ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  // eslint-disable-next-line react/static-property-placement
+  static contextType = LogContext;
+
+  // eslint-disable-next-line react/static-property-placement
+  context!: React.ContextType<typeof LogContext>;
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    const { logEvent } = this.context;
+
+    if (!logEvent) {
+      return;
+    }
+
+    logEvent({
+      event_name: LogEvent.GlobalError,
+      extra: JSON.stringify({
+        msg: error.message,
+        url: window.location.href,
+        error,
+        stack: errorInfo.componentStack,
+        digest: errorInfo.digest,
+      }),
+    });
+  }
+
+  render(): ReactNode {
+    const { children } = this.props;
+
+    return children;
+  }
+}
