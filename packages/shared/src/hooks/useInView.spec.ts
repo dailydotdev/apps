a546db66c39703138e5372f33cf8553cd9fa144a
NensiDosari <nensidosari97@gmail.com>
2024-10-31 16:51:26 +0100

    fix: tests

--- packages/shared/src/hooks/useInView.spec.ts
+++ packages/shared/src/hooks/useInView.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import { useInView } from './useInView';
 import { useIntersectionObserver } from './useIntersectionObserver';
 
