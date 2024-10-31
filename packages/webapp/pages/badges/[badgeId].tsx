a489a819a1a225c09008b2a6589194a325b4a227
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-10-31 12:35:13 +0100

    feat: create endpoint to render the top reader badge (#3738)
    Co-authored-by: Chris Bongers <chrisbongers@gmail.com>

---
+++ packages/webapp/pages/badges/[badgeId].tsx
@@ -0,0 +1,68 @@
+import React, { ReactElement } from 'react';
+import {
+  GetStaticPathsResult,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+} from 'next';
+import { TopReaderBadge } from '@dailydotdev/shared/src/components/badges/TopReaderBadge';
+import type { LoggedUser } from '@dailydotdev/shared/src/lib/user';
+import type { Keyword } from '@dailydotdev/shared/src/graphql/keywords';
+import { TOP_READER_BADGE_BY_ID } from '@dailydotdev/shared/src/graphql/users';
+import { gqlClient } from '@dailydotdev/shared/src/graphql/common';
+
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+  return { paths: [], fallback: 'blocking' };
+}
+
+interface PageProps {
+  topReaderBadge: {
+    id: string;
+    user: LoggedUser;
+    issuedAt: string;
+    keyword: Pick<Keyword, 'value' | 'flags'>;
+    image: string;
+  };
+}
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext): Promise<GetStaticPropsResult<PageProps>> {
+  const { badgeId } = params;
+
+  if (!badgeId) {
+    return {
+      notFound: true,
+      revalidate: false,
+    };
+  }
+
+  const { topReaderBadge } = await gqlClient.request<PageProps>(
+    TOP_READER_BADGE_BY_ID,
+    {
+      id: badgeId,
+    },
+  );
+
+  if (!topReaderBadge) {
+    return {
+      notFound: true,
+      revalidate: false,
+    };
+  }
+
+  return {
+    props: { topReaderBadge },
+    revalidate: 60,
+  };
+}
+
+const BadgePage = ({ topReaderBadge }: PageProps): ReactElement => {
+  const { user, keyword, issuedAt } = topReaderBadge;
+  return (
+    <div id="screenshot_wrapper" className="w-fit">
+      <TopReaderBadge user={user} keyword={keyword} issuedAt={issuedAt} />
+    </div>
+  );
+};
+
+export default BadgePage;
