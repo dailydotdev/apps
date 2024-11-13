a4cc7e65180847f71e47db94e2102cdb24c163a5
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-13 15:37:43 +0100

    fix: add go-back functionality to Back button on plus page

--- packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
+++ packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
@@ -5,13 +5,17 @@ import {
   ButtonVariant,
 } from '@dailydotdev/shared/src/components/buttons/Button';
 import { ArrowIcon } from '@dailydotdev/shared/src/components/icons';
+import { useRouter } from 'next/router';
 
 export const PlusHeader = (): ReactElement => {
+  const { back } = useRouter();
+
   return (
     <header className="flex h-16 w-full items-center gap-4 border-b border-border-subtlest-tertiary px-4">
       <Button
         variant={ButtonVariant.Float}
         icon={<ArrowIcon className="-rotate-90" />}
+        onClick={back}
       >
         Back
       </Button>
