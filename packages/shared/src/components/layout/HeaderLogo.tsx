0dc4bbb9355a5a83d16a9141ffb41e8cc6e79d9f
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-14 17:01:02 +0100

    fix: remove feature from logo

--- packages/shared/src/components/layout/HeaderLogo.tsx
+++ packages/shared/src/components/layout/HeaderLogo.tsx
@@ -4,8 +4,6 @@ import { useFeatureTheme } from '../../hooks/utils/useFeatureTheme';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { withNoSSR } from '../../lib/withNoSSR';
 import type { WithClassNameProps } from '../utilities';
-import { feature } from '../../lib/featureManagement';
-import { useFeature } from '../GrowthBookProvider';
 
 interface HeaderLogoProps extends WithClassNameProps {
   onLogoClick?: (e: React.MouseEvent) => unknown;
@@ -21,7 +19,6 @@ function HeaderLogo({
 }: HeaderLogoProps): ReactElement {
   const featureTheme = useFeatureTheme();
   const { user } = useAuthContext();
-  const showPlusSubscription = useFeature(feature.plusSubscription);
 
   return (
     <Logo
@@ -29,7 +26,7 @@ function HeaderLogo({
       position={position}
       onLogoClick={onLogoClick}
       featureTheme={featureTheme}
-      isPlus={showPlusSubscription && user?.isPlus}
+      isPlus={user?.isPlus}
       className={className}
     />
   );
