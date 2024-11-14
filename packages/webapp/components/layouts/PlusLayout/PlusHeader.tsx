48e636993e59e8c3b2a721c1bd895ed1a6019b71
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-14 18:49:20 +0100

    fix: navigate to main page if navigated directly (#3807)

--- packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
+++ packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback } from 'react';
 import HeaderLogo from '@dailydotdev/shared/src/components/layout/HeaderLogo';
 import {
   Button,
@@ -6,16 +6,25 @@ import {
 } from '@dailydotdev/shared/src/components/buttons/Button';
 import { ArrowIcon } from '@dailydotdev/shared/src/components/icons';
 import { useRouter } from 'next/router';
+import { webappUrl } from '@dailydotdev/shared/src/lib/constants';
 
 export const PlusHeader = (): ReactElement => {
-  const { back } = useRouter();
+  const { back, replace } = useRouter();
+
+  const onBackClick = useCallback(() => {
+    if (window.history?.length > 1) {
+      back();
+    } else {
+      replace(webappUrl);
+    }
+  }, [back, replace]);
 
   return (
     <header className="flex h-16 w-full items-center gap-4 border-b border-border-subtlest-tertiary px-4">
       <Button
         variant={ButtonVariant.Float}
         icon={<ArrowIcon className="-rotate-90" />}
-        onClick={back}
+        onClick={onBackClick}
       >
         Back
       </Button>
