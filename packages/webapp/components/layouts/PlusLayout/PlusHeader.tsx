ef7449eb4bff49d3607e12711f86c1fb0cd0f5fd
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-15 15:30:47 +0100

    fix: plus header hydration (#3821)

--- packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
+++ packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
@@ -12,7 +12,7 @@ import { useViewSize, ViewSize } from '@dailydotdev/shared/src/hooks';
 
 export const PlusHeader = (): ReactElement => {
   const isMobile = useViewSize(ViewSize.MobileL);
-  const { back, replace } = useRouter();
+  const { back, replace, isReady } = useRouter();
 
   const onBackClick = useCallback(() => {
     if (window.history?.length > 1) {
@@ -21,6 +21,10 @@ export const PlusHeader = (): ReactElement => {
       replace(webappUrl);
     }
   }, [back, replace]);
+
+  if (!isReady) {
+    return null;
+  }
 
   return (
     <header className="flex h-16 w-full items-center justify-center gap-4 border-b border-border-subtlest-tertiary bg-background-default px-4 tablet:bg-transparent">
