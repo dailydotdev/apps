daee57f2f714dc23e2ab656eed74ada3eb0a287b
Chris Bongers <chrisbongers@gmail.com>
2024-11-15 12:58:09 +0200

    fix: small fixes for the mobile header view on plus page (#3815)

--- packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
+++ packages/webapp/components/layouts/PlusLayout/PlusHeader.tsx
@@ -7,8 +7,11 @@ import {
 import { ArrowIcon } from '@dailydotdev/shared/src/components/icons';
 import { useRouter } from 'next/router';
 import { webappUrl } from '@dailydotdev/shared/src/lib/constants';
+import { LogoPosition } from '@dailydotdev/shared/src/components/Logo';
+import { useViewSize, ViewSize } from '@dailydotdev/shared/src/hooks';
 
 export const PlusHeader = (): ReactElement => {
+  const isMobile = useViewSize(ViewSize.MobileL);
   const { back, replace } = useRouter();
 
   const onBackClick = useCallback(() => {
@@ -20,15 +23,16 @@ export const PlusHeader = (): ReactElement => {
   }, [back, replace]);
 
   return (
-    <header className="flex h-16 w-full items-center gap-4 border-b border-border-subtlest-tertiary px-4">
+    <header className="flex h-16 w-full items-center justify-center gap-4 border-b border-border-subtlest-tertiary bg-background-default px-4 tablet:bg-transparent">
       <Button
-        variant={ButtonVariant.Float}
+        variant={isMobile ? ButtonVariant.Tertiary : ButtonVariant.Float}
         icon={<ArrowIcon className="-rotate-90" />}
         onClick={onBackClick}
+        className="absolute left-4"
       >
-        Back
+        {!isMobile ? 'Back' : undefined}
       </Button>
-      <HeaderLogo />
+      <HeaderLogo position={LogoPosition.Relative} className="!mt-0" />
     </header>
   );
 };
