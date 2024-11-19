dc4a0002c139678ed8c875764a92df5a4d039ac1
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-19 15:26:24 +0100

    fix: rename cost and currency to local

--- packages/shared/src/contexts/PaymentContext.tsx
+++ packages/shared/src/contexts/PaymentContext.tsx
@@ -68,8 +68,8 @@ export const PaymentContextProvider = ({
               event_name: LogEvent.CompleteCheckout,
               extra: {
                 cycle: event?.data.items?.[0]?.billing_cycle.interval,
-                cost: event?.data.totals.total,
-                currency: event?.data.currency_code,
+                localCost: event?.data.totals.total,
+                localCurrenct: event?.data.currency_code,
                 payment: event?.data.payment.method_details.type,
               },
             });
