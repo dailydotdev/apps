fd74ddb8f1eb72db2a89eeb8028e67d5f6ca8339
Chris Bongers <chrisbongers@gmail.com>
2024-11-20 08:31:16 +0200

    chore: cleanup onboarding_shuffle_tags exp

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -34,7 +34,6 @@ const feature = {
   searchUsers: new Feature('search_users', false),
   showCodeSnippets: new Feature('show_code_snippets', false),
   searchPlaceholder: new Feature('search_placeholder', 'Search'),
-  onboardingShuffleTags: new Feature('onboarding_shuffle_tags', false),
   extensionOverlay: new Feature('onboarding_extension_overlay', false),
   seniorContentOnboarding: new Feature('senior_content_onboarding', false),
   plusSubscription: new Feature('plus_subscription', false),
