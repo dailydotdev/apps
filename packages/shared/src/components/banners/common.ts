5eec740b06c62e93c490f477f5b6f0057ee21002
Amar Trebinjac <amartrebinjac@Amars-MacBook-Pro.local>
2024-11-20 14:09:43 +0100

    remove border

--- packages/shared/src/components/banners/common.ts
+++ packages/shared/src/components/banners/common.ts
@@ -1,7 +1,7 @@
 import classed from '../../lib/classed';
 
 export const bottomBannerBaseClasses =
-  'fixed bottom-0 left-0 z-modal flex w-full flex-row justify-center';
+  'fixed bottom-0 left-0 z-modal flex w-full flex-row justify-center border-none';
 
 export const BottomBannerContainer = classed('div', bottomBannerBaseClasses);
 
