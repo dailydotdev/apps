34716dffa2cb20f7bba9d598d66f7dea231c837d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-21 09:00:47 +0100

    fix: add missing upgrade to plus events

--- packages/shared/src/components/cards/ad/common/RemoveAd.tsx
+++ packages/shared/src/components/cards/ad/common/RemoveAd.tsx
@@ -10,10 +10,13 @@ import {
 import { plusUrl } from '../../../../lib/constants';
 import Link from '../../../utilities/Link';
 import type { WithClassNameProps } from '../../../utilities';
+import { LogEvent, TargetId } from '../../../../lib/log';
+import { usePlusSubscription } from '../../../../hooks/usePlusSubscription';
 
 type Props = WithClassNameProps;
 
 export const RemoveAd = ({ className }: Props): ReactElement => {
+  const { logSubscriptionEvent } = usePlusSubscription();
   return (
     <Link passHref href={plusUrl}>
       <Button
@@ -22,6 +25,12 @@ export const RemoveAd = ({ className }: Props): ReactElement => {
         size={ButtonSize.Small}
         color={ButtonColor.Bacon}
         className={classNames('ml-auto', className)}
+        onClick={() => {
+          logSubscriptionEvent({
+            event_name: LogEvent.UpgradeSubscription,
+            target_id: TargetId.Ads,
+          });
+        }}
       >
         Remove
       </Button>
