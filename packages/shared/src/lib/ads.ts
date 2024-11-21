e2abf05fb2f8d333a4526b486d35bc9e9f3057c1
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-21 16:06:48 +0100

    fix: don't return null

--- packages/shared/src/lib/ads.ts
+++ packages/shared/src/lib/ads.ts
@@ -4,21 +4,11 @@ import type { Ad } from '../graphql/posts';
 export const fetchAd = async (active = false): Promise<Ad | null> => {
   const res = await fetch(`${apiUrl}/v1/a?active=${active}`);
   const ads = (await res.json()) as Ad[];
-
-  if (!ads.length) {
-    return null;
-  }
-
   return ads[0];
 };
 
 export const fetchCommentAd = async (): Promise<Ad | null> => {
   const res = await fetch(`${apiUrl}/v1/a/post`);
   const ads = (await res.json()) as Ad[];
-
-  if (!ads.length) {
-    return null;
-  }
-
   return ads[0];
 };
