72bf55112aa76a7f3209844a8f4c2f87b9d7de87
Luca Pagliaro <luca@sentiero.digital>
2024-11-26 16:46:06 +0100

    refactor: removed onboardingPlus experiment;

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -33,7 +33,6 @@ const feature = {
   seniorContentOnboarding: new Feature('senior_content_onboarding', false),
   postBannerExtensionPrompt: new Feature('post_banner_extension_prompt', false),
   plusSubscription: new Feature('plus_subscription', false),
-  onboardingPlus: new Feature('onboarding_plus', false),
   feedPageSizes: new Feature('feed_page_sizes', {
     default: 7,
     tablet: 9,
