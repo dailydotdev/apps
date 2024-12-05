0dae6786edbb4dee2b54458b65486c4bcff28004
Chris Bongers <chrisbongers@gmail.com>
2024-12-05 15:03:46 +0200

    fix: empty screen

--- packages/shared/src/components/FollowingFeedEmptyScreen.tsx
+++ packages/shared/src/components/FollowingFeedEmptyScreen.tsx
@@ -6,25 +6,40 @@ import {
   EmptyScreenIcon,
   EmptyScreenTitle,
 } from './EmptyScreen';
-import { FilterIcon } from './icons';
 import { PageContainer } from './utilities';
-import { ButtonSize } from './buttons/common';
+import { ButtonSize, ButtonVariant } from './buttons/common';
+import { SquadIcon } from './icons';
+import { webappUrl } from '../lib/constants';
+import Link from './utilities/Link';
 
 function FollowingFeedEmptyScreen(): ReactElement {
   return (
     <PageContainer className="mx-auto">
       <EmptyScreenContainer>
-        <FilterIcon
+        <SquadIcon
           className={EmptyScreenIcon.className}
           style={EmptyScreenIcon.style}
         />
-        <EmptyScreenTitle>Start following.</EmptyScreenTitle>
+        <EmptyScreenTitle>Nothing to See Here—Yet!</EmptyScreenTitle>
         <EmptyScreenDescription>
-          You need to follow more people
+          Your feed is empty because you haven’t followed any Squads or Sources
+          or there is not enough content. Explore more Squads and Sources
         </EmptyScreenDescription>
-        <EmptyScreenButton size={ButtonSize.Large}>
-          Find sources
-        </EmptyScreenButton>
+        <div className="flex flex-col gap-4 tablet:flex-row">
+          <Link href={`${webappUrl}squads`}>
+            <EmptyScreenButton size={ButtonSize.Large}>
+              Find Squads
+            </EmptyScreenButton>
+          </Link>
+          <Link href={`${webappUrl}sources`}>
+            <EmptyScreenButton
+              size={ButtonSize.Large}
+              variant={ButtonVariant.Secondary}
+            >
+              Discover Sources
+            </EmptyScreenButton>
+          </Link>
+        </div>
       </EmptyScreenContainer>
     </PageContainer>
   );
