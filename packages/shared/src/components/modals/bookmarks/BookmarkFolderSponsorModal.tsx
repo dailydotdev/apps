7e5bd189bc74531c7f108dd41985c741c20b90f3
Luca Pagliaro <luca@sentiero.digital>
2024-12-09 10:47:23 +0100

    fix: link font size

--- packages/shared/src/components/modals/bookmarks/BookmarkFolderSponsorModal.tsx
+++ packages/shared/src/components/modals/bookmarks/BookmarkFolderSponsorModal.tsx
@@ -104,6 +104,7 @@ export const BookmarkFolderSponsorModal = (
                 });
               }}
               tag={TypographyTag.Link}
+              type={TypographyType.Callout}
             >
               Upgrade to Plus
             </Typography>
