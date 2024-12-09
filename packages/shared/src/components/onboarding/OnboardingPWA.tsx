4a122a40ec0076ac3a9ddb921a0ef531a0f17666
Amar Trebinjac <amartrebinjac@gmail.com>
2024-12-10 00:08:32 +0100

    There is naught but despair

--- packages/shared/src/components/onboarding/OnboardingPWA.tsx
+++ packages/shared/src/components/onboarding/OnboardingPWA.tsx
@@ -1,44 +1,18 @@
-import React, { useLayoutEffect, useState, type ReactElement } from 'react';
+import React, { type ReactElement } from 'react';
 import classNames from 'classnames';
 import { OnboardingTitle } from './common';
-import {
-  cloudinaryAddToHomeScreen,
-  cloudinaryPWA,
-  cloudinaryPWAVideo,
-} from '../../lib/image';
-import { Image } from '../image/Image';
+import { cloudinaryPWA, cloudinaryPWAVideo } from '../../lib/image';
 import { Typography } from '../typography/Typography';
 
 export const OnboardingPWA = (): ReactElement => {
-  const [video, setVideo] = useState<HTMLVideoElement>();
-
-  useLayoutEffect(() => {
-    if (video) {
-      video.play();
-    }
-  }, [video]);
-
   return (
     <>
       <div
         className={classNames(
           'rounded-lg pointer-events-none absolute top-0 z-2 flex h-screen w-screen flex-col gap-4 p-6 opacity-0 backdrop-blur transition-all duration-200',
         )}
-      >
-        <OnboardingTitle className="mt-32">Scroll & Tap</OnboardingTitle>
-        <Image
-          loading="lazy"
-          src={cloudinaryAddToHomeScreen}
-          alt="Add to home screen"
-          className="tablet:mx-auto tablet:w-1/2"
-        />
-        <div className="relative mx-auto">
-          <div className="h-5 w-5 translate-y-12 rounded-full bg-white transition-all duration-300" />
-          <span className="absolute top-4 h-16 w-5 rounded-20 bg-gradient-to-b from-white/50 to-transparent opacity-0 duration-500 ease-in-out" />
-        </div>
-      </div>
+      />
       <video
-        ref={setVideo}
         className="absolute -bottom-8 max-h-screen w-full"
         poster={cloudinaryPWA}
         src={cloudinaryPWAVideo}
@@ -47,7 +21,6 @@ export const OnboardingPWA = (): ReactElement => {
         loop
         playsInline
         disablePictureInPicture
-        controls={false}
       >
         <track kind="captions" />
       </video>
