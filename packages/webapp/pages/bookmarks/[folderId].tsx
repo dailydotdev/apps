f168f4d6971fcc5f25adad7c08a6567e32bcb142
Luca Pagliaro <luca@sentiero.digital>
2024-12-10 15:30:53 +0100

    feat: removed ssg, render only on client

--- packages/webapp/pages/bookmarks/[folderId].tsx
+++ packages/webapp/pages/bookmarks/[folderId].tsx
@@ -1,10 +1,7 @@
 import React, { ReactElement, useMemo } from 'react';
 import { NextSeoProps } from 'next-seo/lib/types';
-import { GetStaticPathsResult, GetStaticPropsResult } from 'next';
-import {
-  BookmarkFolder,
-  getBookmarkFolder,
-} from '@dailydotdev/shared/src/graphql/bookmarks';
+import { useBookmarkFolder } from '@dailydotdev/shared/src/hooks/bookmark/useBookmarkFolder';
+import { useRouter } from 'next/router';
 import { defaultOpenGraph, defaultSeo } from '../../next-seo';
 import {
   getBookmarkFeedLayout,
@@ -17,41 +14,29 @@ const seo: NextSeoProps = {
   ...defaultSeo,
 };
 
-interface BookmarksFolderPageProps {
-  folder: BookmarkFolder;
-}
+const BookmarksPage = (): ReactElement => {
+  const router = useRouter();
+  const id = `${router.query.folderId}`;
+  const {
+    query: { folder },
+  } = useBookmarkFolder({ id });
+  const props = useMemo(() => {
+    return {
+      ...bookmarkFeedLayoutProps,
+      ...(folder && {
+        folder,
+        title: `${folder.icon} ${folder.name}`,
+      }),
+    };
+  }, [folder]);
 
-const BookmarksPage = ({ folder }: BookmarksFolderPageProps): ReactElement => {
-  const title = `${folder.icon} ${folder.name}`;
-  const props = useMemo(
-    () => ({ ...bookmarkFeedLayoutProps, folder, title }),
-    [folder, title],
-  );
   const layout = getBookmarkFeedLayout(null, { seo }, props);
+
+  if (!folder) {
+    return null;
+  }
 
   return <>{layout}</>;
 };
-
-export async function getStaticPaths(): Promise<GetStaticPathsResult> {
-  return { paths: [], fallback: true };
-}
-
-export async function getStaticProps({
-  params,
-}): Promise<GetStaticPropsResult<BookmarksFolderPageProps>> {
-  try {
-    const folder = await getBookmarkFolder(params.folderId);
-
-    return {
-      props: { folder },
-      revalidate: 60,
-    };
-  } catch (err) {
-    return {
-      redirect: { destination: '/404', permanent: false },
-      revalidate: 60,
-    };
-  }
-}
 
 export default BookmarksPage;
