e432f1a91778b05d3833b55864e138f957018b4f
Amar Trebinjac <amartrebinjac@gmail.com>
2024-12-16 13:03:09 +0100

    feat: Update extension banner experiment

--- packages/shared/src/components/auth/AuthenticationBanner.tsx
+++ packages/shared/src/components/auth/AuthenticationBanner.tsx
@@ -14,12 +14,25 @@ import {
   cloudinaryAuthBannerBackground1440w as laptopBg,
   cloudinaryAuthBannerBackground1920w as desktopBg,
 } from '../../lib/image';
+import { checkIsBrowser, checkIsExtension, UserAgent } from '../../lib/func';
+import { featurePostBannerExtensionPrompt } from '../../lib/featureManagement';
+import { useConditionalFeature } from '../../hooks';
+import { GetExtensionButton } from '../buttons/GetExtensionButton';
 
 const Section = classed('div', 'flex flex-col');
 
 export function AuthenticationBanner({
   children,
 }: PropsWithChildren): ReactElement {
+  const isCompatibleBrowser =
+    (checkIsBrowser(UserAgent.Chrome) || checkIsBrowser(UserAgent.Edge)) &&
+    !checkIsExtension();
+
+  const { value: showExtensionCTA } = useConditionalFeature({
+    feature: featurePostBannerExtensionPrompt,
+    shouldEvaluate: isCompatibleBrowser,
+  });
+
   const { showLogin } = useAuthContext();
 
   return (
@@ -45,31 +58,40 @@ export function AuthenticationBanner({
           />
         )}
       </Section>
-      <Section className="w-[23.25rem]">
-        <AuthOptions
-          ignoreMessages
-          formRef={null}
-          trigger={AuthTriggers.Onboarding}
-          simplified
-          defaultDisplay={AuthDisplay.OnboardingSignup}
-          forceDefaultDisplay
-          className={{
-            onboardingSignup: classNames('!gap-4'),
-          }}
-          onAuthStateUpdate={(props) =>
-            showLogin({
-              trigger: AuthTriggers.Onboarding,
-              options: {
-                formValues: {
-                  email: props?.email,
+      <Section
+        className={classNames(
+          'w-[23.25rem]',
+          showExtensionCTA && 'my-auto flex flex-col gap-4',
+        )}
+      >
+        {showExtensionCTA ? (
+          <GetExtensionButton />
+        ) : (
+          <AuthOptions
+            ignoreMessages
+            formRef={null}
+            trigger={AuthTriggers.Onboarding}
+            simplified
+            defaultDisplay={AuthDisplay.OnboardingSignup}
+            forceDefaultDisplay
+            className={{
+              onboardingSignup: classNames('!gap-4'),
+            }}
+            onAuthStateUpdate={(props) =>
+              showLogin({
+                trigger: AuthTriggers.Onboarding,
+                options: {
+                  formValues: {
+                    email: props?.email,
+                  },
                 },
-              },
-            })
-          }
-          onboardingSignupButton={{
-            variant: ButtonVariant.Primary,
-          }}
-        />
+              })
+            }
+            onboardingSignupButton={{
+              variant: ButtonVariant.Primary,
+            }}
+          />
+        )}
         <MemberAlready
           onLogin={() =>
             showLogin({
@@ -78,8 +100,12 @@ export function AuthenticationBanner({
             })
           }
           className={{
-            container: 'justify-center text-text-secondary typo-callout',
-            login: 'font-bold',
+            container: showExtensionCTA
+              ? '!text-lg leading-5 text-text-tertiary'
+              : 'justify-center text-text-secondary typo-callout',
+            login: showExtensionCTA
+              ? '!text-lg font-bold !text-text-link'
+              : 'font-bold',
           }}
         />
       </Section>
