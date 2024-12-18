18bc5df66e98bc8bb7caf625ad94cde4dfc8d887
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-12-19 00:04:12 +0800

    fix: missing condition

--- packages/shared/src/components/cards/share/ShareList.tsx
+++ packages/shared/src/components/cards/share/ShareList.tsx
@@ -153,7 +153,7 @@ export const ShareList = forwardRef(function ShareList(
           }}
         />
       </CardContent>
-      {actionButtons}
+      {!feedActionSpacing && actionButtons}
       {children}
     </FeedItemContainer>
   );
