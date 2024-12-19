c00d8a97065614ace4a9a771e87792fc4ad643b8
Luca Pagliaro <luca@sentiero.digital>
2024-12-19 13:50:14 +0100

    feat: enable default for test on preview

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -49,11 +49,11 @@ export const featureUpvoteCounter = new Feature(
 
 export const featureOnboardingExtension = new Feature(
   'onboarding_extension',
-  false,
+  true,
 );
 export const featureOnboardingDesktopPWA = new Feature(
   'onboarding_desktop_pwa',
-  false,
+  true,
 );
 export const featureOnboardingPWA = new Feature('onboarding_pwa', false);
 export const featureOnboardingAndroid = new Feature(
