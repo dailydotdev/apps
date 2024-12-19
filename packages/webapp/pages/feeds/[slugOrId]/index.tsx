c479fbc2c6d3f00eaf12fe8ed824448786ac04e9
capJavert <dev@kickass.website>
2024-12-19 17:33:24 +0100

    feat: block custom feed view from date

--- packages/webapp/pages/feeds/[slugOrId]/index.tsx
+++ packages/webapp/pages/feeds/[slugOrId]/index.tsx
@@ -1,8 +1,12 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement, useEffect, useMemo } from 'react';
 import { NextSeoProps } from 'next-seo/lib/types';
 import { NextSeo } from 'next-seo';
-import { useFeeds } from '@dailydotdev/shared/src/hooks';
+import { useFeeds, usePlusSubscription } from '@dailydotdev/shared/src/hooks';
 import { useRouter } from 'next/router';
+import {
+  customFeedsPlusDate,
+  webappUrl,
+} from '@dailydotdev/shared/src/lib/constants';
 import { defaultOpenGraph, defaultSeo } from '../../../next-seo';
 import {
   getMainFeedLayout,
@@ -13,11 +17,18 @@ import { getTemplatedTitle } from '../../../components/layouts/utils';
 const FeedPage = (): ReactElement => {
   const router = useRouter();
   const { feeds } = useFeeds();
+  const { isPlus } = usePlusSubscription();
 
   const feed = useMemo(() => {
     return feeds?.edges.find(({ node }) => node.id === router.query.slugOrId)
       ?.node;
   }, [feeds, router.query.slugOrId]);
+
+  useEffect(() => {
+    if (!isPlus && new Date(feed?.createdAt) > customFeedsPlusDate) {
+      router.replace(webappUrl);
+    }
+  }, [router, isPlus, feed.createdAt]);
 
   const seo: NextSeoProps = {
     title: getTemplatedTitle(
