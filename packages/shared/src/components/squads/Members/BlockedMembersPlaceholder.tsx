7c727d256cdadd64f8abb19461ec38f03d0de5ba
Chris Bongers <chrisbongers@gmail.com>
2024-12-24 15:27:24 +0200

    fix: padding on blocked members not found (#4026)

--- packages/shared/src/components/squads/Members/BlockedMembersPlaceholder.tsx
+++ packages/shared/src/components/squads/Members/BlockedMembersPlaceholder.tsx
@@ -5,7 +5,7 @@ import { IconSize } from '../../Icon';
 
 export function BlockedMembersPlaceholder(): ReactElement {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
+    <div className="flex flex-1 flex-col items-center justify-center py-10">
       <BlockIcon secondary size={IconSize.XXXLarge} />
       <p className="text-text-secondary typo-body">No blocked members found</p>
     </div>
