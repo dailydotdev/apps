b3fae34933484b7dbaf4ada257a97cb1d64a49fd
Chris Bongers <chrisbongers@gmail.com>
2024-12-24 11:46:42 +0200

    fix: revert as only dev

--- packages/shared/tailwind.config.ts
+++ packages/shared/tailwind.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { Config } from 'tailwindcss';
 import colors from './tailwind/colors';
 import boxShadow from './tailwind/boxShadow';
