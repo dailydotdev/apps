508f3426720725d9ac46327cd33a57907bdb8454
Chris Bongers <chrisbongers@gmail.com>
2024-12-30 13:09:05 +0200

    fix: tablet sizing

--- packages/shared/src/components/CustomFeedEmptyScreen.tsx
+++ packages/shared/src/components/CustomFeedEmptyScreen.tsx
@@ -33,7 +33,7 @@ export const CustomFeedEmptyScreen = (): ReactElement => {
   );
   return (
     <div className="flex w-full flex-col">
-      <div className="mr-auto flex gap-3">
+      <div className="mr-auto mt-0 flex gap-3 tablet:mr-0 tablet:mt-2">
         <SearchControlHeader
           algoState={[selectedAlgo, setSelectedAlgo]}
           feedName={SharedFeedPage.Custom}
