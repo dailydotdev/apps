9d1bcabdb89bd7822ad741f89200bfa0d1cd4a68
Amar Trebinjac <amartrebinjac@gmail.com>
2025-01-06 22:30:13 +0100

    update loginbutton test

--- packages/shared/src/components/LoginButton.spec.tsx
+++ packages/shared/src/components/LoginButton.spec.tsx
@@ -3,15 +3,29 @@ import React from 'react';
 import type { RenderResult } from '@testing-library/react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { QueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import { mocked } from 'ts-jest/utils';
 import LoginButton from './LoginButton';
 import { TestBootProvider } from '../../__tests__/helpers/boot';
 import type { LoggedUser } from '../lib/user';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+
 describe('LoginButton', () => {
   const showLogin = jest.fn();
+  const routerPush = jest.fn();
 
   beforeEach(() => {
     showLogin.mockReset();
+    routerPush.mockReset();
+    mocked(useRouter).mockImplementation(() => ({
+      push: routerPush,
+      pathname: '/',
+      query: {},
+      replace: jest.fn(),
+    }));
   });
 
   const renderLayout = (user: LoggedUser = null): RenderResult => {
@@ -24,12 +38,12 @@ describe('LoginButton', () => {
     );
   };
 
-  it('should show login when clicking on the button', async () => {
+  it('should redirect to onboarding when clicking the button', async () => {
     renderLayout();
     const el = await screen.findByText('Log in');
 
     fireEvent.click(el);
-
-    expect(showLogin).toBeCalledTimes(1);
+    const decodedURL = decodeURIComponent(routerPush.mock.calls[0][0]);
+    expect(decodedURL).toBe('/onboarding?authTrigger=main+button&afterAuth=/');
   });
 });
