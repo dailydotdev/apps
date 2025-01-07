614578d7c36dbb0214670aabbd4221f33b80eafe
Luca Pagliaro <luca@sentiero.digital>
2025-01-07 16:08:07 +0100

    fix: revert default value

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -53,7 +53,7 @@ export const featureOnboardingExtension = new Feature(
 );
 export const featureOnboardingDesktopPWA = new Feature(
   'onboarding_desktop_pwa',
-  true,
+  false,
 );
 export const featureOnboardingPWA = new Feature('onboarding_pwa', false);
 export const featureOnboardingAndroid = new Feature(
