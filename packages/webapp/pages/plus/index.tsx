43a32d05fc555ffa9dbbf4f2d7d3d76fb1b4611e
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-01-27 23:27:05 +0800

    fix: redirect when available

--- packages/webapp/pages/plus/index.tsx
+++ packages/webapp/pages/plus/index.tsx
@@ -5,6 +5,10 @@ import dynamic from 'next/dynamic';
 import { useViewSize, ViewSize } from '@dailydotdev/shared/src/hooks';
 import { useRouter } from 'next/router';
 import type { NextSeoProps } from 'next-seo/lib/types';
+import { getUserShortInfo } from '@dailydotdev/shared/src/graphql/users';
+import type { GetStaticPropsContext, GetStaticPropsResult } from 'next';
+import type { ParsedUrlQuery } from 'querystring';
+import type { EmptyObjectLiteral } from '@dailydotdev/shared/src/lib/kratos';
 import { getPlusLayout } from '../../components/layouts/PlusLayout/PlusLayout';
 import { getTemplatedTitle } from '../../components/layouts/utils';
 import { defaultOpenGraph } from '../../next-seo';
@@ -44,5 +48,36 @@ const PlusPage = (): ReactElement => {
 
 PlusPage.getLayout = getPlusLayout;
 PlusPage.layoutProps = { seo };
+
+interface PageProps extends ParsedUrlQuery {
+  giftToUserId: string;
+}
+
+export const getStaticProps = async ({
+  params,
+}: GetStaticPropsContext<PageProps>): Promise<
+  GetStaticPropsResult<EmptyObjectLiteral>
+> => {
+  const validateUserId = (value: string) => !!value && value !== '404';
+  const giftToUserId = params?.giftToUserId;
+
+  if (!validateUserId(giftToUserId)) {
+    return { props: {} };
+  }
+
+  try {
+    const giftToUser = await getUserShortInfo(giftToUserId);
+
+    if (giftToUser.isPlus) {
+      return { props: {} };
+    }
+
+    return {
+      redirect: { destination: `/plus/gift/${giftToUserId}`, permanent: false },
+    };
+  } catch (err) {
+    return { props: {} };
+  }
+};
 
 export default PlusPage;
