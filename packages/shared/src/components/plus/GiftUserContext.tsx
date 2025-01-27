fad98897c30207fdfb83dcf200da9c9dc7d0bf44
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-01-27 22:43:14 +0800

    fix: default value

--- packages/shared/src/components/plus/GiftUserContext.tsx
+++ packages/shared/src/components/plus/GiftUserContext.tsx
@@ -3,9 +3,8 @@ import type { UserShortProfile } from '../../lib/user';
 
 export interface GiftUserContextData {
   giftToUser?: UserShortProfile;
-  onUserChange: (user: UserShortProfile) => void;
 }
-export const GiftUserContext = createContext<GiftUserContextData | null>(null);
+export const GiftUserContext = createContext<GiftUserContextData | null>({});
 
 export const useGiftUserContext = (): GiftUserContextData =>
   useContext(GiftUserContext);
