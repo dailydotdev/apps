ac6eb8aa8f4976e215ddf58cf1085be5e696a650
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-01-30 20:55:03 +0800

    fix: id for one time gifting

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -34,7 +34,7 @@ const feature = {
   onboardingChecklist: new Feature('onboarding_checklist', true),
   showCodeSnippets: new Feature('show_code_snippets', false),
   pricingIds: new Feature('pricing_ids', {
-    pri_01jjbwd5j7k0nm45k8e07yfmwr: PlusPriceType.Yearly, // TODO: this value is from test env and needs to be updated
+    pri_01jjvm32ygwb1ja7w52e668fr2: PlusPriceType.Yearly,
     pri_01jbsccbdbcwyhdy8hy3c2etyn: PlusPriceType.Monthly,
     pri_01jbscda57910yvwjtyapnrrzc: PlusPriceType.Yearly,
   }),
