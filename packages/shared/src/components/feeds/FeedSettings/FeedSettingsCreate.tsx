6df1559823c7cb1719a86ff4cf9edb23356ca1f8
Ante Barić <ante@kickass.website>
2025-02-03 13:25:42 +0100

    feat: custom feed improvements to create flow (#4128)

--- packages/shared/src/components/feeds/FeedSettings/FeedSettingsCreate.tsx
+++ packages/shared/src/components/feeds/FeedSettings/FeedSettingsCreate.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from 'react';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -7,13 +7,8 @@ import type { ClientError } from 'graphql-request';
 import { Modal } from '../../modals/common/Modal';
 
 import type { CreateFeedProps } from '../../../hooks';
-import {
-  useActions,
-  useFeeds,
-  useProgressAnimation,
-  useToastNotification,
-} from '../../../hooks';
-import { emojiOptions, webappUrl } from '../../../lib/constants';
+import { useActions, useFeeds, useToastNotification } from '../../../hooks';
+import { emojiOptions } from '../../../lib/constants';
 import { Button } from '../../buttons/Button';
 import { ButtonSize, ButtonVariant } from '../../buttons/common';
 import { TextField } from '../../fields/TextField';
@@ -34,6 +29,8 @@ import { ModalClose } from '../../modals/common/ModalClose';
 import { ActionType } from '../../../graphql/actions';
 import { useContentPreference } from '../../../hooks/contentPreference/useContentPreference';
 import type { ContentPreferenceType } from '../../../graphql/contentPreference';
+import { Loader } from '../../Loader';
+import { FeedSettingsEdit } from './FeedSettingsEdit';
 
 export const FeedSettingsCreate = (): ReactElement => {
   const [newFeedId] = useState(() => Date.now().toString());
@@ -46,14 +43,33 @@ export const FeedSettingsCreate = (): ReactElement => {
   const [data, setData] = useState<CreateFeedProps>(() => ({
     icon: '',
   }));
-  const { createFeed } = useFeeds();
+  const { createFeed, feeds } = useFeeds();
   const { follow } = useContentPreference({ showToastOnSuccess: false });
 
-  const { onFinished, delayedRedirect, isAnimating } = useProgressAnimation({
-    animationMs: 1000,
-  });
+  const entityId = router?.query?.entityId as string;
+  const entityType = router?.query?.entityType as ContentPreferenceType;
+  const shouldUseQuickFeedCreate = !!(entityId && entityType);
 
-  const { mutate: onSubmit, isPending: isSubmitPending } = useMutation({
+  const getNewFeedName = useCallback(() => {
+    const feedNames = [
+      'My new feed',
+      'Another new feed',
+      'Yet another new feed',
+    ];
+
+    const existingFeedsCount = feeds?.edges?.length || 0;
+    const newFeedName = feedNames.find((name) => {
+      return !feeds?.edges?.some((item) => item.node.flags?.name === name);
+    });
+
+    return newFeedName || `New feed ${existingFeedsCount + 1}`;
+  }, [feeds]);
+
+  const {
+    data: createdFeedData,
+    mutate: onSubmit,
+    isPending: isSubmitPending,
+  } = useMutation({
     mutationFn: createFeed,
     onSuccess: async (newFeed) => {
       logEvent({
@@ -65,9 +81,6 @@ export const FeedSettingsCreate = (): ReactElement => {
         queryKey: getFeedSettingsQueryKey(user, newFeedId),
       });
 
-      const entityId = router?.query?.entityId as string;
-      const entityType = router?.query?.entityType as ContentPreferenceType;
-
       if (entityId && entityType) {
         await follow({
           id: entityId,
@@ -78,12 +91,7 @@ export const FeedSettingsCreate = (): ReactElement => {
 
         // go back to entity page that was followed
         router.back();
-
-        return;
       }
-
-      onFinished();
-      delayedRedirect(`${webappUrl}feeds/${newFeed.id}?created=1`);
     },
     onError: (error) => {
       const clientErrors = (error as ClientError)?.response?.errors || [];
@@ -120,6 +128,19 @@ export const FeedSettingsCreate = (): ReactElement => {
     completeAction(ActionType.CustomFeed);
   }, [completeAction, user]);
 
+  const shouldCreateNewFeed =
+    !!user && !isSubmitPending && !createdFeedData && !shouldUseQuickFeedCreate;
+
+  useEffect(() => {
+    if (!shouldCreateNewFeed) {
+      return;
+    }
+
+    onSubmit({
+      name: getNewFeedName(),
+    });
+  }, [getNewFeedName, onSubmit, shouldCreateNewFeed]);
+
   const logStartEventRef = useRef(false);
 
   useEffect(() => {
@@ -135,131 +156,151 @@ export const FeedSettingsCreate = (): ReactElement => {
   }, [logEvent]);
 
   const onRequestClose = () => {
-    router.replace(webappUrl);
+    router.back();
   };
 
-  return (
-    <Modal
-      isOpen
-      kind={Modal.Kind.FixedCenter}
-      size={Modal.Size.Small}
-      onRequestClose={onRequestClose}
-    >
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          onSubmit(data);
-        }}
+  if (shouldUseQuickFeedCreate) {
+    return (
+      <Modal
+        isOpen
+        kind={Modal.Kind.FixedCenter}
+        size={Modal.Size.Small}
+        onRequestClose={onRequestClose}
       >
-        <ModalClose className="top-2" onClick={onRequestClose} type="button" />
-        <Modal.Header title="" showCloseButton={false}>
-          <div className="hidden items-center gap-4 tablet:flex">
-            <Typography
-              className="tablet:typo-title3"
-              type={TypographyType.Body}
-              bold
-            >
-              New custom feed
-            </Typography>
-            <PlusUser />
-          </div>
-          <div className="flex w-full items-center justify-between gap-2 tablet:hidden">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            onSubmit(data);
+          }}
+        >
+          <ModalClose
+            className="top-2"
+            onClick={onRequestClose}
+            type="button"
+          />
+          <Modal.Header title="" showCloseButton={false}>
+            <div className="hidden items-center gap-4 tablet:flex">
+              <Typography
+                className="tablet:typo-title3"
+                type={TypographyType.Body}
+                bold
+              >
+                New custom feed
+              </Typography>
+              <PlusUser />
+            </div>
+            <div className="flex w-full items-center justify-between gap-2 tablet:hidden">
+              <Button
+                type="button"
+                size={ButtonSize.Small}
+                variant={ButtonVariant.Tertiary}
+                onClick={onRequestClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                size={ButtonSize.Medium}
+                variant={ButtonVariant.Primary}
+                loading={isSubmitPending}
+                disabled={!data.name}
+              >
+                Create feed
+              </Button>
+            </div>
+          </Modal.Header>
+          <Modal.Body className="flex flex-col gap-4">
+            <div className="flex items-center gap-4 tablet:hidden">
+              <Typography
+                className="tablet:typo-title3"
+                type={TypographyType.Body}
+                bold
+              >
+                New custom feed
+              </Typography>
+              <PlusUser />
+            </div>
+            <TextField
+              className={{
+                container: 'w-full',
+              }}
+              name="name"
+              type="text"
+              inputId="feedName"
+              label="Give your feed a name"
+              required
+              maxLength={50}
+              valueChanged={(value) =>
+                setData((current) => ({ ...current, name: value }))
+              }
+            />
+            <div className="flex flex-col gap-4">
+              <Typography type={TypographyType.Body}>
+                <Typography className="inline-flex" bold>
+                  Choose an icon
+                </Typography>{' '}
+                (optional)
+              </Typography>
+              <ul className="flex flex-wrap gap-4" role="radiogroup">
+                {emojiOptions.map((emoji) => (
+                  <Button
+                    type="button"
+                    key={emoji}
+                    onClick={() =>
+                      setData((current) => ({ ...current, icon: emoji }))
+                    }
+                    className={classNames(
+                      '!size-12',
+                      data.icon === emoji && 'border-surface-focus',
+                    )}
+                    variant={ButtonVariant.Float}
+                    aria-checked={
+                      data.icon === emoji || (!emoji && data.icon === '')
+                    }
+                    role="radio"
+                  >
+                    {!emoji ? (
+                      <HashtagIcon size={IconSize.Large} />
+                    ) : (
+                      <Typography
+                        tag={TypographyTag.Span}
+                        type={TypographyType.Title1}
+                      >
+                        {emoji}
+                      </Typography>
+                    )}
+                  </Button>
+                ))}
+              </ul>
+            </div>
             <Button
-              type="button"
-              size={ButtonSize.Small}
-              variant={ButtonVariant.Tertiary}
-              onClick={onRequestClose}
-            >
-              Cancel
-            </Button>
-            <Button
+              className="hidden tablet:flex"
               type="submit"
               size={ButtonSize.Medium}
               variant={ButtonVariant.Primary}
-              loading={isSubmitPending || isAnimating}
+              loading={isSubmitPending}
               disabled={!data.name}
             >
               Create feed
             </Button>
-          </div>
-        </Modal.Header>
-        <Modal.Body className="flex flex-col gap-4">
-          <div className="flex items-center gap-4 tablet:hidden">
-            <Typography
-              className="tablet:typo-title3"
-              type={TypographyType.Body}
-              bold
-            >
-              New custom feed
-            </Typography>
-            <PlusUser />
-          </div>
-          <TextField
-            className={{
-              container: 'w-full',
-            }}
-            name="name"
-            type="text"
-            inputId="feedName"
-            label="Give your feed a name"
-            required
-            maxLength={50}
-            valueChanged={(value) =>
-              setData((current) => ({ ...current, name: value }))
-            }
-          />
-          <div className="flex flex-col gap-4">
-            <Typography type={TypographyType.Body}>
-              <Typography className="inline-flex" bold>
-                Choose an icon
-              </Typography>{' '}
-              (optional)
-            </Typography>
-            <ul className="flex flex-wrap gap-4" role="radiogroup">
-              {emojiOptions.map((emoji) => (
-                <Button
-                  type="button"
-                  key={emoji}
-                  onClick={() =>
-                    setData((current) => ({ ...current, icon: emoji }))
-                  }
-                  className={classNames(
-                    '!size-12',
-                    data.icon === emoji && 'border-surface-focus',
-                  )}
-                  variant={ButtonVariant.Float}
-                  aria-checked={
-                    data.icon === emoji || (!emoji && data.icon === '')
-                  }
-                  role="radio"
-                >
-                  {!emoji ? (
-                    <HashtagIcon size={IconSize.Large} />
-                  ) : (
-                    <Typography
-                      tag={TypographyTag.Span}
-                      type={TypographyType.Title1}
-                    >
-                      {emoji}
-                    </Typography>
-                  )}
-                </Button>
-              ))}
-            </ul>
-          </div>
-          <Button
-            className="hidden tablet:flex"
-            type="submit"
-            size={ButtonSize.Medium}
-            variant={ButtonVariant.Primary}
-            loading={isSubmitPending || isAnimating}
-            disabled={!data.name}
-          >
-            Create feed
-          </Button>
+          </Modal.Body>
+        </form>
+      </Modal>
+    );
+  }
+
+  const createdFeedId = createdFeedData?.id;
+
+  if (!newFeedId) {
+    return (
+      <Modal isOpen kind={Modal.Kind.FixedCenter} size={Modal.Size.Small}>
+        <Modal.Body className="items-center justify-center">
+          <Loader />
         </Modal.Body>
-      </form>
-    </Modal>
-  );
+      </Modal>
+    );
+  }
+
+  return <FeedSettingsEdit isNewFeed feedSlugOrId={createdFeedId} />;
 };
