f65e4c7b0961509311b366863222be15b23bb3e4
Chris Bongers <chrisbongers@gmail.com>
2025-02-05 09:26:04 +0200

    fix: review feedback

--- packages/shared/src/components/feeds/FeedSettings/sections/FeedSettingsAISection.tsx
+++ packages/shared/src/components/feeds/FeedSettings/sections/FeedSettingsAISection.tsx
@@ -41,6 +41,39 @@ export const FeedSettingsAISection = (): ReactElement => {
     <>
       <section className="flex flex-col gap-4" aria-busy={isLoading}>
         <div className="flex flex-col">
+          <Typography
+            tag={TypographyTag.H3}
+            color={TypographyColor.Primary}
+            type={TypographyType.Body}
+            bold
+            className="mb-1"
+          >
+            Preferred language
+          </Typography>
+          <Typography
+            color={TypographyColor.Tertiary}
+            type={TypographyType.Callout}
+          >
+            Choose your preferred language for the post titles on the feed
+          </Typography>
+        </div>
+        <LanguageDropdown
+          className={{ container: 'w-full max-w-60' }}
+          name="language"
+          defaultValue={user.language}
+          onChange={(value) => {
+            onLanguageChange(value);
+
+            displayToast(
+              labels.feed.settings.globalPreferenceNotice.contentLanguage,
+            );
+          }}
+          icon={null}
+        />
+      </section>
+      <Divider className="bg-border-subtlest-tertiary" />
+      <section className="flex flex-col gap-4" aria-busy={isLoading}>
+        <div className="flex flex-col">
           <div className="mb-1 flex items-center gap-2">
             <Typography
               tag={TypographyTag.H3}
@@ -126,39 +159,6 @@ export const FeedSettingsAISection = (): ReactElement => {
             Upgrade to Plus
           </Button>
         )}
-      </section>
-      <Divider className="bg-border-subtlest-tertiary" />
-      <section className="flex flex-col gap-4" aria-busy={isLoading}>
-        <div className="flex flex-col">
-          <Typography
-            tag={TypographyTag.H3}
-            color={TypographyColor.Primary}
-            type={TypographyType.Body}
-            bold
-            className="mb-1"
-          >
-            Preferred language
-          </Typography>
-          <Typography
-            color={TypographyColor.Tertiary}
-            type={TypographyType.Callout}
-          >
-            Choose your preferred language for the post titles on the feed
-          </Typography>
-        </div>
-        <LanguageDropdown
-          className={{ container: 'w-full max-w-60' }}
-          name="language"
-          defaultValue={user.language}
-          onChange={(value) => {
-            onLanguageChange(value);
-
-            displayToast(
-              labels.feed.settings.globalPreferenceNotice.contentLanguage,
-            );
-          }}
-          icon={null}
-        />
       </section>
       <Divider className="bg-border-subtlest-tertiary" />
       <SmartPrompts />
