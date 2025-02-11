2a8790e24872c8b7de910878af48d2962f9fd67d
Amar Trebinjac <amartrebinjac@gmail.com>
2025-02-11 19:30:50 +0100

    feat: add paymentcontext inside modal

--- packages/shared/src/components/plus/PlusModal.tsx
+++ packages/shared/src/components/plus/PlusModal.tsx
@@ -6,6 +6,7 @@ import { checkIsExtension } from '../../lib/func';
 import { useViewSize, ViewSize } from '../../hooks';
 import { ModalClose } from '../modals/common/ModalClose';
 import { useLazyModal } from '../../hooks/useLazyModal';
+import { PaymentContextProvider } from '../../contexts/PaymentContext';
 
 const PlusMobileDrawer = dynamic(
   () => import(/* webpackChunkName: "plusMobileDrawer" */ './PlusMobileDrawer'),
@@ -25,33 +26,36 @@ const PlusModal = (): ReactElement => {
   const { closeModal } = useLazyModal();
   const isExtension = checkIsExtension();
   const isLaptop = useViewSize(ViewSize.Laptop);
+
   if (!isLaptop) {
     return <PlusMobileDrawer />;
   }
 
   return (
-    <Modal
-      className="!max-h-fit !w-fit overflow-hidden !bg-background-default"
-      isOpen
-    >
-      <Modal.Body className="!p-0">
-        <ModalClose onClick={closeModal} top="4" />
-        {isExtension ? (
-          <PlusExtension />
-        ) : (
-          <PlusDesktop
-            shouldShowPlusHeader
-            className="!items-start !gap-0"
-            plusInfoContainerClassName="pr-10 !pt-8"
-            showPlusList={false}
-            checkoutClassName={{
-              container:
-                'border-top-0 border-r-0 border-b-0 border-t-0 h-full pl-10 !pt-8 ',
-            }}
-          />
-        )}
-      </Modal.Body>
-    </Modal>
+    <PaymentContextProvider>
+      <Modal
+        className="!max-h-fit !w-fit overflow-hidden !bg-background-default"
+        isOpen
+      >
+        <Modal.Body className="!p-0">
+          <ModalClose onClick={closeModal} top="4" />
+          {isExtension ? (
+            <PlusExtension />
+          ) : (
+            <PlusDesktop
+              shouldShowPlusHeader
+              className="!items-start !gap-0"
+              plusInfoContainerClassName="pr-10 !pt-8"
+              showPlusList={false}
+              checkoutClassName={{
+                container:
+                  'border-top-0 border-r-0 border-b-0 border-t-0 h-full pl-10 !pt-8 ',
+              }}
+            />
+          )}
+        </Modal.Body>
+      </Modal>
+    </PaymentContextProvider>
   );
 };
 
