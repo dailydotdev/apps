fd43f4101262e7b836f73e42b4084587d5713a9c
Luca Pagliaro <pagliaroluca@gmail.com>
2025-02-11 15:01:28 +0100

    fix: lint missing deps

--- packages/shared/src/components/ProfileMenu.tsx
+++ packages/shared/src/components/ProfileMenu.tsx
@@ -173,6 +173,7 @@ export default function ProfileMenu({
     openModal,
     setShowDnd,
     user.permalink,
+    plusCta,
   ]);
 
   if (!user) {
--- packages/shared/src/components/profile/ProfileSettingsMenu.tsx
+++ packages/shared/src/components/profile/ProfileSettingsMenu.tsx
@@ -196,6 +196,7 @@ const useMenuItems = (): NavItemProps[] => {
     appExperiment,
     promptToInstall,
     androidPWAExperiment,
+    plusCta,
   ]);
 };
 
