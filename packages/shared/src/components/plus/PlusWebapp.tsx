25a0e3ac5e2561f93807441a748502a653272e20
Amar Trebinjac <amartrebinjac@gmail.com>
2025-02-12 23:03:33 +0100

    chore: cleanup webapp

--- packages/shared/src/components/plus/PlusWebapp.tsx
+++ packages/shared/src/components/plus/PlusWebapp.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement } from 'react';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect, useRef, useState } from 'react';
 import { usePaymentContext } from '../../contexts/PaymentContext';
 
 import { PlusInfo } from './PlusInfo';
@@ -14,38 +13,19 @@ const PlusWebapp = (): ReactElement => {
   const { flags } = marketingCta;
   const { title, description } = flags;
   const { openCheckout, paddle, productOptions } = usePaymentContext();
-  const {
-    query: { selectedPlan },
-  } = useRouter();
-  const initialPaymentOption = selectedPlan ? `${selectedPlan}` : null;
+
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const ref = useRef();
-
-  const toggleCheckoutOption = useCallback(
-    (priceId) => {
-      setSelectedOption(priceId);
-      openCheckout({ priceId });
-    },
-    [openCheckout],
-  );
 
   useEffect(() => {
     if (!ref?.current || !paddle) {
       return;
     }
-
-    const option = initialPaymentOption || productOptions?.[0]?.value;
-    if (option && !selectedOption) {
-      setSelectedOption(option);
-      openCheckout({ priceId: option });
+    if (!selectedOption) {
+      setSelectedOption(productOptions?.[0]?.value);
+      openCheckout({ priceId: productOptions?.[0]?.value });
     }
-  }, [
-    initialPaymentOption,
-    openCheckout,
-    paddle,
-    productOptions,
-    selectedOption,
-  ]);
+  }, [openCheckout, paddle, productOptions, selectedOption]);
 
   return (
     <div className="flex flex-1 items-start justify-center !gap-0">
@@ -53,7 +33,10 @@ const PlusWebapp = (): ReactElement => {
         <PlusInfo
           productOptions={productOptions}
           selectedOption={selectedOption}
-          onChange={toggleCheckoutOption}
+          onChange={({ priceId }) => {
+            setSelectedOption(priceId);
+            openCheckout({ priceId });
+          }}
           shouldShowPlusHeader
           showPlusList={false}
           showDailyDevLogo
