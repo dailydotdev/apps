fd3f84a66bb0602302086d1bb7933c5345e5e171
Chris Bongers <chrisbongers@gmail.com>
2025-02-13 17:00:37 +0200

    fix: PR Feedback

--- packages/shared/src/components/modals/plus/MobileSmartPromptsModal.tsx
+++ packages/shared/src/components/modals/plus/MobileSmartPromptsModal.tsx
@@ -25,31 +25,29 @@ export const MobileSmartPromptsModal = ({
 
   return (
     <Modal {...props} isDrawerOnMobile>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2">
+        <PromptButton
+          active={false}
+          size={ButtonSize.Medium}
+          variant={ButtonVariant.Option}
+          flags={{ icon: 'TLDR', color: ColorName.Cabbage }}
+          className="w-full"
+          onClick={(event) => onClick(event, PromptDisplay.TLDR)}
+        >
+          TLDR
+        </PromptButton>
+        {prompts?.map(({ id, label, flags }) => (
           <PromptButton
             active={false}
             size={ButtonSize.Medium}
             variant={ButtonVariant.Option}
-            flags={{ icon: 'TLDR', color: ColorName.Cabbage }}
-            className="w-full"
-            onClick={(event) => onClick(event, PromptDisplay.TLDR)}
+            key={id}
+            flags={flags}
+            onClick={(event) => onClick(event, id)}
           >
-            TLDR
+            {label}
           </PromptButton>
-          {prompts?.map(({ id, label, flags }) => (
-            <PromptButton
-              active={false}
-              size={ButtonSize.Medium}
-              variant={ButtonVariant.Option}
-              key={id}
-              flags={flags}
-              onClick={(event) => onClick(event, id)}
-            >
-              {label}
-            </PromptButton>
-          ))}
-        </div>
+        ))}
       </div>
     </Modal>
   );
