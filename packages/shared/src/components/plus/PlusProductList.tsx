75b904de0083b4bb98ea91825fc92d58592e8be8
Amar Trebinjac <amartrebinjac@gmail.com>
2025-02-14 12:25:33 +0100

    fix: plusProductList should be prefiltered

--- packages/shared/src/components/plus/PlusProductList.tsx
+++ packages/shared/src/components/plus/PlusProductList.tsx
@@ -2,7 +2,6 @@ import type { ReactElement } from 'react';
 import React from 'react';
 import classNames from 'classnames';
 import type { ProductOption } from '../../contexts/PaymentContext';
-import { usePaymentContext } from '../../contexts/PaymentContext';
 import {
   Typography,
   TypographyColor,
@@ -28,7 +27,6 @@ const PlusProductList = ({
   className,
 }: PlusProductListProps): ReactElement => {
   const { logSubscriptionEvent } = usePlusSubscription();
-  const { earlyAdopterPlanId } = usePaymentContext();
 
   return (
     <div
@@ -37,84 +35,73 @@ const PlusProductList = ({
         className,
       )}
     >
-      {productList.map((option) => {
-        const { label, value, price, currencyCode, extraLabel } = option;
-        const checked = selected === value;
-        const isEarlyAccess = value === earlyAdopterPlanId;
-        return (
-          <RadioItem
-            key={label}
-            name={label}
-            id={`${label}-${value}`}
-            value={value}
-            checked={checked}
-            onChange={() => {
-              onChange(value);
-              logSubscriptionEvent({
-                event_name: LogEvent.SelectBillingCycle,
-                target_id: label.toLowerCase(),
-              });
-            }}
-            className={{
-              content: classNames(
-                'min-h-12 w-full rounded-10 !p-2',
-                checked
-                  ? '-m-px border border-border-subtlest-primary bg-surface-float'
-                  : undefined,
-              ),
-            }}
-          >
-            <div className="flex flex-1 flex-wrap items-center gap-x-3 gap-y-1">
-              <Typography
-                tag={TypographyTag.Span}
-                type={TypographyType.Callout}
-                color={TypographyColor.Primary}
-              >
-                {option.label}
-              </Typography>
+      {productList.map(({ label, value, price, currencyCode, extraLabel }) => (
+        <RadioItem
+          key={label}
+          name={label}
+          id={`${label}-${value}`}
+          value={value}
+          checked={selected === value}
+          onChange={() => {
+            onChange(value);
+            logSubscriptionEvent({
+              event_name: LogEvent.SelectBillingCycle,
+              target_id: label.toLowerCase(),
+            });
+          }}
+          className={{
+            content: classNames(
+              'min-h-12 w-full rounded-10 !p-2',
+              selected === value
+                ? '-m-px border border-border-subtlest-primary bg-surface-float'
+                : undefined,
+            ),
+          }}
+        >
+          <div className="flex flex-1 flex-wrap items-center gap-x-3 gap-y-1">
+            <Typography
+              tag={TypographyTag.Span}
+              type={TypographyType.Callout}
+              color={TypographyColor.Primary}
+            >
+              {label}
+            </Typography>
 
-              {extraLabel && (
-                <Typography
-                  tag={TypographyTag.Span}
-                  type={TypographyType.Caption1}
-                  color={
-                    isEarlyAccess
-                      ? TypographyColor.StatusHelp
-                      : TypographyColor.StatusSuccess
-                  }
-                  className={classNames(
-                    'rounded-10 px-2 py-1',
-                    isEarlyAccess
-                      ? 'whitespace-nowrap bg-action-help-float'
-                      : 'bg-action-upvote-float',
-                  )}
-                  bold
-                >
-                  {extraLabel}
-                </Typography>
-              )}
-            </div>
-            <div className="ml-auto mr-1 flex items-center gap-1">
+            {extraLabel && (
               <Typography
                 tag={TypographyTag.Span}
-                type={TypographyType.Body}
-                color={TypographyColor.Primary}
+                type={TypographyType.Caption1}
+                color={TypographyColor.StatusSuccess}
+                className={classNames(
+                  'rounded-10 px-2 py-1',
+                  'bg-action-upvote-float',
+                )}
                 bold
               >
-                {price}
+                {extraLabel}
               </Typography>
-              <Typography
-                tag={TypographyTag.Span}
-                type={TypographyType.Body}
-                color={TypographyColor.Secondary}
-                className="font-normal"
-              >
-                {currencyCode}
-              </Typography>
-            </div>
-          </RadioItem>
-        );
-      })}
+            )}
+          </div>
+          <div className="ml-auto mr-1 flex items-center gap-1">
+            <Typography
+              tag={TypographyTag.Span}
+              type={TypographyType.Body}
+              color={TypographyColor.Primary}
+              bold
+            >
+              {price}
+            </Typography>
+            <Typography
+              tag={TypographyTag.Span}
+              type={TypographyType.Body}
+              color={TypographyColor.Secondary}
+              className="font-normal"
+            >
+              {currencyCode}
+            </Typography>
+          </div>
+        </RadioItem>
+      ))}
     </div>
   );
 };
