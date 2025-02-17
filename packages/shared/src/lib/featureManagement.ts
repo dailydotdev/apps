8241a88232389e9b7b249cd54d69b2523092caa0
Amar Trebinjac <amartrebinjac@gmail.com>
2025-02-17 13:33:16 +0100

    fix: update pricing changes to the ones Chris gave me

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -3,7 +3,6 @@ import {
   cloudinaryOnboardingFullBackgroundDesktop,
   cloudinaryOnboardingFullBackgroundMobile,
 } from './image';
-import { PlusPriceType } from './featureValues';
 import type { FeedAdTemplate } from './feed';
 import type { FeedSettingsKeys } from '../contexts/FeedContext';
 
@@ -34,9 +33,10 @@ const feature = {
   onboardingChecklist: new Feature('onboarding_checklist', true),
   showCodeSnippets: new Feature('show_code_snippets', false),
   pricingIds: new Feature('pricing_ids', {
-    pri_01jjbwd5j7k0nm45k8e07yfmwr: PlusPriceType.Yearly,
-    pri_01jcdp5ef4yhv00p43hr2knrdg: PlusPriceType.Monthly,
-    pri_01jcdn6enr5ap3ekkddc6fv6tq: PlusPriceType.Yearly,
+    pri_01jdkx6sakk5kb6p586nejnqam: 'monthly',
+    pri_01jbsccbdbcwyhdy8hy3c2etyn: 'monthly',
+    pri_01jjvm32ygwb1ja7w52e668fr2: 'yearly',
+    pri_01jbscda57910yvwjtyapnrrzc: 'yearly',
   }),
 };
 
