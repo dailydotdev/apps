7bee8e437aa4ab59e69bff8f322df48e12452868
Chris Bongers <chrisbongers@gmail.com>
2025-02-27 14:01:40 +0200

    fix: show award on profile (#4230)

---
+++ packages/shared/src/components/profile/Awards.tsx
@@ -0,0 +1,62 @@
+import type { ReactElement } from 'react';
+import React from 'react';
+import { ActivityContainer, ActivitySectionHeader } from './ActivitySection';
+import {
+  Typography,
+  TypographyColor,
+  TypographyType,
+} from '../typography/Typography';
+import { topReaderBadgeDocs } from '../../lib/constants';
+import { ClickableText } from '../buttons/ClickableText';
+import { Image } from '../image/Image';
+import { cloudinaryAwardUnicorn } from '../../lib/image';
+
+type AwardProps = {
+  image: string;
+  amount: number;
+};
+const Award = ({ image, amount }: AwardProps): ReactElement => {
+  return (
+    <div className="flex size-fit flex-col items-center justify-center rounded-14 bg-surface-float p-1">
+      <Image src={image} alt="Award unicorn" className="size-20" />
+      <Typography
+        type={TypographyType.Footnote}
+        color={TypographyColor.Secondary}
+      >
+        {amount}
+      </Typography>
+    </div>
+  );
+};
+
+export const Awards = (): ReactElement => {
+  return (
+    <ActivityContainer>
+      <ActivitySectionHeader title="Awards" className="!mb-2" />
+      <Typography
+        type={TypographyType.Callout}
+        color={TypographyColor.Quaternary}
+      >
+        Learn more about how Awards are earned in the{' '}
+        <ClickableText
+          className="!inline"
+          tag="a"
+          target="_blank"
+          href={topReaderBadgeDocs}
+        >
+          daily.dev docs
+        </ClickableText>
+      </Typography>
+      <div className="mt-6 flex flex-wrap gap-2">
+        <Award image={cloudinaryAwardUnicorn} amount={102} />
+        <Award image={cloudinaryAwardUnicorn} amount={43} />
+        <Award image={cloudinaryAwardUnicorn} amount={456} />
+        <Award image={cloudinaryAwardUnicorn} amount={5} />
+        <Award image={cloudinaryAwardUnicorn} amount={102} />
+        <Award image={cloudinaryAwardUnicorn} amount={43} />
+        <Award image={cloudinaryAwardUnicorn} amount={456} />
+        <Award image={cloudinaryAwardUnicorn} amount={5} />
+      </div>
+    </ActivityContainer>
+  );
+};
