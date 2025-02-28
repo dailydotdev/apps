8ba06513aa7392561d9c09755a39507f24503b5a
Amar Trebinjac <amartrebinjac@gmail.com>
2025-02-28 14:34:23 +0100

    fix: revert remaining sidebar code

--- packages/shared/src/components/sidebar/SidebarDesktop.tsx
+++ packages/shared/src/components/sidebar/SidebarDesktop.tsx
@@ -93,7 +93,6 @@ export const SidebarDesktop = ({
               sidebarExpanded ? 'mx-4' : 'mx-auto',
             )}
             compact={!sidebarExpanded}
-            sidebar
             size={sidebarExpanded ? ButtonSize.Small : ButtonSize.XSmall}
             showIcon
           />
