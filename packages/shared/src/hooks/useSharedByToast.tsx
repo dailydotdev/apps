e19acd07ccf5b6eaeec03ebc3d0cf3137d7e1be7
Amar Trebinjac <amartrebinjac@gmail.com>
2025-03-03 23:18:25 +0100

    feat: update to use timeout and ref

--- packages/shared/src/hooks/useSharedByToast.tsx
+++ packages/shared/src/hooks/useSharedByToast.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useAuthContext } from '../contexts/AuthContext';
@@ -16,64 +16,77 @@ import { Button } from '../components/buttons/Button';
 import { Typography, TypographyTag } from '../components/typography/Typography';
 
 const useSharedByToast = (): void => {
+  const hasShownToast = useRef(false);
   const { user: currentUser, isAuthReady } = useAuthContext();
   const { query } = useRouter();
   const userId = (query?.userid as string) || null;
-  const { data: contentPreference, isLoading } =
+  const isSameUser = !!userId && userId === currentUser?.id;
+  const { data: contentPreference, isPending } =
     useContentPreferenceStatusQuery({
       id: userId,
       entity: ContentPreferenceType.User,
-      queryOptions: {
-        enabled: !!userId && userId !== currentUser?.id,
-      },
     });
   const { data: user } = useUserShortByIdQuery({ id: userId });
   const { follow } = useContentPreference({ showToastOnSuccess: true });
   const { displayToast, dismissToast } = useToastNotification();
-  const isFollowing =
-    contentPreference?.status === ContentPreferenceStatus.Follow;
+  const isDataReady = isAuthReady || (currentUser && !isPending);
 
-  useLayoutEffect(() => {
-    if (!user || isLoading || !isAuthReady) {
+  useEffect(() => {
+    if (!user || isSameUser || !isDataReady || hasShownToast.current) {
       return;
     }
 
-    displayToast(
-      <div className="flex items-center gap-4 ">
-        <Link
-          onClick={() => dismissToast()}
-          className="flex items-center gap-2 "
-          href={`/${user.username}`}
-        >
-          <ProfilePicture user={user} size={ProfileImageSize.Medium} />
-          <span>
-            <Typography tag={TypographyTag.Span} bold>
-              {user.name}
-            </Typography>{' '}
-            <Typography tag={TypographyTag.Span}>shared this post</Typography>
-          </span>
-        </Link>
-        {!isFollowing && (
-          <Button
-            onClick={() => {
-              follow({
-                entity: ContentPreferenceType.User,
-                id: userId,
-                entityName: `@${user.username}`,
-              });
-            }}
-            className="bg-background-default text-text-primary"
-            size={ButtonSize.Small}
+    const showFollow =
+      !!currentUser &&
+      contentPreference?.status !== ContentPreferenceStatus.Follow;
+
+    setTimeout(() => {
+      hasShownToast.current = true;
+      displayToast(
+        <div className="flex items-center gap-4 ">
+          <Link
+            onClick={() => dismissToast()}
+            className="flex items-center gap-2 "
+            href={`/${user.username}`}
           >
-            Follow
-          </Button>
-        )}
-      </div>,
-    );
-    // Adding all the dependencies causes too many rerenders.
-    // Only using the ones that determine if the toast should be shown.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user, userId, isLoading, isAuthReady]);
+            <ProfilePicture user={user} size={ProfileImageSize.Medium} />
+            <span>
+              <Typography tag={TypographyTag.Span} bold>
+                {user.name}
+              </Typography>{' '}
+              <Typography tag={TypographyTag.Span}>shared this post</Typography>
+            </span>
+          </Link>
+          {showFollow && (
+            <Button
+              onClick={() => {
+                follow({
+                  entity: ContentPreferenceType.User,
+                  id: user?.id,
+                  entityName: `@${user.username}`,
+                });
+              }}
+              className="bg-background-default text-text-primary"
+              size={ButtonSize.Small}
+            >
+              Follow
+            </Button>
+          )}
+        </div>,
+      );
+      // Set a small timeout to ensure its shown after the page is loaded and won't be cleared by updates.
+    }, 1000);
+  }, [
+    user,
+    contentPreference?.status,
+    hasShownToast,
+    displayToast,
+    dismissToast,
+    follow,
+    isSameUser,
+    isDataReady,
+    currentUser,
+  ]);
 };
 
 export default useSharedByToast;
