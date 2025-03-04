ac288106f1efb26869141aca4a1de37ba39aedd0
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-03-04 15:28:11 +0100

    feat: register appAccountToken in app

--- packages/shared/src/lib/ios.ts
+++ packages/shared/src/lib/ios.ts
@@ -1,5 +1,28 @@
+import { isIOSNative } from './func';
+
 export enum WebKitMessageHandlers {
   IAPProductList = 'iap-product-list',
   IAPSubscriptionManage = 'iap-subscription-manage',
   IAPSubscriptionRequest = 'iap-subscription-request',
+  IAPSetAppAccountToken = 'iap-set-app-account-token',
 }
+
+export const messageHandlerExists = (handler: WebKitMessageHandlers): boolean =>
+  isIOSNative() && globalThis.webkit?.messageHandlers?.[handler];
+
+export const sendMessage = (
+  handler: WebKitMessageHandlers,
+  payload: unknown,
+): void => {
+  if (!isIOSNative()) {
+    console.error('sendMessage is only available on iOS');
+    return;
+  }
+
+  if (!messageHandlerExists(handler)) {
+    console.error(`Message handler ${handler} does not exist`);
+    return;
+  }
+
+  globalThis.webkit.messageHandlers[handler].postMessage(payload);
+};
