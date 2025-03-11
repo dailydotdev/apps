9142af21949af9c9766aced648afb95d01be5f4c
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2025-03-11 20:32:12 +0800

    chore: remove onboarding checklist (#4275)

--- packages/webapp/pages/onboarding.tsx
+++ packages/webapp/pages/onboarding.tsx
@@ -51,7 +51,6 @@ import {
 } from '@dailydotdev/shared/src/hooks';
 import { GenericLoader } from '@dailydotdev/shared/src/components/utilities/loaders';
 import { useSettingsContext } from '@dailydotdev/shared/src/contexts/SettingsContext';
-import { ChecklistViewState } from '@dailydotdev/shared/src/lib/checklist';
 import { getPathnameWithQuery } from '@dailydotdev/shared/src/lib';
 import { webappUrl } from '@dailydotdev/shared/src/lib/constants';
 import dynamic from 'next/dynamic';
@@ -296,10 +295,7 @@ export function OnboardPage(): ReactElement {
       target_id: TargetId.Onboarding,
     });
 
-    setSettings({
-      sidebarExpanded: true,
-      onboardingChecklistView: ChecklistViewState.Open,
-    });
+    setSettings({ sidebarExpanded: true });
 
     return onClickNext();
   };
