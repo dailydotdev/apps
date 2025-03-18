bb7d759a0ccd364bc2190522c42a2aabc08e1fce
Luca Pagliaro <pagliaroluca@gmail.com>
2025-03-18 15:22:01 +0100

    feat: poc app folder

---
+++ packages/webapp/app/layout.tsx
@@ -0,0 +1,13 @@
+import Providers from './providers';
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
