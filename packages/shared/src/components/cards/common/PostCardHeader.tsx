13988c1f17e3b57dc8f6cb03e7e2b37f80fce3d8
Amar Trebinjac <amartrebinjac@gmail.com>
2025-03-18 12:29:38 +0100

    fix: align source icon

--- packages/shared/src/components/cards/common/PostCardHeader.tsx
+++ packages/shared/src/components/cards/common/PostCardHeader.tsx
@@ -83,6 +83,7 @@ export const PostCardHeader = ({
         className={classNames(
           className,
           highlightBookmarkedPost && headerHiddenClassName,
+          interactiveFeedExp && 'mx-0',
         )}
       >
         <SourceButton
