1c20a709bf5e5c039ab5185f7ac303d39f9fea25
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-03-26 16:44:03 +0100

    fix: with window

--- packages/shared/src/hooks/log/useLogQueue.ts
+++ packages/shared/src/hooks/log/useLogQueue.ts
@@ -80,7 +80,7 @@ export default function useLogQueue({
           const blob = new Blob([JSON.stringify({ events })], {
             type: 'application/json',
           });
-          if (!isDevelopment) {
+          if (isDevelopment && window) {
             window.postMessage({
               type: 'FYLLA_LOG_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
               events,
