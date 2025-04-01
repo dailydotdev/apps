24c5e6eb04af22098ef31f331e4ed15c2fd88cb2
Luca Pagliaro <pagliaroluca@gmail.com>
2025-04-01 18:16:15 +0200

    feat: add return type

--- packages/shared/src/features/common/hooks/useRouterQuery.ts
+++ packages/shared/src/features/common/hooks/useRouterQuery.ts
@@ -1,6 +1,8 @@
 import { useSearchParams } from 'next/navigation';
 
-export const useRouterQuery = () => {
+export const useRouterQuery = (): {
+  query: Record<string, string>;
+} => {
   const params = useSearchParams();
   return {
     query: Object.fromEntries(params.entries()),
