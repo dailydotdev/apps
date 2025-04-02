e1310b9d07e1d7f2bb6d86b43b8519d674797aae
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-04-02 17:19:16 +0200

    fix: actually mock ResizeObserver?

--- packages/shared/__tests__/setup.ts
+++ packages/shared/__tests__/setup.ts
@@ -73,13 +73,14 @@ Object.defineProperty(global, 'BroadcastChannel', {
   })),
 });
 
-global.ResizeObserver = jest.fn(() => {
-  return {
+Object.defineProperty(global, 'ResizeObserver', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
     observe: jest.fn(),
     unobserve: jest.fn(),
     disconnect: jest.fn(),
     trigger: jest.fn(),
-  };
+  })),
 });
 
 jest.mock('next/router', () => ({
