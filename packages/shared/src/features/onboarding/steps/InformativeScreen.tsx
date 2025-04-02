6fa09375a8ef486eb294d87bf3fbbfd8a1d2e994
Amar Trebinjac <amartrebinjac@gmail.com>
2025-04-02 13:10:27 +0200

    fix: gradient not occupying full screen on iOS

--- packages/shared/src/features/onboarding/steps/InformativeScreen.tsx
+++ packages/shared/src/features/onboarding/steps/InformativeScreen.tsx
@@ -9,51 +9,58 @@ import { Button, ButtonVariant } from '../../../components/buttons/Button';
 import type { FunnelStepFact } from '../types/funnel';
 
 type InformativeScreenProps = FunnelStepFact & {
-  onTransition: () => void;
+  // onTransition: () => void;
 };
 
 const InformativeScreen = ({
   parameters,
-  onTransition,
-}: InformativeScreenProps): ReactElement => {
+}: // onTransition,
+InformativeScreenProps): ReactElement => {
   return (
-    <div
-      className="flex min-h-screen flex-col px-6 pb-20 pt-10"
-      style={{
-        background:
-          'radial-gradient(192.5% 100% at 50% 100%, #CE3DF3 0%, rgba(113, 71, 237, 0.72) 50%, rgba(113, 71, 237, 0.00) 100%), #0E1217',
-      }}
-    >
+    <>
+      {/* Background gradient */}
       <div
-        className={classNames(
-          'flex flex-1 items-center justify-between',
-          parameters?.reverse ? 'flex-col-reverse' : 'flex-col',
-        )}
-      >
-        <StepHeadline
-          headline={parameters?.headline}
-          explainer={parameters?.explainer}
-          align={parameters?.align as 'center' | 'left'}
-          visualUrl={parameters?.visualUrl}
-        />
-        {parameters?.visualUrl && (
-          <Image
-            src={parameters?.visualUrl}
-            className="max-h-48 w-auto mobileL:max-h-64"
-            alt="Supportive illustration for the information"
-          />
-        )}
-      </div>
-      <div className="absolute inset-x-0 bottom-4 w-full px-4">
-        <Button
-          className="w-full"
-          variant={ButtonVariant.Primary}
-          onClick={onTransition}
+        className={
+          parameters?.reverse
+            ? 'bg-gradient-purple-reverse'
+            : 'bg-gradient-purple'
+        }
+      />
+      {/* Content container */}
+      <div className="flex min-h-screen flex-col px-6 pb-20 pt-10">
+        <div
+          className={classNames(
+            'flex flex-1 items-center gap-6',
+            parameters?.reverse
+              ? 'flex-col-reverse justify-end'
+              : 'flex-col justify-between',
+          )}
         >
-          {parameters?.cta ?? 'Next'}
-        </Button>
+          <StepHeadline
+            headline={parameters?.headline}
+            explainer={parameters?.explainer}
+            align={parameters?.align as 'center' | 'left'}
+            visualUrl={parameters?.visualUrl}
+          />
+          {parameters?.visualUrl && (
+            <Image
+              src={parameters?.visualUrl}
+              className="max-h-48 w-auto mobileL:max-h-64"
+              alt="Supportive illustration for the information"
+            />
+          )}
+        </div>
+        <div className="absolute inset-x-0 bottom-4 w-full px-4">
+          <Button
+            className="w-full"
+            variant={ButtonVariant.Primary}
+            // onClick={onTransition}
+          >
+            {parameters?.cta ?? 'Next'}
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- packages/webapp/app/helloworld/page.tsx
+++ packages/webapp/app/helloworld/page.tsx
@@ -3,8 +3,10 @@ import type { ReactElement } from 'react';
 import React from 'react';
 import { cookies } from 'next/headers';
 import { HydrationBoundary } from '@tanstack/react-query';
+import InformativeScreen from '@dailydotdev/shared/src/features/onboarding/steps/InformativeScreen';
+import type { FunnelStepFact } from '@dailydotdev/shared/src/features/onboarding/types/funnel';
+import { FunnelStepType } from '@dailydotdev/shared/src/features/onboarding/types/funnel';
 import { getAppBootData } from '../app-boot';
-import { ClientTest } from './client/client';
 
 async function getIdAndVersion({
   params,
@@ -15,6 +17,20 @@ async function getIdAndVersion({
 
   return { id, version };
 }
+
+const funnelStepFact: FunnelStepFact = {
+  parameters: {
+    headline: 'Staying motivated is about to become easier',
+    explainer:
+      'Even the most driven people hit roadblocks. We’re here to help turn your ambition into consistent progress. Ready?',
+    visualUrl: 'https://i.imgur.com/OtR3Igy.png',
+    align: 'center',
+    // reverse: false,
+  },
+  type: FunnelStepType.Fact,
+  id: '1',
+  transitions: [],
+};
 
 export default async function Page(props: AppPageProps): Promise<ReactElement> {
   const allCookies = (await cookies()).toString();
@@ -28,12 +44,13 @@ export default async function Page(props: AppPageProps): Promise<ReactElement> {
 
   return (
     <HydrationBoundary state={state}>
-      <h1 className="mb-4 text-xl font-bold">Hello world funnel</h1>
+      {/* <h1 className="mb-4 text-xl font-bold">Hello world funnel</h1>
       <p>
         <strong>Server</strong> says user is {boot?.user?.id ?? 'not logged'} -{' '}
         {boot?.user?.email ?? 'no email'}
       </p>
-      <ClientTest />
+      <ClientTest /> */}
+      <InformativeScreen {...funnelStepFact} />
     </HydrationBoundary>
   );
 }
