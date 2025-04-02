9e4bdf8126d7d8fbd91aa2600649cdf9b468c5a9
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-04-02 11:08:39 +0200

    refactor: remove unnecessary null/undefined check

--- packages/shared/src/lib/cores.ts
+++ packages/shared/src/lib/cores.ts
@@ -5,13 +5,7 @@ import { CoresRole } from './user';
 export const verifyCoresRole = (
   user: LoggedUser,
   coresRole: CoresRole,
-): boolean => {
-  if (isNullOrUndefined(user?.coresRole)) {
-    return false;
-  }
-
-  return user?.coresRole === coresRole;
-};
+): boolean => user?.coresRole === coresRole;
 
 export function hasAccessToCores(user: LoggedUser): boolean {
   if (isNullOrUndefined(user?.coresRole)) {
