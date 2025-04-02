ca3635d40bbe30cf3ecfad44a69e84ae33a75dfe
Amar Trebinjac <amartrebinjac@gmail.com>
2025-04-02 10:32:15 +0200

    feat: update stepheadline to differentiate between quiz and other types

--- packages/shared/src/features/onboarding/shared/StepHeadline.tsx
+++ packages/shared/src/features/onboarding/shared/StepHeadline.tsx
@@ -1,29 +1,45 @@
 import type { ReactElement } from 'react';
 import React from 'react';
+import classNames from 'classnames';
 import {
   Typography,
   TypographyColor,
   TypographyType,
 } from '../../../components/typography/Typography';
+import { FunnelStepType } from '../types/funnel';
 
 export type StepHeadlineProps = {
   headline: string;
   explainer: string;
   align?: 'left' | 'center';
   visualUrl?: string;
+  type?: FunnelStepType;
 };
 
 const StepHeadline = ({
   headline,
   explainer,
+  type,
   align = 'center',
 }: StepHeadlineProps): ReactElement => {
   return (
-    <div className={align === 'center' ? 'text-center' : 'text-left'}>
+    <div
+      className={classNames(
+        'flex flex-col gap-2',
+        align === 'center' ? 'text-center' : 'text-left',
+      )}
+    >
       <Typography bold type={TypographyType.Title1}>
         {headline}
       </Typography>
-      <Typography type={TypographyType.Body} color={TypographyColor.Tertiary}>
+      <Typography
+        type={TypographyType.Body}
+        color={
+          type === FunnelStepType.Quiz
+            ? TypographyColor.Tertiary
+            : TypographyColor.Primary
+        }
+      >
         {explainer}
       </Typography>
     </div>
--- packages/shared/src/features/onboarding/steps/InformativeScreen.tsx
+++ packages/shared/src/features/onboarding/steps/InformativeScreen.tsx
@@ -8,10 +8,17 @@ import { Image } from '../../../components/image/Image';
 import { Button, ButtonVariant } from '../../../components/buttons/Button';
 import type { FunnelStepFact } from '../types/funnel';
 
-const InformativeScreen = ({ parameters }: FunnelStepFact): ReactElement => {
+type InformativeScreenProps = FunnelStepFact & {
+  onTransition: () => void;
+};
+
+const InformativeScreen = ({
+  parameters,
+  onTransition,
+}: InformativeScreenProps): ReactElement => {
   return (
     <div
-      className="flex max-h-screen min-h-screen flex-col px-6 pb-4 pt-10"
+      className="flex min-h-screen flex-col px-6 pb-20 pt-10"
       style={{
         background:
           'radial-gradient(192.5% 100% at 50% 100%, #CE3DF3 0%, rgba(113, 71, 237, 0.72) 50%, rgba(113, 71, 237, 0.00) 100%), #0E1217',
@@ -29,14 +36,20 @@ const InformativeScreen = ({ parameters }: FunnelStepFact): ReactElement => {
           align={parameters?.align as 'center' | 'left'}
           visualUrl={parameters?.visualUrl}
         />
-        <Image
-          src={parameters?.visualUrl}
-          className="max-h-64 w-full"
-          alt="Illustration"
-        />
+        {parameters?.visualUrl && (
+          <Image
+            src={parameters?.visualUrl}
+            className="max-h-48 w-auto mobileL:max-h-64"
+            alt="Supportive illustration for the information"
+          />
+        )}
       </div>
-      <div className="px-4">
-        <Button className="w-full" variant={ButtonVariant.Primary}>
+      <div className="absolute inset-x-0 bottom-4 w-full px-4">
+        <Button
+          className="w-full"
+          variant={ButtonVariant.Primary}
+          onClick={onTransition}
+        >
           {parameters?.cta ?? 'Next'}
         </Button>
       </div>
