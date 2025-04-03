ba17cde16d483aff4683dd53aab8e6a43a8b3a9e
Luca Pagliaro <pagliaroluca@gmail.com>
2025-04-03 14:08:41 +0200

    feat: remove form components from ClientTest

--- packages/webapp/app/helloworld/client/client.tsx
+++ packages/webapp/app/helloworld/client/client.tsx
@@ -7,37 +7,6 @@ import {
   AppAuthActionsKeys,
 } from '@dailydotdev/shared/src/features/common/hooks/useAppAuth';
 import { Button } from '@dailydotdev/shared/src/components/buttons/Button';
-import {
-  FormInputCheckboxGroup,
-  CheckboxGroupVariant,
-} from '@dailydotdev/shared/src/features/common/components/FormInputCheckboxGroup';
-
-const options = [
-  {
-    label: 'JavaScript',
-    value: 'js',
-    image: {
-      src: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
-      alt: 'JavaScript',
-    },
-  },
-  {
-    label: 'Python',
-    value: 'py',
-    image: {
-      src: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
-      alt: 'Python',
-    },
-  },
-  {
-    label: 'Go',
-    value: 'go',
-    image: {
-      src: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Go_Logo_Blue.svg',
-      alt: 'Go',
-    },
-  },
-];
 
 export const ClientTest: FC = () => {
   const { user, dispatch } = useAppAuth();
@@ -57,33 +26,6 @@ export const ClientTest: FC = () => {
       >
         Logout
       </Button>
-      <hr className="my-10" />
-      <div className="flex flex-col gap-10">
-        <FormInputCheckboxGroup
-          name="languages"
-          cols={1}
-          options={options.map(({ label, value }) => ({ label, value }))}
-        />
-        <FormInputCheckboxGroup
-          name="languages"
-          cols={2}
-          defaultValue={['js']}
-          options={options.map(({ label, value }) => ({ label, value }))}
-        />
-        <FormInputCheckboxGroup
-          name="languages"
-          cols={2}
-          defaultValue={['js']}
-          options={options}
-        />
-        <FormInputCheckboxGroup
-          name="languages"
-          cols={2}
-          defaultValue={['js']}
-          variant={CheckboxGroupVariant.Vertical}
-          options={options}
-        />
-      </div>
     </div>
   );
 };
