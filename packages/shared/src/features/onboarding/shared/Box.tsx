d566475e7ce0b82436d248aa495072c61ec7b9ae
Ido Shamun <idoesh1@gmail.com>
2025-04-03 12:47:58 +0300

    refactor: image props

--- packages/shared/src/features/onboarding/shared/Box.tsx
+++ packages/shared/src/features/onboarding/shared/Box.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import type { ReactElement } from 'react';
+import type { ReactElement, ComponentProps } from 'react';
+import classNames from 'classnames';
 import { VIcon } from '../../../components/icons/V';
 import { IconSize } from '../../../components/Icon';
 import classed from '../../../lib/classed';
@@ -43,15 +44,13 @@ export const BoxList = ({
 export interface BoxContentImageProps extends BoxBaseProps {
   title: string;
   content: string;
-  image: string;
-  imageAlt?: string;
+  image: ComponentProps<'img'>;
 }
 
 export const BoxContentImage = ({
   title,
   content,
   image,
-  imageAlt = '',
   className,
 }: BoxContentImageProps): ReactElement => {
   return (
@@ -59,9 +58,12 @@ export const BoxContentImage = ({
       <h3 className="font-bold typo-title3">{title}</h3>
       <div>
         <img
-          src={image}
-          alt={imageAlt}
-          className="float-right mb-2 ml-2 h-20 w-20 object-contain"
+          {...image}
+          alt={image.alt}
+          className={classNames(
+            'float-right mb-2 ml-2 h-20 w-20 object-contain',
+            image.className,
+          )}
         />
         <p className="text-text-tertiary typo-callout">{content}</p>
       </div>
