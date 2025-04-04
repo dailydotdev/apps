81f5636862d10517c45d3a42836e5a80e9d71bf7
Luca Pagliaro <pagliaroluca@gmail.com>
2025-04-04 19:11:03 +0200

    feat: implement funnel navigation in FunnelStepper

--- packages/shared/src/features/onboarding/hooks/useFunnelTracking.ts
+++ packages/shared/src/features/onboarding/hooks/useFunnelTracking.ts
@@ -1,14 +1,33 @@
 import type { UIEventHandler, MouseEventHandler } from 'react';
 import { useLogContext } from '../../../contexts/LogContext';
+import type { FunnelStep } from '../types/funnel';
+
+type TrackOnClickCapture = MouseEventHandler<HTMLElement>;
+type TrackOnScroll = UIEventHandler<HTMLElement>;
+type TrackOnCustomEvent = (event: {
+  eventName: string;
+  eventDetails: Record<string, unknown>;
+}) => void;
+export type TrackOnNavigate = (event: {
+  from: FunnelStep['id'];
+  to: FunnelStep['id'];
+  timeDuration: number;
+}) => void;
 
 interface UseFunnelTrackingReturn {
-  trackOnClickCapture: MouseEventHandler<HTMLElement>;
-  trackOnScroll: UIEventHandler<HTMLElement>;
+  trackOnClickCapture: TrackOnClickCapture;
+  trackOnCustomEvent: TrackOnCustomEvent;
+  trackOnNavigate: TrackOnNavigate;
+  trackOnScroll: TrackOnScroll;
 }
+
+// Since we want to track every interaction in forms and CTAs, it's better to avoid polluting individual input fields or form components.
+// Instead, we should capture these events from a parent element using event bubbling and centralize the tracking logic within a single component/hook.
+// We can also pass a prop `onCustomEvent(eventName, eventDetails)` to the funnel step components in order to handle complex and not shared events.
 
 export const useFunnelTracking = (): UseFunnelTrackingReturn => {
   const { logEvent } = useLogContext();
-  const trackOnClickCapture: MouseEventHandler<HTMLElement> = (event) => {
+  const trackOnClickCapture: TrackOnClickCapture = (event) => {
     if (!(event.target instanceof HTMLElement)) {
       return;
     }
@@ -23,12 +42,28 @@ export const useFunnelTracking = (): UseFunnelTrackingReturn => {
     console.log({ trackedElement });
   };
 
-  const trackOnScroll: UIEventHandler<HTMLElement> = (event) => {
+  const trackOnScroll: TrackOnScroll = (event) => {
     // logEvent
+  };
+
+  const trackOnNavigate: TrackOnNavigate = (event) => {
+    // logEvent
+  };
+
+  const trackOnCustomEvent: TrackOnCustomEvent = ({
+    eventName,
+    eventDetails,
+  }) => {
+    logEvent({
+      event_name: eventName,
+      ...(eventDetails && { extra: JSON.stringify(eventDetails) }),
+    });
   };
 
   return {
     trackOnClickCapture,
+    trackOnCustomEvent,
+    trackOnNavigate,
     trackOnScroll,
   };
 };
--- packages/shared/src/features/onboarding/shared/FunnelStepper.tsx
+++ packages/shared/src/features/onboarding/shared/FunnelStepper.tsx
@@ -1,43 +1,101 @@
-import type { ReactElement, PropsWithChildren } from 'react';
+import type { ReactElement, FC } from 'react';
 import React from 'react';
-import type { FunnelJSON } from '../types/funnel';
+import classNames from 'classnames';
+import type {
+  FunnelJSON,
+  FunnelStep,
+  FunnelStepChapter,
+  FunnelStepFact,
+  FunnelStepAppPromotion,
+  FunnelStepPricing,
+  FunnelStepCheckout,
+  FunnelStepLandingPage,
+  FunnelStepReadingReminder,
+  FunnelStepSignup,
+  FunnelStepTagSelection,
+} from '../types/funnel';
 import { Header } from './Header';
 import { useFunnelTracking } from '../hooks/useFunnelTracking';
+import { useFunnelNavigation } from '../hooks/useFunnelNavigation';
+import { FunnelStepType } from '../types/funnel';
+import { FunnelQuiz } from '../steps/FunnelQuiz';
 
-interface FunnelStepperProps extends PropsWithChildren {
+interface FunnelStepperProps {
   funnel: FunnelJSON;
 }
 
-const initialState = {
-  step: 0,
-  chapters: [],
-  chapter: 0,
+type Step = Exclude<FunnelStep, FunnelStepChapter>;
+
+type StepComponentMap = {
+  [FunnelStepType.Fact]: FC<FunnelStepFact>;
+  [FunnelStepType.Quiz]: typeof FunnelQuiz;
+  [FunnelStepType.AppPromotion]: FC<FunnelStepAppPromotion>;
+  [FunnelStepType.Pricing]: FC<FunnelStepPricing>;
+  [FunnelStepType.Checkout]: FC<FunnelStepCheckout>;
+  [FunnelStepType.LandingPage]: FC<FunnelStepLandingPage>;
+  [FunnelStepType.ReadingReminder]: FC<FunnelStepReadingReminder>;
+  [FunnelStepType.Signup]: FC<FunnelStepSignup>;
+  [FunnelStepType.TagSelection]: FC<FunnelStepTagSelection>;
 };
 
-const funnelReducer = (state: any, action: any) => {
-  switch (action.type) {
-    case 'NEXT_STEP':
-      return { ...state, step: state.step + 1 };
-    case 'PREVIOUS_STEP':
-      return { ...state, step: state.step - 1 };
-    default:
-      throw new Error(`Unknown action: ${action.type}`);
-  }
+const stepComponentMap: StepComponentMap = {
+  [FunnelStepType.Fact]: ({ type }) => <>{type}</>,
+  [FunnelStepType.Quiz]: FunnelQuiz,
+  [FunnelStepType.AppPromotion]: ({ type }) => <>{type}</>,
+  [FunnelStepType.Pricing]: ({ type }) => <>{type}</>,
+  [FunnelStepType.Checkout]: ({ type }) => <>{type}</>,
+  [FunnelStepType.LandingPage]: ({ type }) => <>{type}</>,
+  [FunnelStepType.ReadingReminder]: ({ type }) => <>{type}</>,
+  [FunnelStepType.Signup]: ({ type }) => <>{type}</>,
+  [FunnelStepType.TagSelection]: ({ type }) => <>{type}</>,
 };
 
-export const FunnelStepper = ({
-  children,
-  funnel,
-}: FunnelStepperProps): ReactElement => {
-  const [state, dispatch] = React.useReducer(funnelReducer, initialState);
-  const { trackOnClickCapture, trackOnScroll } = useFunnelTracking();
+function FunnelStepComponent(props: Step) {
+  const { type } = props;
+  const Component = stepComponentMap[type] as FC<typeof props>;
+  return <Component {...props} />;
+}
+
+export const FunnelStepper = ({ funnel }: FunnelStepperProps): ReactElement => {
+  const { trackOnClickCapture, trackOnScroll, trackOnNavigate } =
+    useFunnelTracking();
+  const {
+    canSkip,
+    chapters,
+    hasPrev,
+    navigateNext,
+    navigatePrev,
+    step,
+    position,
+  } = useFunnelNavigation({ funnel, onNavigation: trackOnNavigate });
+
   return (
     <section onClickCapture={trackOnClickCapture} onScroll={trackOnScroll}>
       <Header
-        currentChapter={state.chapter}
-        currentStep={state.step}
-        chapters={state.chapters}
+        currentChapter={position.chapter}
+        currentStep={position.step}
+        chapters={chapters}
+        onBack={navigatePrev}
+        onSkip={navigateNext}
+        showBackButton={hasPrev}
+        showSkipButton={canSkip}
       />
+      <div>
+        {funnel.steps.map((chapter: FunnelStepChapter) => (
+          <div key={chapter.id}>
+            {chapter?.steps?.map((funnelStep: Step) => (
+              <div
+                className={classNames({
+                  hidden: step.id !== funnelStep.id,
+                })}
+                key={`${chapter.id}-${step.id}`}
+              >
+                <FunnelStepComponent {...funnelStep} />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
