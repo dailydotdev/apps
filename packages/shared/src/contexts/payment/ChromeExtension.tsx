a373a7e8f2b85b17e05a3e3694cd5403355c70da
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-04-08 00:28:24 +0800

    fix: redirect

--- packages/shared/src/contexts/payment/ChromeExtension.tsx
+++ packages/shared/src/contexts/payment/ChromeExtension.tsx
@@ -1,13 +1,22 @@
 import type { PropsWithChildren, ReactElement } from 'react';
 import React from 'react';
+import { useRouter } from 'next/router';
 import { BasePaymentProvider } from './BasePaymentProvider';
+import type { OpenCheckoutProps } from './context';
+import { webappUrl } from '../../lib/constants';
 
 export const ChromeExtensionProvider = ({
   children,
 }: PropsWithChildren): ReactElement => {
-  // No-op function since checkout is not available in extension
-  const openCheckout = () => {
+  const router = useRouter();
+  const openCheckout = ({ priceId, giftToUserId }: OpenCheckoutProps) => {
     // Payment not available in extension
+    const params = new URLSearchParams({
+      priceId,
+      giftToUserId: giftToUserId ?? '',
+    });
+    const url = new URL(`${webappUrl}plus/payment?${params.toString()}`);
+    router.push(url.toString());
   };
 
   return (
