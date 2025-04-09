c41369173ffbc2cb4e9203876c2abdead32214ca
Ido Shamun <ido@daily.dev>
2025-04-09 14:29:25 +0300

    feat: funnel boot support (#4363)
    Co-authored-by: Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>

---
+++ packages/webapp/pages/helloworld/[id]/index.tsx
@@ -0,0 +1,7 @@
+import HelloWorldPage from '../index';
+
+// Re-use the same component but with different params
+export default HelloWorldPage;
+
+// Re-use the getServerSideProps logic from the parent page
+export { getServerSideProps } from '../index';
