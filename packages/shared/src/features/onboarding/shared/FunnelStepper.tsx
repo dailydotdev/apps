b719d3800d7030a0c85fb3d2c2fd0386174636c7
Luca Pagliaro <pagliaroluca@gmail.com>
2025-04-17 15:31:48 +0200

    fix: navbar should be wrapped in container

--- packages/shared/src/features/onboarding/shared/FunnelStepper.tsx
+++ packages/shared/src/features/onboarding/shared/FunnelStepper.tsx
@@ -140,22 +140,22 @@ export const FunnelStepper = ({
         <CookieConsent key="cookie-consent" {...cookieConsentProps} />
       )}
       <FunnelStepBackground step={step}>
-        <Header
-          chapters={chapters}
-          className={classNames({
-            hidden: !stepsWithHeader.includes(step.type),
-          })}
-          currentChapter={position.chapter}
-          currentStep={position.step}
-          onBack={back.navigate}
-          onSkip={() => {
-            onTransition({ type: FunnelStepTransitionType.Skip });
-          }}
-          showBackButton={back.hasTarget}
-          showSkipButton={skip.hasTarget}
-          showProgressBar={skip.hasTarget}
-        />
         <div className="mx-auto flex w-full flex-1 flex-col tablet:max-w-md laptopXL:max-w-lg">
+          <Header
+            chapters={chapters}
+            className={classNames({
+              hidden: !stepsWithHeader.includes(step.type),
+            })}
+            currentChapter={position.chapter}
+            currentStep={position.step}
+            onBack={back.navigate}
+            onSkip={() => {
+              onTransition({ type: FunnelStepTransitionType.Skip });
+            }}
+            showBackButton={back.hasTarget}
+            showSkipButton={skip.hasTarget}
+            showProgressBar={skip.hasTarget}
+          />
           {funnel.chapters.map((chapter: FunnelChapter) => (
             <Fragment key={chapter?.id}>
               {chapter?.steps?.map((funnelStep: FunnelStep) => {
