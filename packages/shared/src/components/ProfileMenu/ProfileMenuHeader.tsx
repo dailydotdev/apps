ff70c2faebdb994a8ddf275dc79d12339baa0d5d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-04-23 14:33:48 +0200

    fix: center items of profile menu dropdown header

--- packages/shared/src/components/ProfileMenu/ProfileMenuHeader.tsx
+++ packages/shared/src/components/ProfileMenu/ProfileMenuHeader.tsx
@@ -16,7 +16,7 @@ export const ProfileMenuHeader = (): ReactElement => {
   const { isPlus } = usePlusSubscription();
 
   return (
-    <div className="flex gap-2">
+    <div className="flex items-center gap-2">
       <ProfilePicture
         user={user}
         nativeLazyLoading
