6dc57a7473ade0e2895712058c11a920ecab23b2
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-04-29 18:27:37 +0800

    fix: if is not active

--- packages/shared/src/features/onboarding/steps/FunnelCheckout.tsx
+++ packages/shared/src/features/onboarding/steps/FunnelCheckout.tsx
@@ -31,8 +31,6 @@ export const InnerFunnelCheckout = ({
       return;
     }
 
-    console.log('open checkout');
-
     openCheckout({
       priceId,
       discountId: applyDiscount ? discountCode : undefined,
