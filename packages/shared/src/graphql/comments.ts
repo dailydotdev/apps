f065d13f8c05812877682a71768b47e56b1c04c0
Ante Barić <ante@kickass.website>
2025-05-07 15:39:06 +0200

    feat: featured award (#4468)

--- packages/shared/src/graphql/comments.ts
+++ packages/shared/src/graphql/comments.ts
@@ -7,7 +7,7 @@ import type { Post, UserVote } from './posts';
 import type { Company } from '../lib/userCompany';
 import type { ContentPreference } from './contentPreference';
 import type { TopReader } from '../components/badges/TopReaderBadge';
-import type { Product } from './njord';
+import type { FeaturedAward, Product } from './njord';
 
 export interface Author {
   __typename?: string;
@@ -51,6 +51,9 @@ export interface Comment {
   userState?: CommentUserState;
   fromAward?: boolean;
   award?: Pick<Product, 'name' | 'image'>;
+  featuredAward?: {
+    award?: FeaturedAward;
+  };
 }
 
 export const getCommentHash = (id: string): string => `#c-${id}`;
