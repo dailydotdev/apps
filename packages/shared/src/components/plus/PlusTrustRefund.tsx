bb3d1a4c6c355adcd14d2efb8da48d2892735229
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-05-08 13:55:00 +0800

    refactor: unnecessary mapping

--- packages/shared/src/components/plus/PlusTrustRefund.tsx
+++ packages/shared/src/components/plus/PlusTrustRefund.tsx
@@ -11,50 +11,32 @@ import {
 
 type PlusTrustRefundProps = ComponentProps<'div'>;
 
-const alerts = {
-  refund: {
-    icon: PrivacyIcon,
-    iconClassName: 'bg-action-comment-float text-accent-blueCheese-default',
-    text: '30 day hassle-free refund. No questions asked.',
-  },
-};
-
 export const PlusTrustRefund = ({
   className,
   ...attrs
 }: PlusTrustRefundProps): ReactElement => {
-  const items = [alerts.refund];
-
   return (
-    <>
-      {items.map(({ icon: Icon, iconClassName, text }) => (
-        <div
-          aria-label="Refund policy"
-          className={classNames(
-            'flex items-center gap-2 rounded-10 bg-surface-float px-3 py-2',
-            className,
-          )}
-          key={text}
-          {...attrs}
-        >
-          <div
-            aria-hidden
-            className={classNames(
-              'grid size-8 place-items-center rounded-10',
-              iconClassName,
-            )}
-          >
-            <Icon secondary size={IconSize.Medium} />
-          </div>
-          <Typography
-            className="min-w-0 flex-1"
-            color={TypographyColor.Primary}
-            type={TypographyType.Callout}
-          >
-            {text}
-          </Typography>
-        </div>
-      ))}
-    </>
+    <div
+      aria-label="Refund policy"
+      className={classNames(
+        'flex items-center gap-2 rounded-10 bg-surface-float px-3 py-2',
+        className,
+      )}
+      {...attrs}
+    >
+      <div
+        aria-hidden
+        className="grid size-8 place-items-center rounded-10 bg-action-comment-float text-accent-blueCheese-default"
+      >
+        <PrivacyIcon secondary size={IconSize.Medium} />
+      </div>
+      <Typography
+        className="min-w-0 flex-1"
+        color={TypographyColor.Primary}
+        type={TypographyType.Callout}
+      >
+        30 day hassle-free refund. No questions asked.
+      </Typography>
+    </div>
   );
 };
