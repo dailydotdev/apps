9a8d4875954cb1fda2adc8b01b142d30f6ce0389
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2025-05-15 22:49:45 +0800

    Update FunnelFactDefault.tsx

--- packages/shared/src/features/onboarding/steps/FunnelFact/FunnelFactDefault.tsx
+++ packages/shared/src/features/onboarding/steps/FunnelFact/FunnelFactDefault.tsx
@@ -15,7 +15,7 @@ export const FunnelFactDefault = ({
   onTransition,
 }: FunnelStepFact): ReactElement => {
   const isLayoutReversed =
-    parameters?.layout === 'reversed' || parameters?.reverse;
+    parameters.layout === 'reversed' || parameters.reverse;
 
   return (
     <FunnelStepCtaWrapper
