8085e5d3c381ad06ae25dc0dc835d200980d6e1d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-05-15 23:30:36 +0800

    fix: unnecessary use client

--- packages/shared/src/features/onboarding/steps/FunnelFact/FunnelFactCentered.tsx
+++ packages/shared/src/features/onboarding/steps/FunnelFact/FunnelFactCentered.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import type { ReactElement } from 'react';
 import Head from 'next/head';
--- packages/shared/src/features/onboarding/steps/FunnelFact/FunnelFactDefault.tsx
+++ packages/shared/src/features/onboarding/steps/FunnelFact/FunnelFactDefault.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import type { ReactElement } from 'react';
 import classNames from 'classnames';
