ca4eef4d834c742cefdb506645c4530f7ac38ce0
capJavert <dev@kickass.website>
2025-05-20 02:06:29 +0200

    feat: easter egg, animation on comment award click

--- packages/shared/src/components/AnimatedAward.tsx
+++ packages/shared/src/components/AnimatedAward.tsx
@@ -1,6 +1,8 @@
 import { useTimeout } from '@kickass-coderz/react';
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
+import type { Product } from '../graphql/njord';
+import { Image } from './image/Image';
 
 export type AnimatedAwardProps = {
   className?: string;
@@ -37,5 +39,47 @@ export const AnimatedAward = ({
         loading="eager"
       />
     </div>
+  );
+};
+
+export const AnimatedAwardImage = ({
+  className,
+  award,
+}: {
+  className?: string;
+  award: Pick<Product, 'image' | 'name'>;
+}) => {
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  if (!award) {
+    return null;
+  }
+
+  return (
+    <>
+      <button
+        type="button"
+        className={classNames(
+          'flex size-7 items-center justify-center rounded-10 bg-surface-float',
+          className,
+        )}
+        onClick={() => {
+          setShowAnimation(true);
+        }}
+      >
+        <Image
+          src={award.image}
+          alt={award.name}
+          className="size-5 object-contain"
+        />
+      </button>
+      {showAnimation && (
+        <AnimatedAward
+          src={showAnimation ? award.image : ''}
+          alt={award.name}
+          onDone={() => setShowAnimation(false)}
+        />
+      )}
+    </>
   );
 };
