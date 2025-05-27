973ed2e4ba8908ee44e630be9922f75fa219904d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-05-27 11:38:04 +0200

    fix: update comment

--- packages/shared/src/features/organizations/components/InviteMemberModal.tsx
+++ packages/shared/src/features/organizations/components/InviteMemberModal.tsx
@@ -33,7 +33,7 @@ export const InviteMemberModal = ({
 
   const { organization, referralUrl } = useOrganization(organizationId);
 
-  // Hardcoded for demo purposes
+  // Hardcoded for demo purposes @TODO: replace with actual logic
   const seatsAvailable = false;
 
   const isMobile = useViewSize(ViewSize.MobileL);
