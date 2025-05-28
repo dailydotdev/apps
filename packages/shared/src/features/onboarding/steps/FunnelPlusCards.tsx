c2a7f8c3d716ed5e0be072df9575d3b21a16f3e6
Luca Pagliaro <pagliaroluca@gmail.com>
2025-05-28 11:48:50 +0200

    refactor: added withIsActiveGuard to FunnelPlusCards component

--- packages/shared/src/features/onboarding/steps/FunnelPlusCards.tsx
+++ packages/shared/src/features/onboarding/steps/FunnelPlusCards.tsx
@@ -2,23 +2,26 @@ import React, { useCallback } from 'react';
 import { OnboardingPlus } from '../components/OnboardingPlus';
 import type { FunnelStepPlusCards } from '../types/funnel';
 import { FunnelStepTransitionType } from '../types/funnel';
+import { withIsActiveGuard } from '../shared/withActiveGuard';
 
-export const FunnelPlusCards = ({ onTransition }: FunnelStepPlusCards) => {
-  const onSkip = useCallback(() => {
-    onTransition?.({
-      type: FunnelStepTransitionType.Skip,
-      details: { skip: true },
-    });
-  }, [onTransition]);
+export const FunnelPlusCards = withIsActiveGuard(
+  ({ onTransition }: FunnelStepPlusCards) => {
+    const onSkip = useCallback(() => {
+      onTransition?.({
+        type: FunnelStepTransitionType.Skip,
+        details: { skip: true },
+      });
+    }, [onTransition]);
 
-  const onComplete = useCallback(() => {
-    onTransition?.({
-      type: FunnelStepTransitionType.Complete,
-      details: { skip: false },
-    });
-  }, [onTransition]);
+    const onComplete = useCallback(() => {
+      onTransition?.({
+        type: FunnelStepTransitionType.Complete,
+        details: { skip: false },
+      });
+    }, [onTransition]);
 
-  return <OnboardingPlus onComplete={onComplete} onSkip={onSkip} />;
-};
+    return <OnboardingPlus onComplete={onComplete} onSkip={onSkip} />;
+  },
+);
 
 export default FunnelPlusCards;
