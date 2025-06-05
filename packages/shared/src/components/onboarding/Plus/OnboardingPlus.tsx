50ee6b6771e027688edfd977daeacbefe8c2fcc7
Chris Bongers <chrisbongers@gmail.com>
2025-06-05 16:08:34 +0200

    fix: remove old experiment (#4559)

--- packages/shared/src/components/onboarding/Plus/OnboardingPlus.tsx
+++ packages/shared/src/components/onboarding/Plus/OnboardingPlus.tsx
@@ -1,35 +1,10 @@
-import React from 'react';
 import { withExperiment } from '../../withExperiment';
 import { OnboardingPlusControl } from './OnboardingPlusControl';
-import {
-  featureOnboardingGridVariation,
-  featureOnboardingPlusFeatureGrid,
-} from '../../../lib/featureManagement';
-import { useConditionalFeature } from '../../../hooks';
-import { OnboardingGridVariation } from '../../../lib/featureValues';
 import { OnboardingPlusVariationV1 } from './OnboardingPlusVariationV1';
-import { OnboardingPlusVariation } from './OnboardingPlusVariation';
+import { featureOnboardingGridVariationV1 } from '../../../lib/featureManagement';
 
-const OnboardingPlusVariationExperiment = (props) => {
-  const { value: plusVariation } = useConditionalFeature({
-    shouldEvaluate: true,
-    feature: featureOnboardingGridVariation,
-  });
-  switch (plusVariation) {
-    case OnboardingGridVariation.V1:
-      return <OnboardingPlusVariationV1 {...props} />;
-      break;
-    default:
-      return <OnboardingPlusVariation {...props} />;
-      break;
-  }
-};
-
-export const OnboardingPlus = withExperiment(
-  OnboardingPlusVariationExperiment,
-  {
-    feature: featureOnboardingPlusFeatureGrid,
-    value: true,
-    fallback: OnboardingPlusControl,
-  },
-);
+export const OnboardingPlus = withExperiment(OnboardingPlusVariationV1, {
+  feature: featureOnboardingGridVariationV1,
+  value: true,
+  fallback: OnboardingPlusControl,
+});
