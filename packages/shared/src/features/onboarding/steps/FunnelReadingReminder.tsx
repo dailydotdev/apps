c081d7f18b0b8e47c52b84ad713d0fac07e90214
Luca Pagliaro <pagliaroluca@gmail.com>
2025-06-12 14:22:41 +0200

    feat: add autoskip condition to FunnelReadingReminder

--- packages/shared/src/features/onboarding/steps/FunnelReadingReminder.tsx
+++ packages/shared/src/features/onboarding/steps/FunnelReadingReminder.tsx
@@ -1,21 +1,39 @@
 import type { ReactElement } from 'react';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import type { FunnelStepReadingReminder } from '../types/funnel';
 import { FunnelStepTransitionType } from '../types/funnel';
 import { ReadingReminder } from '../../../components/onboarding';
 import { withIsActiveGuard } from '../shared/withActiveGuard';
+import { useViewSize, ViewSize } from '../../../hooks';
+import { usePushNotificationContext } from '../../../contexts/PushNotificationContext';
 
 function FunnelReadingReminderComponent({
   parameters: { headline },
   onTransition,
 }: FunnelStepReadingReminder): ReactElement | null {
-  const handleSubmit = ({ skipped }: { skipped?: boolean } = {}) => {
-    onTransition({
-      type: skipped
-        ? FunnelStepTransitionType.Skip
-        : FunnelStepTransitionType.Complete,
-    });
-  };
+  const isMobile = useViewSize(ViewSize.MobileXL);
+  const { isPushSupported } = usePushNotificationContext();
+
+  const handleSubmit = useCallback(
+    ({ skipped }: { skipped?: boolean } = {}) => {
+      onTransition({
+        type: skipped
+          ? FunnelStepTransitionType.Skip
+          : FunnelStepTransitionType.Complete,
+      });
+    },
+    [onTransition],
+  );
+
+  useEffect(() => {
+    if (!isMobile || !isPushSupported) {
+      handleSubmit({ skipped: true });
+    }
+  }, [handleSubmit, isMobile, isPushSupported]);
+
+  if (!isMobile || !isPushSupported) {
+    return null;
+  }
 
   return (
     <div className="flex w-full flex-1 flex-col items-center justify-center overflow-hidden p-6 pt-10 tablet:max-w-96">
