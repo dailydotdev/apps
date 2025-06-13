80ed0ef632747411f412634b16e1226d0b14b707
Chris Bongers <chrisbongers@gmail.com>
2025-06-13 14:04:28 +0200

    fix: easier wrapping

--- packages/webapp/pages/search/posts/index.tsx
+++ packages/webapp/pages/search/posts/index.tsx
@@ -1,30 +1,13 @@
-import type { ReactElement, ReactNode } from 'react';
+import type { ReactElement } from 'react';
 import React from 'react';
 import { withFeaturesBoundary } from '@dailydotdev/shared/src/components';
-import { SearchProvider } from '@dailydotdev/shared/src/contexts/search/SearchContext';
 import SearchControlPage from '../../../components/search/SearchPostFinderPage';
-import MainFeedPage from '../../../components/layouts/MainFeedPage';
-import { getLayout } from '../../../components/layouts/FeedLayout';
 
 const SearchPostsPage = (): ReactElement => {
   return <SearchControlPage />;
 };
 
-export function getSearchLayout(
-  page: ReactNode,
-  pageProps,
-  layoutProps,
-): ReactNode {
-  return getLayout(
-    <SearchProvider>
-      <MainFeedPage {...layoutProps}>{page}</MainFeedPage>
-    </SearchProvider>,
-    pageProps,
-    layoutProps,
-  );
-}
-
-SearchPostsPage.getLayout = getSearchLayout;
+SearchPostsPage.getLayout = SearchControlPage.getLayout;
 SearchPostsPage.layoutProps = SearchControlPage.layoutProps;
 
 export default withFeaturesBoundary<
