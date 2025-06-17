60b8cf23507eb01de926c414e06f2f1128cecd27
Amar Trebinjac <amartrebinjac@gmail.com>
2025-06-17 10:22:31 +0200

    nullcheck current member

--- packages/shared/src/components/squads/SquadActionButton.tsx
+++ packages/shared/src/components/squads/SquadActionButton.tsx
@@ -71,7 +71,7 @@ export const SimpleSquadJoinButton = <T extends 'a' | 'button'>({
       {...buttonProps}
       className={classNames(className)}
       onClick={(event) => {
-        if (!squad.currentMember) {
+        if (!squad?.currentMember) {
           logEvent({
             event_name: LogEvent.ClickJoinSquad,
             extra: JSON.stringify({
@@ -110,8 +110,8 @@ export const SquadActionButton = ({
   const { displayToast } = useToastNotification();
   const { user, showLogin } = useAuthContext();
   const isMemberBlocked =
-    squad.currentMember?.role === SourceMemberRole.Blocked;
-  const isCurrentMember = !!squad.currentMember && !isMemberBlocked;
+    squad?.currentMember?.role === SourceMemberRole.Blocked;
+  const isCurrentMember = !!squad?.currentMember && !isMemberBlocked;
 
   const fuzzyQueryKey = generateQueryKey(
     RequestKey.Sources,
