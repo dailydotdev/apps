cab44979f5088287786126bf415fc29739061a16
Chris Bongers <chrisbongers@gmail.com>
2025-06-19 11:40:16 +0200

    fix: lesser imports

--- packages/shared/src/components/dropdown/DropdownMenu.tsx
+++ packages/shared/src/components/dropdown/DropdownMenu.tsx
@@ -1,7 +1,13 @@
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 import type { ReactElement, ReactNode } from 'react';
 import React from 'react';
 import './style.css';
+import {
+  DropdownMenu as DropdownMenuRoot,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+} from '@radix-ui/react-dropdown-menu';
 import type { MenuItemProps } from '../fields/ContextMenu';
 
 export const DropdownMenu = ({
@@ -12,15 +18,12 @@ export const DropdownMenu = ({
   options: MenuItemProps[];
 }): ReactElement => {
   return (
-    <DropdownMenuPrimitive.Root>
-      <DropdownMenuPrimitive.Trigger>{children}</DropdownMenuPrimitive.Trigger>
-      <DropdownMenuPrimitive.Portal>
-        <DropdownMenuPrimitive.Content
-          className="DropdownMenuContent"
-          align="end"
-        >
+    <DropdownMenuRoot>
+      <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent className="DropdownMenuContent" align="end">
           {options.map(({ label, icon, action, disabled }: MenuItemProps) => (
-            <DropdownMenuPrimitive.Item
+            <DropdownMenuItem
               key={label}
               className="DropdownMenuItem"
               onClick={action}
@@ -29,10 +32,10 @@ export const DropdownMenu = ({
               <div className="flex w-full items-center gap-2 typo-callout">
                 {icon} {label}
               </div>
-            </DropdownMenuPrimitive.Item>
+            </DropdownMenuItem>
           ))}
-        </DropdownMenuPrimitive.Content>
-      </DropdownMenuPrimitive.Portal>
-    </DropdownMenuPrimitive.Root>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenuRoot>
   );
 };
