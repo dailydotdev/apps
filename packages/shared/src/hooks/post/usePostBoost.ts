68a48070e0452c39927b2f4a5bd641c3677a78ae
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-06-25 20:18:46 +0800

    fix: usage of ref

--- packages/shared/src/hooks/post/usePostBoost.ts
+++ packages/shared/src/hooks/post/usePostBoost.ts
@@ -1,6 +1,5 @@
 import type { InfiniteData } from '@tanstack/react-query';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useRef } from 'react';
 import { useAuthContext } from '../../contexts/AuthContext';
 import type {
   BoostedPostConnection,
@@ -21,15 +20,15 @@ interface UsePostBoost {
 }
 
 const FIRST_DEFAULT_VALUE = 20;
+const defaultStats = {
+  totalSpend: 0,
+  clicks: 0,
+  impressions: 0,
+  engagements: 0,
+};
 
 export const usePostBoost = (): UsePostBoost => {
   const { user } = useAuthContext();
-  const statsRef = useRef<BoostedPostStats>({
-    totalSpend: 0,
-    clicks: 0,
-    impressions: 0,
-    engagements: 0,
-  });
   const key = generateQueryKey(RequestKey.PostCampaigns, user, {
     first: FIRST_DEFAULT_VALUE,
   });
@@ -39,19 +38,11 @@ export const usePostBoost = (): UsePostBoost => {
     isFetched,
   } = useInfiniteQuery({
     queryKey: key,
-    queryFn: async ({ pageParam }) => {
-      const isFirstRequest = !pageParam;
-      const response = await getBoostedPostCampaigns({
+    queryFn: ({ pageParam }) =>
+      getBoostedPostCampaigns({
         first: FIRST_DEFAULT_VALUE,
         after: pageParam,
-      });
-
-      if (isFirstRequest) {
-        statsRef.current = response.stats;
-      }
-
-      return response;
-    },
+      }),
     initialPageParam: '',
     getNextPageParam: (data, _, lastPageParam) => {
       const nextPageparam = getNextPageParam(data?.pageInfo);
@@ -68,7 +59,7 @@ export const usePostBoost = (): UsePostBoost => {
 
   return {
     data: campaigns,
-    stats: statsRef.current,
+    stats: campaigns?.pages?.[0]?.stats ?? defaultStats,
     isLoading: isPending && !isFetched,
   };
 };
