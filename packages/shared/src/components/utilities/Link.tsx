c9cdbbee5115937201232337035e97ce39a7762d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-06-27 11:03:14 +0200

    fix: ESLint rule to prevent next/link import (#4620)

--- packages/shared/src/components/utilities/Link.tsx
+++ packages/shared/src/components/utilities/Link.tsx
@@ -1,9 +1,14 @@
+// eslint-disable-next-line no-restricted-imports
 import type { LinkProps as NextLinkProps } from 'next/link';
+// eslint-disable-next-line no-restricted-imports
 import NextLink from 'next/link';
 import type { ReactElement, ReactNode } from 'react';
 import React from 'react';
+import type { WithClassNameProps } from './common';
 
-interface LinkProps extends NextLinkProps {
+interface LinkProps extends NextLinkProps, WithClassNameProps {
+  target?: string;
+  rel?: string;
   children?: ReactElement | ReactNode | string;
 }
 
