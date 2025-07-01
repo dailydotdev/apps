e30be3b456645c664a6f0c09965e7d778d536f97
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-07-02 01:10:45 +0800

    refactor: feed context to use partial post for future proofing

--- packages/shared/src/features/boost/FeedCardContext.tsx
+++ packages/shared/src/features/boost/FeedCardContext.tsx
@@ -1,11 +1,14 @@
 import { createContext, useContext } from 'react';
+import type { Post } from '../../graphql/posts';
 
 type FeedCardContextData = {
   isBoostedReach: boolean;
+  post: Partial<Post>;
 };
 
 export const FeedCardContext = createContext<FeedCardContextData>({
   isBoostedReach: false,
+  post: {},
 });
 
 export const useFeedCardContext = () => useContext(FeedCardContext);
