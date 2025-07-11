7390e3af30f1831475fb727be451436b4d74d779
Chris Bongers <chrisbongers@gmail.com>
2025-07-11 15:39:20 +0200

    chore: move notification to new dropdown

--- packages/shared/src/components/notifications/NotificationItem.tsx
+++ packages/shared/src/components/notifications/NotificationItem.tsx
@@ -8,11 +8,25 @@ import { useObjectPurify } from '../../hooks/useDomPurify';
 import NotificationItemIcon from './NotificationIcon';
 import NotificationItemAttachment from './NotificationItemAttachment';
 import NotificationItemAvatar from './NotificationItemAvatar';
-import { NotificationType, notificationTypeTheme } from './utils';
-import OptionsButton from '../buttons/OptionsButton';
+import {
+  notificationMutingCopy,
+  NotificationType,
+  notificationTypeTheme,
+} from './utils';
 import { KeyboardCommand } from '../../lib/element';
 import { ProfileImageSize, ProfilePicture } from '../ProfilePicture';
 import { ProfilePictureGroup } from '../ProfilePictureGroup';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuOptions,
+  DropdownMenuTrigger,
+} from '../dropdown/DropdownMenu';
+import { Button, ButtonSize, ButtonVariant } from '../buttons/Button';
+import { BellDisabledIcon, BellIcon, MenuIcon } from '../icons';
+import { useNotificationPreference } from '../../hooks/notifications';
+import { NotificationPreferenceStatus } from '../../graphql/notifications';
+import { Loader } from '../Loader';
 
 export interface NotificationItemProps
   extends Pick<
@@ -32,8 +46,96 @@ export interface NotificationItemProps
       | React.MouseEvent<HTMLAnchorElement>
       | React.KeyboardEvent<HTMLAnchorElement>,
   ) => void;
-  onOptionsClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
 }
+
+const NotificationOptionsButton = ({
+  notification,
+}: {
+  notification: Pick<Notification, 'type' | 'referenceId'>;
+}): ReactElement => {
+  const {
+    preferences,
+    isFetching,
+    clearNotificationPreference,
+    muteNotification,
+  } = useNotificationPreference({
+    params: notification
+      ? [
+          {
+            notificationType: notification.type,
+            referenceId: notification.referenceId,
+          },
+        ]
+      : [],
+  });
+
+  const onItemClick = () => {
+    const isMuted =
+      preferences?.[0]?.status === NotificationPreferenceStatus.Muted;
+    const preferenceCommand = isMuted
+      ? clearNotificationPreference
+      : muteNotification;
+
+    return preferenceCommand({
+      type: notification.type,
+      referenceId: notification.referenceId,
+    });
+  };
+
+  const Icon = (): ReactElement => {
+    if (!notification) {
+      return null;
+    }
+
+    if (isFetching) {
+      return <Loader />;
+    }
+
+    const NotifIcon =
+      preferences[0]?.status === NotificationPreferenceStatus.Muted
+        ? BellIcon
+        : BellDisabledIcon;
+
+    return <NotifIcon />;
+  };
+
+  const label = useMemo((): string => {
+    if (!notification) {
+      return null;
+    }
+
+    if (isFetching) {
+      return 'Fetching your preference';
+    }
+
+    const isMuted =
+      preferences[0]?.status === NotificationPreferenceStatus.Muted;
+    const copy = notificationMutingCopy[notification?.type];
+
+    return isMuted ? copy.unmute : copy.mute;
+  }, [notification, preferences, isFetching]);
+  const options = [{ icon: <Icon />, label, action: onItemClick }];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        tooltip={{
+          content: 'Options',
+        }}
+      >
+        <Button
+          variant={ButtonVariant.Tertiary}
+          className="invisible absolute right-2 top-3 my-auto group-hover:visible"
+          icon={<MenuIcon />}
+          size={ButtonSize.Small}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuOptions options={options} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 function NotificationItem({
   icon,
@@ -44,9 +146,9 @@ function NotificationItem({
   avatars,
   attachments,
   onClick,
-  onOptionsClick,
   targetUrl,
   numTotalAvatars,
+  referenceId,
 }: NotificationItemProps): ReactElement {
   const {
     isReady,
@@ -120,12 +222,8 @@ function NotificationItem({
           </a>
         </Link>
       )}
-      {onOptionsClick && (
-        <OptionsButton
-          className="absolute right-2 top-3 hidden group-hover:flex"
-          type="button"
-          onClick={onOptionsClick}
-        />
+      {Object.keys(notificationMutingCopy).includes(type) && (
+        <NotificationOptionsButton notification={{ type, referenceId }} />
       )}
       <NotificationItemIcon
         icon={icon}
