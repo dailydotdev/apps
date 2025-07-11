eb551210120d43a9e1a0a631d04b5159160ab959
capJavert <dev@kickass.website>
2025-07-11 09:59:47 +0200

    feat: hide from team members as well

--- packages/shared/src/components/cards/brief/BriefCard/BriefCardFeed.tsx
+++ packages/shared/src/components/cards/brief/BriefCard/BriefCardFeed.tsx
@@ -3,13 +3,11 @@ import type { ReactElement } from 'react';
 import { useActions } from '../../../../hooks';
 import { BriefCard } from './BriefCard';
 import { ActionType } from '../../../../graphql/actions';
-import { useAuthContext } from '../../../../contexts/AuthContext';
 
 export const BriefCardFeed = (): ReactElement => {
   const { checkHasCompleted } = useActions();
-  const { user } = useAuthContext();
 
-  if (!user?.isTeamMember && checkHasCompleted(ActionType.GeneratedBrief)) {
+  if (checkHasCompleted(ActionType.GeneratedBrief)) {
     return null;
   }
 
