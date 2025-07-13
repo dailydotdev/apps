efe840486e58db9c605703b9511da36083f6f9a2
Amar Trebinjac <amartrebinjac@gmail.com>
2025-07-13 22:21:15 +0200

    position squad icons

--- packages/shared/src/components/cards/common/SquadPostCardHeader.tsx
+++ packages/shared/src/components/cards/common/SquadPostCardHeader.tsx
@@ -51,7 +51,7 @@ export const SquadPostCardHeader = ({
       )}
       <div
         className={classNames(
-          postUiExp && 'mt-4 ',
+          postUiExp && 'mx-4 mt-4 ',
           'relative m-2 flex gap-2',
           highlightBookmarkedPost && headerHiddenClassName,
         )}
