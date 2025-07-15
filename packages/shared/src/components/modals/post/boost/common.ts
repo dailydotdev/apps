4239ca7cacc8ad1a7b7427316ce87e92f1b9b57a
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-07-15 16:51:14 +0800

    fix: update tooltip copy

--- packages/shared/src/components/modals/post/boost/common.ts
+++ packages/shared/src/components/modals/post/boost/common.ts
@@ -1,6 +1,6 @@
 export const boostDashboardInfo = {
   engagement:
-    'The combined total of upvotes, comments, bookmarks, and other interactions with your boosted posts',
+    'The combined total of upvotes, comments, and other interactions with your boosted posts',
   impressions:
     'The total number of times your boosted posts were shown to users',
   clicks: 'The number of times users clicked on your boosted posts',
