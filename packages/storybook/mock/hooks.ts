76773012d48e089ac940d60c98cf3d32256a38df
Chris Bongers <chrisbongers@gmail.com>
2025-07-18 15:49:27 +0200

    fix: storybook build

--- packages/storybook/mock/hooks.ts
+++ packages/storybook/mock/hooks.ts
@@ -4,5 +4,14 @@ export const useConditionalFeature = fn()
   .mockName('useConditionalFeature')
   .mockReturnValue({ value: 'control', isLoading: false });
 
+export const usePostById = fn()
+  .mockName('usePostById')
+  .mockReturnValue({ 
+    post: null, 
+    relatedCollectionPosts: null, 
+    isError: false, 
+    isLoading: false 
+  });
+
 export * from '@dailydotdev/shared/src/hooks/utils'
 export * from '@dailydotdev/shared/src/hooks';
