55957aa5c0f9b1bd09a6352b5db7a413b9f04210
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-08-01 17:59:04 +0800

    fix: border on fixed navigation

--- packages/shared/src/components/post/FixedPostNavigation.tsx
+++ packages/shared/src/components/post/FixedPostNavigation.tsx
@@ -23,7 +23,7 @@ function FixedPostNavigation({
       contextMenuId="fixed-post-navigation-context"
       className={{
         container: classNames(
-          'fixed z-postNavigation ml-0 w-full border border-border-subtlest-tertiary bg-background-subtle px-4 py-1 laptop:px-6',
+          'fixed z-postNavigation ml-0 w-full border-b border-border-subtlest-tertiary bg-background-subtle px-4 py-1 laptop:border laptop:px-6',
           'max-w-full laptop:left-[unset]',
           isBannerVisible ? 'top-14' : 'top-0',
           className?.container,
