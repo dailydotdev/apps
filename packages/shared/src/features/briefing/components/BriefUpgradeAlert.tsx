dd9c4c0679b41d2eb9d8e952d6caab548bd8cd91
Luca Pagliaro <pagliaroluca@gmail.com>
2025-08-13 17:44:44 +0200

    feat: added BriefUpgradeAlert for non-plus users in BriefPostContent component

--- packages/shared/src/features/briefing/components/BriefUpgradeAlert.tsx
+++ packages/shared/src/features/briefing/components/BriefUpgradeAlert.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { type ComponentProps } from 'react';
 import { briefCardBg } from '../../../styles/custom';
 import {
   Typography,
   TypographyType,
 } from '../../../components/typography/Typography';
 import { BriefPlusUpgradeCTA } from './BriefPlusUpgradeCTA';
+import classNames from 'classnames';
 
-export const BriefUpgradeAlert = () => {
+export const BriefUpgradeAlert = ({
+  className,
+  ...attrs
+}: ComponentProps<'div'>) => {
   return (
     <div
       style={{
         background: briefCardBg,
       }}
-      className="mb-4 flex w-full flex-wrap items-center justify-between gap-2 rounded-12 border border-white px-4 py-3"
+      className={classNames(
+        'mb-4 flex w-full flex-wrap items-center justify-between gap-2 rounded-12 border border-white px-4 py-3',
+        className,
+      )}
+      {...attrs}
     >
       <Typography
         type={TypographyType.Callout}
-        className="w-full tablet:w-auto"
+        className="w-full tablet:w-auto flex-1"
       >
         Get unlimited access to every past and future presidential briefing with
         daily.dev Plus.
