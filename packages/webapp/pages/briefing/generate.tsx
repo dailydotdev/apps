ee4084ae3dd63c8fb0197d0e55f8e8f42687c375
Luca Pagliaro <pagliaroluca@gmail.com>
2025-08-13 17:26:50 +0200

    refactor: replaced Container in briefing generate page

--- packages/webapp/pages/briefing/generate.tsx
+++ packages/webapp/pages/briefing/generate.tsx
@@ -4,12 +4,21 @@ import { GenerateBriefForFreeUserPage } from '@dailydotdev/shared/src/features/b
 import { useAuthContext } from '@dailydotdev/shared/src/contexts/AuthContext';
 import { webappUrl } from '@dailydotdev/shared/src/lib/constants';
 import { useRouter } from 'next/router';
-import { PostContainer } from '@dailydotdev/shared/src/components/post/common';
-import { PageBodyContainer } from '@dailydotdev/shared/src/components/post/PostContentContainer';
+import classed from '@dailydotdev/shared/src/lib/classed';
+import { pageBorders } from '@dailydotdev/shared/src/components/utilities';
+import { pageMainClassNames } from '@dailydotdev/shared/src/components/layout/PageWrapperLayout';
 import ProtectedPage from '../../components/ProtectedPage';
 import { getLayout } from '../../components/layouts/MainLayout';
 import { getLayout as getFooterNavBarLayout } from '../../components/layouts/FooterNavBarLayout';
 import { getTemplatedTitle } from '../../components/layouts/utils';
+
+const Container = classed(
+  'main',
+  pageMainClassNames,
+  pageBorders,
+  'items-stretch tablet:self-center laptop:min-h-page',
+  '!px-0 mx-auto laptop:max-w-[48rem]',
+);
 
 function Page() {
   const { isAuthReady, user } = useAuthContext();
@@ -27,11 +36,9 @@ function Page() {
 
   return (
     <ProtectedPage>
-      <PostContainer>
-        <PageBodyContainer>
-          <GenerateBriefForFreeUserPage />
-        </PageBodyContainer>
-      </PostContainer>
+      <Container>
+        <GenerateBriefForFreeUserPage />
+      </Container>
     </ProtectedPage>
   );
 }
