6bb141b2f9644306c126042db13966b30c583538
Luca Pagliaro <pagliaroluca@gmail.com>
2025-08-19 15:43:49 +0200

    refactor: renamed component and added logging for brief feed events

--- packages/shared/src/lib/log.ts
+++ packages/shared/src/lib/log.ts
@@ -306,6 +306,8 @@ export enum LogEvent {
   // Start Brief
   ImpressionBrief = 'impression brief',
   ClickBrief = 'click brief',
+  StartBriefPurchase = 'start brief purchase',
+  BriefPaywall = 'brief paywall',
   // End Brief
   // CV Upload
   UploadCv = 'upload cv',
@@ -403,6 +405,7 @@ export enum TargetId {
   SmartPrompt = 'smart prompt',
   Account = 'account',
   Brief = 'brief',
+  BriefPlacement = 'brief placement',
   NotificationSettings = 'notification settings',
   Feed = 'feed',
   CVWidget = 'cv widget',
