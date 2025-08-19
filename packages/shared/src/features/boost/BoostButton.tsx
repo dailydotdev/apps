139a61b7ec4aa7760fa99f34468c1484c70597c2
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-08-20 02:08:26 +0800

    refactor: boost button

--- packages/shared/src/features/boost/BoostButton.tsx
+++ packages/shared/src/features/boost/BoostButton.tsx
@@ -9,21 +9,29 @@ import { LazyModal } from '../../components/modals/common/types';
 import { useLazyModal } from '../../hooks/useLazyModal';
 import type { Squad } from '../../graphql/sources';
 import { boostButton } from '../../styles/custom';
+import { useCampaignById } from '../../graphql/campaigns';
+import { useAuthContext } from '../../contexts/AuthContext';
+import ConditionalWrapper from '../../components/ConditionalWrapper';
+import { Tooltip } from '../../components/tooltip/Tooltip';
+
+interface BoostButtonProps {
+  buttonProps?: ButtonProps<'button'>;
+  campaignId?: string;
+}
 
 export function BoostButton({
   buttonProps = {},
-}: {
-  buttonProps?: ButtonProps<'button'>;
-}): ReactElement {
+  campaignId,
+}: BoostButtonProps): ReactElement {
   return (
     <Button
       style={{ background: boostButton }}
-      variant={ButtonVariant.Primary}
+      variant={campaignId ? ButtonVariant.Subtle : ButtonVariant.Primary}
       {...buttonProps}
       icon={<BoostIcon secondary />}
       color={ButtonColor.BlueCheese}
     >
-      Boost
+      {campaignId ? 'Boosting' : 'Boost'}
     </Button>
   );
 }
@@ -36,13 +44,22 @@ export function BoostPostButton({
   buttonProps?: ButtonProps<'button'>;
 }): ReactElement {
   const { openModal } = useLazyModal();
+  const campaignId = post?.flags?.campaignId;
+  const { data: campaign, isFetched } = useCampaignById(campaignId);
+
+  const onBoost = () =>
+    openModal({ type: LazyModal.BoostPost, props: { post } });
+
+  const onViewBoost = () =>
+    openModal({ type: LazyModal.BoostedPostView, props: { campaign } });
 
   return (
     <BoostButton
+      campaignId={campaignId}
       buttonProps={{
         ...buttonProps,
-        onClick: () =>
-          openModal({ type: LazyModal.BoostPost, props: { post } }),
+        loading: campaignId && !isFetched,
+        onClick: campaignId ? onViewBoost : onBoost,
       }}
     />
   );
@@ -56,14 +73,54 @@ export function BoostSourceButton({
   buttonProps?: ButtonProps<'button'>;
 }): ReactElement {
   const { openModal } = useLazyModal();
+  const { user } = useAuthContext();
+  const campaignId = squad?.flags?.campaignId;
+  const { data: campaign, isFetched } = useCampaignById(campaignId);
+  const isBooster = user.id === campaign?.user.id;
+
+  const onBoost = () =>
+    openModal({ type: LazyModal.BoostSquad, props: { squad } });
+
+  const onViewBoost = () =>
+    openModal({ type: LazyModal.BoostedPostView, props: { campaign } });
+
+  const onClick = () => {
+    if (!isBooster) {
+      return null;
+    }
+
+    if (campaignId) {
+      return onViewBoost();
+    }
+
+    return onBoost();
+  };
+
+  const hasTooltip = campaignId && !isBooster && !!campaign;
 
   return (
-    <BoostButton
-      buttonProps={{
-        ...buttonProps,
-        onClick: () =>
-          openModal({ type: LazyModal.BoostSquad, props: { squad } }),
-      }}
-    />
+    <ConditionalWrapper
+      condition={hasTooltip}
+      wrapper={(component) => (
+        <Tooltip
+          className="max-w-64"
+          content={`The Squad is currently being boosted by ${
+            campaign.user.name
+          }. You can start a new boost once this one ends on ${campaign.endedAt.toLocaleString()}`}
+        >
+          <div>{component}</div>
+        </Tooltip>
+      )}
+    >
+      <BoostButton
+        campaignId={campaignId}
+        buttonProps={{
+          ...buttonProps,
+          onClick,
+          disabled: hasTooltip,
+          loading: campaignId && !isFetched,
+        }}
+      />
+    </ConditionalWrapper>
   );
 }
