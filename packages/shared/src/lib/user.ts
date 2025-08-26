9cb95e8a5b4365fdce17e1d1875d7fd3401d71e6
Ante Barić <ante@kickass.website>
2025-08-26 12:00:31 +0200

    feat: post analytics entry points (#4813)

--- packages/shared/src/lib/user.ts
+++ packages/shared/src/lib/user.ts
@@ -9,6 +9,7 @@ import type { ContentPreference } from '../graphql/contentPreference';
 import type { TopReader } from '../components/badges/TopReaderBadge';
 import type { SubscriptionProvider, SubscriptionStatus } from './plus';
 import type { FeaturedAward, UserTransactionPublic } from '../graphql/njord';
+import type { Post } from '../graphql/posts';
 
 export enum Roles {
   Moderator = 'moderator',
@@ -264,4 +265,18 @@ export const isSpecialUser = ({
 
 export const getFirstName = (name: string): string => {
   return name?.split?.(' ')?.[0] ?? '';
+};
+
+export const canViewPostAnalytics = ({
+  user,
+  post,
+}: {
+  user?: Pick<LoggedUser, 'id' | 'isTeamMember'>;
+  post?: Pick<Post, 'author'>;
+}): boolean => {
+  if (user?.isTeamMember) {
+    return true;
+  }
+
+  return !!user?.id && user.id === post?.author?.id;
 };
