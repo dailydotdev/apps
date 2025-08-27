6be53d98c4ccd09396728c0e32caaabfa9011afd
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-08-27 20:16:14 +0800

    fix: preview alignment

--- packages/shared/src/features/boost/CampaignListViewSquad.tsx
+++ packages/shared/src/features/boost/CampaignListViewSquad.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement } from 'react';
 import React from 'react';
-import { Separator } from '@radix-ui/react-dropdown-menu';
 import { Button } from '../../components/buttons/Button';
 import { ButtonVariant } from '../../components/buttons/common';
 import { OpenLinkIcon } from '../../components/icons';
@@ -12,6 +11,7 @@ import {
 import { CampaignListViewContainer } from './common';
 import { Image } from '../../components/image/Image';
 import type { Squad } from '../../graphql/sources';
+import { Separator } from '../../components/cards/common/common';
 
 interface CampaignListViewPostProps {
   squad: Squad;
@@ -22,7 +22,7 @@ export function CampaignListViewSquad({
 }: CampaignListViewPostProps): ReactElement {
   return (
     <CampaignListViewContainer>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-1 flex-col">
         <span className="flex flex-1 flex-row">
           <Typography
             type={TypographyType.Callout}
@@ -35,7 +35,7 @@ export function CampaignListViewSquad({
         <span className="flex flex-1 flex-row">
           <Typography
             type={TypographyType.Subhead}
-            className="line-clamp-1 flex-1"
+            className="line-clamp-1 flex flex-1 flex-row"
             style={{ lineBreak: 'anywhere' }}
             color={TypographyColor.Tertiary}
           >
