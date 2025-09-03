24982f19103542057dd743fcc31b5d7b6deebd94
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-09-03 21:11:59 +0800

    chore: uncomment cores checks for squad boost

--- packages/shared/src/features/monetization/useFetchAd.ts
+++ packages/shared/src/features/monetization/useFetchAd.ts
@@ -20,7 +20,6 @@ export const useFetchAd = (): UseFetchAds => {
       if (isEnabled) {
         params.append('allow_post_boost', 'true');
         params.append('allow_squad_boost', 'true');
-        params.append('only_squad_boost', 'true'); // TODO: remove before shipping
       }
 
       return fetchAd(params);
