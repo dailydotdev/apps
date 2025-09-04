284801eeb6a36b25c856ae079a6554e053c99773
capJavert <dev@kickass.website>
2025-09-04 16:55:28 +0200

    fix: pixel

--- packages/shared/src/components/cards/ad/common/AdPixel.tsx
+++ packages/shared/src/components/cards/ad/common/AdPixel.tsx
@@ -1,19 +1,23 @@
 import type { ReactElement } from 'react';
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 import type { Ad } from '../../../../graphql/posts';
 
 export const AdPixel = ({ pixel }: Pick<Ad, 'pixel'>): ReactElement => {
+  const { ref, inView } = useInView({ triggerOnce: true });
+
   return (
-    <>
-      {pixel?.map((p) => (
-        <img
-          src={p}
-          key={p}
-          data-testid="pixel"
-          className="hidden size-0"
-          alt="Pixel"
-        />
-      ))}
-    </>
+    <span ref={ref} className="size-0">
+      {inView &&
+        pixel?.map((p) => (
+          <img
+            src={p}
+            key={p}
+            data-testid="pixel"
+            className="hidden size-0"
+            alt="Pixel"
+          />
+        ))}
+    </span>
   );
 };
