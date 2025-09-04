fed2c47aa17152dfd7837a46a6334e6be609b561
capJavert <dev@kickass.website>
2025-09-04 17:12:18 +0200

    fix: in tests

--- packages/shared/src/components/cards/ad/common/AdPixel.tsx
+++ packages/shared/src/components/cards/ad/common/AdPixel.tsx
@@ -2,9 +2,13 @@ import type { ReactElement } from 'react';
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
 import type { Ad } from '../../../../graphql/posts';
+import { isTesting } from '../../../../lib/constants';
 
 export const AdPixel = ({ pixel }: Pick<Ad, 'pixel'>): ReactElement => {
-  const { ref, inView } = useInView({ triggerOnce: true });
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    initialInView: isTesting, // interection observer in tests
+  });
 
   return (
     <span ref={ref} className="size-0">
