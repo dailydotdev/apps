4fb747116f4d0d1a3cd37d7ca43a707ddc1f8f18
Luca Pagliaro <pagliaroluca@gmail.com>
2025-09-04 12:09:07 +0200

    fix: turn default flag value to false

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -101,7 +101,7 @@ export const briefGeneratePricing = new Feature<Record<BriefingType, number>>(
 
 export const briefFeedEntrypointPage = new Feature<false | number>(
   'brief_feed_entrypoint_page',
-  3,
+  false,
 );
 
 export const briefUIFeature = new Feature('brief_ui', isDevelopment);
