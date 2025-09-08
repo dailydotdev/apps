f2ee253a27f53b34beb0305f9bc7c8d22b26b5d0
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2025-09-09 05:27:29 +0800

    fix: auto generate username ux (#4846)

--- packages/shared/src/hooks/onboarding/useGenerateUsername.ts
+++ packages/shared/src/hooks/onboarding/useGenerateUsername.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useRequestProtocol } from '../useRequestProtocol';
 import { GET_USERNAME_SUGGESTION } from '../../graphql/users';
 import { StaleTime } from '../../lib/query';
@@ -8,6 +8,7 @@ import { disabledRefetch } from '../../lib/func';
 interface UseGenerateUsername {
   username?: string;
   setUsername: (value: string) => void;
+  isLoading: boolean;
 }
 
 export const useGenerateUsername = (
@@ -17,33 +18,31 @@ export const useGenerateUsername = (
   const usernameRef = useRef(false);
   const { requestMethod } = useRequestProtocol();
   const usernameQueryKey = ['generateUsername', name];
-  const { data } = useQuery<{
+  const { data, isLoading } = useQuery<{
     generateUniqueUsername: string;
   }>({
     queryKey: usernameQueryKey,
-
     queryFn: () =>
       requestMethod(
         GET_USERNAME_SUGGESTION,
         { name },
         { requestKey: JSON.stringify(usernameQueryKey) },
       ),
-
     enabled: !!name?.length && usernameRef.current !== true,
     staleTime: StaleTime.Default,
     ...disabledRefetch,
   });
 
-  if (data?.generateUniqueUsername && !usernameRef.current) {
-    usernameRef.current = true;
-
-    if (!username.length) {
+  useEffect(() => {
+    if (data?.generateUniqueUsername && !usernameRef.current) {
+      usernameRef.current = true;
       setUsername(data.generateUniqueUsername);
     }
-  }
+  }, [data?.generateUniqueUsername]);
 
   return {
     username,
     setUsername,
+    isLoading,
   };
 };
