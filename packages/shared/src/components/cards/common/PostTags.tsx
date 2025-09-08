b687ac5f30c34c23fb5773641a38fb89d6cd582f
Chris Bongers <chrisbongers@gmail.com>
2025-09-08 15:20:58 +0200

    fix: calculation of tags

--- packages/shared/src/components/cards/common/PostTags.tsx
+++ packages/shared/src/components/cards/common/PostTags.tsx
@@ -22,8 +22,7 @@ export default function PostTags({
 }: PostTagsProps): ReactElement {
   const [width, setWidth] = useState(0);
   const { isListMode } = useFeedLayout();
-  // const tags = post?.tags || [];
-  const tags = ['webdev', 'automation', 'nocode', 'productivity', 'javascript'];
+  const tags = post?.tags || [];
   const elementRef = useRef<HTMLDivElement>(null);
   const list = useFeedTags({
     tags,
