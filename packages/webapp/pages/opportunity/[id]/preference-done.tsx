2b084e234e0166069d14d942f7fc1aa296a5f52f
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-09-10 12:34:43 +0200

    fix: missed one

--- packages/webapp/pages/opportunity/[id]/preference-done.tsx
+++ packages/webapp/pages/opportunity/[id]/preference-done.tsx
@@ -25,6 +25,7 @@ import {
   defaultSeo,
   defaultSeoTitle,
 } from '../../../next-seo';
+import { opportunityPageLayoutProps } from '../../../components/layouts/utils';
 
 const seo: NextSeoProps = {
   title: defaultSeoTitle,
@@ -123,8 +124,7 @@ const getPageLayout: typeof getLayout = (...page) => getLayout(...page);
 
 PreferenceDonePage.getLayout = getPageLayout;
 PreferenceDonePage.layoutProps = {
-  className: 'gap-10 laptop:pt-10 pb-10',
-  screenCentered: true,
+  ...opportunityPageLayoutProps,
   hideBackButton: true,
   seo,
 };
