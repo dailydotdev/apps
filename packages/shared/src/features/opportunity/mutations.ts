effae2dcfc574f3e048aec6f087300d153173904
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-09-15 14:47:10 +0200

    feat: clear uploaded CV

--- packages/shared/src/features/opportunity/mutations.ts
+++ packages/shared/src/features/opportunity/mutations.ts
@@ -2,6 +2,7 @@ import type { DefaultError, MutationOptions } from '@tanstack/react-query';
 import { gqlClient } from '../../graphql/common';
 import {
   ACCEPT_OPPORTUNITY_MATCH,
+  CLEAR_RESUME_MUTATION,
   SAVE_OPPORTUNITY_SCREENING_ANSWERS,
   UPDATE_CANDIDATE_PREFERENCES_MUTATION,
 } from './graphql';
@@ -65,6 +66,25 @@ export const acceptOpportunityMatchMutationOptions = (
       return gqlClient.request(ACCEPT_OPPORTUNITY_MATCH, {
         id: opportunityId,
       });
+    },
+  };
+};
+
+export const clearResumeMutationOptions = (
+  [get, set]: UseUpdateQuery<UserCandidatePreferences>,
+  successCallback?: () => void,
+): MutationOptions<EmptyResponse> => {
+  const preferences = get();
+  return {
+    mutationFn: async () => {
+      return gqlClient.request(CLEAR_RESUME_MUTATION);
+    },
+    onSuccess: () => {
+      set({
+        ...preferences,
+        cv: undefined,
+      });
+      successCallback?.();
     },
   };
 };
