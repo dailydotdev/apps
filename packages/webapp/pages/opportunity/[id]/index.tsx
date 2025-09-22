47452e7606ddab71bd60f6249d6fdb3f5598da86
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-09-22 11:38:12 +0200

    fix: hide footer bar when no match

--- packages/webapp/pages/opportunity/[id]/index.tsx
+++ packages/webapp/pages/opportunity/[id]/index.tsx
@@ -320,15 +320,17 @@ const JobPage = ({
         />
       )}
       {!hasCompletedInitialView && <JobPageIntro />}
-      <ResponseButtons
-        id={opportunity.id}
-        className={{
-          buttons: 'flex-1',
-          container:
-            'fixed bottom-0 z-header flex min-h-14 w-full items-center gap-4 border-t border-border-subtlest-tertiary bg-background-default px-4 tablet:hidden',
-        }}
-        size={ButtonSize.Medium}
-      />
+      {!!match && (
+        <ResponseButtons
+          id={opportunity.id}
+          className={{
+            buttons: 'flex-1',
+            container:
+              'fixed bottom-0 z-header flex min-h-14 w-full items-center gap-4 border-t border-border-subtlest-tertiary bg-background-default px-4 tablet:hidden',
+          }}
+          size={ButtonSize.Medium}
+        />
+      )}
       <div className="mx-auto flex w-full max-w-[69.25rem] flex-col gap-4 laptop:flex-row">
         <div className="h-full min-w-0 max-w-full flex-1 flex-shrink-0 rounded-16 border border-border-subtlest-tertiary">
           {/* Header */}
