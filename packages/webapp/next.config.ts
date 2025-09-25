aa944c23b8a3a85c0cd7cba36f25d4e20aa90358
Chris Bongers <chrisbongers@gmail.com>
2025-09-25 13:46:09 +0200

    cross-fetch disable

--- packages/webapp/next.config.ts
+++ packages/webapp/next.config.ts
@@ -58,9 +58,6 @@ const nextConfig: NextConfig = {
         as: '*.js',
       },
     },
-    resolveAlias: {
-      'cross-fetch': '',
-    },
   },
   ...withSerwist({
     ...withBundleAnalyzer({
