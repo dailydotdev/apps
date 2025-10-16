4f4c6fff55000c63840014219e9bbc2fde7cf5fc
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2025-10-16 21:50:41 +0800

    feat: autofill profile cv upload (#5004)

--- packages/webapp/components/layouts/ProfileLayout/index.tsx
+++ packages/webapp/components/layouts/ProfileLayout/index.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement, ReactNode } from 'react';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { PublicProfile } from '@dailydotdev/shared/src/lib/user';
 import {
   getProfile,
@@ -22,11 +22,6 @@ import { useProfile } from '@dailydotdev/shared/src/hooks/profile/useProfile';
 import CustomAuthBanner from '@dailydotdev/shared/src/components/auth/CustomAuthBanner';
 import { useLogContext } from '@dailydotdev/shared/src/contexts/LogContext';
 import { LogEvent, TargetType } from '@dailydotdev/shared/src/lib/log';
-import ConditionalWrapper from '@dailydotdev/shared/src/components/ConditionalWrapper';
-import { ProfileUploadBanner } from '@dailydotdev/shared/src/features/profile/components/ProfileUploadBanner';
-import { useUploadCv } from '@dailydotdev/shared/src/features/profile/hooks/useUploadCv';
-import { ActionType } from '@dailydotdev/shared/src/graphql/actions';
-import { useActions } from '@dailydotdev/shared/src/hooks';
 import { usePostReferrerContext } from '@dailydotdev/shared/src/contexts/PostReferrerContext';
 import { getLayout as getFooterNavBarLayout } from '../FooterNavBarLayout';
 import { getLayout as getMainLayout } from '../MainLayout';
@@ -80,15 +75,9 @@ export default function ProfileLayout({
 }: ProfileLayoutProps): ReactElement {
   const router = useRouter();
   const { isFallback } = router;
-  const { user, isUserSame } = useProfile(initialUser);
+  const { user } = useProfile(initialUser);
   const [trackedView, setTrackedView] = useState(false);
   const { logEvent } = useLogContext();
-  const { status, onUpload, shouldShow, onCloseBanner } = useUploadCv();
-  const { checkHasCompleted } = useActions();
-  const hasClosedBanner = useMemo(
-    () => checkHasCompleted(ActionType.ClosedProfileBanner),
-    [checkHasCompleted],
-  );
   const { referrerPost } = usePostReferrerContext();
 
   useEffect(() => {
@@ -119,37 +108,20 @@ export default function ProfileLayout({
   }
 
   return (
-    <ConditionalWrapper
-      condition={isUserSame && shouldShow && !hasClosedBanner}
-      wrapper={(component) => (
-        <div className="flex w-full flex-col">
-          <ProfileUploadBanner
-            className={{ container: '!mt-0 tablet:mt-3' }}
-            onClose={onCloseBanner}
-            onUpload={onUpload}
-            status={status}
-          />
-          {component}
-        </div>
-      )}
-    >
-      <div className="m-auto flex w-full max-w-screen-laptop flex-col pb-12 tablet:pb-0 laptop:min-h-page laptop:flex-row laptop:border-l laptop:border-r laptop:border-border-subtlest-tertiary laptop:pb-6 laptopL:pb-0">
-        <Head>
-          <link rel="preload" as="image" href={user.image} />
-        </Head>
-        <main className="relative flex flex-1 flex-col tablet:border-r tablet:border-border-subtlest-tertiary">
-          {children}
-        </main>
-        <PageWidgets className="hidden !px-0 laptop:flex">
-          <ProfileWidgets
-            user={user}
-            userStats={userStats}
-            sources={sources}
-            className="w-full"
-          />
-        </PageWidgets>
-      </div>
-    </ConditionalWrapper>
+    <div className="m-auto flex w-full max-w-screen-laptop flex-col pb-12 tablet:pb-0 laptop:min-h-page laptop:flex-row laptop:pb-6 laptopL:pb-0">
+      <Head>
+        <link rel="preload" as="image" href={user.image} />
+      </Head>
+      <main className="relative flex flex-1 flex-col">{children}</main>
+      <PageWidgets className="hidden !px-0 laptop:flex">
+        <ProfileWidgets
+          user={user}
+          userStats={userStats}
+          sources={sources}
+          className="w-full"
+        />
+      </PageWidgets>
+    </div>
   );
 }
 
