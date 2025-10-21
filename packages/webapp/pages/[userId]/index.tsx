9dc377394b45188496f5799bc9539a4bfe5f31da
NensiDosari <nensidosari97@gmail.com>
2025-10-21 10:43:40 +0200

    fix: lint

--- packages/webapp/pages/[userId]/index.tsx
+++ packages/webapp/pages/[userId]/index.tsx
@@ -16,17 +16,16 @@ import {
   TypographyColor,
   Typography,
 } from '@dailydotdev/shared/src/components/typography/Typography';
-import type { ProfileLayoutProps } from '../../components/layouts/ProfileLayout';
+import { useDynamicHeader } from '@dailydotdev/shared/src/useDynamicHeader';
+import { Header } from '@dailydotdev/shared/src/components/profile/Header';
+import classNames from 'classnames';
 import {
   getLayout as getProfileLayout,
   getProfileSeoDefaults,
   getStaticPaths as getProfileStaticPaths,
   getStaticProps as getProfileStaticProps,
 } from '../../components/layouts/ProfileLayout';
-import { useDynamicHeader } from '@dailydotdev/shared/src/useDynamicHeader';
-import { Header } from '@dailydotdev/shared/src/components/profile/Header';
-import classNames from 'classnames';
-
+import type { ProfileLayoutProps } from '../../components/layouts/ProfileLayout';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ProfilePage = ({
@@ -45,9 +44,8 @@ const ProfilePage = ({
 
   const { user, isUserSame } = useProfile(initialUser);
   const { ref: stickyRef, progress: stickyProgress } =
-  useDynamicHeader<HTMLDivElement>(true);
+    useDynamicHeader<HTMLDivElement>(true);
   const hideSticky = !stickyProgress;
-
 
   const seo: NextSeoProps = {
     ...getProfileSeoDefaults(user, {}, noindex),
@@ -58,16 +56,16 @@ const ProfilePage = ({
   return (
     <div className="rounded-16 border border-border-subtlest-tertiary">
       <NextSeo {...seo} />
-        <Header
-          user={user}
-          isSameUser={isUserSame}
-          sticky={!hideSticky}
-          className={classNames(
-            'left-0 top-0 z-3 w-full bg-background-default transition-all duration-75',
-            !hideSticky ? 'fixed tablet:pl-20' : 'relative',
-          )}
-        />
-      <div ref={stickyRef}/>
+      <Header
+        user={user}
+        isSameUser={isUserSame}
+        sticky={!hideSticky}
+        className={classNames(
+          'left-0 top-0 z-3 w-full bg-background-default transition-all duration-75',
+          !hideSticky ? 'fixed tablet:pl-20' : 'relative',
+        )}
+      />
+      <div ref={stickyRef} />
       <ProfileHeader user={user} userStats={userStats} />
       <div className="flex flex-col gap-6 p-6">
         {shouldShowBanner && (
