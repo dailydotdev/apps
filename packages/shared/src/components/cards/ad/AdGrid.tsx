4ecb1c2921c30c0d1d52811f579e5af4cfeceb22
Chris Bongers <chrisbongers@gmail.com>
2025-10-28 19:39:17 +0200

    feat: add favicon for ad links

--- packages/shared/src/components/cards/ad/AdGrid.tsx
+++ packages/shared/src/components/cards/ad/AdGrid.tsx
@@ -1,7 +1,13 @@
 import type { ReactElement } from 'react';
 import React, { forwardRef, useCallback } from 'react';
 
-import { Card, CardImage, CardTextContainer, CardTitle } from '../common/Card';
+import {
+  Card,
+  CardHeader,
+  CardImage,
+  CardTextContainer,
+  CardTitle,
+} from '../common/Card';
 import AdLink from './common/AdLink';
 import AdAttribution from './common/AdAttribution';
 import { AdImage } from './common/AdImage';
@@ -13,11 +19,15 @@ import type { InViewRef } from '../../../hooks/feed/useAutoRotatingAds';
 import { useAutoRotatingAds } from '../../../hooks/feed/useAutoRotatingAds';
 import { AdRefresh } from './common/AdRefresh';
 import { ButtonSize, ButtonVariant } from '../../buttons/common';
+import { apiUrl } from '../../../lib/config';
+import { ProfileImageSize, ProfilePicture } from '../../ProfilePicture';
 
 export const AdGrid = forwardRef(function AdGrid(
   { ad, onLinkClick, onRefresh, domProps, index, feedIndex }: AdCardProps,
   inViewRef: InViewRef,
 ): ReactElement {
+  const pixelRatio = globalThis?.window.devicePixelRatio ?? 1;
+  const iconSize = Math.round(24 * pixelRatio);
   const { isPlus } = usePlusSubscription();
   const { ref, refetch, isRefetching } = useAutoRotatingAds(
     ad,
@@ -34,6 +44,19 @@ export const AdGrid = forwardRef(function AdGrid(
   return (
     <Card {...domProps} data-testid="adItem" ref={ref}>
       <AdLink ad={ad} onLinkClick={onLinkClick} />
+      <CardHeader className="mx-4">
+        <ProfilePicture
+          size={ProfileImageSize.Medium}
+          user={{
+            id: ad.link,
+            image: `${apiUrl}/icon?url=${encodeURIComponent(
+              ad.link,
+            )}&size=${iconSize}`,
+            username: ad.description,
+          }}
+          nativeLazyLoading
+        />
+      </CardHeader>
       <CardTextContainer className="flex-1">
         <CardTitle className="line-clamp-4 typo-title3">
           {ad.description}
--- packages/shared/src/components/cards/ad/AdList.tsx
+++ packages/shared/src/components/cards/ad/AdList.tsx
@@ -15,6 +15,9 @@ import type { InViewRef } from '../../../hooks/feed/useAutoRotatingAds';
 import { useAutoRotatingAds } from '../../../hooks/feed/useAutoRotatingAds';
 import { AdRefresh } from './common/AdRefresh';
 import AdAttribution from './common/AdAttribution';
+import { ProfileImageSize, ProfilePicture } from '../../ProfilePicture';
+import { apiUrl } from '../../../lib/config';
+import { CardHeader } from '../common/Card';
 
 const getLinkProps = ({
   ad,
@@ -36,6 +39,8 @@ export const AdList = forwardRef(function AdCard(
   { ad, onLinkClick, onRefresh, domProps, index, feedIndex }: AdCardProps,
   inViewRef: InViewRef,
 ): ReactElement {
+  const pixelRatio = globalThis?.window.devicePixelRatio ?? 1;
+  const iconSize = Math.round(24 * pixelRatio);
   const { isPlus } = usePlusSubscription();
   const { ref, refetch, isRefetching } = useAutoRotatingAds(
     ad,
@@ -60,6 +65,19 @@ export const AdList = forwardRef(function AdCard(
         <CardTitle
           className={classNames('!mt-0 mr-4 line-clamp-4 flex-1 typo-title3')}
         >
+          <CardHeader className="mx-0 !mt-0 mb-2">
+            <ProfilePicture
+              size={ProfileImageSize.Medium}
+              user={{
+                id: ad.link,
+                image: `${apiUrl}/icon?url=${encodeURIComponent(
+                  ad.link,
+                )}&size=${iconSize}`,
+                username: ad.description,
+              }}
+              nativeLazyLoading
+            />
+          </CardHeader>
           {ad.description}
           <AdAttribution
             ad={ad}
