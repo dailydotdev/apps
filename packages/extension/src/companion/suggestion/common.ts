e1cd0dbd1f416c5c8233b5a8ff601e0f2dd2b538
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-10-28 21:03:46 +0800

    fix: duplication bug

--- packages/extension/src/companion/suggestion/common.ts
+++ packages/extension/src/companion/suggestion/common.ts
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 const containerClass = 'msg-s-message-list-content';
 
 interface GenerateReplySuggestionProps {
@@ -6,28 +8,40 @@ interface GenerateReplySuggestionProps {
   wrapper?: HTMLElement | Document;
 }
 
+const customClass = 'dd-custom-suggestion';
+
 export const generateReplySuggestion = ({
   cta,
   onClick,
   wrapper = document,
 }: GenerateReplySuggestionProps) => {
+  const exists = wrapper.querySelector(`.${customClass}`);
+
+  if (exists) {
+    return;
+  }
+
   const parent = wrapper.querySelector(`.${containerClass}`);
+
+  if (!parent) {
+    return;
+  }
+
   const container = document.createElement('div');
   container.style =
     'display: flex; justify-content: center; margin-bottom: 8px;';
   const constructed = document.createElement('button');
   constructed.setAttribute(
     'class',
-    'conversations-quick-replies__reply-button artdeco-button artdeco-button--2 artdeco-button--secondary',
+    classNames(
+      'conversations-quick-replies__reply-button artdeco-button artdeco-button--2 artdeco-button--secondary',
+      customClass,
+    ),
   );
   constructed.style.marginLeft = 'auto';
   constructed.style.marginRight = 'auto';
   constructed.innerText = cta;
   constructed.onclick = onClick;
-
-  if (!parent) {
-    return;
-  }
 
   container.appendChild(constructed);
   parent.appendChild(container);
