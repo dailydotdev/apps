83bf0d0335541f5d497ecf6dea1f98ff9e9be753
Amar Trebinjac <amartrebinjac@gmail.com>
2025-11-06 20:08:22 +0100

    fix type errors

--- packages/shared/src/hooks/useUserExperienceForm.ts
+++ packages/shared/src/hooks/useUserExperienceForm.ts
@@ -14,17 +14,17 @@ import {
 } from '../graphql/user/profile';
 import { useDirtyForm } from './useDirtyForm';
 import { ApiError } from '../graphql/common';
+import type { ApiErrorResult } from '../graphql/common';
 import { labels } from '../lib/labels';
 import { applyZodErrorsToForm } from '../lib/form';
 import { useToastNotification } from './useToastNotification';
-import type { GraphQLError } from '../lib/errors';
 
 export const userExperienceInputBaseSchema = z.object({
   type: z.enum(['work', 'education', 'project', 'certification']),
-  title: z.string().max(1000).nonempty('Title is required.'),
+  title: z.string().min(1, 'Title is required.').max(1000),
   description: z.string().max(5000).optional(),
   subtitle: z.string().max(1000).optional(),
-  startedAt: z.date('Start date is required.'),
+  startedAt: z.date({ message: 'Start date is required.' }),
   endedAt: z.date().optional(),
   companyId: z.string().nullable().optional().default(null),
   customCompanyName: z
@@ -32,7 +32,6 @@ export const userExperienceInputBaseSchema = z.object({
     .trim()
     .normalize()
     .max(100)
-    .nonempty()
     .nullable()
     .optional()
     .default(null),
@@ -78,16 +77,16 @@ const useUserExperienceForm = ({
     resolver: zodResolver(userExperienceInputBaseSchema),
   });
   const { mutate, isPending } = useMutation({
-    mutationFn: (data: UserExperienceWork) =>
+    mutationFn: (data: UserExperience | UserExperienceWork) =>
       type === UserExperienceType.Work
-        ? upsertUserWorkExperience(data, id)
+        ? upsertUserWorkExperience(data as UserExperienceWork, id)
         : upsertUserGeneralExperience(data, id),
     onSuccess: () => {
       dirtyFormRef.current?.allowNavigation();
+      methods.reset();
       router.back();
     },
-    onError: (error: GraphQLError) => {
-      console.log('*** error', error.response?.errors);
+    onError: (error: ApiErrorResult) => {
       if (
         error.response?.errors?.[0]?.extensions?.code ===
         ApiError.ZodValidationError
@@ -113,7 +112,6 @@ const useUserExperienceForm = ({
     },
   });
   dirtyFormRef.current = dirtyForm;
-  console.log('*** errors', methods.formState.errors);
   return { methods, save: dirtyFormRef.current?.save, isPending };
 };
 
