78dfe058c8753b7ee7b848b11a540a324f52f541
Amar Trebinjac <amartrebinjac@gmail.com>
2025-11-09 19:51:05 +0100

    revert: opp button padding

--- packages/shared/src/components/feeds/FeedNav.tsx
+++ packages/shared/src/components/feeds/FeedNav.tsx
@@ -234,7 +234,7 @@ function FeedNav(): ReactElement {
         </div>
       </div>
       {hasOpportunityAlert && isMobile && (
-        <div className={classNames('mx-4', isHeaderVisible && 'pt-2')}>
+        <div className="mx-4">
           <JobOpportunityButton className="w-full" />
         </div>
       )}
