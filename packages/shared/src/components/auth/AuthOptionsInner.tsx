3e7cda04ad679035e96402462cd4d5e5a06aea74
Amar Trebinjac <36768584+AmarTrebinjac@users.noreply.github.com>
2025-11-13 13:01:37 +0100

    fix: add image prop to social signup (#5064)

--- packages/shared/src/components/auth/AuthOptionsInner.tsx
+++ packages/shared/src/components/auth/AuthOptionsInner.tsx
@@ -429,7 +429,7 @@ function AuthOptionsInner({
   };
 
   const onSocialCompletion = async (params) => {
-    updateUserProfile({ ...params });
+    updateUserProfile({ ...params, image: user?.image });
     await syncSettings();
   };
 
