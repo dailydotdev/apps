3ce30b51db2269cf8be8580cde905bff22c94983
Amar Trebinjac <36768584+AmarTrebinjac@users.noreply.github.com>
2025-11-21 05:50:44 -0800

    refactor: minor tweaks to autocomplete props (#5090)

--- packages/shared/src/components/fields/Autocomplete.tsx
+++ packages/shared/src/components/fields/Autocomplete.tsx
@@ -1,5 +1,5 @@
 import { Popover, PopoverAnchor } from '@radix-ui/react-popover';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import type { TextFieldProps } from './TextField';
 import { TextField } from './TextField';
@@ -9,15 +9,16 @@ import { GenericLoaderSpinner } from '../utilities/loaders';
 import { IconSize } from '../Icon';
 import { Image } from '../image/Image';
 
+type AutocompleteOption = { value: string; label: string; image?: string };
+
 interface AutocompleteProps
   extends Omit<TextFieldProps, 'inputId' | 'onChange' | 'onSelect'> {
   name: string;
   onChange: (value: string) => void;
   onSelect: (value: string) => void;
   selectedValue?: string;
-  selectedImage?: string;
-  options: Array<{ value: string; label: string; image?: string }>;
-  isLoading?: boolean;
+  options: Array<AutocompleteOption>;
+  isLoading: boolean;
   resetOnBlur?: boolean;
 }
 
@@ -29,26 +30,42 @@ const Autocomplete = ({
   onChange,
   onSelect,
   selectedValue,
-  selectedImage,
   defaultValue,
   resetOnBlur = true,
   ...restProps
 }: AutocompleteProps) => {
   const [input, setInput] = useState(defaultValue || '');
-  const [isOpen, setIsOpen] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [selectedOption, setSelectedOption] = useState(undefined);
+
+  /* 
+   To prevent flickering of the selected option image as the user types.
+   We want to set the selected option only when the selected value changes or the user clears the selected value.
+  */
+  useEffect(() => {
+    if (!selectedValue) {
+      setSelectedOption(undefined);
+      return;
+    }
+    if (selectedOption?.value !== selectedValue) {
+      setSelectedOption(options.find((opt) => opt.value === selectedValue));
+    }
+  }, [selectedValue, options, selectedOption]);
+
   const handleChange = (val: string) => {
     setInput(val);
     onChange(val);
   };
-  const handleSelect = (opt: { value: string; label: string }) => {
+
+  const handleSelect = (opt: AutocompleteOption) => {
     setInput(opt.label);
     onSelect(opt.value);
-    setIsOpen(false);
+    setIsFocused(false);
     inputRef.current?.focus();
   };
   const handleBlur = () => {
-    setIsOpen(false);
+    setIsFocused(false);
     if (resetOnBlur) {
       setInput(options.find((opt) => opt.value === selectedValue)?.label || '');
     }
@@ -59,14 +76,14 @@ const Autocomplete = ({
       <Typography type={TypographyType.Callout} bold>
         {label}
       </Typography>
-      <Popover open={isOpen}>
+      <Popover open={isFocused && (isLoading || options.length > 0)}>
         <PopoverAnchor asChild>
           <TextField
             leftIcon={
-              selectedImage ? (
+              selectedOption?.image ? (
                 <Image
                   className="size-6 rounded-full"
-                  src={selectedImage}
+                  src={selectedOption.image}
                   alt={selectedValue}
                 />
               ) : undefined
@@ -79,16 +96,16 @@ const Autocomplete = ({
             {...restProps}
             onChange={(e) => {
               handleChange(e.target.value);
-              setIsOpen(true);
+              setIsFocused(true);
             }}
-            onFocus={() => setIsOpen(true)}
+            onFocus={() => setIsFocused(true)}
             onBlur={handleBlur}
             value={input}
             autoComplete="off"
           />
         </PopoverAnchor>
         <PopoverContent
-          className="rounded-16 border border-border-subtlest-tertiary bg-background-popover p-4 data-[side=bottom]:mt-1 data-[side=top]:mb-1"
+          className="rounded-16 border border-border-subtlest-tertiary bg-background-popover data-[side=bottom]:mt-1 data-[side=top]:mb-1"
           side="bottom"
           align="start"
           avoidCollisions
@@ -97,14 +114,15 @@ const Autocomplete = ({
           onCloseAutoFocus={(e) => e.preventDefault()} // avoid refocus jumps
         >
           {!isLoading ? (
-            <div className="flex w-full flex-col gap-1">
+            <div className="flex w-full flex-col">
               {options?.length > 0 ? (
                 options.map((opt) => (
                   <button
                     type="button"
                     className={classNames(
-                      'flex flex-row items-center gap-2 text-left',
-                      selectedValue === opt.value && 'font-bold',
+                      'flex flex-row items-center gap-2 px-4 py-2 text-left hover:bg-surface-hover',
+                      selectedValue === opt.value &&
+                        'font-bold hover:bg-surface-hover',
                     )}
                     key={opt.value}
                     onMouseDown={(e) => {
@@ -123,7 +141,9 @@ const Autocomplete = ({
                   </button>
                 ))
               ) : (
-                <Typography>No results</Typography>
+                <div className="px-4 py-2">
+                  <Typography>No results</Typography>
+                </div>
               )}
             </div>
           ) : (
