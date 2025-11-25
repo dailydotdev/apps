c94e537e7a9683ca1d3823cab416a281a5388e51
Chris Bongers <chrisbongers@gmail.com>
2025-11-25 11:03:57 +0200

    feat: page sizing adjustment

--- packages/webapp/pages/jobs/how-it-works.tsx
+++ packages/webapp/pages/jobs/how-it-works.tsx
@@ -12,9 +12,22 @@ import { OpportunityFAQ } from '@dailydotdev/shared/src/features/opportunity/com
 import { OpportunityBenefits } from '@dailydotdev/shared/src/features/opportunity/components/OpportunityBenefits';
 import { OpportunityHowItWorks } from '@dailydotdev/shared/src/features/opportunity/components/OpportunityHowItWorks';
 import { IntroHeader } from '@dailydotdev/shared/src/features/opportunity/components/IntroHeader';
-import { defaultOpenGraph, defaultSeo, defaultSeoTitle } from '../../next-seo';
-import { getLayout as getFooterNavBarLayout } from '../../components/layouts/FooterNavBarLayout';
+import {
+  Typography,
+  TypographyType,
+} from '@dailydotdev/shared/src/components/typography/Typography';
+import Link from '@dailydotdev/shared/src/components/utilities/Link';
+import { Button } from '@dailydotdev/shared/src/components/buttons/Button';
+import {
+  ButtonSize,
+  ButtonVariant,
+} from '@dailydotdev/shared/src/components/buttons/common';
+import { opportunityUrl } from '@dailydotdev/shared/src/lib/constants';
+import { MoveToIcon } from '@dailydotdev/shared/src/components/icons';
+import { IconSize } from '@dailydotdev/shared/src/components/Icon';
 import { getLayout } from '../../components/layouts/MainLayout';
+import { getLayout as getFooterNavBarLayout } from '../../components/layouts/FooterNavBarLayout';
+import { defaultOpenGraph, defaultSeo, defaultSeoTitle } from '../../next-seo';
 
 const seo: NextSeoProps = {
   title: defaultSeoTitle,
@@ -34,8 +47,23 @@ const JobsHowItWorksPage = (): ReactElement => {
   }
 
   return (
-    <div className="relative mx-4 mt-10 max-w-[47.875rem] tablet:mx-auto">
-      <FlexCol className="gap-8 tablet:mx-4 laptop:mx-0">
+    <div className="relative border-border-subtlest-tertiary tablet:mx-auto laptop:max-w-[48rem] laptop:border-x">
+      <div className="flex items-center gap-2 border-b border-border-subtlest-tertiary px-5 py-3">
+        <div className="flex gap-2">
+          <Link href={opportunityUrl}>
+            <Button
+              tag="a"
+              variant={ButtonVariant.Tertiary}
+              size={ButtonSize.Small}
+              icon={<MoveToIcon size={IconSize.Small} className="rotate-180" />}
+            />
+          </Link>
+        </div>
+        <Typography type={TypographyType.Title3} bold>
+          How it works
+        </Typography>
+      </div>
+      <FlexCol className="gap-8 px-4 py-6">
         <IntroHeader />
         <OpportunityBenefits />
         <OpportunityHowItWorks />
