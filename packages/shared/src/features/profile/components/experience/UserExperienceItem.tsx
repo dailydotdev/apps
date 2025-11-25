bb182051d14482d68da79a002396a52ff5e5ff13
Amar Trebinjac <amartrebinjac@gmail.com>
2025-11-25 23:40:37 +0100

    remove top border and break words

--- packages/shared/src/features/profile/components/experience/UserExperienceItem.tsx
+++ packages/shared/src/features/profile/components/experience/UserExperienceItem.tsx
@@ -146,7 +146,12 @@ export function UserExperienceItem({
           )}
         >
           <div className="flex flex-wrap items-center gap-1">
-            <Typography truncate type={TypographyType.Subhead} bold>
+            <Typography
+              className="whitespace-break-spaces"
+              truncate
+              type={TypographyType.Subhead}
+              bold
+            >
               {primaryCopy}
             </Typography>
             {!grouped && !endedAt && currentPill}
