61afd9bea6226322320746cc64868f5159033eb1
Chris Bongers <chrisbongers@gmail.com>
2026-01-07 14:05:27 +0200

    chore: remove marketing fluff for now

--- packages/shared/src/components/recruiter/ConnectHeader.tsx
+++ packages/shared/src/components/recruiter/ConnectHeader.tsx
@@ -99,11 +99,7 @@ export const ConnectHeader = ({
               secondary
               size={IconSize.XSmall}
             />{' '}
-            <strong>Autopilot ON:</strong>
-            {!!flags?.batchSize &&
-              `We will reach out to ${flags.batchSize} candidates per day`}
-            {!flags?.batchSize &&
-              'We will start reaching out to candidates soon'}
+            <strong>Autopilot ON</strong>
           </Typography>
         </div>
         <FlexRow className="gap-4">
