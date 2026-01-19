4fb4718dcaa1394f6a089a3b613fb9f8f57618e9
capJavert <dev@kickass.website>
2026-01-19 15:52:00 +0100

    feat(recruiter): remove require payment from questions page

--- packages/webapp/pages/recruiter/[opportunityId]/questions.tsx
+++ packages/webapp/pages/recruiter/[opportunityId]/questions.tsx
@@ -12,7 +12,6 @@ import {
 import { MoveToIcon, PlusIcon } from '@dailydotdev/shared/src/components/icons';
 import { useRouter } from 'next/router';
 import { opportunityByIdOptions } from '@dailydotdev/shared/src/features/opportunity/queries';
-import { useRequirePayment } from '@dailydotdev/shared/src/features/opportunity/hooks/useRequirePayment';
 import { NoOpportunity } from '@dailydotdev/shared/src/features/opportunity/components/NoOpportunity';
 import {
   OpportunityEditProvider,
@@ -65,11 +64,6 @@ const QuestionsSetupPage = (): ReactElement => {
 
   const { data: opportunity, isPending } = useQuery({
     ...opportunityByIdOptions({ id: opportunityId }),
-  });
-
-  const { isCheckingPayment } = useRequirePayment({
-    opportunity,
-    opportunityId,
   });
 
   const onValidationError = async ({
@@ -169,7 +163,7 @@ const QuestionsSetupPage = (): ReactElement => {
     },
   });
 
-  if (!isAuthReady || isPending || !isLoggedIn || isCheckingPayment) {
+  if (!isAuthReady || isPending || !isLoggedIn) {
     return (
       <div className="flex flex-1 items-center justify-center">
         <Loader />
