3f0377994884402bfd63a25fad35c08a2dd06363
Amar Trebinjac <36768584+AmarTrebinjac@users.noreply.github.com>
2026-01-27 12:02:50 +0100

    feat: add debounce to tools and stack (#5387)

--- packages/shared/src/features/profile/hooks/useStackSearch.ts
+++ packages/shared/src/features/profile/hooks/useStackSearch.ts
@@ -2,16 +2,23 @@ import { useQuery } from '@tanstack/react-query';
 import type { DatasetTool } from '../../../graphql/user/userTool';
 import { searchStack } from '../../../graphql/user/userStack';
 import { generateQueryKey, RequestKey, StaleTime } from '../../../lib/query';
+import useDebounce from '../../../hooks/useDebounce';
+import { defaultSearchDebounceMs } from '../../../lib/func';
 
 export function useStackSearch(query: string) {
   const trimmedQuery = query.trim();
-  const enabled = trimmedQuery.length >= 1;
+  const debouncedQuery = useDebounce(trimmedQuery, defaultSearchDebounceMs);
+  const enabled = debouncedQuery.length >= 1;
 
-  const queryKey = generateQueryKey(RequestKey.StackSearch, null, trimmedQuery);
+  const queryKey = generateQueryKey(
+    RequestKey.StackSearch,
+    null,
+    debouncedQuery,
+  );
 
   const searchQuery = useQuery<DatasetTool[]>({
     queryKey,
-    queryFn: () => searchStack(trimmedQuery),
+    queryFn: () => searchStack(debouncedQuery),
     staleTime: StaleTime.Default,
     enabled,
   });
