c066ed568c1a207560a260a3a4b73df9a42e18ef
rebelchris <chrisbongers@gmail.com>
2026-03-13 10:05:04 +0000

    refactor(webapp): simplify safari service worker fix

--- packages/webapp/sw.ts
+++ packages/webapp/sw.ts
@@ -2,7 +2,6 @@ import { defaultCache } from '@serwist/next/worker';
 import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Serwist } from 'serwist';
-import { shouldEnableNavigationPreload } from './lib/serviceWorker';
 
 // This declares the value of `injectionPoint` to TypeScript.
 // `injectionPoint` is the string that will be replaced by the
@@ -21,29 +20,13 @@ const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
+  // Safari can fail the first navigation when preload is enabled.
   navigationPreload: false,
   runtimeCaching: defaultCache,
   precacheOptions: {
     concurrency: 3,
     cleanupOutdatedCaches: true,
   },
-});
-
-const updateNavigationPreload = async (): Promise<void> => {
-  if (!self.registration.navigationPreload) {
-    return;
-  }
-
-  if (shouldEnableNavigationPreload(self.navigator.userAgent)) {
-    await self.registration.navigationPreload.enable();
-    return;
-  }
-
-  await self.registration.navigationPreload.disable();
-};
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(updateNavigationPreload());
 });
 
 serwist.addEventListeners();
