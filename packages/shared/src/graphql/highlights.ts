b05e75a6878c2f0e9403624e7ef22cd1fd13b9b0
Ido Shamun <1993245+idoshamun@users.noreply.github.com>
2026-03-22 13:12:41 +0200

    fix(agents): support highlightedAt in happening now (#5770)

--- packages/shared/src/graphql/highlights.ts
+++ packages/shared/src/graphql/highlights.ts
@@ -2,14 +2,11 @@ import { gql } from 'graphql-request';
 
 export interface PostHighlight {
   channel: string;
-  rank: number;
   headline: string;
-  createdAt: string;
-  updatedAt: string;
+  highlightedAt: string;
   post: {
     id: string;
     commentsPermalink: string;
-    createdAt: string;
   };
 }
 
@@ -17,13 +14,11 @@ export const POST_HIGHLIGHTS_QUERY = gql`
   query PostHighlights($channel: String!) {
     postHighlights(channel: $channel) {
       channel
-      rank
       headline
-      createdAt
+      highlightedAt
       post {
         id
         commentsPermalink
-        createdAt
       }
     }
   }
