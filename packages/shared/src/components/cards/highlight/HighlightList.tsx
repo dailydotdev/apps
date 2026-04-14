5705de3dc444afae5a6991d50191124c5d33671a
Ido Shamun <1993245+idoshamun@users.noreply.github.com>
2026-04-14 12:52:47 +0300

    feat: add /highlights page with channel navigation (#5857)

--- packages/shared/src/components/cards/highlight/HighlightList.tsx
+++ packages/shared/src/components/cards/highlight/HighlightList.tsx
@@ -1,12 +1,8 @@
 import type { ReactElement, Ref } from 'react';
 import React, { forwardRef } from 'react';
-import classNames from 'classnames';
 import { ListCard } from '../common/list/ListCard';
 import type { HighlightCardProps } from './common';
-import {
-  getHighlightCardContainerHandlers,
-  HighlightCardContent,
-} from './common';
+import { HighlightCardContent } from './common';
 
 export const HighlightList = forwardRef(function HighlightList(
   { highlights, onHighlightClick, onReadAllClick }: HighlightCardProps,
@@ -16,14 +12,7 @@ export const HighlightList = forwardRef(function HighlightList(
     <ListCard
       ref={ref}
       data-testid="highlightItem"
-      role={onReadAllClick ? 'button' : undefined}
-      tabIndex={onReadAllClick ? 0 : undefined}
-      aria-label={onReadAllClick ? 'Open highlights card' : undefined}
-      {...getHighlightCardContainerHandlers(onReadAllClick)}
-      className={classNames(
-        'group overflow-hidden !border-0 !border-t !border-border-subtlest-tertiary !bg-gradient-to-b !from-surface-float !to-background-default !px-4 !py-6',
-        onReadAllClick && 'cursor-pointer',
-      )}
+      className="group overflow-hidden !border-0 !border-t !border-border-subtlest-tertiary !bg-gradient-to-b !from-surface-float !to-background-default !px-4 !py-6"
     >
       <HighlightCardContent
         highlights={highlights}
