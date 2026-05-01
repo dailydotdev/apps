c6ef14b4b5d7de34167fad6eb1e6a446763ff6c3
Chris Bongers <chrisbongers@gmail.com>
2026-05-01 14:42:29 +0200

    chore: another ts-strict flow (#5958)

--- packages/shared/src/components/cards/squad/UnfeaturedSquadGrid.spec.tsx
+++ packages/shared/src/components/cards/squad/UnfeaturedSquadGrid.spec.tsx
@@ -24,7 +24,7 @@ import {
 const squads = [generateTestSquad()];
 const members = generateMembersList();
 const admin = generateTestAdmin();
-admin.source.members.edges = members;
+admin.source.members!.edges = members;
 admin.source.membersCount = members.length;
 
 beforeEach(async () => {
@@ -70,7 +70,7 @@ it('should render the component and member count when members are provided', () 
   renderComponent();
 
   const memberCount = screen.getByTestId('squad-members-count');
-  const { length } = admin.source.members.edges;
+  const { length } = admin.source.members!.edges;
 
   expect(memberCount).toBeInTheDocument();
   expect(memberCount.innerHTML).toEqual(`${length} members`);
