427dae0810864aff74d46c68650ec8c249339e96
Chris Bongers <chrisbongers@gmail.com>
2026-05-11 15:39:21 +0200

    refactor(shared): remove shortcuts extension promo flag (#6029)

--- packages/shared/src/graphql/actions.ts
+++ packages/shared/src/graphql/actions.ts
@@ -61,7 +61,6 @@ export enum ActionType {
   AchievementSyncPrompt = 'achievement_sync_prompt',
   DisableAchievementCompletion = 'disable_achievement_completion',
   DismissInstallExtension = 'dismiss_install_extension',
-  DismissShortcutsExtensionPromo = 'dismiss_shortcuts_extension_promo',
   DismissBriefCard = 'dismiss_brief_card',
   DigestUpsell = 'digest_upsell',
   AskUpsellSearch = 'ask_upsell_search',
