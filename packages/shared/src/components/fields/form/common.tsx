78e3a02dc37c950c67764b2a46269f26d4442e73
Ido Shamun <1993245+idoshamun@users.noreply.github.com>
2026-05-18 20:24:30 +0300

    feat: standup creation tab on the post composer (#6071)

--- packages/shared/src/components/fields/form/common.tsx
+++ packages/shared/src/components/fields/form/common.tsx
@@ -2,10 +2,14 @@ export enum WriteFormTab {
   NewPost = 'New post',
   Share = 'Share a link',
   Poll = 'Poll',
+  Standup = 'Standup',
 }
+
+export const CREATE_LIVE_ROOM_FORM_ID = 'create-live-room-form';
 
 export const WriteFormTabToFormID = {
   [WriteFormTab.Share]: 'write-post-link',
   [WriteFormTab.NewPost]: 'write-post-freeform',
   [WriteFormTab.Poll]: 'write-post-poll',
+  [WriteFormTab.Standup]: CREATE_LIVE_ROOM_FORM_ID,
 };
