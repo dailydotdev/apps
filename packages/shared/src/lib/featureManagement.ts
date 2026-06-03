ae980a7a6e3594c4f86945352f1f30f76a4910b0
capJavert <dev@kickass.website>
2026-06-03 09:54:22 +0200

    chore: post highlight cards exp key

--- packages/shared/src/lib/featureManagement.ts
+++ packages/shared/src/lib/featureManagement.ts
@@ -215,7 +215,7 @@ export const featureLayoutV2 = new Feature('layout_v2', false);
 export const featureEngagementBarV2 = new Feature('engagement_bar_v2', false);
 
 export const featurePostHighlightCards = new Feature(
-  'post_highlight_cards',
+  'post_highlight_cards_v2',
   false,
 );
 
