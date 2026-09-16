78a928497b09b4618b0143ece9243e922c18039f
rebelchris <chrisbongers@gmail.com>
2026-09-16 12:06:05 +0000

    fix(webapp): preserve settings card top corners

--- packages/webapp/components/layouts/SettingsLayout/common.tsx
+++ packages/webapp/components/layouts/SettingsLayout/common.tsx
@@ -42,7 +42,7 @@ export const AccountPageSection = classed(
 );
 export const AccountPageHeading = classed(
   'h1',
-  'flex h-14 w-full flex-row items-center border-b border-border-subtlest-tertiary px-4 font-bold typo-body tablet:px-6 tablet:typo-title3',
+  'flex h-14 w-full flex-row items-center rounded-t-16 border-b border-border-subtlest-tertiary px-4 font-bold typo-body tablet:px-6 tablet:typo-title3',
 );
 
 export const CommonTextField = classed(TextField, { container: 'max-w-sm' });
